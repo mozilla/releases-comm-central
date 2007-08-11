5a38bb6a8431ac65a7f47b489debb6532914e599
Florian Quèze <florian@instantbird.org>
2007-08-11 18:13:41 +0200

    Add an account.xul window with a list of the supported protocols.

---
+++ im/base/content/instantbird/account.js
@@ -0,0 +1,19 @@
+const Ci = Components.interfaces;
+
+var account = {
+  onload: function account_onload() {
+    var protoList = document.getElementById("protolist");
+    var pcs = Components.classes["@instantbird.org/purple/core;1"]
+                        .getService(Ci.purpleICoreService);
+    var protocols = pcs.getProtocols();
+    while (protocols.hasMoreElements()) {
+      var proto = protocols.getNext().QueryInterface(Ci.purpleIProtocol);
+      dump(" " + proto.name + " " + proto.id + "\n");
+      var id = proto.id;
+      var item = protoList.appendItem(proto.name, id, "toto");
+      item.setAttribute("image", "chrome://instantbird/skin/prpl/" + id + ".png");
+      item.setAttribute("class", "menuitem-iconic");
+    }
+    protoList.selectedIndex = 0;
+  }
+};
