bade5a8d857f52ac0fb506b99475841e420a7ce5
Florian Quèze <florian@instantbird.org>
2007-09-25 05:15:34 +0200

    Change the old new account window in an account properties window.

--- im/base/content/instantbird/account.js
+++ im/base/content/instantbird/account.js
@@ -1,26 +1,28 @@
 
 var account = {
   onload: function account_onload() {
-    var protoList = document.getElementById("protolist");
-    this.pcs = Components.classes["@instantbird.org/purple/core;1"]
-                         .getService(Ci.purpleICoreService);
-    for (let proto in this.getProtocols()) {
-      var id = proto.id;
-      var item = protoList.appendItem(proto.name, id, id);
-      item.setAttribute("image", "chrome://instantbird/skin/prpl/" + id + ".png");
-      item.setAttribute("class", "menuitem-iconic");
-    }
-    protoList.selectedIndex = 0;
-  },
+    this.account = window.arguments[0];
+    this.proto = this.account.protocol;
+    document.getElementById("accountName").value = this.account.name;
+    document.getElementById("protocolName").value = this.proto.name;
+    document.getElementById("protocolIcon").src =
+      "chrome://instantbird/skin/prpl/" + this.proto.id + "-48.png"
 
-  select: function account_select() {
-    var protoList = document.getElementById("protolist");
-    var id = protoList.selectedItem.value;
-    this.proto = this.pcs.getProtocolById(id);
-    document.getElementById("passwordBox").hidden = this.proto.noPassword;
+    if (this.proto.noPassword)
+      document.getElementById("passwordBox").hidden = true;
+    else
+      document.getElementById("password").value = this.account.password;
+
+    document.getElementById("alias").value = this.account.alias;
+
+/* FIXME
     document.getElementById("newMailNotification").hidden =
       !this.proto.newMailNotification;
+*/
 
+    this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                           .getService(Ci.nsIPrefService)
+                           .getBranch("messenger.account." + this.account.id + ".options.");
     this.populateProtoSpecificBox();
   },
 
@@ -42,30 +44,49 @@ var account = {
     return box;
   },
 
+  getBool: function account_getBool(aOpt) {
+    if (this.prefs.prefHasUserValue(aOpt.name))
+      return this.prefs.getBoolPref(aOpt.name);
+
+    return aOpt.getBool();
+  },
+
+  getInt: function account_getInt(aOpt) {
+    if (this.prefs.prefHasUserValue(aOpt.name))
+      return this.prefs.getIntPref(aOpt.name);
+
+    return aOpt.getInt();
+  },
+
+  getString: function account_getString(aOpt) {
+    if (this.prefs.prefHasUserValue(aOpt.name))
+      return this.prefs.getCharPref(aOpt.name);
+
+    return aOpt.getString();
+  },
+
   populateProtoSpecificBox: function account_populate() {
     var gbox = document.getElementById("protoSpecific");
     var bundle = document.getElementById("prplbundle");
     var id = this.proto.id;
-    var child;
-    while (child = gbox.firstChild)
-      gbox.removeChild(child);
     for (let opt in this.getProtoOptions()) {
       var text = bundle.getString(id + "." + opt.name);
       var name = id + "-" + opt.name;
       switch (opt.type) {
       case opt.typeBool:
 	var chk = document.createElement("checkbox");
-	chk.check = opt.getBool();
+        if (this.getBool(opt))
+	  chk.setAttribute("checked", "true");
 	chk.setAttribute("label", text);
 	chk.setAttribute("id", name);
 	gbox.appendChild(chk);
 	break;
       case opt.typeInt:
-	gbox.appendChild(this.createTextbox("number", opt.getInt(),
+	gbox.appendChild(this.createTextbox("number", this.getInt(opt),
 					    text, name));
 	break;
       case opt.typeString:
-	gbox.appendChild(this.createTextbox(null, opt.getString(),
+	gbox.appendChild(this.createTextbox(null, this.getString(opt),
 					    text, name));
 	break;
       default:
@@ -82,35 +103,36 @@ var account = {
   },
 
   create: function account_create() {
-    var acc = this.pcs.createAccount(this.getValue("name"), this.proto.id);
-    acc.password = this.getValue("password");
-    acc.rememberPassword = this.getValue("rememberPassword");
+    var password = this.getValue("password");
+    if (password != this.account.password)
+      this.account.password = password;
+
+    //acc.rememberPassword = this.getValue("rememberPassword");
+
     var alias = this.getValue("alias");
-    if (alias)
-      acc.alias = alias;
+    if (alias != this.account.alias)
+      this.account.alias = alias;
 
     for (let opt in this.getProtoOptions()) {
       var name = this.proto.id + "-" + opt.name;
       var val = this.getValue(name);
       switch (opt.type) {
       case opt.typeBool:
-	if (val != opt.getBool())
-	  acc.setBool(opt.name, val);
+	if (val != this.getBool(opt))
+	  this.account.setBool(opt.name, val);
 	break;
       case opt.typeInt:
-	if (val != opt.getInt())
-	  acc.setInt(opt.name, val);
+	if (val != this.getInt(opt))
+	  this.account.setInt(opt.name, val);
 	break;
       case opt.typeString:
-	if (val != opt.getString())
-	  acc.setString(opt.name, val);
+	if (val != this.getString(opt))
+	  this.account.setString(opt.name, val);
 	break;
       default:
 	throw "unknown preference type " + opt.type;
       }
     }
-    acc.save();
-    acc.connect();
   },
 
   getProtocols: function account_getProtocols() {
