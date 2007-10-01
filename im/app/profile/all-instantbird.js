83d661bd3918000b2db241128e0bdc2441bc26e4
Florian Quèze <florian@instantbird.org>
2007-10-01 14:15:21 +0200

    InstantBird --> Instantbird

--- im/app/profile/all-instantbird.js
+++ im/app/profile/all-instantbird.js
@@ -1,5 +1,5 @@
 pref("toolkit.defaultChromeURI", "chrome://instantbird/content/blist.xul");
-pref("general.useragent.extra.instantbird", "InstantBird/0.1");
+pref("general.useragent.extra.instantbird", "Instantbird/0.1");
 pref("accessibility.browsewithcaret", true);
 
 pref("javascript.options.showInConsole", true);
