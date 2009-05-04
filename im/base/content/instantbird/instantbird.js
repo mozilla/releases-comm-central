69094c8e7ea00bd26d6a8c7894074fb09f913709
Florian Quèze <florian@instantbird.org>
2009-05-04 11:35:15 +0200

    Display title attributes in tooltips in conversation.

--- im/base/content/instantbird/instantbird.js
+++ im/base/content/instantbird/instantbird.js
@@ -78,6 +78,7 @@ var msgObserver = {
       return this.convs[aConv.id];
 
     var conv = document.createElement("conversation");
+    conv.setAttribute("contenttooltip", "aHTMLTooltip");
     var panels = document.getElementById("panels");
     panels.appendChild(conv);
 
@@ -246,6 +247,42 @@ function setStatusText(aMsg)
 {
   var status = document.getElementById("status");
   status.setAttribute("label", aMsg);
+}
+
+// Inspired from the same function in mozilla/browser/base/content/browser.js
+function FillInHTMLTooltip(tipElement)
+{
+  if (tipElement.namespaceURI == "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul")
+    return false;
+
+  var defView = tipElement.ownerDocument.defaultView;
+  // XXX Work around bug 350679:
+  // "Tooltips can be fired in documents with no view".
+  if (!defView)
+    return false;
+
+  while (tipElement) {
+    if (tipElement.nodeType == Node.ELEMENT_NODE) {
+      let titleText = tipElement.getAttribute("title");
+      if (titleText && /\S/.test(titleText)) {
+        let direction = defView.getComputedStyle(tipElement, "")
+                               .getPropertyValue("direction");
+        let tipNode = document.getElementById("aHTMLTooltip");
+        tipNode.style.direction = direction;
+        // Per HTML 4.01 6.2 (CDATA section), literal CRs and tabs should be
+        // replaced with spaces, and LFs should be removed entirely.
+        // XXX Bug 322270: We don't preserve the result of entities like &#13;,
+        // which should result in a line break in the tooltip, because we can't
+        // distinguish that from a literal character in the source by this point.
+        titleText = titleText.replace(/[\r\t]/g, ' ').replace(/\n/g, '');
+        tipNode.setAttribute("label", titleText);
+        return true;
+      }
+    }
+    tipElement = tipElement.parentNode;
+  }
+
+  return false;
 }
 
 this.addEventListener("load", msgObserver.load, false);
