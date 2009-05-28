67eeb249ef56c126b1c053b6f302f1398b1d8c72
Florian Quèze <florian@instantbird.org>
2009-05-28 18:42:44 +0200

    Fix compare locale (remove false positives with a filter.py script).

---
+++ im/locales/filter.py
@@ -0,0 +1,28 @@
+def test(mod, path, entity = None):
+  import re
+  # ignore anyhting but Instantbird
+  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
+                 "instantbird", "purple", "extensions/reporter", "extensions/spellcheck",
+                 "instantbird/branding/release", "instantbird/branding/nightly"):
+    return False
+  if mod == "instantbird" and path.startswith("branding/"):
+    return False
+  if mod != "instantbird" and mod != "extensions/spellcheck":
+    # we only have exceptions for instantbird and extensions/spellcheck
+    return True
+  if not entity:
+    if mod == "extensions/spellcheck":
+      return False
+    # instantbird
+    return not (re.match(r"searchplugins\/.+\.xml", path))
+  if mod == "extensions/spellcheck":
+    # l10n ships en-US dictionary or something, do compare
+    return True
+  if path == "defines.inc":
+    return entity != "MOZ_LANGPACK_CONTRIBUTORS"
+
+  if path != "chrome/instantbird/region.properties":
+    # only region.properties exceptions remain, compare all others
+    return True
+
+  return not (re.match(r"browser\.search\.order\.[1-9]", entity))
