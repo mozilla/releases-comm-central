a0a91c07da294438be3bac46c02017dead732747
Florian Quèze <florian@instantbird.org>
2009-05-30 01:46:32 +0200

    Sort protocols in the list of the account wizard.

--- im/base/content/instantbird/accountWizard.js
+++ im/base/content/instantbird/accountWizard.js
@@ -44,12 +44,17 @@ var accountWizard = {
     var protoList = document.getElementById("protolist");
     this.pcs = Components.classes["@instantbird.org/purple/core;1"]
                          .getService(Ci.purpleICoreService);
-    for (let proto in this.getProtocols()) {
+    var protos = [];
+    for (let proto in this.getProtocols())
+      protos.push(proto);
+    protos.sort(function(a, b) a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
+    protos.forEach(function(proto) {
       var id = proto.id;
       var item = protoList.appendItem(proto.name, id, id);
       item.setAttribute("image", proto.iconBaseURI + "icon.png");
       item.setAttribute("class", "listitem-iconic");
-    }
+    });
+
     // there is a strange selection bug without this timeout
     setTimeout(function() {
       protoList.selectedIndex = 0;
