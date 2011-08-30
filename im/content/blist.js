8a84be42d932e736edf364ab562d32e410a93da5
Florian Quèze <florian@instantbird.org>
2011-08-30 17:20:11 +0200

    Bug 954416 - Make the 'hidden conversations' list keyboard accessible.

--- im/content/blist.js
+++ im/content/blist.js
@@ -728,7 +728,7 @@ var buddyList = {
 
   // Handle key pressing
   keyPress: function bl_keyPress(aEvent) {
-    var item = document.getElementById("buddylistbox").selectedItem;
+    var item = aEvent.target.selectedItem;
     if (!item || !item.parentNode) // empty list or item no longer in the list
       return;
     item.keyPress(aEvent);
