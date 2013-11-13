fd152e81819cc7d0d838cdd509073e17ea1489b7
Jens Mueller <blog@tessarakt.de>
2013-11-13 08:16:42 -0500

    Bug 61491 - Autocomplete newsgroup names (based on a patch by Jim Porter). r=neil/mconley, moa=neil, sr=Standard8; small followup fix for test: r=jcranmer CLOSED TREE

--- mailnews/addrbook/test/unit/test_nsAbAutoCompleteSearch4.js
+++ mailnews/addrbook/test/unit/test_nsAbAutoCompleteSearch4.js
@@ -128,7 +128,7 @@ function run_test()
 
   function checkSearch(element, index, array) {
     print("Checking " + element);
-    acs.startSearch(element, null, null, obs)
+    acs.startSearch(element, JSON.stringify({ type: "addr_to"  }), null, obs);
 
     do_check_eq(obs._search, acs);
     do_check_eq(obs._result.searchString, element);
@@ -153,7 +153,7 @@ function run_test()
   var lastResult = null;
 
   function checkReductionSearch(element, index, array) {
-    acs.startSearch(element, null, lastResult, obs);
+    acs.startSearch(element, JSON.stringify({ type: "addr_to"  }), lastResult, obs);
 
     do_check_eq(obs._search, acs);
     do_check_eq(obs._result.searchString, element);
