239ab00bcb798a3cfe8b6217f454b8edcbd49bac
aceman <acelists@atlas.sk>
2013-11-26 13:12:10 -0500

    Bug 925871 - Add a helper function to click a row of a XUL tree element for the TB mozmill tests. r=mconley

--- mail/test/mozmill/composition/test-send-button.js
+++ mail/test/mozmill/composition/test-send-button.js
@@ -161,8 +161,10 @@ function test_send_enabled_address_contacts_sidebar() {
   let sidebar = cwc.e("sidebar");
   wait_for_frame_load(sidebar,
     "chrome://messenger/content/addressbook/abContactsPanel.xul");
-  let abView = sidebar.contentDocument.getElementById("abResultsTree").treeBoxObject.view;
-  abView.selection.select(0);
+
+  let abTree = sidebar.contentDocument.getElementById("abResultsTree");
+  click_tree_row(abTree, 0, cwc);
+
   sidebar.contentDocument.getElementById("ccButton").click();
 
   // The recipient is filled in, Send must be enabled.
