36c92dfc2c9624816c46adf06e2108cdb32b3953
Magnus Melin <mkmelin+mozilla@iki.fi>
2014-01-10 22:58:51 +0200

    Bug 956586: tooltip modernization - FillInHTMLTooltip is deprecated. r=protz

--- mail/base/content/mailWindow.js
+++ mail/base/content/mailWindow.js
@@ -541,64 +541,6 @@ function InformUserOfCertError(socketInfo, targetSite)
                   '','chrome,centerscreen,modal', params);
 }
 
-/**
- * Content area tooltip.
- * XXX see bug 480356 - this must move into XBL binding/equiv!
- **/
-function FillInHTMLTooltip(tipElement)
-{
-  const XLinkNS = "http://www.w3.org/1999/xlink";
-  const XULNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-
-  var retVal = false;
-  if (tipElement.namespaceURI == XULNS &&
-      tipElement.ownerDocument.documentElement.nameSpaceURI == XULNS)
-    return retVal;
-
-  var titleText = null;
-  var XLinkTitleText = null;
-  var tooltipText = null;
-  var direction = tipElement.ownerDocument.dir;
-
-  while (!titleText && !XLinkTitleText && !tooltipText && tipElement) {
-    if (tipElement.nodeType == Node.ELEMENT_NODE) {
-      titleText = tipElement.getAttribute("title");
-      XLinkTitleText = tipElement.getAttributeNS(XLinkNS, "title");
-      if (tipElement.namespaceURI == XULNS) // only XUL nodes can get tooltiptext
-        tooltipText = tipElement.getAttribute("tooltiptext");
-      var defView = tipElement.ownerDocument.defaultView;
-      // XXX Work around bug 350679:
-      // "Tooltips can be fired in documents with no view".
-      if (!defView)
-        return retVal;
-      direction = defView.getComputedStyle(tipElement, "")
-        .getPropertyValue("direction");
-    }
-    tipElement = tipElement.parentNode;
-  }
-
-  var tipNode = document.getElementById("aHTMLTooltip");
-  tipNode.style.direction = direction;
-
-  /* tooltiptext has the highest priority but is only non-null for XUL nodes. */
-  for each (var t in [titleText, XLinkTitleText, tooltipText]) {
-    if (t && /\S/.test(t)) {
-
-      // Per HTML 4.01 6.2 (CDATA section), literal CRs and tabs should be
-      // replaced with spaces, and LFs should be removed entirely.
-      // XXX Bug 322270: We don't preserve the result of entities like &#13;,
-      // which should result in a line break in the tooltip, because we can't
-      // distinguish that from a literal character in the source by this point.
-      t = t.replace(/[\r\t]/g, ' ');
-      t = t.replace(/\n/g, '');
-
-      tipNode.setAttribute("label", t);
-      retVal = true;
-    }
-  }
-  return retVal;
-}
-
 function nsBrowserAccess() { }
 
 nsBrowserAccess.prototype = {
--- mail/test/mozmill/folder-display/test-tooltip-multimessage.js
+++ mail/test/mozmill/folder-display/test-tooltip-multimessage.js
@@ -23,40 +23,32 @@ var setupModule = function(module) {
 };
 
 function test_tooltips() {
-  /* Trigger the message summary. This is needed otherwise we cannot compute the
-   * style of aHTMLTooltip in mailWindow.js
-   */
+  // Trigger the message summary. This is needed otherwise we cannot compute the
+  // style of aHTMLTooltip in mailWindow.js
   be_in_folder(folder);
   select_click_row(0);
   make_display_threaded();
   toggle_thread_row(0);
 
-  /* Get the needed elements */
+  // Get the needed elements.
   let mm = mc.eid("multimessage").node;
   let mmDoc = mm.contentDocument;
   let tooltip = mc.eid("aHTMLTooltip").node;
 
-  /* Test a XUL element */
+  // Test a XUL element.
   let trashButton = mmDoc.querySelector(".hdrTrashButton");
   trashButton.setAttribute("title", "Title1");
   trashButton.setAttribute("tooltiptext", "TTT1");
-  mc.window.FillInHTMLTooltip(trashButton);
+  tooltip.fillInPageTooltip(trashButton);
   assert_equals(tooltip.getAttribute("label"), "TTT1", "This XUL element had its title taken as the tooltip instead of the tooltiptext attribute.");
-  
-  /* Test an HTML element */
+
+  // Test an HTML element.
   let a = mmDoc.createElement("a");
   a.setAttribute("href", "#");
   a.setAttribute("title", "Title2");
   a.setAttribute("tooltiptext", "TTT2");
-  mc.window.FillInHTMLTooltip(a);
+  mm.appendChild(a);
+  tooltip.fillInPageTooltip(a);
   assert_equals(tooltip.getAttribute("label"), "Title2", "This HTML element had its tooltiptext taken as the tooltip instead of the title attribute.");
-
-  /* Create an element with xlink */
-  let div = mmDoc.createElement("div");
-  div.innerHTML = '<span xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0">'+
-    '<a href="#" title="Title3" xlink:title="XTitle3" tooltiptext="TTT3">Hi there</a>'+
-    '</span>';
-  let xlink = div.querySelector("a");
-  mc.window.FillInHTMLTooltip(xlink);
-  assert_equals(tooltip.getAttribute("label"), "XTitle3", "This HTML element had its something else taken as the tooltip instead of the xlink:title attribute.");
+  mm.removeChild(a);
 }
