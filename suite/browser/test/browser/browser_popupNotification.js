eaf5a8c2bba3fc330178bf8a77b92bb8c129af7b
Sebastian Hengst <archaeopteryx@coole-files.de>
2014-02-21 14:14:31 +0100

    Bug 975381 - Use DOM_VK_RETURN/NS_VK_RETURN instead of DOM_VK_ENTER/NS_VK_ENTER in /suite/browser. r=IanN

--- suite/browser/test/browser/browser_popupNotification.js
+++ suite/browser/test/browser/browser_popupNotification.js
@@ -754,7 +754,7 @@ function triggerSecondaryCommand(popup, index) {
       EventUtils.synthesizeKey("VK_DOWN", {});
 
     // Activate
-    EventUtils.synthesizeKey("VK_ENTER", {});
+    EventUtils.synthesizeKey("VK_RETURN", {});
   }, false);
 
   // One down event to open the popup
