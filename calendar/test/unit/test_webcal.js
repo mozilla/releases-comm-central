70b48f7e04f918bc5e8bcf639e2bb8020b77a440
Philipp Kewisch <mozilla@kewis.ch>
2014-03-10 10:47:35 +0100

    Fix bug 680201 - test_webcal.js fails due to redefinition of Cc. r=redDragon
    --HG--
    extra : rebase_source : 8c3ce27b47a4219264130c5c18da568f2b01ac64

--- calendar/test/unit/test_webcal.js
+++ calendar/test/unit/test_webcal.js
@@ -3,58 +3,33 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 Components.utils.import("resource://gre/modules/Services.jsm");
-
-do_load_httpd_js();
-
-var httpserver = null;
-
-// TODO webcals doesn't work since httpd.js doesn't support ssl without ssltunnel
-var testUris = ["webcal://localhost:4444/test_webcal"  /* ,
-                "webcals://localhost:4445/test_webcals" */
-               ];
-
-var testListener = {
-    onStartRequest: function onStartRequest(request, context) {
-        var chan = request.QueryInterface(Components.interfaces.nsIChannel);
-        do_check_eq(chan.status, Components.results.NS_OK);
-    },
-    onDataAvailable: function onDataAvailable(request, context, stream, offset, count) {
-        stream.read(count);
-    },
-    onStopRequest: function onStopRequest(request, context, statusCode) {
-        // TODO For some reason we're getting error 0x80570021 here
-        // (NS_ERROR_XPC_JAVASCRIPT_ERROR_WITH_DETAILS), but we can't get the
-        // actual error :-(
-        // var chan = request.QueryInterface(Components.interfaces.nsIChannel);
-        // do_check_eq(chan.status, Components.results.NS_OK);
-
-        startNextTest();
-        do_test_finished();
-    }
-};
-
-function startNextTest() {
-    let nextUri = testUris.shift();
-    if (nextUri) {
-        let chan = Services.io.newChannel(nextUri, null, null);
-        chan.asyncOpen(testListener, null);
-        do_test_pending();
-    } else {
-        do_test_pending();
-        httpserv.stop(do_test_finished);
-    }
-}
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+Components.utils.import("resource://testing-common/httpd.js");
 
 function run_test() {
-
-    httpserv = new nsHttpServer();
-
-    httpserv.registerPathHandler("/", {
+    let httpserv = new HttpServer();
+    httpserv.registerPrefixHandler("/", {
         handle: function(request, response) {
+          response.setStatusLine(request.httpVersion, 200, "OK");
+          do_check_eq(request.path, "/test_webcal");
         }
     });
+    httpserv.start(-1);
 
-    httpserv.identity.add("https", "localhost", 4445);
-    httpserv.start(4444);
-    startNextTest();
+    let baseUri = "://localhost:" + httpserv.identity.primaryPort + "/test_webcal";
+    add_test(check_webcal_uri.bind(null, "webcal" + baseUri));
+    // TODO webcals needs bug 466524 to be fixed
+    // add_test(check_webcal_uri.bind(null, "webcals" + baseUri));
+    add_test(() =>  httpserv.stop(run_next_test));
+
+    // Now lets go...
+    run_next_test();
+}
+
+function check_webcal_uri(uri) {
+    let chan = Services.io.newChannel(uri, null, null);
+    NetUtil.asyncFetch(chan, function(data, status, request) {
+        do_check_true(Components.isSuccessCode(status));
+        run_next_test();
+    });
 }
