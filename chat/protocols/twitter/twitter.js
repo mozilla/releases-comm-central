24a0bfdcfd4536fc76fbd6627c73814296aa1165
Patrick Cloke <clokep@gmail.com>
2014-03-12 09:27:08 -0400

    Bug 955747 - Twitter support in russian locale is broken. r=florian

--- chat/protocols/twitter/twitter.js
+++ chat/protocols/twitter/twitter.js
@@ -1082,6 +1082,7 @@ function TwitterProtocol() {
 }
 TwitterProtocol.prototype = {
   __proto__: GenericProtocolPrototype,
+  get normalizedName() "twitter",
   get name() _("twitter.protocolName"),
   get iconBaseURI() "chrome://prpl-twitter/skin/",
   get noPassword() true,
