001ab1f5985d6c8104571cce66b90fd0d7187954
Philipp Kewisch <mozilla@kewis.ch>
2014-04-20 04:38:00 -0400

    Bug 998760 - Make sure cleanup functions are run. r=redDragon

--- calendar/test/unit/head_consts.js
+++ calendar/test/unit/head_consts.js
@@ -79,10 +79,13 @@ function getMemoryCal() {
 }
 
 function getStorageCal() {
-    var db = Services.dirsvc.get("TmpD", Ci.nsIFile);
-    db.append("test_storage.sqlite");
+    // Whenever we get the storage calendar we need to request a profile,
+    // otherwise the cleanup functions will not run
+    do_get_profile();
 
     // create URI
+    var db = Services.dirsvc.get("TmpD", Ci.nsIFile);
+    db.append("test_storage.sqlite");
     var uri = Services.io.newFileURI(db);
 
     // Make sure timezone service is initialized
