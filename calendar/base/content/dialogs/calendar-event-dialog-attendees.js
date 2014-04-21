57174714f27e625b2853c994355007daa4ebc47e
MakeMyDay <none@none>
2014-04-21 18:42:05 +0200

    Bug 969823 - Attendee Dialog: attendee list entries are not properly aligned with time grid;r=Decathlon

--- calendar/base/content/dialogs/calendar-event-dialog-attendees.js
+++ calendar/base/content/dialogs/calendar-event-dialog-attendees.js
@@ -53,6 +53,10 @@ function onLoad() {
 
     // Make sure zoom factor is set up correctly (from persisted value)
     setZoomFactor(zoom.value);
+    if(gZoomFactor == 100) {
+        // if zoom factor was not changed, make sure it is applied at least once
+        applyCurrentZoomFactor();
+    }
 
     initTimeRange();
 
@@ -752,6 +756,14 @@ function setZoomFactor(aValue) {
     }
 
     gZoomFactor = aValue;
+    applyCurrentZoomFactor();
+    return aValue;
+}
+
+/**
+ * applies the current zoom factor for the time grid
+ */
+function applyCurrentZoomFactor() {
     var timebar = document.getElementById("timebar");
     timebar.zoomFactor = gZoomFactor;
     var selectionbar = document.getElementById("selection-bar");
@@ -773,8 +785,6 @@ function setZoomFactor(aValue) {
         grid.scroll = ratio;
         selectionbar.ratio = ratio;
     }
-
-    return aValue;
 }
 
 /**
