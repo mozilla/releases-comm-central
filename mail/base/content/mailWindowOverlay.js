af18f119f5467282c82b8101cce239d5d52beb3a
alta88 <none@none>
2014-04-24 07:55:11 -0600

    Bug 996690 - Enhance tag management given implementation of autotags. r=mkmelin

--- mail/base/content/mailWindowOverlay.js
+++ mail/base/content/mailWindowOverlay.js
@@ -919,12 +919,15 @@ function InitMessageTags(menuPopup)
   for (var i = 0; i < tagCount; ++i)
   {
     var taginfo = tagArray[i];
+    let removeKey = (" " + curKeys + " ").contains(" " + taginfo.key + " ");
+    if (taginfo.ordinal.contains("~AUTOTAG") && !removeKey)
+      continue;
+
     // TODO we want to either remove or "check" the tags that already exist
     var newMenuItem = document.createElement("menuitem");
     SetMessageTagLabel(newMenuItem, i + 1, taginfo.tag);
     newMenuItem.setAttribute("value", taginfo.key);
     newMenuItem.setAttribute("type", "checkbox");
-    let removeKey = (" " + curKeys + " ").contains(" " + taginfo.key + " ");
     newMenuItem.setAttribute('checked', removeKey);
     newMenuItem.setAttribute('oncommand', 'ToggleMessageTagMenu(event.target);');
     var color = taginfo.color;
