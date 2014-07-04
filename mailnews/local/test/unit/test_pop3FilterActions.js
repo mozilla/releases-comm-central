31f8e5be16f1deb6f08009a319dd11d4215257d0
Suyash Agarwal <syshagarwal@gmail.com>
2014-07-04 14:11:07 -0700

    Bug 1030291 - Fix test_pop3FilterActions.js to work with maildir, r=rkent

--- mailnews/local/test/unit/test_pop3FilterActions.js
+++ mailnews/local/test/unit/test_pop3FilterActions.js
@@ -18,8 +18,17 @@ const gFiles = ["../../../data/bugmail10", "../../../data/bugmail11"];
 
 Services.prefs.setBoolPref("mail.server.default.leave_on_server", true);
 
-const bugmail10_preview = 'Do not reply to this email. You can add comments to this bug at https://bugzilla.mozilla.org/show_bug.cgi?id=436880 -- Configure bugmail: https://bugzilla.mozilla.org/userprefs.cgi?tab=email ------- You are receiving this mail because: -----';
-const bugmail11_preview = 'Bugzilla has received a request to create a user account using your email address (example@example.org). To confirm that you want to create an account using that email address, visit the following link: https://bugzilla.mozilla.org/token.cgi?t=xxx';
+// Currently we have two mailbox storage formats.
+var gPluggableStores = [
+  "@mozilla.org/msgstore/berkeleystore;1",
+  "@mozilla.org/msgstore/maildirstore;1"
+];
+
+// Map subject to previews using subject as the key.
+var previews = {
+"[Bug 436880] IMAP itemDeleted and itemMoveCopyCompleted notifications quite broken": 'Do not reply to this email. You can add comments to this bug at https://bugzilla.mozilla.org/show_bug.cgi?id=436880 -- Configure bugmail: https://bugzilla.mozilla.org/userprefs.cgi?tab=email ------- You are receiving this mail because: -----',
+"Bugzilla: confirm account creation": 'Bugzilla has received a request to create a user account using your email address (example@example.org). To confirm that you want to create an account using that email address, visit the following link: https://bugzilla.mozilla.org/token.cgi?t=xxx'
+}
 
 var gFilter; // the test filter
 var gFilterList;
@@ -74,26 +83,26 @@ const gTestArray =
   function verifyMessages() {
     let hdrs = [];
     let keys = [];
-    let enumerator = localAccountUtils.inboxFolder.msgDatabase.EnumerateMessages();
-    while (enumerator.hasMoreElements())
-    {
+    let enumerator = localAccountUtils.inboxFolder.msgDatabase
+                                                  .EnumerateMessages();
+    while (enumerator.hasMoreElements()) {
       let hdr = enumerator.getNext().QueryInterface(Ci.nsIMsgDBHdr);
       keys.push(hdr.messageKey);
       hdrs.push(hdr);
     }
-    do_check_false(localAccountUtils.inboxFolder.fetchMsgPreviewText(keys, keys.length, false, null));
-    do_check_eq(hdrs[0].getStringProperty('preview'), bugmail10_preview);
-    do_check_eq(hdrs[1].getStringProperty('preview'), bugmail11_preview);
+    do_check_false(localAccountUtils.inboxFolder
+                                    .fetchMsgPreviewText(keys, keys.length,
+                                                         false, null));
+    let preview1 = hdrs[0].getStringProperty('preview');
+    let preview2 = hdrs[1].getStringProperty('preview');
+    do_check_eq(preview1, previews[hdrs[0].subject]);
+    do_check_eq(preview2, previews[hdrs[1].subject]);
     do_check_eq(hdrs[0].getStringProperty('keywords'), "TheTag");
     do_check_eq(hdrs[1].getStringProperty('keywords'), "TheTag");
     do_check_eq(hdrs[0].flags, Ci.nsMsgMessageFlags.Read |
                                Ci.nsMsgMessageFlags.Marked);
     do_check_eq(hdrs[1].flags, Ci.nsMsgMessageFlags.Read |
                                Ci.nsMsgMessageFlags.Marked);
-  },
-  function endTest() {
-    dump("Exiting mail tests\n");
-    gPOP3Pump = null;
   }
 ];
 
@@ -109,14 +118,37 @@ function folderCount(folder)
   return count;
 }
 
+function setup_store(storeID)
+{
+  return function _setup_store() {
+    // Initialize pop3Pump with correct mailbox format.
+    gPOP3Pump.resetPluggableStore(storeID);
+
+    // Set the default mailbox store.
+    Services.prefs.setCharPref("mail.serverDefaultStoreContractID",
+                               storeID);
+
+    // Make sure we're not quarantining messages
+    Services.prefs.setBoolPref("mailnews.downloadToTempFile", false);
+
+    if (!localAccountUtils.inboxFolder)
+      localAccountUtils.loadLocalMailAccount();
+  }
+}
+
 function run_test()
 {
-  // Make sure we're not quarantining messages
-  Services.prefs.setBoolPref("mailnews.downloadToTempFile", false);
-  if (!localAccountUtils.inboxFolder)
-    localAccountUtils.loadLocalMailAccount();
+  for (let store of gPluggableStores) {
+    add_task(setup_store(store));
+    gTestArray.forEach(add_task);
+  }
 
-  gTestArray.forEach(add_task);
-
+  add_task(exitTest);
   run_next_test();
+}
+
+function exitTest()
+{
+  do_print("Exiting mail tests\n");
+  gPOP3Pump = null;
 }
