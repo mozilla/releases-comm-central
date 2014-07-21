da4f3ab585812a6d6bdfb10bdc9e1e0bcb4129cb
Edmund Wong <ewong@pw-wspx.org>
2014-07-21 10:44:27 +0800

    Bug 918474 - Default search dropdown doesn't get refreshed after adding new search engine. r=IanN

--- suite/common/pref/pref-search.js
+++ suite/common/pref/pref-search.js
@@ -5,7 +5,38 @@
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 function Startup() {
+  MakeList();
+  SearchObserver.init();
+}
+
+var SearchObserver = {
+  init: function searchEngineListObserver_init() {
+    Services.obs.addObserver(this, "browser-search-engine-modified", false);
+    window.addEventListener("unload", this, false);
+  },
+
+  observe: function searchEngineListObj_observe(aEngine, aTopic, aVerb) {
+    if (aTopic != "browser-search-engine-modified")
+      return;
+    MakeList();
+    var pref = document.getElementById("browser.search.defaultenginename");
+    if (pref)
+      pref.updateElements();
+  },
+
+  handleEvent: function searchEngineListEvent(aEvent) {
+    if (aEvent.type == "unload") {
+      window.removeEventListener("unload", this, false);
+      Services.obs.removeObserver(this, "browser-search-engine-modified");
+    }
+  }
+};
+
+function MakeList() {
   var menulist = document.getElementById("engineList");
+  while (menulist.hasChildNodes())
+    menulist.removeChild(menulist.lastChild);
+
   var engines = Services.search.getVisibleEngines();
   for (let i = 0; i < engines.length; i++) {
     let name = engines[i].name;
