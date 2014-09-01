4306cfd88fa533456780af46a34553d3f521285b
Suyash Agarwal <syshagarwal@gmail.com>
2014-09-01 08:50:52 +0100

    Bug 1046998 - Modify test_copyThenMoveManual.js to use Promises. r=rkent,a=Standard8 for CLOSED TREE

--- mailnews/base/test/unit/test_copyThenMoveManual.js
+++ mailnews/base/test/unit/test_copyThenMoveManual.js
@@ -8,13 +8,13 @@
 load("../../../resources/POP3pump.js");
 
 Components.utils.import("resource:///modules/mailServices.js");
+Components.utils.import("resource://testing-common/mailnews/PromiseTestUtils.jsm");
 
 const gFiles = ["../../../data/bugmail1"];
 var gCopyFolder;
 var gMoveFolder;
 var gFilter; // the test filter
 var gFilterList;
-var gCurTestNum = 1;
 const gTestArray =
 [
   function createFilters() {
@@ -35,24 +35,23 @@ const gTestArray =
     gFilter.enabled = true;
     gFilter.filterType = Ci.nsMsgFilterType.Manual;
     gFilterList.insertFilterAt(0, gFilter);
-    ++gCurTestNum;
-    doTest();
   },
   // just get a message into the local folder
-  function getLocalMessages1() {
+  function *getLocalMessages1() {
     gPOP3Pump.files = gFiles;
-    gPOP3Pump.onDone = doTest;
-    ++gCurTestNum;
-    gPOP3Pump.run();
+    yield gPOP3Pump.run();
   },
   // test applying filters to a message header
-  function applyFilters() {
+  function *applyFilters() {
     let messages = Cc["@mozilla.org/array;1"]
                      .createInstance(Ci.nsIMutableArray);
     messages.appendElement(localAccountUtils.inboxFolder.firstNewMessage, false);
-    ++gCurTestNum;
+    let promiseFolderEvent =
+      PromiseTestUtils.promiseFolderEvent(localAccountUtils.inboxFolder,
+                                          "DeleteOrMoveMsgCompleted");
     MailServices.filters.applyFilters(Ci.nsMsgFilterType.Manual,
                                       messages, localAccountUtils.inboxFolder, null);
+    yield promiseFolderEvent;
   },
   function verifyFolders1() {
     // Copy and Move should each now have 1 message in them.
@@ -61,23 +60,22 @@ const gTestArray =
     // the local inbox folder should now be empty, since the second
     // operation was a move
     do_check_eq(folderCount(localAccountUtils.inboxFolder), 0);
-    ++gCurTestNum;
-    doTest();
   },
   // just get a message into the local folder
-  function getLocalMessages2() {
+  function *getLocalMessages2() {
     gPOP3Pump.files = gFiles;
-    gPOP3Pump.onDone = doTest;
-    ++gCurTestNum;
-    gPOP3Pump.run();
+    yield gPOP3Pump.run();
   },
   // use the alternate call into the filter service
-  function applyFiltersToFolders() {
+  function *applyFiltersToFolders() {
     let folders = Cc["@mozilla.org/array;1"]
                     .createInstance(Ci.nsIMutableArray);
     folders.appendElement(localAccountUtils.inboxFolder, false);
-    ++gCurTestNum;
+    let promiseFolderEvent =
+      PromiseTestUtils.promiseFolderEvent(localAccountUtils.inboxFolder,
+                                          "DeleteOrMoveMsgCompleted");
     MailServices.filters.applyFiltersToFolders(gFilterList, folders, null);
+    yield promiseFolderEvent;
   },
   function verifyFolders2() {
     // Copy and Move should each now have 2 message in them.
@@ -86,8 +84,12 @@ const gTestArray =
     // the local inbox folder should now be empty, since the second
     // operation was a move
     do_check_eq(folderCount(localAccountUtils.inboxFolder), 0);
-    ++gCurTestNum;
-    doTest();
+  },
+  function endTest() {
+    // Cleanup, null out everything, close all cached connections and stop the
+    // server
+    dump(" Exiting mail tests\n");
+    gPOP3Pump = null;
   }
 ];
 
@@ -111,65 +113,7 @@ function run_test()
   gCopyFolder = localAccountUtils.rootFolder.createLocalSubfolder("CopyFolder");
   gMoveFolder = localAccountUtils.rootFolder.createLocalSubfolder("MoveFolder");
 
-  MailServices.mailSession.AddFolderListener(FolderListener, Ci.nsIFolderListener.event |
-                                                             Ci.nsIFolderListener.added |
-                                                             Ci.nsIFolderListener.removed);
+  gTestArray.forEach(add_task);
 
-  // "Master" do_test_pending(), paired with a do_test_finished() at the end of
-  // all the operations.
-  do_test_pending();
-
-  //start first test
-  doTest();
-}
-
-function doTest()
-{
-  var test = gCurTestNum;
-  if (test <= gTestArray.length)
-  {
-    var testFn = gTestArray[test-1];
-    dump("Doing test " + test + " " + testFn.name + "\n");
-
-    try {
-      testFn();
-    } catch(ex) {
-      do_throw ('TEST FAILED ' + ex);
-    }
-  }
-  else
-    do_timeout(1000, endTest);
-}
-
-// nsIFolderListener implementation
-var FolderListener = {
-  OnItemAdded: function OnItemAdded(aParentItem, aItem) {
-    this._showEvent(aParentItem, "OnItemAdded");
-  },
-  OnItemRemoved: function OnItemRemoved(aParentItem, aItem) {
-    this._showEvent(aParentItem, "OnItemRemoved");
-    // continue test, as all tests remove a message during the move
-    do_timeout(0, doTest);
-  },
-  OnItemEvent: function OnItemEvent(aEventFolder, aEvent) {
-    this._showEvent(aEventFolder, aEvent.toString())
-  },
-  _showEvent: function showEvent(aFolder, aEventString) {
-        dump("received folder event " + aEventString +
-         " folder " + aFolder.name +
-         "\n");
-  }
-};
-
-function endTest()
-{
-  // Cleanup, null out everything, close all cached connections and stop the
-  // server
-  dump(" Exiting mail tests\n");
-  let thread = gThreadManager.currentThread;
-  while (thread.hasPendingEvents())
-    thread.processNextEvent(true);
-  gPOP3Pump = null;
-
-  do_test_finished(); // for the one in run_test()
+  run_next_test();
 }
