9532474d8d448547030b2c66ae7a3dfa49f3c89b
Joshua Cranmer <Pidgeot18@gmail.com>
2014-09-29 18:20:08 -0500

    Bug 1074034, part 3: Use mozilla-central's baseconfig.mk instead of forking it, r=Standard8. a=jcranmer
    A small complication arises in that we need to maintain distinct topsrcdir
    variables, so we can't just delete this file. But we can make it as small as
    possible.
    
    Note that we don't include the mozilla-central version if INCLUDED_AUTOCONF_MK
    is not defined, since topsrcdir (and hence MOZILLA_SRCDIR) is undefined.
    
    Pushing to a CLOSED TREE because this is important to get to developers, since
    it makes incremental builds work again!
    
    --HG--
    extra : amend_source : 5b4bca492ace69b5df399f49bf7aee5203b29adc

--- config/baseconfig.mk
+++ config/baseconfig.mk
@@ -2,38 +2,12 @@
 # directly in python/mozbuild/mozbuild/base.py for gmake validation.
 # We thus use INCLUDED_AUTOCONF_MK to enable/disable some parts depending
 # whether a normal build is happening or whether the check is running.
-includedir := $(includedir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
-idldir = $(datadir)/idl/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
-installdir = $(libdir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
-sdkdir = $(libdir)/$(MOZ_APP_NAME)-devel-$(MOZ_APP_VERSION)
+
 MOZILLA_SRCDIR = $(topsrcdir)/mozilla
-DIST = $(DEPTH)/dist
-
-# We do magic with OBJ_SUFFIX in config.mk, the following ensures we don't
-# manually use it before config.mk inclusion
-_OBJ_SUFFIX := $(OBJ_SUFFIX)
-OBJ_SUFFIX = $(error config/config.mk needs to be included before using OBJ_SUFFIX)
-
-ifeq ($(HOST_OS_ARCH),WINNT)
-# We only support building with a non-msys gnu make version
-# strictly above 4.0.
-ifdef .PYMAKE
-$(error Pymake is no longer supported. Please upgrade to MozillaBuild 1.9 or newer and build with 'mach' or 'mozmake')
-endif
-
-ifeq (,$(filter mozmake%,$(notdir $(MAKE))))
-$(error Only building with pymake or mozmake is supported.)
-endif
-
-ifdef INCLUDED_AUTOCONF_MK
-ifeq (a,$(firstword a$(subst /, ,$(srcdir))))
-$(error MSYS-style srcdir are not supported for Windows builds.)
-endif
-endif
-endif # WINNT
-
 ifndef INCLUDED_AUTOCONF_MK
 default::
+else
+include $(MOZILLA_SRCDIR)/config/baseconfig.mk
 endif
 
 # WIN_TOP_SRC is converted by config.mk to mozilla-central, but this needs to be comm-central.
