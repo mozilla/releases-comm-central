2e20eceba3bbb7ac8e031265f7927f426bed5400
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2014-10-06 20:44:00 +0300

    Bug 1078524 - Set intl.charset.detector to pass the test_shiftjis_csv.js test. r=standard8 a=jcranmer

--- mailnews/import/test/unit/test_shiftjis_csv.js
+++ mailnews/import/test/unit/test_shiftjis_csv.js
@@ -2,6 +2,11 @@ Components.utils.import("resource:///modules/mailServices.js");
 
 function run_test()
 {
+  Services.prefs.setCharPref("intl.charset.detector", "ja_parallel_state_machine");
+  do_register_cleanup(function() {
+      Services.prefs.clearUserPref("intl.charset.detector");
+  });
+
   // Due to the import code using nsIAbManager off the main thread, we need
   // to ensure that it is initialized before we start the main test.
   let abMgr = MailServices.ab;
