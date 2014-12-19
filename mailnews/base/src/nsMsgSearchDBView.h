2ed6dbf0655bd89c35986c135e36125482d704ef
Sylvestre Ledru <sledru@mozilla.com>
2014-12-19 12:13:41 +0100

    Bug 1109058 - Fix the guard (different #ifdef & #define) r=standard8 a=aleth
    --HG--
    extra : rebase_source : c6500868e29f663a629d314b5110bbe97f187bc1

--- mailnews/base/src/nsMsgSearchDBView.h
+++ mailnews/base/src/nsMsgSearchDBView.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef _nsMsgSearchDBViewsH_
-#define _nsMsgSearchDBView_H_
+#ifndef _nsMsgSearchDBViews_H_
+#define _nsMsgSearchDBViews_H_
 
 #include "mozilla/Attributes.h"
 #include "nsMsgGroupView.h"
