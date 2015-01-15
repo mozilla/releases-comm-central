04fe703d8c878a76141afaa6c08cdb77c89b9370
rsx11m <rsx11m.pub@gmail.com>
2015-01-15 17:47:24 -0600

    Bug 912216 - Temporarily disable CONDSTORE support for GMail; just flip the default for now. r=jcranmer, a=mkmelin

--- mailnews/mailnews.js
+++ mailnews/mailnews.js
@@ -431,7 +431,7 @@ pref("mail.server.default.canFileMessages", true);
 pref("mail.server.default.is_gmail", false);
 pref("mail.server.default.use_idle", true);
 // in case client or server has bugs in condstore implementation
-pref("mail.server.default.use_condstore", true);
+pref("mail.server.default.use_condstore", false);
 // in case client or server has bugs in compress implementation
 pref("mail.server.default.use_compress_deflate", true);
 // for spam
