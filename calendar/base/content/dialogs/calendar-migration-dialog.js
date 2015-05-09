554fa911ea1080a5e34e548bf2a44e60cf67b8bb
Kushan Joshi <0o3ko0@gmail.com>
2015-05-09 16:13:04 +0200

    Bug 1159638 - Getter fails in calender-migration-dialog on first run after installation. r=philipp

--- calendar/base/content/dialogs/calendar-migration-dialog.js
+++ calendar/base/content/dialogs/calendar-migration-dialog.js
@@ -461,12 +461,15 @@ var gDataMigrator = {
     },
 
     checkWindowsMail: function gdm_windowsMail() {
-        let maildir = this.dirService.get("LocalAppData",
-                                          Components.interfaces.nsILocalFile);
-        if (!maildir || !maildir.exists()) {
+
+        if (!this.dirService.has("LocalAppData")) {
             // We are probably not on windows
             return [];
         }
+
+        let maildir = this.dirService.get("LocalAppData",
+                                          Components.interfaces.nsILocalFile);
+
         maildir.append("Microsoft");
         maildir.append("Windows Calendar");
         maildir.append("Calendars");
