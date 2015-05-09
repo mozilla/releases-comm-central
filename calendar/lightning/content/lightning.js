b194dfd0c764635823a716db7dd1d0d3499b2e27
Stefan Sitter <ssitter@gmail.com>
2015-05-09 16:11:32 +0200

    Bug 1162300 - enable ical.js by default in nightly builds because binary XPCOM components are no longer supported. r=philipp

--- calendar/lightning/content/lightning.js
+++ calendar/lightning/content/lightning.js
@@ -139,7 +139,11 @@ pref("calendar.view.useSystemColors", false);
 pref("calendar.filter.maxiterations", 50);
 
 // Backend to use. false: libical, true: ical.js
+#ifdef LIGHTNING_PRERELEASE_VERSION
+pref("calendar.icaljs", true);
+#else
 pref("calendar.icaljs", false);
+#endif
 
 // Calendar integration notification
 pref("calendar.integration.notify", true);
