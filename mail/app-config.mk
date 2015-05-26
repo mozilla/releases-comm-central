b253e4f2e34e584bd858c92ff539e3e397af6cbe
Ian Neal <iann_cvs@blueyonder.co.uk>
2015-05-26 12:21:09 +0100

    Bug 1158774 - Port |Bug 1155776 - move USE_EXTENSION_MANIFEST to moz.build| to comm-central - remove app-config.mk r=rkent

--- mail/app-config.mk
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-ifdef COMM_BUILD
-ifdef MOZ_CALENDAR
-include $(topsrcdir)/calendar/lightning/app-config.mk
-endif
-endif
