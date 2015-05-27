fd648930c371ae3ac595e1a552f84b9ed68b7968
R Kent James <rkent@caspia.com>
2015-05-27 15:17:31 -0700

    Bug 1166206 - Use makeFromDisplayAddress in setting Compose from field, r=mkmelin

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -2446,7 +2446,9 @@ function GenericSendMessage(msgType)
   var msgCompFields = gMsgCompose.compFields;
 
   Recipients2CompFields(msgCompFields);
-  msgCompFields.from = GetMsgIdentityElement().value;
+  let addresses = MailServices.headerParser
+                              .makeFromDisplayAddress(GetMsgIdentityElement().value);
+  msgCompFields.from = MailServices.headerParser.makeMimeHeader(addresses, 1);
   var subject = GetMsgSubjectElement().value;
   msgCompFields.subject = subject;
   Attachments2CompFields(msgCompFields);
