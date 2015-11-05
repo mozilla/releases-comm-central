78c21cf667e536c278399b7ffba5438dc54da36f
Magnus Melin <mkmelin+mozilla@iki.fi>
2015-11-05 22:31:09 +0200

    Bug 1221762: - port "Bug 1151537: Support hotfixes signed by the AMO trusted root" to Thunderbird. r=bwinton, a=mkmelin

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -167,6 +167,7 @@ pref("extensions.update.autoUpdateDefault", true);
 pref("extensions.hotfix.id", "thunderbird-hotfix@mozilla.org");
 pref("extensions.hotfix.cert.checkAttributes", true);
 pref("extensions.hotfix.certs.1.sha1Fingerprint", "91:53:98:0C:C1:86:DF:47:8F:35:22:9E:11:C9:A7:31:04:49:A1:AA");
+pref("extensions.hotfix.certs.2.sha1Fingerprint", "39:E7:2B:7A:5B:CF:37:78:F9:5D:4A:E0:53:2D:2F:3D:68:53:C5:60");
 
 // Disable add-ons installed into the shared user and shared system areas by
 // default. This does not include the application directory. See the SCOPE
