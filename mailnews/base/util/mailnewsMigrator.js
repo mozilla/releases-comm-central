460e58454b83b4e81916143df9f54dc5c43e72a4
alta88 <none@none>
2015-11-20 17:12:29 -0700

    Bug 1177830 - consider removing Chinese Simplified (GBK) from outgoing mail encoding options. r=mkmelin, r=ewong

--- mailnews/base/util/mailnewsMigrator.js
+++ mailnews/base/util/mailnewsMigrator.js
@@ -19,7 +19,7 @@ var Ci = Components.interfaces;
 var kServerPrefVersion = 1;
 var kSmtpPrefVersion = 1;
 var kABRemoteContentPrefVersion = 1;
-var kDefaultCharsetsPrefVersion = 1;
+var kDefaultCharsetsPrefVersion = 2;
 
 function migrateMailnews()
 {
@@ -172,7 +172,8 @@ function MigrateABRemoteContentSettings()
  */
 function MigrateDefaultCharsets()
 {
-  if (Services.prefs.prefHasUserValue("mail.default_charsets.migrated"))
+  if (Services.prefs.prefHasUserValue("mail.default_charsets.migrated") &&
+      Services.prefs.getIntPref("mail.default_charsets.migrated") == kDefaultCharsetsPrefVersion)
     return;
 
   let charsetConvertManager = Components.classes['@mozilla.org/charset-converter-manager;1']
