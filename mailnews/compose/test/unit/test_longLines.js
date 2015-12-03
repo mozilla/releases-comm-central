df592f326cae98b7213dd1f5150acbe1605ffbf4
Jorg K <mozilla@jorgk.com>
2015-12-03 14:23:00 +0100

    Bug 653342 - Properly set serializer flags. Support delsp. Use OutputDisallowLineBreaking. Test. r=mkmelin.

--- mailnews/compose/test/unit/test_longLines.js
+++ mailnews/compose/test/unit/test_longLines.js
@@ -10,10 +10,19 @@ Components.utils.import("resource:///modules/mimeParser.jsm");
 var CompFields = CC("@mozilla.org/messengercompose/composefields;1",
                     Ci.nsIMsgCompFields);
 
-function checkDraftHeadersAndBody(expectedHeaders, expectedBody, partNum = "") {
+// Copied from jsmime.js.
+function stringToTypedArray(buffer) {
+  var typedarray = new Uint8Array(buffer.length);
+  for (var i = 0; i < buffer.length; i++) {
+    typedarray[i] = buffer.charCodeAt(i);
+  }
+  return typedarray;
+}
+
+function checkDraftHeadersAndBody(expectedHeaders, expectedBody, charset = "UTF-8") {
   let msgData = mailTestUtils
     .loadMessageToString(gDraftFolder, mailTestUtils.firstMsgHdr(gDraftFolder));
-  checkMessageHeaders(msgData, expectedHeaders, partNum);
+  checkMessageHeaders(msgData, expectedHeaders);
 
   // Get the message body, decode from base64 and check.
   let endOfHeaders = msgData.indexOf("\r\n\r\n");
@@ -25,12 +34,19 @@ function checkDraftHeadersAndBody(expectedHeaders, expectedBody, partNum = "") {
   else
     body = body.slice(0, body.length);
 
-  // Remove line breaks and decode from base64.
-  body = atob(body.replace(/\r\n/g, ""));
+  // Remove line breaks and decode from base64 if required.
+  if (expectedHeaders["Content-Transfer-Encoding"] == "base64")
+    body = atob(body.replace(/\r\n/g, ""));
 
-  let expectedBinary = String.fromCharCode.apply(undefined,
-    new TextEncoder("UTF-8").encode(expectedBody));
-  do_check_eq(body, expectedBinary);
+  if (charset == "UTF-8") {
+    let expectedBinary = String.fromCharCode.apply(undefined,
+      new TextEncoder("UTF-8").encode(expectedBody));
+    do_check_eq(body, expectedBinary);
+  } else {
+    let strView = stringToTypedArray(body);
+    let decodedBody = new TextDecoder(charset).decode(strView);
+    do_check_eq(decodedBody, expectedBody);
+  }
 }
 
 function checkMessageHeaders(msgData, expectedHeaders, partNum = "") {
@@ -60,6 +76,13 @@ function checkMessageHeaders(msgData, expectedHeaders, partNum = "") {
 // Create a line with 600 letters 'a' with acute accent, encoded as
 // two bytes c3a1 in UTF-8.
 let longMultibyteLine = "\u00E1".repeat(600);
+
+// And here a line with a Korean character, encoded as three bytes
+// ec9588 in UTF-8.
+let longMultibyteLineCJK = "안".repeat(400);
+
+// And some Japanese.
+let longMultibyteLineJapanese = "語".repeat(400);
 
 function* testBodyWithLongLine() {
   let fields = new CompFields();
@@ -96,6 +119,87 @@ function* testBodyWithLongLine() {
     longMultibyteLine + "\r\n" // Expected body: The message without the tags.
   );
 
+  // Now CJK.
+  fields.forcePlainText = false;
+  htmlMessage = "<htmt><head>" +
+    "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">" +
+    "</head><body>" + longMultibyteLineCJK + "</body></html>";
+  fields.body = htmlMessage;
+  yield richCreateMessage(fields, [], identity);
+  checkDraftHeadersAndBody(
+    {
+      "Content-Type": "text/html; charset=UTF-8",
+      "Content-Transfer-Encoding": "base64"
+    },
+    htmlMessage
+  );
+
+  // Again, but this time as plain text.
+  fields.forcePlainText = true;
+  fields.useMultipartAlternative = false;
+  yield richCreateMessage(fields, [], identity);
+  checkDraftHeadersAndBody(
+    {
+      "Content-Type": "text/plain; charset=UTF-8; format=flowed",
+      "Content-Transfer-Encoding": "base64"
+    },
+    longMultibyteLineCJK + "\r\n" // Expected body: The message without the tags.
+  );
+
+  // Now a special test for ISO-2022-JP.
+  fields.characterSet = "ISO-2022-JP";
+
+  fields.forcePlainText = false;
+  htmlMessage = "<htmt><head>" +
+    "<meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-2022-JP\">" +
+    "</head><body>" + longMultibyteLineJapanese + "</body></html>";
+  fields.body = htmlMessage;
+  yield richCreateMessage(fields, [], identity);
+  checkDraftHeadersAndBody(
+    {
+      "Content-Type": "text/html; charset=ISO-2022-JP",
+      "Content-Transfer-Encoding": "base64"
+    },
+    htmlMessage,
+    "ISO-2022-JP"
+  );
+
+  // Again, but this time as plain text.
+  fields.forcePlainText = true;
+  fields.useMultipartAlternative = false;
+  yield richCreateMessage(fields, [], identity);
+
+  // Expected body: The message without the tags and chopped up in
+  // chunks of 36 characters with a space appended to each line.
+  let expectedBody = "";
+  let lastIndex = 0;
+  for (let i = 0; i + 36 < longMultibyteLineJapanese.length; i = i + 36) {
+    expectedBody = expectedBody + longMultibyteLineJapanese.substr(i, 36) + " \r\n";
+    lastIndex = i + 36;
+  }
+  expectedBody += longMultibyteLineJapanese.substr(lastIndex) + "\r\n";
+
+  checkDraftHeadersAndBody(
+    {
+      "Content-Type": "text/plain; charset=ISO-2022-JP; format=flowed; delsp=yes",
+      "Content-Transfer-Encoding": "7bit"
+    },
+    expectedBody,
+    "ISO-2022-JP"
+  );
+
+  // Again, but this time not flowed.
+  Services.prefs.setBoolPref("mailnews.send_plaintext_flowed", false);
+
+  yield richCreateMessage(fields, [], identity);
+  checkDraftHeadersAndBody(
+    {
+      "Content-Type": "text/plain; charset=ISO-2022-JP",
+      "Content-Transfer-Encoding": "7bit"
+    },
+    expectedBody.replace(/ /g, ""), // No spaces expected this time.
+    "ISO-2022-JP"
+  );
 }
 
 var tests = [
