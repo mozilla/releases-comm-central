25fcc893a7dd7f26c19b1a33eaa47b5c734ddbda
aleth <aleth@instantbird.org>
2015-12-08 18:45:30 +0100

    Bug 1230843 - Add AppConstants to instantbird.js. r=clokep
    --HG--
    extra : rebase_source : 211bd39d776e1d0651df94213db68637c20b7a66
    extra : amend_source : 6d84ec329b0650c3c1d87a5b76fc29942a64ba99

--- im/content/instantbird.js
+++ im/content/instantbird.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "AppConstants",
+                                  "resource://gre/modules/AppConstants.jsm");
 
 var TAB_DROP_TYPE = "application/x-moz-tabbrowser-tab";
 
