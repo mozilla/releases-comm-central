3b1c1bf8566b3e5b53a5f01a98b6e9a133e32d6f
aceman <acelists@atlas.sk>
2016-01-22 00:21:37 +0100

    Bug 1240890 - include ABQueryUtils.jsm in ABSearchDialog.js for the needed encodeABTermValue(). r=mkmelin, r=Ratty

--- suite/mailnews/search/ABSearchDialog.js
+++ suite/mailnews/search/ABSearchDialog.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource:///modules/ABQueryUtils.jsm");
 Components.utils.import("resource:///modules/mailServices.js");
 
 var searchSessionContractID = "@mozilla.org/messenger/searchSession;1";
