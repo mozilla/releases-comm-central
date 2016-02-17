87387d9bd8a70103775b96f1f9523e7fcf337673
aceman <acelists@atlas.sk>
2016-02-17 22:36:00 +0100

    Bug 1202165 - Add mozmill test for draft deliveryformat. r=jorgk DONTBUILD
    --HG--
    extra : amend_source : 190378716c9b4491dddc43761493a98b2edcfee2

--- mail/test/mozmill/composition/test-drafts.js
+++ mail/test/mozmill/composition/test-drafts.js
@@ -38,7 +38,14 @@ function setupModule(module) {
                              .getChildNamed("Drafts");
 }
 
+function setupComposeWin(aCwc, toAddr, subj, body) {
+  aCwc.type(aCwc.eid("addressCol2#1"), toAddr);
+  aCwc.type(aCwc.eid("msgSubject"), subj);
+  aCwc.type(aCwc.eid("content-frame"), body);
+}
+
 /**
+ * Bug 349547.
  * Tests that we only open one compose window for one instance of a draft.
  */
 function test_open_draft_again() {
@@ -81,6 +88,80 @@ function test_open_draft_again() {
   close_compose_window(cwc); // close compose window
 
   press_delete(mc); // clean up after ourselves
+}
+
+/**
+ * Bug 1202165
+ * Test that the user set delivery format is preserved in a draft message.
+ */
+function test_save_delivery_format() {
+  let cwc = open_compose_new_mail();
+
+  setupComposeWin(cwc, "test@example.invalid",
+                  "Testing storing of the composition properties in the draft!",
+                  "Hello!");
+
+  // Select our wanted format.
+  if (!mc.mozmillModule.isMac) {
+    formatMenu = cwc.click_menus_in_sequence(cwc.e("optionsMenuPopup"),
+                                             [ { id: "outputFormatMenu" },
+                                               { id: "format_both" } ]);
+  } else {
+    // On OS X the main menu seems not accessible for clicking from mozmill.
+    assert_true(cwc.e("outputFormatMenu").getAttribute("oncommand").startsWith("OutputFormatMenuSelect("));
+    cwc.window.OutputFormatMenuSelect(cwc.e("format_both"));
+  }
+
+  /**
+   * Check if the right format is selected in the menu.
+   *
+   * @param aMenuItemId  The id of the menuitem expected to be selected.
+   * @param aValue       A value of nsIMsgCompSendFormat contants of the expected selected format.
+   */
+  function assert_format_value(aMenuItemId, aValue) {
+    if (!mc.mozmillModule.isMac) {
+      let formatMenu = cwc.click_menus_in_sequence(cwc.e("optionsMenuPopup"),
+                                                   [ { id: "outputFormatMenu" } ], true);
+      let formatItem = cwc.e("outputFormatMenuPopup")
+                          .querySelector("[name=output_format][checked=true]");
+      assert_equals(formatItem.id, aMenuItemId);
+      cwc.close_popup_sequence(formatMenu);
+    } else {
+      assert_equals(cwc.window.gSendFormat, aValue);
+    }
+  }
+
+  cwc.window.SaveAsDraft();
+  utils.waitFor(() => !cwc.window.gSaveOperationInProgress && !cwc.window.gWindowLock,
+                "Saving of draft did not finish");
+  wait_for_window_focused(cwc.window);
+
+  close_compose_window(cwc);
+
+  // Open a new composition see if the menu is again at default value, not the one
+  // chosen above, even in a recycled compose window.
+  cwc = open_compose_new_mail();
+
+  assert_format_value("format_auto", Ci.nsIMsgCompSendFormat.AskUser);
+
+  close_compose_window(cwc);
+
+  be_in_folder(draftsFolder);
+  select_click_row(0);
+
+  // Wait for the notification with the Edit button.
+  wait_for_notification_to_show(mc, kBoxId, "draftMsgContent");
+
+  plan_for_new_window("msgcompose");
+  mc.click(mc.eid(kBoxId, {tagName: "button", label: "Edit"}));
+  cwc = wait_for_compose_window();
+
+  // Check if format value was restored.
+  assert_format_value("format_both", Ci.nsIMsgCompSendFormat.Both);
+
+  close_compose_window(cwc);
+
+  press_delete(mc); // clean up the created draft
 }
 
 function teardownModule() {
