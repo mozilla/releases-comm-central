ae65b80f9cc8c34cd263237c3d1d0d1db00372f1
Philip Chee <philip.chee@gmail.com>
2016-03-07 01:25:47 +0800

    Bug 1253846 - Port client.mk changes from Bug 1250294 r=jcranmer

--- client.mk
+++ client.mk
@@ -320,8 +320,10 @@ CONFIG_CACHE  = $(wildcard $(OBJDIR)/config.cache)
 EXTRA_CONFIG_DEPS := \
 	$(TOPSRCDIR)/aclocal.m4 \
 	$(TOPSRCDIR)/mozilla/aclocal.m4 \
+	$(TOPSRCDIR)/mozilla/old-configure.in \
 	$(wildcard $(TOPSRCDIR)/mozilla/build/autoconf/*.m4) \
 	$(TOPSRCDIR)/mozilla/js/src/aclocal.m4 \
+	$(TOPSRCDIR)/mozilla/js/src/old-configure.in \
 	$(NULL)
 
 $(CONFIGURES): %: %.in $(EXTRA_CONFIG_DEPS)
