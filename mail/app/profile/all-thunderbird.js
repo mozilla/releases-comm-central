e01d34c93796bf2c44593d83d4bba5efbfa3972a
Jorg K <none@none>
2016-03-25 12:41:00 +0100

    Bug 1259815 - Add services.kinto.update_enabled preference. r=mkmelin a=bustage fix CLOSED TREE
    --HG--
    extra : amend_source : 8ddb78c832ddf05d5b56da49028e292d7d756a04

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -194,9 +194,17 @@ pref("extensions.blocklist.itemURL", "https://blocklist.addons.mozilla.org/%LOCA
 
 // Kinto blocklist preferences
 pref("services.kinto.base", "https://firefox.settings.services.mozilla.com/v1");
+pref("services.kinto.changes.path", "/buckets/monitor/collections/changes/records");
 pref("services.kinto.bucket", "blocklists");
 pref("services.kinto.onecrl.collection", "certificates");
 pref("services.kinto.onecrl.checked", 0);
+
+// For now, let's keep kinto update out of the release channel.
+#ifdef RELEASE_BUILD
+pref("services.kinto.update_enabled", false);
+#else
+pref("services.kinto.update_enabled", true);
+#endif
 
 // Enables some extra Extension System Logging (can reduce performance)
 pref("extensions.logging.enabled", false);
