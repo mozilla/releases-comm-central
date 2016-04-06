9fbcf1eb2bc9b20af0a44d85e1fac05aaf2d6e17
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2016-04-06 11:47:00 +0200

    Bug 684455 - Test for Becky! Internet Mail importer. r=mconley, sr=standard8

---
+++ mailnews/import/test/unit/test_becky_addressbook.js
@@ -0,0 +1,23 @@
+function run_test()
+{
+  if (!("nsIWindowsRegKey" in Ci))
+    return;
+
+  // Due to the import code using nsIAbManager off the main thread, we need
+  // to ensure that it is initialized before we start the main test.
+  let abMgr = Cc["@mozilla.org/abmanager;1"].getService(Ci.nsIAbManager);
+
+  let file = do_get_file("resources/becky/addressbooks");
+  let helper = new AbImportHelper(file, "Becky!", "addressbooks",
+                                  "becky_addressbook");
+  let vcfSupportedAttributes =
+    ["FirstName", "LastName", "DisplayName", "NickName", "PrimaryEmail",
+     "SecondEmail", "WorkPhone", "HomePhone", "FaxNumber", "PagerNumber",
+     "CellularNumber", "HomeAddress", "HomeAddress2", "HomeCity", "HomeState",
+     "HomeZipCode", "HomeCountry", "WorkAddress", "WorkAddress2", "WorkCity",
+     "WorkState", "WorkZipCode", "WorkCountry", "JobTitle", "Department",
+     "Company", "BirthYear", "BirthMonth", "BirthDay", "WebPage1", "WebPage2",
+     "Custom1", "Custom2", "Custom3", "Custom4", "Notes", "_AimScreenName"];
+  helper.setSupportedAttributes(vcfSupportedAttributes);
+  helper.beginImport();
+}
