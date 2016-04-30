dd15012981571a3d4d9c5f5f517f264ef0a0aba9
Philip Chee <philip.chee@gmail.com>
2016-04-30 23:34:33 +0800

    Bug 1268975 - Port Bug 1255570 to SeaMonkey r=IanN a=IanN comm-aurora CLOSED TREE

--- suite/browser/navigator.js
+++ suite/browser/navigator.js
@@ -2064,18 +2064,30 @@ function URLBarSetURI(aURI, aValid) {
 
 function losslessDecodeURI(aURI) {
   var value = aURI.spec;
+  var scheme = aURI.scheme;
+
+  var decodeASCIIOnly = !["https", "http", "file", "ftp"].includes(scheme);
   // Try to decode as UTF-8 if there's no encoding sequence that we would break.
-  if (!/%25(?:3B|2F|3F|3A|40|26|3D|2B|24|2C|23)/i.test(value))
-    try {
-      value = decodeURI(value)
-                // decodeURI decodes %25 to %, which creates unintended
-                // encoding sequences. Re-encode it, unless it's part of
-                // a sequence that survived decodeURI, i.e. one for:
-                // ';', '/', '?', ':', '@', '&', '=', '+', '$', ',', '#'
-                // (RFC 3987 section 3.2)
-                .replace(/%(?!3B|2F|3F|3A|40|26|3D|2B|24|2C|23)/ig,
-                         encodeURIComponent);
-    } catch (e) {}
+  if (!/%25(?:3B|2F|3F|3A|40|26|3D|2B|24|2C|23)/i.test(value)) {
+    if (decodeASCIIOnly) {
+      // This only decodes ascii characters (hex) 20-7e, except 25 (%).
+      // This avoids both cases stipulated below (%-related issues, and \r, \n
+      // and \t, which would be %0d, %0a and %09, respectively) as well as any
+      // non-US-ascii characters.
+      value = value.replace(/%(2[0-4]|2[6-9a-f]|[3-6][0-9a-f]|7[0-9a-e])/g, decodeURI);
+    } else {
+      try {
+        value = decodeURI(value)
+                  // decodeURI decodes %25 to %, which creates unintended
+                  // encoding sequences. Re-encode it, unless it's part of
+                  // a sequence that survived decodeURI, i.e. one for:
+                  // ';', '/', '?', ':', '@', '&', '=', '+', '$', ',', '#'
+                  // (RFC 3987 section 3.2)
+                  .replace(/%(?!3B|2F|3F|3A|40|26|3D|2B|24|2C|23)/ig,
+                           encodeURIComponent);
+      } catch (e) {}
+    }
+  }
 
   // Encode invisible characters (soft hyphen, zero-width space, BOM,
   // line and paragraph separator, word joiner, invisible times,
