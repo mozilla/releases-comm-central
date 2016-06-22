fa71f2eec70ccac8616a7f7d25f9ad2124c9b4c8
rsx11m <rsx11m.pub@gmail.com>
2016-06-22 16:53:26 -0500

    Bug 1275679 - Add option to the HTTP Networking preferences to disable Lightning user-agent addition. ui-r=Ratty r=IanN,Ratty a=IanN

--- suite/common/pref/pref-http.js
+++ suite/common/pref/pref-http.js
@@ -3,10 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var gLightningUAold;
+
 function Startup()
 {
   CheckPipelining();
   CheckPipeliningProxy();
+  CheckLightningUA();
 }
 
 function CheckPipelining()
@@ -23,4 +26,33 @@ function CheckPipeliningProxy()
 
   var enabled = prefHTTPVersion.value == "1.1";
   EnableElementById("enablePipeliningProxy", enabled, false);
+}
+
+// Lightning adds a UA token if calendar.useragent.extra is not empty.
+// Checkbox is visible if Lightning is installed and enabled.
+// Checkbox is checked if the pref contains any non-whitespace character.
+function CheckLightningUA()
+{
+  var prefLightningUA = document.getElementById("calendar.useragent.extra");
+  var boxLightningUA  = document.getElementById("uaLightningShow");
+
+  if (prefLightningUA.defaultValue)
+  {
+    boxLightningUA.hidden = false;
+    boxLightningUA.checked = prefLightningUA.value.trim() != "";
+    boxLightningUA.disabled = prefLightningUA.locked;
+    gLightningUAold = prefLightningUA.hasUserValue ? prefLightningUA.value : null;
+  }
+}
+
+// If the checkbox is checked, use previous user-set or default value.
+// If the checkbox is not checked, set the pref to an empty string.
+function OnLightningChanged(aChecked)
+{
+  var prefLightningUA = document.getElementById("calendar.useragent.extra");
+
+  if (aChecked && !gLightningUAold)
+    prefLightningUA.reset();
+  else
+    prefLightningUA.value = (aChecked && gLightningUAold) || "";
 }
