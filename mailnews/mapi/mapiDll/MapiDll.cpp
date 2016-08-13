3e60ac08a8cf6ed6a68f55e5f88dde6a854d4708
Jorg K <jorgk@jorgk.com>
2016-08-13 22:00:02 +0200

    Bug 1294260 - Supress deprecation warning for Windows in MapiDll and remove _WIN32_WINNT define from MAPI. r=aceman CLOSED TREE

--- mailnews/mapi/mapiDll/MapiDll.cpp
+++ mailnews/mapi/mapiDll/MapiDll.cpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#pragma warning (disable : 4996) // MAPILogoff is deprecated
+
 #include <windows.h>
 #include <tchar.h>
 #include <mapidefs.h>
