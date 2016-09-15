b24c6832c6b334283c7035d8116da52756415b7f
Axel Hecht <axel@pike.org>
2016-09-15 17:06:41 +0200

    bug 1303027, clean up seamonkey l10n configurations, r=IanN, a=ewong
    Remove old configurations for 2.0.
    Make filter.py return "ignore" and "error" to make it easier
    to understand, also remove devtools/client for now.
    Fix that for real in bug 1301940.
    
    Also remove spellcheck and inspector [extras], they're
    only used for automation job scheduling, and are
    never passed in with a 'mozilla' prefix. That makes
    this change effectively a no-op.
    
    MozReview-Commit-ID: GbzYr2Xs5i9
    
    --HG--
    extra : rebase_source : 7657c12f155479c20fd646416dc76c5c0197541d

--- suite/locales/filter.py
+++ suite/locales/filter.py
@@ -2,36 +2,41 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-def test(mod, path, entity = None):
+def test(mod, path, entity=None):
   import re
   # ignore anything but SeaMonkey
   if mod not in ("netwerk", "dom", "toolkit", "security/manager",
-                 "devtools/client", "devtools/shared",
+                 "devtools/shared",
                  "editor/ui", "suite", "services/sync"):
-    return False
+    return "ignore"
   # ignore temporary files, hiden files and files from rejects
   if (re.match(r".*?\/[.#].+", path) or
       re.match(r".*~$", path) or
       re.match(r".+\.(orig|rej)", path)):
-    return False
+    return "ignore"
   if mod not in ("suite"):
     # we only have exceptions for suite
-    return True
+    return "error"
   if entity is None:
     # missing and obsolete files
-    return not (re.match(r"searchplugins\/.+\.xml", path) or
-                re.match(r"chrome\/common\/help\/images\/[A-Za-z-_]+\.[a-z]+", path))
+    return ("ignore" if (re.match(r"searchplugins\/.+\.xml", path) or
+                         re.match(r"chrome\/common\/help\/images\/[A-Za-z-_]+\.[a-z]+", path))
+            else "error")
   if path == "defines.inc":
-    return entity != "MOZ_LANGPACK_CONTRIBUTORS"
+    return ("ignore" if (entity == "MOZ_LANGPACK_CONTRIBUTORS")
+            else "error")
   if path == "profile/bookmarks.extra" or path == "profile/panels.extra":
     # ignore files for additional bookmarks and panels
-    return False
-    
+    return "ignore"
+
   if path == "chrome/common/region.properties":
-    return not re.match(r"browser\.search\.order\.[1-9]", entity)
+    return ("ignore" if (re.match(r"browser\.search\.order\.[1-9]", entity))
+            else "error")
 
   if path != "chrome/browser/region.properties":
     # only region.properties exceptions remain, compare all others
-    return True
+    return "error"
 
-  return not re.match(r"browser\.contentHandlers\.types\.[0-5]", entity)
+  return ("ignore"
+          if (re.match(r"browser\.contentHandlers\.types\.[0-5]", entity))
+          else "error")
