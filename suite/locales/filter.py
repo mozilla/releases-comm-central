7ef4c569de5f867224ddb919ee4f6b3ed204d1d9
Axel Hecht <axel@pike.org>
2016-09-24 15:44:28 +0530

    bug 1301940, including devtools/client for SeaMonkey, r/a=IanN
    This depends on bug 1305206, which created the
    devtools/client/locales/l10n.ini.
    
    MozReview-Commit-ID: KDMmWSArGWi
    
    --HG--
    extra : rebase_source : ddc310d2870cefa103f93d8b0a3c6e8b24e7e955
    extra : amend_source : a3af216b92effc631adde7672c0109a9eb403ab0

--- suite/locales/filter.py
+++ suite/locales/filter.py
@@ -6,7 +6,7 @@ def test(mod, path, entity=None):
   import re
   # ignore anything but SeaMonkey
   if mod not in ("netwerk", "dom", "toolkit", "security/manager",
-                 "devtools/shared",
+                 "devtools/shared", "devtools/client",
                  "editor/ui", "suite", "services/sync"):
     return "ignore"
   # ignore temporary files, hiden files and files from rejects
