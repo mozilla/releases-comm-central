19111d7c7dd638b8fed5291253df87f75ab079ae
Frank-Rainer Grahl <frgrahl@gmx.net>
2016-11-09 20:02:56 +0100

    Bug 1313288 - Port Bug 1312143. Followup Fix from Bug 1313039 for editor. r=Ratty a=Ratty

--- editor/ui/composer/content/EditorContextMenu.js
+++ editor/ui/composer/content/EditorContextMenu.js
@@ -3,6 +3,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyGetter(this, "InlineSpellCheckerUI", function() {
+  let tmp = {};
+  Components.utils.import("resource://gre/modules/InlineSpellChecker.jsm", tmp);
+  return new tmp.InlineSpellChecker();
+});
+
 // Overrides the main contentAreaContext onpopupshowing so needs to do
 // everything that does plus call Composer specific code.
 function editorContextPopupShowing(aNode)
