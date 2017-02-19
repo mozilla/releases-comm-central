845cd8062c7f350544c88c0d82decd4e249ef7ff
alta88@gmail.com <alta88@gmail.com>
2017-02-19 10:03:48 -0700

    Bug 1345141 - Part 2: Modernize feed xpcom factory code. r=mkmelin

--- mailnews/extensions/newsblog/js/newsblog.js
+++ mailnews/extensions/newsblog/js/newsblog.js
@@ -8,92 +8,43 @@ var {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import("resource:///modules/FeedUtils.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-var nsNewsBlogFeedDownloader =
-{
-  downloadFeed: function(aFolder, aUrlListener, aIsBiff, aMsgWindow)
-  {
+function FeedDownloader() { }
+FeedDownloader.prototype = {
+  downloadFeed: function(aFolder, aUrlListener, aIsBiff, aMsgWindow) {
     FeedUtils.downloadFeed(aFolder, aUrlListener, aIsBiff, aMsgWindow);
   },
-
-  subscribeToFeed: function(aUrl, aFolder, aMsgWindow)
-  {
+  subscribeToFeed: function(aUrl, aFolder, aMsgWindow) {
     FeedUtils.subscribeToFeed(aUrl, aFolder, aMsgWindow);
   },
-
-  updateSubscriptionsDS: function(aFolder, aOrigFolder, aAction)
-  {
+  updateSubscriptionsDS: function(aFolder, aOrigFolder, aAction) {
     FeedUtils.updateSubscriptionsDS(aFolder, aOrigFolder, aAction);
   },
 
-  QueryInterface: function(aIID)
-  {
-    if (aIID.equals(Ci.nsINewsBlogFeedDownloader) ||
-        aIID.equals(Ci.nsISupports))
-      return this;
+  classID: Components.ID("{5c124537-adca-4456-b2b5-641ab687d1f6}"),
+  classInfo: XPCOMUtils.generateCI({
+               classID: "{5c124537-adca-4456-b2b5-641ab687d1f6}",
+               contractID: "@mozilla.org/newsblog-feed-downloader;1",
+               classDescription: "Feed Downloader",
+               interfaces: [Ci.nsINewsBlogFeedDownloader],
+               flags: Ci.nsIClassInfo.SINGLETON}),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsINewsBlogFeedDownloader]),
+};
 
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  }
-}
-
-var nsNewsBlogAcctMgrExtension =
-{
+function FeedAcctMgrExtension() { }
+FeedAcctMgrExtension.prototype = {
   name: "newsblog",
   chromePackageName: "messenger-newsblog",
-  showPanel: function (server)
-  {
-    return false;
-  },
-  QueryInterface: function(aIID)
-  {
-    if (aIID.equals(Ci.nsIMsgAccountManagerExtension) ||
-        aIID.equals(Ci.nsISupports))
-      return this;
+  showPanel: server => false,
 
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  }
-}
-
-function FeedDownloader() {}
-
-FeedDownloader.prototype =
-{
-  classID: Components.ID("{5c124537-adca-4456-b2b5-641ab687d1f6}"),
-  _xpcom_factory:
-  {
-    createInstance: function (aOuter, aIID)
-    {
-      if (aOuter != null)
-        throw Cr.NS_ERROR_NO_AGGREGATION;
-      if (!aIID.equals(Ci.nsINewsBlogFeedDownloader) &&
-          !aIID.equals(Ci.nsISupports))
-        throw Cr.NS_ERROR_INVALID_ARG;
-
-      // return the singleton
-      return nsNewsBlogFeedDownloader.QueryInterface(aIID);
-    }
-  } // factory
-}; // feed downloader
-
-function AcctMgrExtension() {}
-
-AcctMgrExtension.prototype =
-{
   classID: Components.ID("{E109C05F-D304-4ca5-8C44-6DE1BFAF1F74}"),
-  _xpcom_factory:
-  {
-    createInstance: function (aOuter, aIID)
-    {
-      if (aOuter != null)
-        throw Cr.NS_ERROR_NO_AGGREGATION;
-      if (!aIID.equals(Ci.nsIMsgAccountManagerExtension) &&
-          !aIID.equals(Ci.nsISupports))
-        throw Cr.NS_ERROR_INVALID_ARG;
+  classInfo: XPCOMUtils.generateCI({
+               classID: "{E109C05F-D304-4ca5-8C44-6DE1BFAF1F74}",
+               contractID: "@mozilla.org/accountmanager/extension;1?name=newsblog",
+               classDescription: "Feed Account Manager Extension",
+               interfaces: [Ci.nsIMsgAccountManagerExtension],
+               flags: Ci.nsIClassInfo.SINGLETON}),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIMsgAccountManagerExtension]),
+};
 
-      // return the singleton
-      return nsNewsBlogAcctMgrExtension.QueryInterface(aIID);
-    }
-  } // factory
-}; // account manager extension
-
-var components = [FeedDownloader, AcctMgrExtension];
+var components = [FeedDownloader, FeedAcctMgrExtension];
 var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
