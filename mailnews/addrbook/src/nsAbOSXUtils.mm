f1c4427f8dcdec18b3b792c131556660f65a7c15
Jorg K <jorgk@jorgk.com>
2017-03-10 23:08:48 +0100

    Bug 1332717 - include nsString.h instead of nsStringGlue.h (take 2). r+a=me DONTBUILD

--- mailnews/addrbook/src/nsAbOSXUtils.mm
+++ mailnews/addrbook/src/nsAbOSXUtils.mm
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsAbOSXUtils.h"
-#include "nsStringGlue.h"
+#include "nsString.h"
 #include "nsAbOSXCard.h"
 #include "nsMemory.h"
 #include "mozilla/ArrayUtils.h"
