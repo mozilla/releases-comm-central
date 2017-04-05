7d8a3070d53fcefbb70345e5deae9193760ee61a
Jorg K <jorgk@jorgk.com>
2017-04-05 08:21:36 +0200

    Bug 1353326 - set defaultparagraphseparator to "br". r=bustage-fix

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -5361,6 +5361,9 @@ function InitEditor()
   // Control insertion of line breaks.
   editor.returnInParagraphCreatesNewParagraph =
     Services.prefs.getBoolPref("editor.CR_creates_new_p");
+  // Traditionally we want <br> elements for newlines, unless we're
+  // splitting a paragraph (which is not affected by this setting).
+  editor.document.execCommand("defaultparagraphseparator", false, "br");
 
   editor.QueryInterface(nsIEditorStyleSheets);
   // We use addOverrideStyleSheet rather than addStyleSheet so that we get
