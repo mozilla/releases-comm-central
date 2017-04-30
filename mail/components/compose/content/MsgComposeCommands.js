d2b14f3f8e5ff9ca0d304a90f6cda0092b948b9b
Jorg K <jorgk@jorgk.com>
2017-04-30 20:32:12 +0200

    Backed out changeset ae3cb512e8d2 (bug 1354002) for causing unwanted side effects. rs=backout

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -5361,10 +5361,9 @@ function InitEditor()
   // Control insertion of line breaks.
   editor.returnInParagraphCreatesNewParagraph =
     Services.prefs.getBoolPref("editor.CR_creates_new_p");
-  editor.document.execCommand("defaultparagraphseparator", false,
-    gMsgCompose.composeHTML &&
-    Services.prefs.getBoolPref("mail.compose.default_to_paragraph") ?
-                               "p" : "br");
+  // Traditionally we want <br> elements for newlines, unless we're
+  // splitting a paragraph (which is not affected by this setting).
+  editor.document.execCommand("defaultparagraphseparator", false, "br");
 
   editor.QueryInterface(nsIEditorStyleSheets);
   // We use addOverrideStyleSheet rather than addStyleSheet so that we get
