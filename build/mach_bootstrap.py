006eda7880f41ad03cd7bc2a4c1635e110b99f34
Tom Prince <mozilla@hocat.ca>
2017-07-25 14:23:04 -0600

    Bug 1384319 - Make mach ignore comm-central's mach_boostrap.py when it isn't topsrcdir; r=Fallen
    MozReview-Commit-ID: AHSJD1lb8J5
    
    --HG--
    extra : rebase_source : 8a0755938d17d68109488e2712ee206f950ca6c5
    extra : histedit_source : 001493f6fd0a79d339bb05fc4bb8ed1b1f186681

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -9,6 +9,17 @@ import os, sys
 def bootstrap(topsrcdir, mozilla_dir=None):
     if mozilla_dir is None:
         mozilla_dir = os.path.join(topsrcdir, 'mozilla')
+
+    if not os.path.exists(mozilla_dir):
+        # If we can't find `mozilla` as a subdirectory of the `comm` checkout,
+        # assume that we are a subdirectory of a mozilla checkout. If we return
+        # `None` here, the `mach` entrypoint will keep searching for a build
+        # directory.  Since the entry point imports this into `sys.modules`,
+        # clean it up, since we aren't the `mach_bootstrap` that `mach` is
+        # looking for.
+        del sys.modules[__name__]
+        return None
+
     sys.path[0:0] = [mozilla_dir]
     import build.mach_bootstrap
     return build.mach_bootstrap.bootstrap(topsrcdir, mozilla_dir)
