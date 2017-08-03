ffb5ade40cd522d8e744cbdcba200c55ee41420d
aceman <acelists@atlas.sk>
2017-08-03 13:39:00 +0200

    Bug 1278067 - remove STEEL module from Thunderbird. r=mkmelin

--- mail/steel/test/head_steel.js
+++
@@ -1,6 +0,0 @@
-// These tests need an nsIXULAppInfo
-Components.utils.import("resource://testing-common/AppInfo.jsm");
-updateAppInfo();
-
-var Ci = Components.interfaces;
-var Cc = Components.classes;
