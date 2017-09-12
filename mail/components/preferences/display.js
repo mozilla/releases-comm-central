2ac4170ff909955802e72a510d898ef5ddc1e05a
Richard Marti <richard.marti@gmail.com>
2017-09-12 21:22:14 +0200

    Bug 1399206 - Port bug 1375978 to TB: enumerate fonts asynchronously. r=jorgk DONTBUILD

--- mail/components/preferences/display.js
+++ mail/components/preferences/display.js
@@ -39,15 +39,24 @@ var gDisplayPane = {
   _rebuildFonts: function ()
   {
     var langGroupPref = document.getElementById("font.language.group");
-    this._selectDefaultLanguageGroup(langGroupPref.value,
-          this._readDefaultFontTypeForLanguage(langGroupPref.value) == "serif");
+    this._safelySelectDefaultLanguageGroup(langGroupPref.value);
   },
 
   /**
    * Select the default language group.
    */
-  _selectDefaultLanguageGroup: function (aLanguageGroup, aIsSerif)
-  {
+  _selectDefaultLanguageGroupPromise: Promise.resolve(),
+
+  async _selectDefaultLanguageGroup(aLanguageGroup, aIsSerif) {
+    // Avoid overlapping language group selections by awaiting the resolution
+    // of the previous one.  We do this because this function is re-entrant,
+    // as inserting <preference> elements into the DOM sometimes triggers a call
+    // back into this function.  And since this function is also asynchronous,
+    // that call can enter this function before the previous run has completed,
+    // which would corrupt the font menulists.  Awaiting the previous call's
+    // resolution avoids that fate.
+    await this._selectDefaultLanguageGroupPromise;
+
     const kFontNameFmtSerif         = "font.name.serif.%LANG%";
     const kFontNameFmtSansSerif     = "font.name.sans-serif.%LANG%";
     const kFontNameListFmtSerif     = "font.name-list.serif.%LANG%";
@@ -92,13 +101,20 @@ var gDisplayPane = {
         // exact semantics of when it is called (whether during setAttribute or
         // during setElementValue) aren't obvious.
         if (prefs[i].fonttype)
-          FontBuilder.buildFontList(aLanguageGroup, prefs[i].fonttype, element);
+          await FontBuilder.buildFontList(aLanguageGroup, prefs[i].fonttype, element);
 
         element.setAttribute("preference", preference.id);
 
         preference.setElementValue(element);
       }
     }
+  },
+
+  _safelySelectDefaultLanguageGroup(aLanguageGroup) {
+    var isSerif = this._readDefaultFontTypeForLanguage(aLanguageGroup) == "serif";
+    this._selectDefaultLanguageGroupPromise =
+      this._selectDefaultLanguageGroup(aLanguageGroup, isSerif)
+        .catch(Components.utils.reportError);
   },
 
   /**
