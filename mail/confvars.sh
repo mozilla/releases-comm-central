b3f015cfadfe6161bc0ffdf30bed18c88a38eea2
Tom Prince <mozilla@hocat.ca>
2017-10-16 15:46:30 -0600

    Bug 1366607: Don't use bashisms in confvars.sh; rs=bustage-fix
    MozReview-Commit-ID: 5xWbVq6ko34
    
    --HG--
    extra : histedit_source : 19be5702da8462924ddb3143abc6f050276e8993

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-source ${_topsrcdir}/$MOZ_BUILD_APP/../comm-confvars.sh
+. ${_topsrcdir}/$MOZ_BUILD_APP/../comm-confvars.sh
 
 MOZ_APP_BASENAME=Thunderbird
 MOZ_APP_NAME=thunderbird
