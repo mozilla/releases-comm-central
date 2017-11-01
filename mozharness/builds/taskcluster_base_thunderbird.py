9d52ff40228a19fd8c215f696d05e2332dfc411e
Tom Prince <mozilla@hocat.ca>
2017-11-01 12:33:54 -0600

    Bug 1418593: Enable linux64 builds in taskcluster; r=Fallen
    MozReview-Commit-ID: 7GLGUZM8ahI
    
    --HG--
    extra : rebase_source : 1e1f4ad545d711cda0d8f468019bde41a2ef9496

---
+++ mozharness/builds/taskcluster_base_thunderbird.py
@@ -0,0 +1,7 @@
+config = {
+    'stage_product': 'thunderbird',
+    'stage_username': 'tbirdbld',
+    'stage_ssh_key': 'tbirdbld_rsa',
+    'app_name': 'comm/mail',
+    'objdir': 'obj-thunderbird',
+}
