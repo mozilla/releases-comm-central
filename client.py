071dd1bb79835c60fbe9e6bd4bba533894987380
Tom Prince <mozilla@hocat.ca>
2017-11-18 00:17:10 -0700

    HACK: Point to mozilla-cc-taskcluster repo.
    --HG--
    extra : rebase_source : 5528287523da53f6b34a4fad61108a70a3e4f946
    extra : source : 626eac6fa410a3175dd8321cdfe7ee16a7069a9a

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ DEFAULTS = {
     #  'INSPECTOR_REV':  'DOMI_2_0_17',
 
     # URL of the default hg repository to clone for Mozilla.
-    'MOZILLA_REPO': 'https://hg.mozilla.org/mozilla-central/',
+    'MOZILLA_REPO': 'https://hg.mozilla.org/users/mozilla_hocat.ca/mozilla-cc-taskcluster',
 }
 
 REPO_SHORT_NAMES = {
