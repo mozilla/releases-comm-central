4168e932109195b00c17a32db0f0123b4ee0e886
Tom Prince <mozilla@hocat.ca>
2017-12-31 14:36:04 -0700

    Bug 1427505: Get rid of setting MOZ_LDAP_XPCOM from configure.in; r=Fallen,frg
    It is now set in `moz.configure`.
    
    MozReview-Commit-ID: 7GXjetTH4MO
    
    --HG--
    extra : rebase_source : e2aa9e502a8ae558dae0b1a6e1fae51dd493b6c0
    extra : histedit_source : a390a2e8b226f6e9d84fa5f219d6acb3dce18a44

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -9,7 +9,6 @@ MOZ_APP_BASENAME=SeaMonkey
 MOZ_APP_VENDOR=Mozilla
 MOZ_APP_NAME=seamonkey
 MOZ_APP_DISPLAYNAME=SeaMonkey
-MOZ_LDAP_XPCOM=1
 MOZ_SUITE=1
 MOZ_BRANDING_DIRECTORY=$commreltopsrcdir/suite/branding/nightly
 MOZ_OFFICIAL_BRANDING_DIRECTORY=$commreltopsrcdir/suite/branding/nightly
