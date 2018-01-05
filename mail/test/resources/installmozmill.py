dd053fe438d86b9604a06ecf5829fbc997a426a8
Tom Prince <mozilla@hocat.ca>
2018-01-05 12:21:02 -0700

    Bug 1428494: Clean up linting of installmozmill.py; r=Fallen
    Differential Revision: https://phabricator.services.mozilla.com/D393
    
    --HG--
    extra : rebase_source : 9934e8c98b24834ce407112f766fee9afeabf4ac

--- mail/test/resources/installmozmill.py
+++ mail/test/resources/installmozmill.py
@@ -12,97 +12,108 @@ import os
 import sys
 from subprocess import call
 
-### utility functions for cross-platform
+
+# utility functions for cross-platform
 
 def is_windows():
-  return sys.platform.startswith('win')
+    return sys.platform.startswith('win')
+
 
 def esc(path):
-  """quote and escape a path for cross-platform use"""
-  return '"%s"' % repr(path)[1:-1]
+    """quote and escape a path for cross-platform use"""
+    return '"%s"' % repr(path)[1:-1]
+
 
 def scripts_path(virtual_env):
-  """path to scripts directory"""
-  if is_windows():
-    return os.path.join(virtual_env, 'Scripts')
-  return os.path.join(virtual_env, 'bin')
+    """path to scripts directory"""
+    if is_windows():
+        return os.path.join(virtual_env, 'Scripts')
+    return os.path.join(virtual_env, 'bin')
+
 
 def python_script_path(virtual_env, script_name):
-  """path to a python script in a virtualenv"""
-  scripts_dir = scripts_path(virtual_env)
-  if is_windows():
-    script_name = script_name + '-script.py'
-  return os.path.join(scripts_dir, script_name)
+    """path to a python script in a virtualenv"""
+    scripts_dir = scripts_path(virtual_env)
+    if is_windows():
+        script_name = script_name + '-script.py'
+    return os.path.join(scripts_dir, script_name)
+
 
 def entry_point_path(virtual_env, entry_point):
-  path = os.path.join(scripts_path(virtual_env), entry_point)
-  if is_windows():
-    path += '.exe'
-  return path
+    path = os.path.join(scripts_path(virtual_env), entry_point)
+    if is_windows():
+        path += '.exe'
+    return path
 
-### command-line entry point
+
+# command-line entry point
 
 def main(args=None):
-  """command line front-end function"""
+    """command line front-end function"""
 
-  # parse command line arguments
-  args = args or sys.argv[1:]
-  usage = "Usage: %prog destination path/to/mozbase"
+    # parse command line arguments
+    args = args or sys.argv[1:]
 
-  # Print the python version
-  print 'Python: %s' % sys.version
+    # Print the python version
+    print 'Python: %s' % sys.version
 
-  # The data is kept in the same directory as the script
-  source=os.path.abspath(os.path.dirname(__file__))
+    # The data is kept in the same directory as the script
+    source = os.path.abspath(os.path.dirname(__file__))
 
-  # directory to install to
-  if len(args) == 2:
-    destination = os.path.abspath(args[0])
-    mozbase = os.path.abspath(args[1])
-  else:
-    print "Usage: %s destination path/to/mozbase" % sys.argv[0]
-    sys.exit(1)
+    # directory to install to
+    if len(args) == 2:
+        destination = os.path.abspath(args[0])
+        mozbase = os.path.abspath(args[1])
+    else:
+        print "Usage: %s destination path/to/mozbase" % sys.argv[0]
+        sys.exit(1)
 
-  os.chdir(source)
+    os.chdir(source)
 
-  # check for existence of necessary files
-  if not os.path.exists('virtualenv'):
-    print "File not found: virtualenv"
-    sys.exit(1)
+    # check for existence of necessary files
+    if not os.path.exists('virtualenv'):
+        print "File not found: virtualenv"
+        sys.exit(1)
 
-  # packages to install in dependency order
-  packages = ["jsbridge", "mozmill"]
+    # packages to install in dependency order
+    packages = ["jsbridge", "mozmill"]
 
-  # create the virtualenv and install packages
-  env = os.environ.copy()
-  env.pop('PYTHONHOME', None)
-  returncode = call([sys.executable, os.path.join('virtualenv', 'virtualenv.py'),
-                     # Without this, virtualenv.py may attempt to contact the outside
-                     # world and search for or download a newer version of pip,
-                     # setuptools, or wheel. This is bad for security, reproducibility,
-                     # and speed.
-                     "--no-download",
-                     destination], env=env)
-  if returncode:
-    print 'Failure to install virtualenv'
-    sys.exit(returncode)
-  pip = entry_point_path(destination, 'pip')
+    # create the virtualenv and install packages
+    env = os.environ.copy()
+    env.pop('PYTHONHOME', None)
+    returncode = call([sys.executable, os.path.join('virtualenv', 'virtualenv.py'),
+                       # Without this, virtualenv.py may attempt to contact the outside
+                       # world and search for or download a newer version of pip,
+                       # setuptools, or wheel. This is bad for security, reproducibility,
+                       # and speed.
+                       "--no-download",
+                       destination], env=env)
+    if returncode:
+        print 'Failure to install virtualenv'
+        sys.exit(returncode)
+    pip = entry_point_path(destination, 'pip')
 
-  # Install mozbase packages to the virtualenv
-  mozbase_packages = ['manifestparser', 'mozfile', 'mozinfo', 'mozlog',
-    'mozprofile', 'mozcrash', 'moznetwork', 'mozprocess', 'mozdevice',
-    'mozrunner']
-  returncode = call([pip, 'install'] +
-    [os.path.join(mozbase, package) for package in mozbase_packages], env=env)
-  if returncode:
-    print 'Failure to install packages'
-    sys.exit(returncode)
+    # Install mozbase packages to the virtualenv
+    mozbase_packages = [
+        'manifestparser', 'mozfile', 'mozinfo', 'mozlog',
+        'mozprofile', 'mozcrash', 'moznetwork', 'mozprocess', 'mozdevice',
+        'mozrunner',
+    ]
+    returncode = call(
+        [pip, 'install'] +
+        [os.path.join(mozbase, package) for package in mozbase_packages], env=env)
+    if returncode:
+        print 'Failure to install packages'
+        sys.exit(returncode)
 
-  # Install mozmill
-  returncode = call([pip, 'install'] + [os.path.abspath(package) for package in packages], env=env)
-  if returncode:
-    print 'Failure to install packages'
-    sys.exit(returncode)
+    # Install mozmill
+    returncode = call(
+        [pip, 'install'] +
+        [os.path.abspath(package) for package in packages], env=env)
+    if returncode:
+        print 'Failure to install packages'
+        sys.exit(returncode)
+
 
 if __name__ == '__main__':
-  main()
+    main()
