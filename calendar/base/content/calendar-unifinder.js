d3c89bf925f77d29705888aa022d0af1b94bca7e
eslint <eslint@bugzilla.kewis.ch>
2018-01-19 17:19:27 +0100

    Bug 1431761 - Remove getProgressMode from tree views. r=MakeMyDay
    MozReview-Commit-ID: A62AnpbBSql

--- calendar/base/content/calendar-unifinder.js
+++ calendar/base/content/calendar-unifinder.js
@@ -751,8 +751,6 @@ var unifinderTreeView = {
 
     getImageSrc: function(aRow, aOrientation) {},
 
-    getProgressMode: function(aRow, aCol) {},
-
     getCellValue: function(aRow, aCol) {
         return null;
     },
