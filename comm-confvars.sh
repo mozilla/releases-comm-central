1bb4ae82b306a393c62494311a39878dc573b991
Tom Prince <mozilla@hocat.ca>
2018-01-23 11:30:39 -0700

    Bug 1399679: Use posix shell in comm-confvars.sh; r=me
    --HG--
    extra : rebase_source : 97beafc86fddf8999d7f806a214dfbe63d46fb64
    extra : histedit_source : e2852972d6220c7fd0d903d7aa70ce88f7559e5c

--- comm-confvars.sh
+++ comm-confvars.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-if [[ "$MOZ_BUILD_APP" == *comm* ]]
-then
-  # we are building with comm/ as the subdirectory
-  # $srcdir is the mozilla repo, comm is in the subdir
-  moztopsrcdir=$srcdir
-  commtopsrcdir=$srcdir/comm
+case "$MOZ_BUILD_APP" in
+  *comm*)
+    # we are building with comm/ as the subdirectory
+    # $srcdir is the mozilla repo, comm is in the subdir
+    moztopsrcdir=$srcdir
+    commtopsrcdir=$srcdir/comm
 
-  mozreltopsrcdir=.
-  commreltopsrcdir=comm
+    mozreltopsrcdir=.
+    commreltopsrcdir=comm
 
-  commtopobjdir=$_objdir/comm
-else
-  # we are building with mozilla/ as the subdirectory
-  # $srcdir is still the mozilla repo, so use the parent for comm
-  moztopsrcdir=$srcdir
-  commtopsrcdir=$srcdir/..
+    commtopobjdir=$_objdir/comm
+    ;;
+  *)
+    # we are building with mozilla/ as the subdirectory
+    # $srcdir is still the mozilla repo, so use the parent for comm
+    moztopsrcdir=$srcdir
+    commtopsrcdir=$srcdir/..
 
-  mozreltopsrcdir=mozilla
-  commreltopsrcdir=.
+    mozreltopsrcdir=mozilla
+    commreltopsrcdir=.
 
-  commtopobjdir=$_objdir
-fi
+    commtopobjdir=$_objdir
+    ;;
+esac
