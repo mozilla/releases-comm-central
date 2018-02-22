c4f215b9281f35f45c73a5c8f23ebd87791521db
Philipp Kewisch <mozilla@kewis.ch>
2018-02-22 16:02:28 +0100

    Bug 1440587 - Move calProviderUtils to the cal.provider namespace - rfc3339 automatic changes. r=MakeMyDay
    MozReview-Commit-ID: BiUGFO9JdB6
    
    --HG--
    extra : rebase_source : 8035c4b87017004c05a329c3dce7b1b87affe9d0

--- calendar/test/unit/test_rfc3339_parser.js
+++ calendar/test/unit/test_rfc3339_parser.js
@@ -15,7 +15,7 @@ function really_run_test() {
     function testRfc3339(aRfc3339Date, aTimezone, aExpectedDateTime,
                           aExpectedRfc3339Date=aRfc3339Date) {
         // Test creating a dateTime object from an RFC 3339 string.
-        let dateTime = cal.fromRFC3339(aRfc3339Date, aTimezone);
+        let dateTime = cal.dtz.fromRFC3339(aRfc3339Date, aTimezone);
 
         // Check that each property is as expected.
         let expectedDateProps = {
@@ -40,7 +40,7 @@ function really_run_test() {
         }
 
         // Test round tripping that dateTime object back to an RFC 3339 string.
-        let rfc3339Date = cal.toRFC3339(dateTime);
+        let rfc3339Date = cal.dtz.toRFC3339(dateTime);
 
         // In theory this should just match the input RFC 3339 date, but there are
         // multiple ways of generating the same time, e.g. 2006-03-14Z is
