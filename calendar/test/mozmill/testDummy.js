ec1f5c4381236e6ff00308625ad57c46b439ee86
Jorg K <jorgk@jorgk.com>
2018-03-29 22:20:53 +0200

    Bug 1449487 - temporarily remove all Mozmill testing in Calendar. rs=bustage-fix

---
+++ calendar/test/mozmill/testDummy.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Dummy file to keep mozmilltests.list non-empty. See bug 1449487.
+
+var MODULE_NAME = "testDummy";
+var RELATIVE_ROOT = "./shared-modules";
+var MODULE_REQUIRES = [];
+
+function setupModule(module) {}
+
+function teardownTest(module) {}
