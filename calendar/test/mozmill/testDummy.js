c6b58a33a9b4d9842e6497dcba8c4533e900c9f6
Jorg K <jorgk@jorgk.com>
2018-04-14 11:11:42 +0200

    Bug 1450128 - Make testDummy.js do something (sleep) to avoid quick shutdown and crash (suggestion by Andrew McCreight). rs=bustage-fix

--- calendar/test/mozmill/testDummy.js
+++ calendar/test/mozmill/testDummy.js
@@ -7,7 +7,15 @@
 var MODULE_NAME = "testDummy";
 var RELATIVE_ROOT = "./shared-modules";
 var MODULE_REQUIRES = [];
+var controller = {};
+ChromeUtils.import("chrome://mozmill/content/modules/controller.js", controller);
 
 function setupModule(module) {}
+
+function test_dummy() {
+    // Make the test do something so it doesn't cause bug 1450128.
+    dump("testDummy: Sleeping 20 seconds\n");
+    controller.sleep(20000);
+}
 
 function teardownTest(module) {}
