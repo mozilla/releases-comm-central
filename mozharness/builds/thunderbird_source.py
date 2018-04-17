a8970ac731132a8259857080d1b15d9676838db7
Tom Prince <mozilla@hocat.ca>
2018-04-17 10:33:46 -0600

    Bug 1455757: Add support building Thunderbird source packages in taskcluster during releases; r=Fallen
    Differential Revision: https://phabricator.services.mozilla.com/D1006
    
    --HG--
    extra : source : 8e4d00d056c3ca7fb9689b13c0b3be8dd4265d8e
    extra : amend_source : 4f39bb344ba0c2fa7d91b8ea61ca196fa20b297f
    extra : intermediate-source : 203617a5affb571e9282082e184292bdfea3a3eb

---
+++ mozharness/builds/thunderbird_source.py
@@ -0,0 +1,13 @@
+config = {
+    'default_actions': ['package-source'],
+    'objdir': 'obj-firefox',
+    'stage_platform': 'source',  # Not used, but required by the script
+    'buildbot_json_path': 'buildprops.json',
+    'app_ini_path': 'FAKE',  # Not used, but required by the script
+    'env': {
+        'HG_SHARE_BASE_DIR': '/builds/hg-shared',
+        'TINDERBOX_OUTPUT': '1',
+        'LC_ALL': 'C',
+    },
+    'src_mozconfig': 'comm/mail/config/mozconfigs/linux64/source',
+}
