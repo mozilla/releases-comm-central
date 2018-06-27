8caad13c1ea974b6b68c1ea15d348156a553e872
Geoff Lankow <geoff@darktrojan.net>
2018-06-27 23:35:11 +1200

    Bug 1454041 - Follow-up: Fix view-source links from Error Console on server builds. r=jorgk

--- mail/base/content/mailCore.js
+++ mail/base/content/mailCore.js
@@ -37,6 +37,31 @@ XPCOMUtils.defineLazyGetter(this, "gViewSourceUtils", function() {
   return scope.gViewSourceUtils;
 });
 
+Services.obs.addObserver({
+  observe(win) {
+    win.addEventListener("load", function() {
+      if (this.location.href != "chrome://devtools/content/webconsole/browserconsole.xul") {
+        return;
+      }
+
+      this.setTimeout(() => {
+        this.gViewSourceUtils.viewSource = async function (aArgs) {
+          // Check if external view source is enabled. If so, try it. If it fails,
+          // fallback to internal view source.
+          if (Services.prefs.getBoolPref("view_source.editor.external")) {
+            try {
+              await this.openInExternalEditor(aArgs);
+              return;
+            } catch (ex) {}
+          }
+
+          window.openDialog("chrome://messenger/content/viewSource.xul", "_blank", "all,dialog=no", aArgs);
+        };
+      }, 0);
+    }, { capture: false, once: true });
+  },
+}, "chrome-document-global-created");
+
 var gCustomizeSheet = false;
 
 function overlayRestoreDefaultSet() {
