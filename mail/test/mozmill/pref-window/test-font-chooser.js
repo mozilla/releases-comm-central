64b6eb34f060122f571ae51686b4dfef39c6d6ad
Geoff Lankow <geoff@darktrojan.net>
2018-07-11 04:15:00 +0200

    Bug 1474862 - improve test-font-chooser.js so it clicks instead of using openDialog(). r=aceman

--- mail/test/mozmill/pref-window/test-font-chooser.js
+++ mail/test/mozmill/pref-window/test-font-chooser.js
@@ -80,42 +80,40 @@ function _verify_fonts_displayed(aDefaults, aSerif, aSansSerif, aMonospace) {
                 "No font names were populated in the font picker.");
   assert_fonts_equal("display pane", displayPaneExpected, displayPaneActual.value);
 
-  // Now verify the advanced dialog.
-  function verify_advanced(fontc) {
-    // The font pickers are populated async so we need to wait for it.
-    for (let fontElemId of ["serif", "sans-serif", "monospace"]) {
-      fontc.waitFor(() => fontc.e(fontElemId).label != "",
-                    "Timeout waiting for font picker '" + fontElemId + "' to populate.");
-    }
+  // Now open the advanced dialog.
+  mc.click(content_tab_eid(prefTab, "advancedFonts"));
+  let fontc = wait_for_frame_load(
+    content_tab_e(prefTab, "dialogOverlay-0").querySelector("browser"),
+    "chrome://messenger/content/preferences/fonts.xul"
+  );
 
-    if (!aDefaults) {
-      assert_fonts_equal("serif", aSerif, fontc.e("serif").value);
-      assert_fonts_equal("sans-serif", aSansSerif, fontc.e("sans-serif").value);
-      assert_fonts_equal("monospace", aMonospace, fontc.e("monospace").value);
-    } else {
-      // When default fonts are displayed in the menulist, there is no value set,
-      // only the label, in the form "Default (font name)".
-      if (AppConstants.platform == "linux") {
-        // On Linux the prefs we set contained only the generic font names,
-        // like 'serif', but here a specific font name will be shown, but it is
-        // system-dependent what it will be. So we just check for the 'Default'
-        // prefix.
-        assert_fonts_equal("serif", `Default (`, fontc.e("serif").label, true);
-        assert_fonts_equal("sans-serif", `Default (`, fontc.e("sans-serif").label, true);
-        assert_fonts_equal("monospace", `Default (`, fontc.e("monospace").label, true);
-      } else {
-        assert_fonts_equal("serif", `Default (${aSerif})`, fontc.e("serif").label);
-        assert_fonts_equal("sans-serif", `Default (${aSansSerif})`, fontc.e("sans-serif").label);
-        assert_fonts_equal("monospace", `Default (${aMonospace})`, fontc.e("monospace").label);
-      }
-    }
+  // The font pickers are populated async so we need to wait for it.
+  for (let fontElemId of ["serif", "sans-serif", "monospace"]) {
+    fontc.waitFor(() => fontc.e(fontElemId).label != "",
+                  "Timeout waiting for font picker '" + fontElemId + "' to populate.");
   }
 
-  // Now open the advanced dialog.
-  plan_for_modal_dialog("FontsDialog", verify_advanced);
-  mc.window.openDialog("chrome://messenger/content/preferences/fonts.xul",
-                       "Fonts", "chrome,titlebar,toolbar,centerscreen,modal");
-  wait_for_modal_dialog("FontsDialog");
+  if (!aDefaults) {
+    assert_fonts_equal("serif", aSerif, fontc.e("serif").value);
+    assert_fonts_equal("sans-serif", aSansSerif, fontc.e("sans-serif").value);
+    assert_fonts_equal("monospace", aMonospace, fontc.e("monospace").value);
+  } else {
+    // When default fonts are displayed in the menulist, there is no value set,
+    // only the label, in the form "Default (font name)".
+    if (AppConstants.platform == "linux") {
+      // On Linux the prefs we set contained only the generic font names,
+      // like 'serif', but here a specific font name will be shown, but it is
+      // system-dependent what it will be. So we just check for the 'Default'
+      // prefix.
+      assert_fonts_equal("serif", `Default (`, fontc.e("serif").label, true);
+      assert_fonts_equal("sans-serif", `Default (`, fontc.e("sans-serif").label, true);
+      assert_fonts_equal("monospace", `Default (`, fontc.e("monospace").label, true);
+    } else {
+      assert_fonts_equal("serif", `Default (${aSerif})`, fontc.e("serif").label);
+      assert_fonts_equal("sans-serif", `Default (${aSansSerif})`, fontc.e("sans-serif").label);
+      assert_fonts_equal("monospace", `Default (${aMonospace})`, fontc.e("monospace").label);
+    }
+  }
 
   close_pref_tab(prefTab);
 }
