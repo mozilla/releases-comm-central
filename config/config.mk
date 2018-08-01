31248a268462865bd5115cc7da61788c8c5732d1
Tom Prince <mozilla@hocat.ca>
2018-08-01 05:43:36 +0000

    Bug 1479904: Remove build files used for buildbot/comm-topdir configurations; r=Fallen
    Differential Revision: https://phabricator.services.mozilla.com/D2581
    
    --HG--
    extra : moz-landing-system : lando

--- config/config.mk
+++
@@ -1,7 +0,0 @@
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-# Just use mozilla-central's copy of config.mk now.
-include $(MOZILLA_DIR)/config/config.mk
--- config/recurse.mk
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-
-ifndef INCLUDED_RULES_MK
-include $(topsrcdir)/config/rules.mk
-endif
-
-include $(MOZILLA_DIR)/config/recurse.mk
