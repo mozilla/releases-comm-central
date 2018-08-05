6520e0768c31119ffb4ce9b44d8e061c9bff84e9
Frank-Rainer Grahl <frgrahl@gmx.net>
2018-08-05 12:29:58 +0200

    Bug 1479890 - Fix invalid assignment to readonly tab.hidden. r=IanN

--- suite/components/sessionstore/nsSessionStore.js
+++ suite/components/sessionstore/nsSessionStore.js
@@ -2499,7 +2499,13 @@ SessionStoreService.prototype = {
       let browser = tabbrowser.getBrowserForTab(tab);
       let tabData = aTabData[t];
 
-      tab.hidden = tabData.hidden;
+      if (tabData.hidden) {
+        tab.setAttribute("hidden", true);
+      } else {
+        if (tab.hidden) {
+          tab.removeAttribute("hidden");
+        }
+      }
 
       for (let name in tabData.attributes)
         this.xulAttributes[name] = true;
