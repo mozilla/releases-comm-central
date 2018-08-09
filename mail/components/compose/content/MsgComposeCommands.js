33870a928aca59c7227d6c648270f342bbfbce27
Magnus Melin <mkmelin+mozilla@iki.fi>
2018-08-09 12:38:04 +0300

    Bug 1482058 - fix 'edit as new message' not working when identity email is null. r=jorgk

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -2855,7 +2855,7 @@ function ComposeStartup(aParams)
     // Search for a matching identity.
     if (from) {
       for (let ident of fixIterator(identities, Ci.nsIMsgIdentity)) {
-        if (from == ident.email.toLowerCase()) {
+        if (ident.email && from == ident.email.toLowerCase()) {
           if (suitableCount == 0)
             params.identity = ident;
           suitableCount++;
