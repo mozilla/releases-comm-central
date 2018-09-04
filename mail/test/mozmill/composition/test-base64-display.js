5ea8b6688dc9bfd8ed42474e3a5f9056865488c1
Jorg K <jorgk@jorgk.com>
2018-09-04 09:46:33 +0200

    Bug 1487421 - add MozMill test for display of message in base64 with white-space in between. r=aceman

---
+++ mail/test/mozmill/composition/test-base64-display.js
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Tests that messages with "broken" base64 are correctly displayed.
+ */
+
+// mozmake SOLO_TEST=composition/test-base64-display.js mozmill-one
+
+var MODULE_NAME = "test-base64-display.js";
+
+var RELATIVE_ROOT = "../shared-modules";
+var MODULE_REQUIRES = ["folder-display-helpers", "window-helpers"];
+
+var os = {};
+ChromeUtils.import("chrome://mozmill/content/stdlib/os.js", os);
+
+function setupModule(module) {
+  for (let lib of MODULE_REQUIRES) {
+    collector.getModule(lib).installInto(module);
+  }
+}
+
+function test_base64_display() {
+  let file = os.getFileForPath(os.abspath("./base64-with-whitespace.eml",
+                               os.getFileForPath(__file__)));
+  let msgc = open_message_from_file(file);
+  let bodyText = msgc.e("messagepane").contentDocument
+                     .querySelector("body").textContent;
+  close_window(msgc);
+
+  assert_true(bodyText.includes("abcdefghijklmnopqrstuvwxyz"),
+              "Decoded base64 text not found in message.");
+}
