3379931af8a313277eb6f7fcf5b18fc60af23d87
Geoff Lankow <geoff@darktrojan.net>
2018-09-05 20:00:56 +1200

    Bug 1488674 - Replace the XPCOM component registration for nsBaseCommandController removed in bug 1488112. r=jorgk

--- common/src/nsCommonModule.cpp
+++ common/src/nsCommonModule.cpp
@@ -2,6 +2,7 @@
 #include "nsCommonBaseCID.h"
 #include "nsComponentManagerExtra.h"
 #include "nsTransactionManagerExtra.h"
+#include "nsBaseCommandController.h"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsComponentManagerExtra)
 NS_DEFINE_NAMED_CID(NS_COMPONENTMANAGEREXTRA_CID);
@@ -9,15 +10,25 @@ NS_DEFINE_NAMED_CID(NS_COMPONENTMANAGEREXTRA_CID);
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsTransactionManagerExtra)
 NS_DEFINE_NAMED_CID(NS_TRANSACTIONMANAGEREXTRA_CID);
 
+#define NS_BASECOMMANDCONTROLLER_CID \
+  { 0xbf88b48c, 0xfd8e, 0x40b4, { 0xba, 0x36, 0xc7, 0xc3, 0xad, 0x6d, 0x8a, 0xc9 } }
+#define NS_BASECOMMANDCONTROLLER_CONTRACTID \
+  "@mozilla.org/embedcomp/base-command-controller;1"
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsBaseCommandController)
+NS_DEFINE_NAMED_CID(NS_BASECOMMANDCONTROLLER_CID);
+
 const mozilla::Module::CIDEntry kCommonCIDs[] = {
   { &kNS_COMPONENTMANAGEREXTRA_CID, false, nullptr, nsComponentManagerExtraConstructor },
   { &kNS_TRANSACTIONMANAGEREXTRA_CID, false, nullptr, nsTransactionManagerExtraConstructor },
+  { &kNS_BASECOMMANDCONTROLLER_CID, false, nullptr, nsBaseCommandControllerConstructor },
   { nullptr }
 };
 
 const mozilla::Module::ContractIDEntry kCommonContracts[] = {
   { NS_COMPONENTMANAGEREXTRA_CONTRACTID, &kNS_COMPONENTMANAGEREXTRA_CID },
   { NS_TRANSACTIONMANAGEREXTRA_CONTRACTID, &kNS_TRANSACTIONMANAGEREXTRA_CID },
+  { NS_BASECOMMANDCONTROLLER_CONTRACTID, &kNS_BASECOMMANDCONTROLLER_CID },
   { nullptr }
 };
 
