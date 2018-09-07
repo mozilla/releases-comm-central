0ce071ba90f9f2a6ac9d2b8c030abf1c5ccbad25
Rob Lemley <rob@thunderbird.net>
2018-09-07 01:34:53 +0000

    Bug 1488045 - Port Bug 1488045: Move repackage configuration from mozharness into taskcluster. r=tomprince
    Specify package-name in base.py. Fixes Thunderbird installer build on Windows.
    Fix paths to Thunderbird installer components.
    
    Differential Revision: https://phabricator.services.mozilla.com/D4880

---
+++ mozharness/repackage/base.py
@@ -0,0 +1,7 @@
+# Paths are relative to mozilla-central
+config = {
+    "package-name": "thunderbird",
+    "installer-tag": "comm/mail/installer/windows/app.tag",
+    "sfx-stub": "comm/other-licenses/7zstub/thunderbird/7zSD.sfx",
+    "stub-installer-tag": "",
+}
