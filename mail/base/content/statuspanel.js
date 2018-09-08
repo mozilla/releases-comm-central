db40333316d1847fce1dd110ac3a0560f686b5d0
Arshad Khan <arshdkhn1@gmail.com>
2018-09-09 00:34:20 +0530

    Bug 1489795 - Migrate statuspanel binding to custom element. r=mkmelin

---
+++ mail/base/content/statuspanel.js
@@ -0,0 +1,76 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global MozXULElement */
+
+class MozStatuspanel extends MozXULElement {
+  static get observedAttributes() {
+    return ["label", "mirror"];
+  }
+
+  connectedCallback() {
+    const hbox = document.createElement("hbox");
+    hbox.classList.add("statuspanel-inner");
+
+    const label = document.createElement("label");
+    label.classList.add("statuspanel-label");
+    label.setAttribute("flex", "1");
+    label.setAttribute("crop", "end");
+
+    hbox.appendChild(label);
+    this.appendChild(hbox);
+
+    this._labelElement = label;
+
+    this._updateAttributes();
+    this._setupEventListeners();
+  }
+
+  attributeChangedCallback() {
+    this._updateAttributes();
+  }
+
+  set label(val) {
+    if (!this.label) {
+      this.removeAttribute("mirror");
+    }
+    this.setAttribute("label", val);
+    return val;
+  }
+
+  get label() {
+    return this.getAttribute("label");
+  }
+
+  _updateAttributes() {
+    if (!this._labelElement) {
+      return;
+    }
+
+    if (this.hasAttribute("label")) {
+      this._labelElement.setAttribute("value", this.getAttribute("label"));
+    } else {
+      this._labelElement.removeAttribute("value");
+    }
+
+    if (this.hasAttribute("mirror")) {
+      this._labelElement.setAttribute("mirror", this.getAttribute("mirror"));
+    } else {
+      this._labelElement.removeAttribute("mirror");
+    }
+  }
+
+  _setupEventListeners() {
+    this.addEventListener("mouseover", event => {
+      if (this.hasAttribute("mirror")) {
+        this.removeAttribute("mirror");
+      } else {
+        this.setAttribute("mirror", "true");
+      }
+    });
+  }
+}
+
+customElements.define("statuspanel", MozStatuspanel);
