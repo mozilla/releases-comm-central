d5c6eaac6d9efc96a8ff14961e21f1463e3de97b
Arshad Khan <arshdkhn1@gmail.com>
2018-09-17 13:21:35 +0530

    Bug 1491756 - Convert mail-tagfield to custom element; r=mkmelin

--- mail/base/content/mailWidgets.js
+++ mail/base/content/mailWidgets.js
@@ -3,8 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global MozXULElement */
-/* global openUILink */
+/* global MozXULElement, openUILink */
 
 class MozMailHeaderfield extends MozXULElement {
   connectedCallback() {
@@ -29,5 +28,51 @@ class MozMailUrlfield extends MozMailHeaderfield {
   }
 }
 
+class MozMailHeaderfieldTags extends MozXULElement {
+  connectedCallback() {
+    this.classList.add("headerValue");
+    ChromeUtils.import("resource:///modules/MailServices.jsm");
+  }
+
+  set headerValue(val) {
+    return this.buildTags(val);
+  }
+
+  buildTags(tags) {
+    // tags contains a list of actual tag names (not the keys), delimited by spaces
+    // each tag name is encoded.
+
+    // remove any existing tag items we've appended to the list
+    while (this.hasChildNodes()) {
+      this.lastChild.remove();
+    }
+
+    // tokenize the keywords based on ' '
+    const tagsArray = tags.split(" ");
+    for (let i = 0; i < tagsArray.length; i++) {
+      // for each tag, create a label, give it the font color that corresponds to the
+      // color of the tag and append it.
+      let tagName;
+      try {
+        // if we got a bad tag name, getTagForKey will throw an exception, skip it
+        // and go to the next one.
+        tagName = MailServices.tags.getTagForKey(tagsArray[i]);
+      } catch (ex) {
+        continue;
+      }
+
+      let color = MailServices.tags.getColorForKey(tagsArray[i]);
+
+      // now create a label for the tag name, and set the color
+      const label = document.createElement("label");
+      label.setAttribute("value", tagName);
+      label.className = "tagvalue blc-" + color.substr(1);
+
+      this.appendChild(label);
+    }
+  }
+}
+
 customElements.define("mail-headerfield", MozMailHeaderfield);
 customElements.define("mail-urlfield", MozMailUrlfield);
+customElements.define("mail-tagfield", MozMailHeaderfieldTags);
