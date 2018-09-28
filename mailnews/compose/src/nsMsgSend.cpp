3d73529de77bb1c1e4c790ec48c4861756edf40e
Magnus Melin <mkmelin+mozilla@iki.fi>
2018-09-28 15:42:29 +0300

    Bug 1444379 - add error checking to call sites of GetDefaultPrompt() to avoid crash in nsMsgComposeAndSend::NotifyListenerOnStopCopy() and elsewhere. r=jorgk
    Especially for the shutdown case, it may not be possible to get a prompt

--- mailnews/compose/src/nsMsgSend.cpp
+++ mailnews/compose/src/nsMsgSend.cpp
@@ -462,14 +462,11 @@ nsMsgComposeAndSend::GatherMimeAttachments()
 
   uint32_t multipartRelatedCount = GetMultipartRelatedCount(); // The number of related part we will have to generate
 
-  nsCOMPtr<nsIPrompt> promptObject; // only used if we have to show an alert here....
-  GetDefaultPrompt(getter_AddRefs(promptObject));
-
   char *hdrs = 0;
   bool maincontainerISrelatedpart = false;
   const char * toppart_type = nullptr;
-
   status = m_status;
+
   if (NS_FAILED(status))
     goto FAIL;
 
@@ -3204,7 +3201,8 @@ nsMsgComposeAndSend::DeliverMessage()
     if (!msg.IsEmpty())
     {
       nsCOMPtr<nsIPrompt> prompt;
-      GetDefaultPrompt(getter_AddRefs(prompt));
+      rv = GetDefaultPrompt(getter_AddRefs(prompt));
+      NS_ENSURE_SUCCESS(rv, rv);
       nsMsgAskBooleanQuestionByString(prompt, msg.get(), &abortTheSend);
       if (!abortTheSend)
       {
@@ -3241,9 +3239,6 @@ nsMsgComposeAndSend::DeliverFileAsMail()
 
   if (mSendReport)
     mSendReport->SetCurrentProcess(nsIMsgSendReport::process_SMTP);
-
-  nsCOMPtr<nsIPrompt> promptObject;
-  GetDefaultPrompt(getter_AddRefs(promptObject));
 
   if (!buf)
   {
@@ -3393,9 +3388,6 @@ nsMsgComposeAndSend::DeliverFileAsNews()
   if (mSendReport)
     mSendReport->SetCurrentProcess(nsIMsgSendReport::process_NNTP);
 
-  nsCOMPtr<nsIPrompt> promptObject;
-  GetDefaultPrompt(getter_AddRefs(promptObject));
-
   nsCOMPtr<nsINntpService> nntpService(do_GetService(NS_NNTPSERVICE_CONTRACTID, &rv));
 
   if (NS_SUCCEEDED(rv) && nntpService)
@@ -3438,7 +3430,8 @@ nsMsgComposeAndSend::Fail(nsresult aFailureCode, const char16_t *aErrorMsg,
   if (NS_FAILED(aFailureCode))
   {
     nsCOMPtr<nsIPrompt> prompt;
-    GetDefaultPrompt(getter_AddRefs(prompt));
+    nsresult rv = GetDefaultPrompt(getter_AddRefs(prompt));
+    NS_ENSURE_SUCCESS(rv, rv);
 
     if (mSendReport)
     {
@@ -3788,11 +3781,11 @@ nsMsgComposeAndSend::NotifyListenerOnStopCopy(nsresult aStatus)
 
   SetStatusMessage(msg);
   nsCOMPtr<nsIPrompt> prompt;
-  GetDefaultPrompt(getter_AddRefs(prompt));
+  nsresult rv = GetDefaultPrompt(getter_AddRefs(prompt));
+  NS_ENSURE_SUCCESS(rv, rv);
 
   if (NS_FAILED(aStatus))
   {
-    nsresult rv;
     nsCOMPtr<nsIStringBundleService> bundleService =
       mozilla::services::GetStringBundleService();
     NS_ENSURE_TRUE(bundleService, NS_ERROR_UNEXPECTED);
@@ -3898,7 +3891,8 @@ nsMsgComposeAndSend::NotifyListenerOnStopCopy(nsresult aStatus)
       {
         // Save to Local Folders failed. Inform the user.
         nsCOMPtr<nsIPrompt> prompt;
-        GetDefaultPrompt(getter_AddRefs(prompt));
+        rv = GetDefaultPrompt(getter_AddRefs(prompt));
+        NS_ENSURE_SUCCESS(rv, rv);
         nsMsgDisplayMessageByName(prompt, "saveToLocalFoldersFailed");
       }
     }
@@ -3914,7 +3908,7 @@ nsMsgComposeAndSend::NotifyListenerOnStopCopy(nsresult aStatus)
       !mPerformingSecondFCC && m_messageKey != nsMsgKey_None &&
       (m_deliver_mode == nsMsgDeliverNow || m_deliver_mode == nsMsgSendUnsent))
   {
-    nsresult rv = FilterSentMessage();
+    rv = FilterSentMessage();
     if (NS_FAILED(rv))
       OnStopOperation(rv);
     return rv;
@@ -3976,7 +3970,8 @@ nsMsgComposeAndSend::OnStopOperation(nsresult aStatus)
     if (NS_SUCCEEDED(rv))
     {
       nsCOMPtr<nsIPrompt> prompt;
-      GetDefaultPrompt(getter_AddRefs(prompt));
+      rv = GetDefaultPrompt(getter_AddRefs(prompt));
+      NS_ENSURE_SUCCESS(rv, rv);
       nsMsgDisplayMessageByString(prompt, msg.get(), nullptr);
     }
 
