f14861943fb074d02d593601545fcae5bbd0c3f6
Richard Marti <richard.marti@gmail.com>
2018-10-11 19:29:10 +0200

    Bug 1496752 - Part 2: Make it possible to enable the extra drag space in customize window. r=jorgk

--- common/src/customizeToolbar.js
+++ common/src/customizeToolbar.js
@@ -82,12 +82,14 @@ function initDialog() {
     smallIconsCheckbox.disabled = true;
 
   if (AppConstants.MOZ_APP_NAME == "thunderbird") {
-    let titlebarCheckbox = document.getElementById("showTitlebar");
+      document.getElementById("showTitlebar").checked =
+        !Services.prefs.getBoolPref("mail.tabs.drawInTitlebar");
+      document.getElementById("showDragSpace").checked =
+        Services.prefs.getBoolPref("mail.tabs.extraDragSpace");
     if (window.opener &&
        (window.opener.document.documentElement.getAttribute("windowtype") ==
         "mail:3pane")) {
-      titlebarCheckbox.hidden = false;
-      titlebarCheckbox.checked = !Services.prefs.getBoolPref("mail.tabs.drawInTitlebar");
+      document.getElementById("titlebarSettings").hidden = false;
     }
   }
 
@@ -545,6 +547,15 @@ function updateIconSize(aSize) {
 function updateTitlebar() {
   let titlebarCheckbox = document.getElementById("showTitlebar");
   Services.prefs.setBoolPref("mail.tabs.drawInTitlebar", !titlebarCheckbox.checked);
+
+  // Bring the customizeToolbar window to front (on linux it's behind the main
+  // window). Otherwise the customization window gets left in the background.
+  setTimeout(() => window.focus(), 100);
+}
+
+function updateDragSpace() {
+  let dragSpaceCheckbox = document.getElementById("showDragSpace");
+  Services.prefs.setBoolPref("mail.tabs.extraDragSpace", dragSpaceCheckbox.checked);
 
   // Bring the customizeToolbar window to front (on linux it's behind the main
   // window). Otherwise the customization window gets left in the background.
