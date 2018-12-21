11c85f9095297f9f910a5752f1ac4bd0c1b15c85
Geoff Lankow <geoff@darktrojan.net>
2018-12-21 22:12:14 +1300

    Bug 1515877 - Turn on ESLint in mailnews - move more-specific rules into mailnews/extensions/newsblog. r=aceman

--- mailnews/extensions/newsblog/.eslintrc.js
+++ mailnews/extensions/newsblog/.eslintrc.js
@@ -8,7 +8,14 @@ module.exports = {
     "FeedParser": true,
     "FeedUtils": true,
     "GetNumSelectedMessages": true,
+    "Log4Moz": true,
+    "MailServices": true,
+    "MsgHdrToMimeMessage": true,
+    "ReloadMessage": true,
+    "Services": true,
+    "gDBView": true,
     "gMessageNotificationBar": true,
+    "getBrowser": true,
     "onCheckItem": true,
     "openContentTab": true,
   },
@@ -16,5 +23,23 @@ module.exports = {
   "rules": {
     // Warn about cyclomatic complexity in functions.
     "complexity": ["error", 70],
+
+    // Require braces around blocks that start a new line.
+    "curly": ["error", "multi-line"],
+
+    // Enforce valid JSDoc comments.
+    "valid-jsdoc": ["error", {
+      prefer: { return: "returns" },
+      preferType: {
+        "boolean": "Boolean",
+        "string": "String",
+        "number": "Number",
+        "object": "Object",
+        "function": "Function",
+        "map": "Map",
+        "set": "Set",
+        "date": "Date",
+      },
+    }],
   },
 };
