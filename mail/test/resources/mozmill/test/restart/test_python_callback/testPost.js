15869a628f91889311f5f809bbd56f71bd265004
Jorg K <jorgk@jorgk.com>
2019-01-03 01:58:25 +0100

    Bug 1517059 - Tidy up Mozmill/JSBridge resources: remove mail/test/resources/mozmill/test/. r=darktrojan

--- mail/test/resources/mozmill/test/restart/test_python_callback/testPost.js
+++
@@ -1,6 +0,0 @@
-var jum = {}; ChromeUtils.import("chrome://mozmill/content/modules/jum.js", jum);
-
-var testPythonCallPost = function() {
-  var status = "post";
-  mozmill.firePythonCallbackAfterRestart("postCallback", status);
-}
--- mail/test/resources/mozmill/test/restart/test_user_restart/test5.js
+++
@@ -1,8 +0,0 @@
-var setupModule = function () {
-  controller = mozmill.getBrowserController();
-}
-
-var teardownModule = function () {
-  controller.startUserShutdown(1000, true);
-  controller.window.Application.restart();
-}
