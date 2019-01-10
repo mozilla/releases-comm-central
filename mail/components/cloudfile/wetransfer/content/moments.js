fe5a4e48b622e54493c5ab0111074afe510ea6be
Geoff Lankow <geoff@darktrojan.net>
2019-01-11 10:31:25 +1300

    Bug 1518076 - Fix linting error in WeTransfer extension; rs=me DONTBUILD

--- mail/components/cloudfile/wetransfer/content/moments.js
+++ mail/components/cloudfile/wetransfer/content/moments.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* globals browser */
+
 (async () => {
   for (let element of document.querySelectorAll("[data-message]")) {
     element.textContent = browser.i18n.getMessage(element.dataset.message);
