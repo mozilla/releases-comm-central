332b5a35eb18552288b9f4fd6dcbd200f85c84be
Rob Lemley <rob@thunderbird.net>
2019-01-24 19:53:50 -0500

    Bug 1522715 - Port Bug 1520874: Simplify Mozharness l10n configs. rs=bustage-fix DONTBUILD
    This simplifies the configuration for the l10n repackaging step.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17585

--- mozharness/single_locale/thunderbird.py
+++ mozharness/single_locale/thunderbird.py
@@ -1,6 +1,11 @@
 config = {
     # Source path
     'app_name': 'comm/mail',
+    'mozconfig_variant': 'l10n-mozconfig',
+
+    # FIXME: See bug 1522715. -rjl
+    "objdir": "obj-firefox",
+    "l10n_dir": "l10n",
     "locales_file": "src/comm/mail/locales/all-locales",
     "locales_dir": "comm/mail/locales",
 }
