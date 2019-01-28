8c94e0d72b0558d2a65f610f816047f7ef59cce4
Ben Campbell <benc@thunderbird.net>
2019-01-28 16:42:35 +1300

    Bug 1527764 - Remove rdf use from folder-lookup-service. r=aceman

--- mailnews/base/src/folderLookupService.js
+++ mailnews/base/src/folderLookupService.js
@@ -3,9 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /**
- * This module implements the folder lookup service. Presently, this uses RDF as
- * the backing store, but the intent is that this will eventually become the
- * authoritative map.
+ * This module implements the folder lookup service (nsIFolderLookupService).
  */
 
 "use strict";
@@ -14,24 +12,14 @@ var { XPCOMUtils } = ChromeUtils.import(
   "resource://gre/modules/XPCOMUtils.jsm"
 );
 
-function isValidFolder(folder) {
-  // RDF is liable to return folders that don't exist, and we may be working
-  // with a deleted folder but we're still holding on to the reference. For
-  // valid folders, one of two scenarios is true: either the folder has a parent
-  // (the deletion code clears the parent to indicate its nonvalidity), or the
-  // folder is a root folder of some server. Getting the root folder may throw
-  // an exception if we attempted to create a server that doesn't exist, so we
-  // need to guard for that error.
-  try {
-    return folder.parent != null || folder.rootFolder == folder;
-  } catch (e) {
-    return false;
-  }
-}
-
-// This insures that the service is only created once
+// This ensures that the service is only created once.
 var gCreated = false;
 
+/**
+ * folderLookupService maintains an index of folders and provides
+ * lookup by folder URI.
+ * @class
+ */
 function folderLookupService() {
   if (gCreated) {
     throw Cr.NS_ERROR_ALREADY_INITIALIZED;
@@ -39,79 +27,127 @@ function folderLookupService() {
   this._map = new Map();
   gCreated = true;
 }
+
 folderLookupService.prototype = {
-  // XPCOM registration stuff
   QueryInterface: ChromeUtils.generateQI([Ci.nsIFolderLookupService]),
   classID: Components.ID("{a30be08c-afc8-4fed-9af7-79778a23db23}"),
 
-  // nsIFolderLookupService impl
-  getFolderForURL(aUrl) {
-    let folder = null;
-    // First, see if the folder is in our cache.
-    if (this._map.has(aUrl)) {
-      let valid = false;
-      try {
-        folder = this._map.get(aUrl).QueryReferent(Ci.nsIMsgFolder);
-        // We don't want to return "dangling" (parentless) folders.
-        valid = isValidFolder(folder);
-      } catch (e) {
-        // The object was deleted, so it's not valid
-      }
+  /**
+   * Fetch the folder corresponding to the given URI.
+   * Will only return folders which already exist and have a parent. If this
+   * not the case then null is returned.
+   *
+   * @param {String} uri - URI of folder to get.
+   * @returns {nsIMsgFolder|null}
+   */
+  getFolderForURL(uri) {
+    let folder = this._getExisting(uri);
 
-      if (valid) {
-        return folder;
-      }
-
-      // Don't keep around invalid folders.
-      this._map.delete(aUrl);
-      folder = null;
+    if (folder && !this._isValidFolder(folder)) {
+      folder = null; // no dangling folders!
     }
-
-    // If we get here, then the folder was not in our map. It could be that the
-    // folder was created by somebody else, so try to find that folder.
-    // For now, we use the RDF service, since it results in minimal changes. But
-    // RDF has a tendency to create objects without checking to see if they
-    // really exist---use the parent property to see if the folder is a real
-    // folder.
-    if (folder == null) {
-      folder = this.getOrCreateFolderForURL(aUrl);
-      if (!folder) {
-        return null;
-      }
-    }
-    // We don't want to return "dangling" (parentless) folders.
-    if (!isValidFolder(folder)) {
-      return null;
-    }
-
-    // Add the new folder to our map. Store a weak reference instead, so that
-    // the folder can be closed when necessary.
-    let weakRef = folder
-      .QueryInterface(Ci.nsISupportsWeakReference)
-      .GetWeakReference();
-    this._map.set(aUrl, weakRef);
     return folder;
   },
-  getOrCreateFolderForURL(aUrl) {
-    // Check that aUrl has an active scheme, in case this folder is from
+
+  /**
+   * Fetch the folder corresponding to the given URI, creating it if it does
+   * not exist. If the folder is created, it will be a "dangling" folder,
+   * without a parent and not part of a normal folder hierarchy.
+   * A lot of code relies on this behaviour, but for new code this
+   * call should probably be avoided.
+   *
+   * @param {String} uri - URI of folder to get.
+   * @returns {nsIMsgFolder}
+   */
+  getOrCreateFolderForURL(uri) {
+    let folder = this._getExisting(uri);
+    if (folder) {
+      return folder;
+    }
+
+    // Create new folder.
+
+    // Check that uri has an active scheme, in case this folder is from
     // an extension that is currently disabled or hasn't started up yet.
-    // Extract the scheme in the same way that the RDF service does.
-    let scheme = aUrl.match(/\w*/)[0];
+    let scheme = uri.match(/\w*/)[0];
     let contractID = "@mozilla.org/rdf/resource-factory;1?name=" + scheme;
     if (!(contractID in Cc)) {
+      Cu.reportError(
+        "getOrCreateFolderForURL: factory not registered for " + uri
+      );
       return null;
     }
 
-    // NOTE: this doesn't update _map, but it'll work fine and
-    // it's a transitional function we want deleted anyway.
-    let rdf = Cc["@mozilla.org/rdf/rdf-service;1"].getService(Ci.nsIRDFService);
-    try {
-      let folder = rdf.GetResource(aUrl).QueryInterface(Ci.nsIMsgFolder);
-      return folder;
-    } catch (e) {
-      // If the QI fails, then we somehow picked up an RDF resource that isn't
-      // a folder. Return null in this case.
+    let factory = Components.manager.getClassObject(
+      Cc[contractID],
+      Ci.nsIFactory
+    );
+    if (!factory) {
+      Cu.reportError(
+        "getOrCreateFolderForURL: failed to get factory for " + uri
+      );
       return null;
+    }
+
+    // TODO: we'll need to move init() up into nsIMsgFolder interface
+    let res = factory.createInstance(null, Ci.nsIRDFResource);
+    if (!res) {
+      return null;
+    }
+    folder = res.QueryInterface(Ci.nsIMsgFolder);
+    if (folder) {
+      res.Init(uri);
+      // Add the new folder to our map. Store a weak reference instead, so that
+      // the folder can be closed when necessary.
+      let weakRef = folder
+        .QueryInterface(Ci.nsISupportsWeakReference)
+        .GetWeakReference();
+      this._map.set(uri, weakRef);
+    }
+
+    return folder;
+  },
+
+  // "private" stuff starts here.
+
+  /**
+   * Internal helper to find a folder (which may or may not be dangling).
+   *
+   * @param {String} uri - URI of folder to look up.
+   *
+   * @returns {nsIMsgFolder|null} - The folder, if in the index, else null.
+   */
+  _getExisting(uri) {
+    let folder = null;
+    // already created?
+    if (this._map.has(uri)) {
+      try {
+        folder = this._map.get(uri).QueryReferent(Ci.nsIMsgFolder);
+      } catch (e) {
+        // The object was deleted, so we can drop it.
+        this._map.delete(uri);
+      }
+    }
+    return folder;
+  },
+
+  /**
+   * Internal helper function to test if a folder is dangling or parented.
+   * Because we can return folders that don't exist, and we may be working
+   * with a deleted folder but we're still holding on to the reference. For
+   * valid folders, one of two scenarios is true: either the folder has a parent
+   * (the deletion code clears the parent to indicate its nonvalidity), or the
+   * folder is a root folder of some server. Getting the root folder may throw
+   * an exception if we attempted to create a server that doesn't exist, so we
+   * need to guard for that error.
+   *
+   * @returns {Boolean} - true if folder valid (and parented).
+   */
+  _isValidFolder(folder) {
+    try {
+      return folder.parent != null || folder.rootFolder == folder;
+    } catch (e) {
+      return false;
     }
   },
 };
