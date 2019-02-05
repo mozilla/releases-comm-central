dd29ee63fac07dc405ceb695e4b21180e9b57de9
Geoff Lankow <geoff@darktrojan.net>
2019-02-06 11:01:36 +1300

    Bug 1520643 - Fix redefinition of Services in webext mochitests; rs=me

--- mail/components/extensions/test/browser/head.js
+++ mail/components/extensions/test/browser/head.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-ChromeUtils.defineModuleGetter(this, "Services", "resource://gre/modules/Services.jsm");
-ChromeUtils.defineModuleGetter(this, "MailServices", "resource:///modules/MailServices.jsm");
+var {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var {MailServices} = ChromeUtils.import("resource:///modules/MailServices.jsm");
 
 function createAccount() {
   registerCleanupFunction(() => {
