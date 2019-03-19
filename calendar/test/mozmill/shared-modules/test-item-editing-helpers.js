a8baeb501c655f42b1ad5233aa9d35b3af165ee1
Geoff Lankow <geoff@darktrojan.net>
2019-03-20 12:28:03 +1300

    Bug 1533702 - Bug 1534930 follow-up: Fix broken element selector in test-item-editing-helpers.js. r=me

--- calendar/test/mozmill/shared-modules/test-item-editing-helpers.js
+++ calendar/test/mozmill/shared-modules/test-item-editing-helpers.js
@@ -52,8 +52,7 @@ var ATTENDEES_ROW = `
 // Only for Tasks.
 var PERCENT_COMPLETE_INPUT = `
     id("event-grid-todo-status-row")/id("event-grid-todo-status-picker-box")/
-    id("percent-complete-textbox")/anon({"class":"textbox-input-box"})/
-    anon({"anonid":"input"})
+    id("percent-complete-textbox")
 `;
 
 // To be appended to the path for a date- or timepicker.
