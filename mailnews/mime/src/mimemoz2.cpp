d22b8a9ee584bae594259f6c5d612d5979398c48
Jorg K <jorgk@jorgk.com>
2019-04-21 22:37:35 +0200

    Bug 1545998 - Use nsDependentCSubstring() to avoid MOZ_RELEASE_ASSERT() introduced in bug 1536689. r=aceman

--- mailnews/mime/src/mimemoz2.cpp
+++ mailnews/mime/src/mimemoz2.cpp
@@ -822,7 +822,7 @@ int ConvertToUTF8(const char *stringToUse, int32_t inLength,
 
   if (newCharset.Equals("UTF-7", nsCaseInsensitiveCStringComparator())) {
     nsAutoString utf16;
-    rv = CopyUTF7toUTF16(nsDependentCString(stringToUse, inLength), utf16);
+    rv = CopyUTF7toUTF16(nsDependentCSubstring(stringToUse, inLength), utf16);
     if (NS_FAILED(rv))
       return -1;
     CopyUTF16toUTF8(utf16, outString);
