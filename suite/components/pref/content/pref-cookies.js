710c636fab250e96235f0f7277a98f09a5ae31a3
Frank-Rainer Grahl <frgrahl@gmx.net>
2019-05-12 14:19:23 +0200

    Bug 1546747 - Remove references to non existing element alwaysAcceptSession in cookie preferences. r=IanN

--- suite/components/pref/content/pref-cookies.js
+++ suite/components/pref/content/pref-cookies.js
@@ -9,10 +9,11 @@ function Startup()
 
 function SetDisables(aSetFocus)
 {
+  // Policy 1 was "ask before accepting" and is no longer valid.
+
   // const for Cookie Accept Policy
   const kCookiesDisabled = 2;
   // const for Cookie Lifetime Policy
-  const kAskBeforeAccepting = 1;
   const kAcceptForNDays = 3;
 
   var behavior = document.getElementById("networkCookieBehavior");
@@ -23,15 +24,10 @@ function SetDisables(aSetFocus)
   var days = document.getElementById("lifetimeDays");
   var daysPref = document.getElementById(days.getAttribute("preference"));
 
-  var session = document.getElementById("alwaysAcceptSession");
-  var sessionPref = document.getElementById(session.getAttribute("preference"));
-
   var cookiesDisabled = (behaviorPref.value == kCookiesDisabled);
   lifetime.disabled = cookiesDisabled || lifetimePref.locked;
   days.disabled = cookiesDisabled || daysPref.locked ||
                   (lifetimePref.value != kAcceptForNDays);
-  session.disabled = cookiesDisabled || sessionPref.locked ||
-                     (lifetimePref.value != kAskBeforeAccepting);
 
   if (!days.disabled && aSetFocus)
     days.focus();
