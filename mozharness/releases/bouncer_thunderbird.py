dd9d03ae97af4786f59b270e1aaaf15c26145b10
Rob Lemley <rob@thunderbird.net>
2019-05-15 11:19:54 +0200

    Bug 1550685 - Add bouncer-check config for comm-beta, comm-esr60. r=darktrojan
    Fix failed release-bouncer-check task during release promotion.
    Continue the effort to move Thunderbird code out of M-C.

---
+++ mozharness/releases/bouncer_thunderbird.py
@@ -0,0 +1,55 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# lint_ignore=E501
+config = {
+    "products": {
+        "installer": {
+            "product-name": "Thunderbird-%(version)s",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
+        },
+        "installer-ssl": {
+            "product-name": "Thunderbird-%(version)s-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
+        },
+        "complete-mar": {
+            "product-name": "Thunderbird-%(version)s-Complete",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
+        },
+    },
+    "partials": {
+        "releases-dir": {
+            "product-name": "Thunderbird-%(version)s-Partial-%(prev_version)s",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
+        },
+    },
+}
