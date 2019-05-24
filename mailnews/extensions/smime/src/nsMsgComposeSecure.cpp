f97ec060e3a2bee521ecd1c0fa678f85ed7edba4
Kai Engert <kaie@kuix.de>
2019-05-24 16:53:48 +0200

    Bug 1519093 - prompt for smartcard PIN when S/MIME signing. r=keeler
    With advice from Daiki Ueno and testing by Jan Horak.
    
    --HG--
    extra : rebase_source : 89be4205a91d7e39b81ae39390fd6fd639c5ac14

--- mailnews/extensions/smime/src/nsMsgComposeSecure.cpp
+++ mailnews/extensions/smime/src/nsMsgComposeSecure.cpp
@@ -28,6 +28,7 @@
 #include "nspr.h"
 #include "mozpkix/Result.h"
 #include "nsNSSCertificate.h"
+#include "nsNSSHelper.h"
 
 using namespace mozilla::mailnews;
 using namespace mozilla;
@@ -803,6 +804,14 @@ nsresult nsMsgComposeSecure::MimeCryptoHackCerts(const char *aRecipients,
 
   RefPtr<SharedCertVerifier> certVerifier(GetDefaultCertVerifier());
   NS_ENSURE_TRUE(certVerifier, NS_ERROR_UNEXPECTED);
+
+  // Calling CERT_GetCertNicknames has the desired side effect of
+  // traversing all tokens, and bringing up prompts to unlock them.
+  nsCOMPtr<nsIInterfaceRequestor> ctx = new PipUIContext();
+  CERTCertNicknames *result_unused =
+    CERT_GetCertNicknames(CERT_GetDefaultCertDB(),
+    SEC_CERT_NICKNAMES_USER, ctx);
+  CERT_FreeNicknames(result_unused);
 
   UniqueCERTCertList builtChain;
   if (!mEncryptionCertDBKey.IsEmpty()) {
