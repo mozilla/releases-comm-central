3bfc0971410849a2f59f9df45509764087bc53bf
aceman <acelists@atlas.sk>
2019-05-29 12:38:00 +0200

    Bug 1555461 - clean up mozmill/folder-display/test-invalid-db-folder-load.js. r=jorgk

--- mail/test/mozmill/folder-display/test-invalid-db-folder-load.js
+++ mail/test/mozmill/folder-display/test-invalid-db-folder-load.js
@@ -15,42 +15,36 @@
 
 var MODULE_NAME = "test-invalid-db-folder-load";
 var RELATIVE_ROOT = "../shared-modules";
-var MODULE_REQUIRES = ["folder-display-helpers", "window-helpers"];
+var MODULE_REQUIRES = ["folder-display-helpers"];
 
 var folder;
 var setA;
-var curMessage;
-
-var nsMsgViewSortType = Ci.nsMsgViewSortType;
 
 function setupModule(module) {
   let fdh = collector.getModule("folder-display-helpers");
   fdh.installInto(module);
-  let wh = collector.getModule("window-helpers");
-  wh.installInto(module);
 
   folder = create_folder("InvalidMSF");
   [setA] = make_new_sets_in_folder(folder, [{count: 3}]);
 }
 
 /**
- *
+ * Check if the db of a folder assumed to be invalid can be restored.
  */
 function test_load_folder_with_invalidDB() {
-  folder.msgDatabase.dBFolderInfo.sortType = nsMsgViewSortType.bySubject;
+  folder.msgDatabase.dBFolderInfo.sortType = Ci.nsMsgViewSortType.bySubject;
   folder.msgDatabase.summaryValid = false;
   folder.msgDatabase.ForceClosed();
   folder.msgDatabase = null;
-  dump("entering invalidmsf folder\n");
   be_in_folder(folder);
 
   assert_messages_in_view(setA);
-  curMessage = select_click_row(0);
+  var curMessage = select_click_row(0);
   assert_selected_and_displayed(curMessage);
 }
 
 function test_view_sort_maintained() {
-  if (mc.dbView.sortType != nsMsgViewSortType.bySubject)
+  if (mc.dbView.sortType != Ci.nsMsgViewSortType.bySubject)
       throw new Error("view sort type not restored from invalid db");
 }
 
