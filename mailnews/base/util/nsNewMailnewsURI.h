1477e0605b26af9bc683f19d83e97b8d31371634
Jorg K <jorgk@jorgk.com>
2019-05-29 11:25:35 +0200

    Bug 1550945 - Introduce NS_NewMailnewsURI. rs=bustage-fix CLOSED TREE DONTBUILD
    --HG--
    extra : amend_source : 1b8d5e55840e6e27c5f68b003a38adcf53442dea

---
+++ mailnews/base/util/nsNewMailnewsURI.h
@@ -0,0 +1,16 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsNewMailnewsURI_h__
+#define nsNewMailnewsURI_h__
+
+#include "nsIURI.h"
+#include "nsNetUtil.h"
+
+nsresult NS_NewMailnewsURI(nsIURI** aURI, const nsACString& aSpec,
+                           const char* aCharset /* = nullptr */,
+                           nsIURI* aBaseURI /* = nullptr */,
+                           nsIIOService* aIOService /* = nullptr */);
+#endif
