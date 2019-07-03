87fa0d570ae34ea2525cafe31ef9903f86d662fc
Richard Marti <richard.marti@gmail.com>
2019-07-03 06:38:19 +0200

    Bug 1498332 - Redesign and rearrange Thunderbird Preferences/Options similarly to Firefox. r=darktrojan, ui-r=aleca Part 5: Calendar pane

--- calendar/lightning/content/messenger-overlay-preferences.js
+++ calendar/lightning/content/messenger-overlay-preferences.js
@@ -15,31 +15,12 @@
 Preferences.add({ id: "calendar.preferences.lightning.selectedTabIndex", type: "int" });
 
 var gLightningPane = {
-    mInitialized: false,
-
     init: function() {
-        let preference = Preferences.get("calendar.preferences.lightning.selectedTabIndex");
-        let ltnPrefs = document.getElementById("calPreferencesTabbox");
-        if (preference.value) {
-            ltnPrefs.selectedIndex = preference.value;
-        }
-        ltnPrefs.addEventListener("select", gLightningPane.tabSelectionChanged.bind(this));
-        this.mInitialized = true;
-
         let elements = document.querySelectorAll("#paneLightning preference");
         for (let element of elements) {
             element.updateElements();
         }
     },
-
-    tabSelectionChanged: function() {
-        if (!this.mInitialized) {
-            return;
-        }
-        let ltnPrefs = document.getElementById("calPreferencesTabbox");
-        let preference = Preferences.get("calendar.preferences.lightning.selectedTabIndex");
-        preference.valueFromPreferences = ltnPrefs.selectedIndex;
-    }
 };
 
 gCalendarGeneralPane.init();
