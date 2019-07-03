d2a3a2f9275d505f470afb2f926c91a9dcd95945
Geoff Lankow <geoff@darktrojan.net>
2019-07-03 17:47:32 +1200

    Bug 1535116 - Override text in profile-downgrade dialog; r=aleca

---
+++ mail/base/content/profileDowngrade.js
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let gParams;
+
+const { AppConstants } = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+function init() {
+  /*
+   * The C++ code passes a dialog param block using its integers as in and out
+   * arguments for this UI. The following are the uses of the integers:
+   *
+   *  0: A set of flags from nsIToolkitProfileService.downgradeUIFlags.
+   *  1: A return argument, one of nsIToolkitProfileService.downgradeUIChoice.
+   */
+  gParams = window.arguments[0].QueryInterface(Ci.nsIDialogParamBlock);
+
+  document.addEventListener("dialogextra1", createProfile);
+  document.addEventListener("dialogaccept", quit);
+  document.addEventListener("dialogcancel", quit);
+
+  document.documentElement.getButton("accept").focus();
+}
+
+function quit() {
+  gParams.SetInt(1, Ci.nsIToolkitProfileService.quit);
+}
+
+function createProfile() {
+  gParams.SetInt(1, Ci.nsIToolkitProfileService.createNewProfile);
+  window.close();
+}
+
+function moreInfo(event) {
+  if (event.type == "keypress" && event.key != "Enter") {
+    return;
+  }
+  event.preventDefault();
+
+  let uri = Services.io.newURI("https://support.mozilla.org/en-US/kb/unable-launch-older-version-on-profile");
+  Cc["@mozilla.org/uriloader/external-protocol-service;1"]
+    .getService(Ci.nsIExternalProtocolService)
+    .loadURI(uri);
+}
