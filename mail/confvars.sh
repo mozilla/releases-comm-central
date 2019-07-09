dc6ccdacf786ef22b6e099916577c4302b2faf05
Rob Lemley <rob@thunderbird.net>
2019-07-09 18:44:24 -0400

    Bug 1564611 - Port bug 1458385: Repackaging Windows builds on Linux. r=darktrojan DONTBUILD
    Based on the changes in the referenced M-C bug.

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -19,12 +19,7 @@ MOZ_BRANDING_DIRECTORY=$commreltopsrcdir/mail/branding/nightly
 MOZ_OFFICIAL_BRANDING_DIRECTORY=$commreltopsrcdir/mail/branding/thunderbird
 
 MOZ_APP_ID={3550f703-e582-4d05-9a08-453d09bdfdc6}
-# This should usually be the same as the value MAR_CHANNEL_ID.
-# If more than one ID is needed, then you should use a comma separated list
-# of values.
-ACCEPTED_MAR_CHANNEL_IDS=thunderbird-comm-central
-# The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
-MAR_CHANNEL_ID=thunderbird-comm-central
+
 MOZ_PROFILE_MIGRATOR=1
 MOZ_BINARY_EXTENSIONS=1
 MOZ_SEPARATE_MANIFEST_FOR_THEME_OVERRIDES=1
