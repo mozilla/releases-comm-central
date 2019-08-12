9bcc5607c2ded74511eab11e2ac1feaac0b13898
Paul Morris <paul@thunderbird.net>
2019-08-12 13:53:43 -0400

    Bug 1577606 - Turn on Prettier but ignore all directories. r=darktrojan

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,6 @@ module.exports = {
       },
       ignoreEOLComments: true,
     }],
-    "prettier/prettier": "off",
     "semi-spacing": ["error", {"before": false, "after": true}],
     "space-in-parens": ["error", "never"],
 
