0347f45900dd7df32b3f16988169c13ecf817ce4
aceman <acelists@atlas.sk>
2019-09-13 09:10:00 +0200

    Bug 1484270 - Do not load Account Central until we have a folder selected on startup. r=mkmelin

--- mail/base/content/folderDisplay.js
+++ mail/base/content/folderDisplay.js
@@ -1808,6 +1808,12 @@ FolderDisplayWidget.prototype = {
    *  central page.
    */
   _showAccountCentral() {
+    if (!this.displayedFolder && MailServices.accounts.accounts.length > 0) {
+      // If we have any accounts set up, but no folder is selected yet,
+      // we expect another selection event to come when session restore finishes.
+      // Until then, do nothing.
+      return;
+    }
     var accountBox = document.getElementById("accountCentralBox");
     document.getElementById("displayDeck").selectedPanel = accountBox;
     var acctCentralPage = Services.prefs.getComplexValue(
