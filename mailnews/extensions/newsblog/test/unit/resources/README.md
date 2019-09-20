24425fbaa587543a3cf2b64b256a6abf590bdc6c
Ben Campbell <benc@thunderbird.net>
2019-09-21 10:58:49 +1200

    Bug 1534163 - Add unit test to cover feeds config migration. r=mkmelin

--- mailnews/extensions/newsblog/test/unit/resources/README.md
+++ mailnews/extensions/newsblog/test/unit/resources/README.md
@@ -1,11 +1,20 @@
 Data files for unit testing the feeds code.
 
-- `rss_7_1.rdf`        - Simple RSS1.0 feed example, from:
-                         https://www.w3.org/2000/10/rdf-tests/RSS_1.0/rss_7_1.rdf
-- `rss_7_1_BORKED.rdf` - Sabotaged version of `rss_7_1.rdf` with a bad
-                         <items> list, pointing to all sorts of URLs not
-                         represented as <item>s in the feed (see Bug 476641).
-- `rss2_example.xml`   - RSS2.0 example from wikipedia, but with
-                         Japanese text in the title, with leading/trailing
-                         whitespace.
+- `rss_7_1.rdf`
+   Simple RSS1.0 feed example, from:
+   https://www.w3.org/2000/10/rdf-tests/RSS_1.0/rss_7_1.rdf
 
+- `rss_7_1_BORKED.rdf`
+   Sabotaged version of `rss_7_1.rdf` with a bad
+   <items> list, pointing to all sorts of URLs not
+   represented as <item>s in the feed (see Bug 476641).
+
+- `rss2_example.xml`
+   RSS2.0 example from wikipedia, but with
+   Japanese text in the title, with leading/trailing
+   whitespace.
+
+- `feeds-*/`
+  Test cases for migrating legacy .rdf config files to
+  the new json ones. The .rdf files are the old data,
+  and the .json files are the expected results.
---
+++ mailnews/extensions/newsblog/test/unit/test_rdfmigration.js
@@ -0,0 +1,63 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { MailMigrator } = ChromeUtils.import(
+  "resource:///modules/MailMigrator.jsm"
+);
+const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
+
+/**
+ * Tests migration of old .rdf feed config files to the new .json files.
+ *
+ * @param {String} testDataDir - A directory containing legacy feeds.rdf and
+ *                               feeditems.rdf files, along with coressponding
+ *                               .json files containing the expected results
+ *                               of the migration.
+ * @returns {void}
+ */
+async function migrationTest(testDataDir) {
+  // Set up an RSS account/server.
+  let account = FeedUtils.createRssAccount("rss_migration_test");
+  let rootFolder = account.incomingServer.rootMsgFolder.QueryInterface(
+    Ci.nsIMsgLocalMailFolder
+  );
+  // Note, we don't create any folders to hold downloaded feed items,
+  // that's OK here, because we're only migrating the config files, not
+  // downloading feeds. The migration doesn't check destFolder existance.
+  let rootDir = rootFolder.filePath.path;
+
+  // Install legacy feeds.rdf/feeditems.rdf
+  for (let f of ["feeds.rdf", "feeditems.rdf"]) {
+    await OS.File.copy(OS.Path.join(testDataDir, f), OS.Path.join(rootDir, f));
+  }
+
+  // Perform the migration
+  MailMigrator._migrateRSSServer(account.incomingServer);
+
+  // Check actual results against expectations.
+  let loadJSON = async function(filename) {
+    let raw = await OS.File.read(filename, { encoding: "UTF-8" });
+    return JSON.parse(raw);
+  };
+  for (let f of ["feeds.json", "feeditems.json"]) {
+    let got = await loadJSON(OS.Path.join(rootDir, f));
+    let expected = await loadJSON(OS.Path.join(testDataDir, f));
+    Assert.deepEqual(got, expected, `match ${testDataDir}/${f}`);
+  }
+
+  // Delete the account and all it's files.
+  MailServices.accounts.removeAccount(account, true);
+}
+
+add_task(async function test_rdfmigration() {
+  let testDataDirs = [
+    "feeds-simple",
+    "feeds-empty",
+    "feeds-missing-timestamp",
+    "feeds-bad",
+  ];
+  for (let d of testDataDirs) {
+    await migrationTest(do_get_file(OS.Path.join("resources", d)).path);
+  }
+});
