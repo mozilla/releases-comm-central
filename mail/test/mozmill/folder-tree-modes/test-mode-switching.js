b3ab7478bd49d1b6a6a39e0d55e65c92d224907d
Jorg K <jorgk@jorgk.com>
2019-10-18 23:36:29 +0200

    Bug 1490639 - re-enable test-mode-switching::test_toggling_modes on Linux. a=backout

--- mail/test/mozmill/folder-tree-modes/test-mode-switching.js
+++ mail/test/mozmill/folder-tree-modes/test-mode-switching.js
@@ -304,7 +304,6 @@ function test_toggling_modes() {
   subtest_switch_to_smart_folders(true);
   subtest_switch_to_all_folders(true);
 }
-test_toggling_modes.EXCLUDED_PLATFORMS = ["linux"];
 
 function teardownModule() {
   tree.mode = "all";
