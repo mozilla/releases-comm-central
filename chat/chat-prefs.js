9170c63b8a366c45b9d83ccbd47fa972a1d6449e
Patrick Cloke <clokep>
2019-11-20 15:35:54 +0200

    Bug 1580807 - Disable new Twitter accounts due to bug 1445778. r=mkmelin

--- chat/chat-prefs.js
+++ chat/chat-prefs.js
@@ -88,6 +88,8 @@ pref("chat.prpls.prpl-skype.disable", true);
 pref("chat.prpls.prpl-facebook.disable", true);
 // Disable experimental Matrix support.
 pref("chat.prpls.prpl-matrix.disable", true);
+// Disable Twitter until support for the updated API is complete. See bug 1445778.
+pref("chat.prpls.prpl-twitter.disable", true);
 // Disable Yahoo Messenger as legacy Yahoo was shut down.
 pref("chat.prpls.prpl-yahoo.disable", true);
 // Whether to disable SRV lookups that use the system DNS library.
