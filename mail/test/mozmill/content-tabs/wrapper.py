00a7c1c0fc0bbed4b3d804b5584ddd10246e8724
Geoff Lankow <geoff@darktrojan.net>
2019-12-23 20:24:14 +1300

    Bug 1605030 - Remove mozmill tests from the tree. r=mkmelin
    --HG--
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/EventUtils.jsm => mail/test/browser/shared-modules/EventUtils.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/arrays.jsm => mail/test/browser/shared-modules/arrays.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/controller.jsm => mail/test/browser/shared-modules/controller.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/dom.jsm => mail/test/browser/shared-modules/dom.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/elementslib.jsm => mail/test/browser/shared-modules/elementslib.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/events.jsm => mail/test/browser/shared-modules/events.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/frame.jsm => mail/test/browser/shared-modules/frame.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/httpd.jsm => mail/test/browser/shared-modules/httpd.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/init.jsm => mail/test/browser/shared-modules/init.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/jum.jsm => mail/test/browser/shared-modules/jum.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/mozmill.jsm => mail/test/browser/shared-modules/mozmill.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/os.jsm => mail/test/browser/shared-modules/os.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/securable-module.jsm => mail/test/browser/shared-modules/securable-module.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/stdlib/strings.jsm => mail/test/browser/shared-modules/strings.jsm
    rename : mail/test/resources/mozmill/mozmill/extension/content/modules/utils.jsm => mail/test/browser/shared-modules/utils.jsm
    extra : rebase_source : 23e08c1a53f4e96200c650e3f97a782f1a89281b
    extra : amend_source : 49647aad4cef98f2f4c539e94e798691553721e5

--- mail/test/mozmill/content-tabs/wrapper.py
+++
@@ -1,8 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-PREFS = {
-    "extensions.blocklist.url": "http://localhost:43336/dummy.xml",
-    "plugin.load_flash_only": False,
-}
--- mail/test/mozmill/multiple-identities/readme.txt
+++
@@ -1,4 +0,0 @@
-Caution! The tests in this directory modify or outright delete the default
-accounts/identities. If you are writing new tests to go into this directory,
-don't assume the existence of any account except Local Folders (with a single
-identity).
