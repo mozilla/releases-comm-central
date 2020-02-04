36230db63edcf0e092d522be59ee262619262d3a
Rob Lemley <rob@thunderbird.net>
2020-02-04 14:09:30 -0500

    Bug 1410512 - Include release history on nightly builds. r=darktrojan
    The decision task for nightly builds needs to query Balrog for a recent release
    history in order to set up the partial mar generation tasks.
    
    --HG--
    extra : rebase_source : c2066a0f0debe3ae71638ebbde80099f822de871

--- taskcluster/comm_taskgraph/__init__.py
+++ taskcluster/comm_taskgraph/__init__.py
@@ -9,10 +9,13 @@ import logging
 from importlib import import_module
 
 from taskgraph import GECKO
+from taskgraph.util.partials import populate_release_history
 
 logger = logging.getLogger(__name__)
 
 COMM = os.path.join(GECKO, 'comm')
+
+BALROG_PRODUCT = 'Thunderbird'
 
 
 def register(graph_config):
@@ -27,3 +30,15 @@ def register(graph_config):
 def _import_modules(modules):
     for module in modules:
         import_module(".{}".format(module), package=__name__)
+
+
+def get_decision_parameters(graph_config, parameters):
+    logger.info("{}.get_decision_parameters called".format(__name__))
+    # If the target method is nightly, we should build partials. This means
+    # knowing what has been released previously.
+    # An empty release_history is fine, it just means no partials will be built
+    project = parameters['project']
+
+    parameters.setdefault('release_history', dict())
+    if 'nightly' in parameters.get('target_tasks_method', ''):
+        parameters['release_history'] = populate_release_history(BALROG_PRODUCT, project)
