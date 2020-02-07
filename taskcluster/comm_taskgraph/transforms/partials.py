f4ea2ae8a58953395acd214065dfe9fa300403ef
Rob Lemley <rob@thunderbird.net>
2020-02-07 14:45:52 -0500

    Bug 1410512 - Follow-up: Disable mbsdiff cache when building partial updates. rs=mkmelin
    Firefox does some caching on their partial generation. Right now, there is no S3 bucket
    for Thunderbird, so remove the scope to disable the cache.
    
    --HG--
    extra : amend_source : bdd305469ced6de5f90bfe6d62e94fa86e8ccbe5

---
+++ taskcluster/comm_taskgraph/transforms/partials.py
@@ -0,0 +1,35 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Thunderbird modifications to partial update building
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from taskgraph.transforms.base import TransformSequence
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def update_scopes(config, jobs):
+    """
+    Firefox does some caching when building partial updates, but there's no bucket for Thunderbird
+    at the moment. In the meantime, remove the scope from the task to avoid an error.
+    """
+    # If no balrog release history, then don't run
+    if not config.params.get('release_history'):
+        return
+
+    MBSDIFF_SCOPE = 'auth:aws-s3:read-write:tc-gp-private-1d-us-east-1/releng/mbsdiff-cache/'
+
+    for job in jobs:
+        task = job['task']
+        if MBSDIFF_SCOPE in task['scopes']:
+            task['scopes'].remove(MBSDIFF_SCOPE)
+
+        yield job
