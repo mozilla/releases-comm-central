20df33b7f0b2e3e922852181a2449f7ecdfd52da
Rob Lemley <rob@thunderbird.net>
2020-02-20 12:24:19 -0500

    Bug 1614463 - Port bug 1527313: Source directory caching for Windows builds part 2. r=darktrojan
    --HG--
    extra : rebase_source : f11aaf5df7644e0372eb538b0eaddea902cf84d6
    extra : amend_source : def6be045cd12bbca385e534f3a0ead0d1411362

--- mozharness/builds/taskcluster_base_thunderbird.py
+++ mozharness/builds/taskcluster_base_thunderbird.py
@@ -3,7 +3,6 @@ config = {
     'stage_username': 'tbirdbld',
     'stage_ssh_key': 'tbirdbld_rsa',
     'app_name': 'comm/mail',
-    'objdir': 'obj-thunderbird',
 
     # Thunderbird doesn't compile under pgo
     'pgo_platforms': []
--- mozharness/single_locale/thunderbird.py
+++ mozharness/single_locale/thunderbird.py
@@ -2,10 +2,6 @@ config = {
     # Source path
     'app_name': 'comm/mail',
     'mozconfig_variant': 'l10n-mozconfig',
-
-    # FIXME: See bug 1522715. -rjl
-    "objdir": "obj-firefox",
-    "l10n_dir": "l10n",
     "locales_file": "src/comm/mail/locales/all-locales",
     "locales_dir": "comm/mail/locales",
 }
