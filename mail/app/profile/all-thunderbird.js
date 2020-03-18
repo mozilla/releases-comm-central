e890be88b508a88721d51c233666aeee2cc3dedd
Geoff Lankow <geoff@darktrojan.net>
2020-03-18 20:13:03 +1300

    Port bug 1602635 - [macOS] Remove security.sandbox.content.mac.earlyinit. r=Paenglab
    --HG--
    extra : histedit_source : de37c1e1999c3441c92087ccf493d37f05b63ac2

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -774,12 +774,6 @@ pref("mail.save_msg_filename_underscores_for_space", false);
 pref("security.sandbox.content.level", 0);
 #endif
 
-#if defined(NIGHTLY_BUILD) && defined(XP_MACOSX) && defined(MOZ_SANDBOX)
-// Start the Mac sandbox immediately during child process startup instead
-// of when messaged by the parent after the message loop is running.
-pref("security.sandbox.content.mac.earlyinit", false);
-#endif
-
 #ifdef NIGHTLY_BUILD
 // See bug 1572568 for details. Disallow eval() with system principal.
 pref("security.allow_eval_with_system_principal", false);
