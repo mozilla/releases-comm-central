308e4fa253a017f4d615e9addf49f8b95f83a312
Frank-Rainer Grahl <frgrahl@gmx.net>
2020-03-29 21:30:33 +0200

    Bug 1625750 - Drop "Lightning/version" from SeaMonkey user agent string. r=IanN Port Bug 1624241 "Drop "Lightning/version" from Thunderbird user agent string".

--- suite/components/pref/content/pref-http.js
+++ suite/components/pref/content/pref-http.js
@@ -3,38 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var gLightningUAold;
-
-function Startup()
-{
-  CheckLightningUA();
-}
-
-// Lightning adds a UA token if calendar.useragent.extra is not empty.
-// Checkbox is visible if Lightning is installed and enabled.
-// Checkbox is checked if the pref contains any non-whitespace character.
-function CheckLightningUA()
-{
-  var prefLightningUA = document.getElementById("calendar.useragent.extra");
-  var boxLightningUA  = document.getElementById("uaLightningShow");
-
-  if (prefLightningUA.defaultValue)
-  {
-    boxLightningUA.hidden = false;
-    boxLightningUA.checked = prefLightningUA.value.trim() != "";
-    boxLightningUA.disabled = prefLightningUA.locked;
-    gLightningUAold = prefLightningUA.hasUserValue ? prefLightningUA.value : null;
-  }
-}
-
-// If the checkbox is checked, use previous user-set or default value.
-// If the checkbox is not checked, set the pref to an empty string.
-function OnLightningChanged(aChecked)
-{
-  var prefLightningUA = document.getElementById("calendar.useragent.extra");
-
-  if (aChecked && !gLightningUAold)
-    prefLightningUA.reset();
-  else
-    prefLightningUA.value = (aChecked && gLightningUAold) || "";
+function Startup() {
+  // Go away! Currently nothing here to see.
 }
