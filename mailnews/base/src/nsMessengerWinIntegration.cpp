0ddefcdee320b66cefcb7f4f417cdc3d215e24be
Magnus Melin <mkmelin+mozilla@iki.fi>
2020-04-03 13:38:10 +0300

    Bug 1626161 - followup - rs=clang-format

--- mailnews/base/src/nsMessengerWinIntegration.cpp
+++ mailnews/base/src/nsMessengerWinIntegration.cpp
@@ -1063,24 +1063,24 @@ nsresult nsMessengerWinIntegration::SetupUnreadCountUpdateTimer() {
 }
 
 NOTIFYICONDATAW sMailIconData = {
-  /* cbSize */ (DWORD)NOTIFYICONDATAW_V2_SIZE,
-  /* hWnd */ 0,
-  /* uID */ 2,
-  /* uFlags */ NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO,
-  /* uCallbackMessage */ WM_USER,
-  /* hIcon */ 0,
-  /* szTip */ L"",
-  /* dwState */ 0,
-  /* dwStateMask */ 0,
-  /* szInfo */ L"",
-  /* uVersion */ {30000},
-  /* szInfoTitle */ L"",
-  /* dwInfoFlags */ NIIF_USER | NIIF_NOSOUND
-};
+    /* cbSize */ (DWORD)NOTIFYICONDATAW_V2_SIZE,
+    /* hWnd */ 0,
+    /* uID */ 2,
+    /* uFlags */ NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO,
+    /* uCallbackMessage */ WM_USER,
+    /* hIcon */ 0,
+    /* szTip */ L"",
+    /* dwState */ 0,
+    /* dwStateMask */ 0,
+    /* szInfo */ L"",
+    /* uVersion */ {30000},
+    /* szInfoTitle */ L"",
+    /* dwInfoFlags */ NIIF_USER | NIIF_NOSOUND};
 
 static nsCOMArray<nsIBaseWindow> sHiddenWindows;
 static HWND sIconWindow;
-static LRESULT CALLBACK IconWindowProc(HWND msgWindow, UINT msg, WPARAM wp, LPARAM lp) {
+static LRESULT CALLBACK IconWindowProc(HWND msgWindow, UINT msg, WPARAM wp,
+                                       LPARAM lp) {
   if (msg == WM_USER && lp == WM_LBUTTONDOWN) {
     ::Shell_NotifyIconW(NIM_DELETE, &sMailIconData);
 
@@ -1102,19 +1102,18 @@ static LRESULT CALLBACK IconWindowProc(HWND msgWindow, UINT msg, WPARAM wp, LPAR
 }
 
 WNDCLASS sClassStruct = {
-  /* style */ 0,
-  /* lpfnWndProc */ &IconWindowProc,
-  /* cbClsExtra */ 0,
-  /* cbWndExtra */ 0,
-  /* hInstance */ 0,
-  /* hIcon */ 0,
-  /* hCursor */ 0,
-  /* hbrBackground */ 0,
-  /* lpszMenuName */ 0,
-  /* lpszClassName */ L"IconWindowClass"
-};
+    /* style */ 0,
+    /* lpfnWndProc */ &IconWindowProc,
+    /* cbClsExtra */ 0,
+    /* cbWndExtra */ 0,
+    /* hInstance */ 0,
+    /* hIcon */ 0,
+    /* hCursor */ 0,
+    /* hbrBackground */ 0,
+    /* lpszMenuName */ 0,
+    /* lpszClassName */ L"IconWindowClass"};
 
-nsresult nsMessengerWinIntegration::HideWindow(nsIBaseWindow* aWindow) {
+nsresult nsMessengerWinIntegration::HideWindow(nsIBaseWindow *aWindow) {
   aWindow->SetVisibility(false);
   sHiddenWindows.AppendElement(aWindow);
 
@@ -1122,29 +1121,30 @@ nsresult nsMessengerWinIntegration::HideWindow(nsIBaseWindow* aWindow) {
     // Register the window class.
     NS_ENSURE_TRUE(::RegisterClass(&sClassStruct), NS_ERROR_FAILURE);
     // Create the window.
-    NS_ENSURE_TRUE(
-      sIconWindow = ::CreateWindow(
-        /* className */ L"IconWindowClass",
-        /* title */ 0,
-        /* style */ WS_CAPTION,
-        /* x, y, cx, cy */ 0, 0, 0, 0,
-        /* parent */ 0,
-        /* menu */ 0,
-        /* instance */ 0,
-        /* create struct */ 0
-      ),
-      NS_ERROR_FAILURE
-    );
+    NS_ENSURE_TRUE(sIconWindow = ::CreateWindow(
+                       /* className */ L"IconWindowClass",
+                       /* title */ 0,
+                       /* style */ WS_CAPTION,
+                       /* x, y, cx, cy */ 0, 0, 0, 0,
+                       /* parent */ 0,
+                       /* menu */ 0,
+                       /* instance */ 0,
+                       /* create struct */ 0),
+                   NS_ERROR_FAILURE);
     sMailIconData.hWnd = sIconWindow;
-    sMailIconData.hIcon = ::LoadIcon(::GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPLICATION));
+    sMailIconData.hIcon =
+        ::LoadIcon(::GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPLICATION));
 
-    nsCOMPtr<nsIStringBundleService> bundleService = mozilla::services::GetStringBundleService();
+    nsCOMPtr<nsIStringBundleService> bundleService =
+        mozilla::services::GetStringBundleService();
     NS_ENSURE_TRUE(bundleService, NS_ERROR_UNEXPECTED);
     nsCOMPtr<nsIStringBundle> bundle;
-    bundleService->CreateBundle("chrome://branding/locale/brand.properties", getter_AddRefs(bundle));
+    bundleService->CreateBundle("chrome://branding/locale/brand.properties",
+                                getter_AddRefs(bundle));
     nsString brandShortName;
     bundle->GetStringFromName("brandShortName", brandShortName);
-    ::wcsncpy(sMailIconData.szTip, brandShortName.get(), brandShortName.Length());
+    ::wcsncpy(sMailIconData.szTip, brandShortName.get(),
+              brandShortName.Length());
   }
 
   ::Shell_NotifyIconW(NIM_ADD, &sMailIconData);
