2ade5cc37ecef9f5feb8e331199b3a3c1f15b2f9
Rob Lemley <rob@thunderbird.net>
2020-04-06 20:28:46 -0400

    Bug 1623907 - Move comm-central oriented global exclude code. r=darktrojan
    The exclude file is read in mozilla-central code, this moves reading of
    comm-central excludes into code in comm-central. The code in mozilla-central
    that reads GlobalExclude.txt will be removed.
    
    --HG--
    rename : tools/lint/GlobalExclude.txt => tools/lint/ThirdPartyPaths.txt
    extra : rebase_source : cb3fbecd93074278a7d62861b637b6ce1ac6eccc
    extra : histedit_source : e011311a2de9ffdf276e0e56babb33b846173726%2C81fc82b199cb89453dd3800fccb29998be67dce7

--- tools/lint/commlint/__init__.py
+++ tools/lint/commlint/__init__.py
@@ -9,25 +9,33 @@ import os
 from mozpack import path as mozpath
 from mozlint.pathutils import findobject
 
+COMM_EXCLUSION_FILES = [
+    os.path.join('comm', 'tools', 'lint', 'ThirdPartyPaths.txt')
+]
+
 TASKCLUSTER_EXCLUDE_PATHS = (
-    'comm/editor',
-    'comm/suite',
+    os.path.join('comm', 'editor'),
+    os.path.join('comm', 'suite'),
 )
 
 
-def _taskcluster_excludes(root, config):
+def _apply_global_excludes(root, config):
+    exclude = config.get('exclude', [])
+
+    for path in COMM_EXCLUSION_FILES:
+        with open(os.path.join(root, path), 'r') as fh:
+            exclude.extend([f.strip() for f in fh.readlines()])
+
     if os.environ.get('MOZLINT_NO_SUITE', None):
         # Ignore Seamonkey-only paths when run from Taskcluster
-        excludes = [mozpath.join(root, path) for path in TASKCLUSTER_EXCLUDE_PATHS]
+        suite_excludes = [mozpath.join(root, path) for path in TASKCLUSTER_EXCLUDE_PATHS]
+        exclude.extend(suite_excludes)
 
-        config.setdefault('exclude', [])
-        config['exclude'].extend(excludes)
+    config['exclude'] = exclude
 
 
 def lint_wrapper(paths, config, **lintargs):
-    log = lintargs['log']
-
-    _taskcluster_excludes(lintargs['root'], config)
+    _apply_global_excludes(lintargs['root'], config)
 
     payload = findobject(config['wraps'])
     config['payload'] = config['wraps']
