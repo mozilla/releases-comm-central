b0d012c9353aeeee1084e30938b68b89ea9b1bce
Geoff Lankow <geoff@darktrojan.net>
2020-04-10 11:42:03 +1200

    Bug 1628701 follow-up - make browser_menulist aware of the shadow DOM. rs=bustage-fix

--- mail/base/test/browser/browser_menulist.js
+++ mail/base/test/browser/browser_menulist.js
@@ -57,14 +57,16 @@ add_task(async () => {
   is(testDocument.activeElement, menulists[2]);
 
   EventUtils.synthesizeKey("VK_TAB", { shiftKey: false }, testWindow);
-  is(testDocument.activeElement.closest("input"), menulists[2]._inputField);
+  is(testDocument.activeElement, menulists[2]);
+  is(menulists[2].shadowRoot.activeElement, menulists[2]._inputField);
 
   EventUtils.synthesizeKey("VK_TAB", { shiftKey: false }, testWindow);
   is(testDocument.activeElement, testDocument.querySelector("button#after"));
 
   // Now go back again.
   EventUtils.synthesizeKey("VK_TAB", { shiftKey: true }, testWindow);
-  is(testDocument.activeElement.closest("input"), menulists[2]._inputField);
+  is(testDocument.activeElement, menulists[2]);
+  is(menulists[2].shadowRoot.activeElement, menulists[2]._inputField);
 
   EventUtils.synthesizeKey("VK_TAB", { shiftKey: true }, testWindow);
   is(testDocument.activeElement, menulists[2]);
@@ -115,7 +117,8 @@ add_task(async () => {
     );
   });
   ok(!menulists[2].hasAttribute("open"));
-  is(testDocument.activeElement.closest("input"), menulists[2]._inputField);
+  is(testDocument.activeElement, menulists[2]);
+  is(menulists[2].shadowRoot.activeElement, menulists[2]._inputField);
   is(menulists[2]._inputField.value, "foo");
   is(menulists[2].value, "foo");
   is(menulists[2].getAttribute("value"), "foo");
@@ -140,7 +143,8 @@ add_task(async () => {
     );
   });
   ok(!menulists[2].hasAttribute("open"));
-  is(testDocument.activeElement.closest("input"), menulists[2]._inputField);
+  is(testDocument.activeElement, menulists[2]);
+  is(menulists[2].shadowRoot.activeElement, menulists[2]._inputField);
   is(menulists[2]._inputField.value, "bar");
   is(menulists[2].value, "bar");
   is(menulists[2].getAttribute("value"), "bar");
@@ -183,7 +187,8 @@ add_task(async () => {
     );
   });
   ok(!menulists[2].hasAttribute("open"));
-  is(testDocument.activeElement.closest("input"), menulists[2]._inputField);
+  is(testDocument.activeElement, menulists[2]);
+  is(menulists[2].shadowRoot.activeElement, menulists[2]._inputField);
   is(menulists[2]._inputField.value, "foo");
   is(menulists[2].value, "foo");
   is(menulists[2].getAttribute("value"), "foo");
