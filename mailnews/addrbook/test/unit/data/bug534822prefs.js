4b471953d19a905939fab533dd063a3365260f94
Magnus Melin <mkmelin+mozilla@iki.fi>
2020-04-30 15:52:36 +0300

    Bug 1632279 - adjust comm/mailnews/addrbook/test/unit/test_bug534822.js to use user_pref not pref. rs=test-bustage-fix DONTBUILD

--- mailnews/addrbook/test/unit/data/bug534822prefs.js
+++ mailnews/addrbook/test/unit/data/bug534822prefs.js
@@ -1,4 +1,4 @@
 /* globals pref */
-pref("ldap_2.servers.extension.description", "extension");
-pref("ldap_2.servers.extension.filename", "ldap1.mab");
-pref("ldap_2.servers.extension.uri", "ldap://test.invalid:389/o=invalid??sub");
+user_pref("ldap_2.servers.extension.description", "extension");
+user_pref("ldap_2.servers.extension.filename", "ldap1.mab");
+user_pref("ldap_2.servers.extension.uri", "ldap://test.invalid:389/o=invalid??sub");
