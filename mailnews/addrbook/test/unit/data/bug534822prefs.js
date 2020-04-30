5a15a2c303a70b36062a195da72f38019da15945
Magnus Melin <mkmelin+mozilla@iki.fi>
2020-04-30 16:15:28 +0300

    Bug 1632279 - followup to fix linting . rs=eslint

--- mailnews/addrbook/test/unit/data/bug534822prefs.js
+++ mailnews/addrbook/test/unit/data/bug534822prefs.js
@@ -1,4 +1,7 @@
-/* globals pref */
+/* globals user_pref */
 user_pref("ldap_2.servers.extension.description", "extension");
 user_pref("ldap_2.servers.extension.filename", "ldap1.mab");
-user_pref("ldap_2.servers.extension.uri", "ldap://test.invalid:389/o=invalid??sub");
+user_pref(
+  "ldap_2.servers.extension.uri",
+  "ldap://test.invalid:389/o=invalid??sub"
+);
