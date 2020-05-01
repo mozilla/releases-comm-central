030da41d8f288d00ac2126bf6a62cd7aba0e2c4a
Ben Campbell <benc@thunderbird.net>
2020-05-01 13:51:00 +0300

    Bug 1625677 - Remove failed LDAP operations from pending list (or we'd crash). r=mkmelin

--- ldap/xpcom/src/nsLDAPConnection.cpp
+++ ldap/xpcom/src/nsLDAPConnection.cpp
@@ -313,7 +313,7 @@ nsresult nsLDAPConnection::AddPendingOperation(uint32_t aOperationID,
     MutexAutoLock lock(mPendingOperationsMutex);
     mPendingOperations.Put((uint32_t)aOperationID, aOperation);
     MOZ_LOG(gLDAPLogModule, mozilla::LogLevel::Debug,
-            ("pending operation added; total pending operations now = %d",
+            ("Operation id=%d added (%d now pending)", aOperationID,
              mPendingOperations.Count()));
   }
 
@@ -353,15 +353,11 @@ nsresult nsLDAPConnection::StartOp(nsIRunnable *aOp) {
 nsresult nsLDAPConnection::RemovePendingOperation(uint32_t aOperationID) {
   NS_ENSURE_TRUE(aOperationID > 0, NS_ERROR_UNEXPECTED);
 
-  MOZ_LOG(gLDAPLogModule, mozilla::LogLevel::Debug,
-          ("nsLDAPConnection::RemovePendingOperation(): operation removed"));
-
   {
     MutexAutoLock lock(mPendingOperationsMutex);
     mPendingOperations.Remove(aOperationID);
     MOZ_LOG(gLDAPLogModule, mozilla::LogLevel::Debug,
-            ("nsLDAPConnection::RemovePendingOperation(): operation "
-             "removed; total pending operations now = %d",
+            ("Operation id=%d removed (%d now pending)", aOperationID,
              mPendingOperations.Count()));
   }
 
@@ -623,9 +619,17 @@ NS_IMETHODIMP nsLDAPConnectionRunnable::Run() {
     case 0:
       // XXX do we need a timer?
       return thread->Dispatch(this, nsIEventTarget::DISPATCH_NORMAL);
-    case -1:
-      NS_ERROR("We don't know what went wrong with the ldap operation");
+    case -1: {
+      int errCode;
+      ldap_get_option(mConnection->mConnectionHandle, LDAP_OPT_ERROR_NUMBER,
+                      &errCode);
+      MOZ_LOG(gLDAPLogModule, mozilla::LogLevel::Error,
+              ("ldap_result() failed (on id=%d): %s", mOperationID,
+               ldap_err2string(errCode)));
+      // Remove operation from the Pending table.
+      mConnection->RemovePendingOperation((uint32_t)mOperationID);
       return NS_ERROR_FAILURE;
+    }
 
     case LDAP_RES_SEARCH_ENTRY:
     case LDAP_RES_SEARCH_REFERENCE:
