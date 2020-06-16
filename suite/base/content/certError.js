4491409dd72778bd8e99c5e4b1f50e6ea3e779ba
Ian Neal <iann_cvs@blueyonder.co.uk>
2020-06-16 11:54:23 +0100

    Bug 1646079 - Port |Bug 1415279 - Move error strings for certError and netError pages to frontend| to SeaMonkey. r=frg

--- suite/base/content/certError.js
+++ suite/base/content/certError.js
@@ -32,18 +32,6 @@ let gIsNetError;
 
 initPage();
 
-function getErrorCode() {
-  return gSearchParams.get("e");
-}
-
-function getCSSClass() {
-  return gSearchParams.get("s");
-}
-
-function getDescription() {
-  return gSearchParams.get("d");
-}
-
 function retryThis(buttonEl) {
   // Note: The application may wish to handle switching off "offline mode"
   // before this event handler runs, but using a capturing event handler.
@@ -64,7 +52,7 @@ function retryThis(buttonEl) {
 function initPage() {
   gSearchParams = new URLSearchParams(document.documentURI.split("?")[1]);
 
-  let err = getErrorCode();
+  let err = gSearchParams.get("e");
   // List of neterror pages which have no error code and
   // could have an illustration instead.
   let illustratedErrors = [
@@ -103,21 +91,6 @@ function initPage() {
       sd.innerHTML = errDesc.innerHTML;
     } else if (!err || err == "unknownProtocolFound") {
       sd.remove();
-    } else {
-      let desc = getDescription();
-      if (!illustratedErrors.includes(err) && gIsNetError) {
-        let codeRe = /<a id="errorCode" title="([^"]+)">/;
-        let codeResult = codeRe.exec(desc);
-        if (codeResult) {
-          let msg = desc.slice(0, codeResult.index) + codeResult[1];
-          sd.textContent = msg;
-          sd.className = "wrap";
-        } else {
-          sd.textContent = desc;
-        }
-      } else {
-        sd.textContent = desc;
-      }
     }
   }
 
@@ -158,7 +131,7 @@ function initPage() {
     document.getElementById("netErrorButtonContainer").style.display = "none";
   }
 
-  let className = getCSSClass();
+  let className = gSearchParams.get("s");
   if (className && className != "expertBadCert") {
     // Associate a CSS class with the root of the page, if one was passed in,
     // to allow custom styling.
@@ -194,111 +167,9 @@ function initPage() {
     whatShould.remove();
     let technicalContent = document.getElementById("technicalContent");
     technicalContent.remove();
-  } else {
-    addDomainErrorLinks();
   }
-}
-
-/* In the case of SSL error pages about domain mismatch, see if
-   we can hyperlink the user to the correct site.  We don't want
-   to do this generically since it allows MitM attacks to redirect
-   users to a site under attacker control, but in certain cases
-   it is safe (and helpful!) to do so.  Bug 402210
-*/
-function addDomainErrorLinks() {
-  // Rather than textContent, we need to treat description as HTML
-  var sd = document.getElementById("technicalContentText");
-  if (!sd)
-    return;
-
-  var desc = getDescription();
-
-  // sanitize description text - see bug 441169
-
-  // First, find the index of the <a> tag we care about, being careful not to
-  // use an over-greedy regex
-  var codeRe = /<a id="errorCode" title="([^"]+)">/;
-  var codeResult = codeRe.exec(desc);
-  var domainRe = /<a id="cert_domain_link" title="([^"]+)">/;
-  var domainResult = domainRe.exec(desc);
-  // The order of these links in the description is fixed in
-  // TransportSecurityInfo.cpp:formatOverridableCertErrorMessage.
-  var firstResult = domainResult;
-  if (!domainResult)
-    firstResult = codeResult;
-  if (!firstResult) {
-    sd.textContent = desc;
-    return;
-  }
-
-  // Remove sd's existing children.
-  sd.textContent = "";
-
-  // Everything up to the link should be text content.
-  sd.appendChild(document.createTextNode(desc.slice(0, firstResult.index)));
-
-  // Now create the actual links.
-  var link;
-  if (domainResult) {
-    link = createLink(sd, "cert_domain_link", domainResult[1]);
-    // Append text for anything between the two links.
-    sd.appendChild(document.createTextNode(desc.slice(desc.indexOf("</a>") + "</a>".length, codeResult.index)));
-  }
-  createLink(sd, "errorCode", codeResult[1]);
-
-  // Finally, append text for anything after the closing </a>
-  sd.appendChild(document.createTextNode(desc.slice(desc.indexOf("</a>") + "</a>".length)));
-
-  if (!link)
-    return;
-
-  // Then initialize the cert domain link.
-  var okHost = link.getAttribute("title");
-  var thisHost = document.location.hostname;
-  var proto = document.location.protocol;
-
-  // If okHost is a wildcard domain ("*.example.com") let's
-  // use "www" instead.  "*.example.com" isn't going to
-  // get anyone anywhere useful. bug 432491
-  okHost = okHost.replace(/^\*\./, "www.");
-
-  /* case #1:
-   * example.com uses an invalid security certificate.
-   *
-   * The certificate is only valid for www.example.com
-   *
-   * Make sure to include the "." ahead of thisHost so that
-   * a MitM attack on paypal.com doesn't hyperlink to "notpaypal.com"
-   *
-   * We'd normally just use a RegExp here except that we lack a
-   * library function to escape them properly (bug 248062), and
-   * domain names are famous for having '.' characters in them,
-   * which would allow spurious and possibly hostile matches.
-   */
-  if (okHost.endsWith("." + thisHost))
-    link.href = proto + "//" + okHost;
-
-  /* case #2:
-   * browser.garage.maemo.org uses an invalid security certificate.
-   *
-   * The certificate is only valid for garage.maemo.org
-   */
-  if (thisHost.endsWith("." + okHost))
-    link.href = proto + "//" + okHost;
-
-  // If we set a link, meaning there's something helpful for
-  // the user here, expand the section by default
-  if (link.href && getCSSClass() != "expertBadCert")
-    toggle("technicalContent");
-}
-
-function createLink(el, id, text) {
-  var anchorEl = document.createElement("a");
-  anchorEl.setAttribute("id", id);
-  anchorEl.setAttribute("title", text);
-  anchorEl.appendChild(document.createTextNode(text));
-  el.appendChild(anchorEl);
-  return anchorEl;
+  var event = new CustomEvent("AboutNetAndCertErrorLoad", {bubbles: true});
+  document.dispatchEvent(event);
 }
 
 function toggle(id) {
