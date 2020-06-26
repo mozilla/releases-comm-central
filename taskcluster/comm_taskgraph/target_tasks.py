ebe8b9da581e49e7569c5926ff67b30766aa5d4f
Rob Lemley <rob@thunderbird.net>
2020-06-26 20:45:37 +0000

    Bug 1641403 - Follow-up: Create target_task_method for Thunderbird Searchfox indexing. r=darktrojan DONTBUILD
    The actual searchfox_index method includes tasks that do not exist in the
    Thunderbird taskgraph. Register a new method specific to Thunderbird that
    includes only what is needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81261
    
    --HG--
    extra : moz-landing-system : lando

---
+++ taskcluster/comm_taskgraph/target_tasks.py
@@ -0,0 +1,16 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from taskgraph.target_tasks import _target_task
+
+
+@_target_task('comm_searchfox_index')
+def target_tasks_searchfox(full_task_graph, parameters, graph_config):
+    """Select tasks required for indexing Thunderbird for Searchfox web site each day"""
+    return ['searchfox-linux64-searchfox/debug',
+            'searchfox-macosx64-searchfox/debug',
+            'searchfox-win64-searchfox/debug',
+            ]
