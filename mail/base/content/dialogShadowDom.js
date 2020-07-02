9734925836aa3685394daa761b54824564c28e70
Richard Marti <richard.marti@gmail.com>
2020-07-02 19:17:33 +0200

    Bug 1649577 - Make the Addressbook dialogs themeable. r=aleca

---
+++ mail/base/content/dialogShadowDom.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * When the dialog window loads, add a stylesheet to the shadow DOM of the
+ * dialog to style the accept and cancel buttons, etc.
+ */
+window.addEventListener("load", () => {
+  let link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute(
+    "href",
+    "chrome://messenger/skin/themeableDialog.css"
+  );
+  document.querySelector("dialog").shadowRoot.appendChild(link);
+});
