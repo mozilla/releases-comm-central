7834dfdd0de5105f24101b023b5a2d885c13aa17
Bill Gianopoulos <wgianopoulos@gmail.com>
2020-08-12 08:38:21 -0400

    Bug 1661940 - Move suite MOZ_APP_BASENAME to moz.configure. r=frg Port relevant parts of Bug 1657954 "Move various branding options from old-configure".

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=SeaMonkey
 MOZ_APP_VENDOR=Mozilla
 MOZ_APP_NAME=seamonkey
 MOZ_APP_DISPLAYNAME=SeaMonkey
