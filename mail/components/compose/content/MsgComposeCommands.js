28075656a704fb2295cc2b720400e8e2c865983f
Khushil Mistry <khushil324@gmail.com>
2020-08-12 16:28:12 +1200

    Bug 1658114 - Fix order of file attachments cannot be changed by drag and drop. r=mkmelin
    --HG--
    extra : histedit_source : 690ba169f8874b738879cd4a0ef9758eb7e8fd15

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -7671,6 +7671,8 @@ var envelopeDragObserver = {
         this._hideDropMarker();
         event.dataTransfer.dropEffect = "none";
       }
+      event.stopPropagation();
+      event.preventDefault();
       return;
     }
 
