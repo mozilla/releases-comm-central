4dea26b260a97260921a157bfbd46078c098e6e6
Geoff Lankow <geoff@darktrojan.net>
2020-08-12 18:15:23 +1200

    Bug 1658682 - Port bug 1657954 - Move various branding options from `old-configure`. rs=bustage-fix CLOSED TREE
    --HG--
    extra : rebase_source : a74711dc147d79a278fe3af3dce2ee8b94e0916a
    extra : amend_source : a9764e720ab7e4670a649677de6a042d5d3bdb1d

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Thunderbird
 MOZ_APP_NAME=thunderbird
 MOZ_UPDATER=1
 
