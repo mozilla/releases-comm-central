9e1b90a9f1f96a7b10a220b194e73f483c93df7b
Paul Morris <paul@thunderbird.net>
2020-08-28 21:27:51 +0000

    Bug 306495 - Move openLocalCalendar out of calendar-creation.js. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D78370
    
    Depends on D78369

--- calendar/base/content/calendar-management.js
+++ calendar/base/content/calendar-management.js
@@ -6,7 +6,7 @@
  *         calendarListTooltipShowing, calendarListSetupContextMenu,
  *         ensureCalendarVisible, toggleCalendarVisible, showAllCalendars,
  *         showOnlyCalendar, openCalendarSubscriptionsDialog,
- *         calendarOfflineManager
+ *         calendarOfflineManager, openLocalCalendar
  */
 
 /* import-globals-from calendar-migration.js */
@@ -687,6 +687,47 @@ var compositeObserver = {
     document.commandDispatcher.updateCommands("calendar_commands");
   },
 };
+
+/**
+ * Shows the filepicker and creates a new calendar with a local file using the ICS
+ * provider.
+ */
+function openLocalCalendar() {
+  let picker = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
+  picker.init(window, cal.l10n.getCalString("Open"), Ci.nsIFilePicker.modeOpen);
+  let wildmat = "*.ics";
+  let description = cal.l10n.getCalString("filterIcs", [wildmat]);
+  picker.appendFilter(description, wildmat);
+  picker.appendFilters(Ci.nsIFilePicker.filterAll);
+
+  picker.open(rv => {
+    if (rv != Ci.nsIFilePicker.returnOK || !picker.file) {
+      return;
+    }
+
+    let calMgr = cal.getCalendarManager();
+    let calendars = calMgr.getCalendars();
+    let calendar = calendars.find(x => x.uri.equals(picker.fileURL));
+    if (!calendar) {
+      calendar = calMgr.createCalendar("ics", picker.fileURL);
+
+      // Strip ".ics" from filename for use as calendar name, taken from
+      // calendarCreation.js
+      let prettyName = picker.fileURL.spec.match(/([^/:]+)\.ics$/);
+      if (prettyName) {
+        calendar.name = decodeURIComponent(prettyName[1]);
+      } else {
+        calendar.name = cal.l10n.getCalString("untitledCalendarName");
+      }
+
+      calMgr.registerCalendar(calendar);
+    }
+
+    let calendarList = document.getElementById("calendar-list");
+    let item = calendarList.getElementsByAttribute("calendar-id", calendar.id)[0];
+    calendarList.selectedItem = item;
+  });
+}
 
 /**
  * Opens the subscriptions dialog modally.
