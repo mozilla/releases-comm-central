1721efbbc590c6573ed6786dccd48b218e0e7308
Rob Lemley <rob@thunderbird.net>
2020-09-14 20:25:22 +0000

    Bug 1664885 - [RNP] Use RNP's built-in support for MSVC rather than our own. r=kaie
    With https://github.com/rnpgp/rnp/pull/1232 being merged upstream, we can
    remove most of the workarounds from the niwcompat directory.
    What remains is dirent.h, which RNP pulls in via vcpkg and an empty getopt.h
    header file.
    getopt itself is not required by librnp.so, it's only used by some commandline
    utilities. However, uniwin.h imports it unconditionally so an empty header file
    is included for compatibilty.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90161
    
    --HG--
    extra : moz-landing-system : lando

---
+++ third_party/niwcompat/getopt.h
@@ -0,0 +1,10 @@
+/* getopt.h
+ * For compatibility with rnp/src/common/getoptwin.h which imports
+ * getopt.h unconditionally.
+ * Thunderbird does not make use of any of the getopt functions so this is an
+ * empty file.
+ */
+
+#ifndef __GETOPT_H_
+#define __GETOPT_H_
+#endif
