be1498e51649885301f55d20b773c766c536fece
Rob Lemley <rob@thunderbird.net>
2020-11-05 14:13:45 -0500

    Bug 1672798 - Run black formatting on suite/editor Python/mozbuild/mozconfigure files. r=IanN
    Differential Revision: https://phabricator.services.mozilla.com/D96098
    
    --HG--
    extra : rebase_source : 4eb11a5e6c34d151e9b57dece6872232c2728596
    extra : amend_source : cbc1824df74640bc5a3e8427db079167ca0d0cac

--- suite/locales/filter.py
+++ suite/locales/filter.py
@@ -2,49 +2,72 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 def test(mod, path, entity=None):
-  import re
-  # ignore anything but SeaMonkey
-  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
-                 "devtools/client", "devtools/shared", "devtools/startup",
-                 "suite",
-                 "editor/ui",
-                 "extensions/spellcheck",
-                 "suite/branding/seamonkey",
-                 "services/sync"):
-    return "ignore"
-  # ignore temporary files, hiden files and files from rejects
-  if (re.match(r".*?\/[.#].+", path) or
-      re.match(r".*~$", path) or
-      re.match(r".+\.(orig|rej)", path)):
-    return "ignore"
-  if mod not in ("suite"):
-    # we only have exceptions for suite
-    return "error"
-  if entity is None:
-    # missing and obsolete files
-    return ("ignore" if (re.match(r"searchplugins\/.+\.xml", path) or
-                         path == "profile/bookmarks.extra" or
-                         path == "profile/panels.extra" or
-                         path == "defines.inc" or
-                         re.match(r"chrome\/common\/help\/images\/[A-Za-z-_]+\.[a-z]+", path))
-            else "error")
-  if path == "defines.inc":
-    return ("ignore" if (entity == "MOZ_LANGPACK_CONTRIBUTORS")
-            else "error")
+    import re
 
-  if path == "chrome/common/region.properties":
-    return ("ignore" if (re.match(r"browser\.search\.order\.[1-9]", entity))
-            else "error")
+    # ignore anything but SeaMonkey
+    if mod not in (
+        "netwerk",
+        "dom",
+        "toolkit",
+        "security/manager",
+        "devtools/client",
+        "devtools/shared",
+        "devtools/startup",
+        "suite",
+        "editor/ui",
+        "extensions/spellcheck",
+        "suite/branding/seamonkey",
+        "services/sync",
+    ):
+        return "ignore"
+    # ignore temporary files, hiden files and files from rejects
+    if (
+        re.match(r".*?\/[.#].+", path)
+        or re.match(r".*~$", path)
+        or re.match(r".+\.(orig|rej)", path)
+    ):
+        return "ignore"
+    if mod not in ("suite"):
+        # we only have exceptions for suite
+        return "error"
+    if entity is None:
+        # missing and obsolete files
+        return (
+            "ignore"
+            if (
+                re.match(r"searchplugins\/.+\.xml", path)
+                or path == "profile/bookmarks.extra"
+                or path == "profile/panels.extra"
+                or path == "defines.inc"
+                or re.match(r"chrome\/common\/help\/images\/[A-Za-z-_]+\.[a-z]+", path)
+            )
+            else "error"
+        )
+    if path == "defines.inc":
+        return "ignore" if (entity == "MOZ_LANGPACK_CONTRIBUTORS") else "error"
 
-  if path == "chrome/mailnews/region.properties":
-    return ("ignore" if (re.match(r"mail\.addr_book\.mapit_url\.[1-5]", entity))
-            else "error")
+    if path == "chrome/common/region.properties":
+        return (
+            "ignore"
+            if (re.match(r"browser\.search\.order\.[1-9]", entity))
+            else "error"
+        )
 
-  if path != "chrome/browser/region.properties":
-    # only region.properties exceptions remain, compare all others
-    return "error"
+    if path == "chrome/mailnews/region.properties":
+        return (
+            "ignore"
+            if (re.match(r"mail\.addr_book\.mapit_url\.[1-5]", entity))
+            else "error"
+        )
 
-  return ("ignore"
-          if (re.match(r"browser\.contentHandlers\.types\.[0-5]", entity))
-          else "error")
+    if path != "chrome/browser/region.properties":
+        # only region.properties exceptions remain, compare all others
+        return "error"
+
+    return (
+        "ignore"
+        if (re.match(r"browser\.contentHandlers\.types\.[0-5]", entity))
+        else "error"
+    )
