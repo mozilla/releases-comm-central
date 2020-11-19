92e7fca3981b7ba8631e5a6b973226278f9a595e
Rob Lemley <rob@thunderbird.net>
2020-11-19 20:49:55 +0000

    Bug 1678441 - Add license headers to build related files. r=justdave
    Differential Revision: https://phabricator.services.mozilla.com/D97675

--- third_party/niwcompat/getopt.h
+++ third_party/niwcompat/getopt.h
@@ -3,6 +3,8 @@
  * getopt.h unconditionally.
  * Thunderbird does not make use of any of the getopt functions so this is an
  * empty file.
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
 #ifndef __GETOPT_H_
