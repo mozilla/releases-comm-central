dca6ae2d16ea1982ea1fbeae8331e209d81acc0e
Geoff Lankow <geoff@darktrojan.net>
2021-01-13 16:02:43 +1300

    Backed out changeset 46a5ae8ca59c (bug 1685222) for breaking a test.
    --HG--
    extra : rebase_source : 6393821f0654c778858168280ed12ad10f5f78eb
    extra : histedit_source : 2a4cd0b7230512f8309a59f8983db4991baa8bb1

--- mail/base/content/aboutAddonsExtra.js
+++ mail/base/content/aboutAddonsExtra.js
@@ -15,36 +15,52 @@ const THUNDERBIRD_THEME_PREVIEWS = new Map([
   ],
 ]);
 
+/* This file runs in both the outer window, which controls the categories list, search bar, etc.,
+ * and the inner window which is the list of add-ons or the detail view. */
 (async function() {
-  // Fix the "Search on addons.mozilla.org" placeholder text in the searchbox.
-  let textbox = document.querySelector("search-addons > search-textbox");
-  let placeholder = textbox.getAttribute("placeholder");
-  placeholder = placeholder.replace(
-    "addons.mozilla.org",
-    "addons.thunderbird.net"
-  );
-  textbox.setAttribute("placeholder", placeholder);
+  if (window.location.href == "about:addons") {
+    // Fix the "Search on addons.mozilla.org" placeholder text in the searchbox.
+    let browser = document.getElementById("html-view-browser");
+    if (!/(interactive|complete)/.test(browser.contentDocument.readyState)) {
+      await new Promise(resolve =>
+        browser.contentWindow.addEventListener("DOMContentLoaded", resolve, {
+          once: true,
+        })
+      );
+    }
 
-  // Add our stylesheet.
+    // Wait for custom elements and L10n.
+    await new Promise(resolve =>
+      browser.contentWindow.requestAnimationFrame(resolve)
+    );
+
+    let textbox = browser.contentDocument.querySelector(
+      "search-addons > search-textbox"
+    );
+    let placeholder = textbox.getAttribute("placeholder");
+    placeholder = placeholder.replace(
+      "addons.mozilla.org",
+      "addons.thunderbird.net"
+    );
+    textbox.setAttribute("placeholder", placeholder);
+    return;
+  }
+
   let contentStylesheet = document.createProcessingInstruction(
     "xml-stylesheet",
     'href="chrome://messenger/content/aboutAddonsExtra.css" type="text/css"'
   );
   document.insertBefore(contentStylesheet, document.documentElement);
 
-  // Override logic for detecting unsigned add-ons.
   window.isCorrectlySigned = function() {
     return true;
   };
-  document.querySelectorAll("addon-card").forEach(card => card.updateMessage());
 
-  // Load our permissions strings.
   delete window.browserBundle;
   window.browserBundle = Services.strings.createBundle(
     "chrome://messenger/locale/addons.properties"
   );
 
-  // Load our theme screenshots.
   let _getScreenshotUrlForAddon = getScreenshotUrlForAddon;
   getScreenshotUrlForAddon = function(addon) {
     if (THUNDERBIRD_THEME_PREVIEWS.has(addon.id)) {
