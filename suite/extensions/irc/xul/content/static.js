bd8ed325da908b763d4be77866ef003f7f011af2
Ian Neal <iann_cvs@blueyonder.co.uk>
2021-04-22 21:56:55 +0100

    Bug 1707103 - Use correct date time format service in chatZilla. r=frg

--- suite/extensions/irc/xul/content/static.js
+++ suite/extensions/irc/xul/content/static.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-Components.utils.import("resource://gre/modules/Services.jsm");
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 const __cz_version   = "0.9.96";
 const __cz_condition = "green";
@@ -252,44 +252,13 @@ function initStatic()
 
     try
     {
-        // nsIScriptableDateFormat was removed from Gecko 57
-        // The replacement function was not available in a stable version
-        // prior to Gecko 56.
-        if (Services.vc.compare(Services.appinfo.platformVersion, "56.0") < 0)
-        {
-            const nsISDateFormat = Components.interfaces.nsIScriptableDateFormat;
-            const DTFMT_CID = "@mozilla.org/intl/scriptabledateformat;1";
-            client.dtFormatter =
-                Components.classes[DTFMT_CID].createInstance(nsISDateFormat);
-
-            // Mmmm, fun. This ONLY affects the ChatZilla window, don't worry!
-            Date.prototype.toStringInt = Date.prototype.toString;
-            Date.prototype.toString = function() {
-                var dtf = client.dtFormatter;
-                return dtf.FormatDateTime("", dtf.dateFormatLong,
-                                          dtf.timeFormatSeconds,
-                                          this.getFullYear(), this.getMonth() + 1,
-                                          this.getDate(), this.getHours(),
-                                          this.getMinutes(), this.getSeconds()
-                                         );
-            }
-        }
-        else
-        {
-            if (Services.vc.compare(Services.appinfo.platformVersion, "59.0") < 0) {
-                client.dtFormatter = Services.intl.createDateTimeFormat(
-                    undefined, { dateStyle: "full", timeStyle: "long" });
-            }
-            else {
-                client.dtFormatter = new Services.intl.DateTimeFormat(
-                    undefined, { dateStyle: "full", timeStyle: "long" });
-            }
-
-            // Mmmm, fun. This ONLY affects the ChatZilla window, don't worry!
-            Date.prototype.toStringInt = Date.prototype.toString;
-            Date.prototype.toString = function() {
-                return client.dtFormatter.format(this);
-            }
+        // Mmmm, fun. This ONLY affects the ChatZilla window, don't worry!
+        Date.prototype.toStringInt = Date.prototype.toString;
+        Date.prototype.toString = function() {
+            let dtf = new Services.intl.DateTimeFormat(undefined,
+                                                       { dateStyle: "full",
+                                                         timeStyle: "long" });
+            return dtf.format(this);
         }
     }
     catch (ex)
