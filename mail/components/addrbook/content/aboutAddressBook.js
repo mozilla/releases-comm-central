a6b8f6524b2c69d84a566e206dc8c672ef018d81
Geoff Lankow <geoff@darktrojan.net>
2021-04-29 14:12:18 +1200

    Bug 1708572 - Create a layout skeleton for about:addressbook. r=mkmelin
    This patch creates the basic layout of the page. The various sections will be filled by subsequent patches.
    
    I've done the whole page with a grid layout so that if we choose to rearrange the panes at runtime it can be done without removing and replacing elements.
    Doing that causes problems, as demonstrated by the 3-pane mail window.
    
    Also included is the code to open in-content dialogs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113890
    
    --HG--
    extra : rebase_source : 3d8bb1830a51d6422e34517badccb6bc81e81697
    extra : histedit_source : 5c4e284c8835f2c0e6d4fbe4f56725d88cad207f

--- mail/components/addrbook/content/aboutAddressBook.js
+++ mail/components/addrbook/content/aboutAddressBook.js
@@ -2,4 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// This comment exists to avoid aboutAddressBook.js being a duplicate.
+var { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyGetter(this, "SubDialog", function() {
+  const { SubDialogManager } = ChromeUtils.import(
+    "resource://gre/modules/SubDialog.jsm"
+  );
+  return new SubDialogManager({
+    dialogStack: document.getElementById("dialogStack"),
+    dialogTemplate: document.getElementById("dialogTemplate"),
+    dialogOptions: {
+      styleSheets: ["chrome://messenger/skin/preferences/dialog.css"],
+    },
+  });
+});
