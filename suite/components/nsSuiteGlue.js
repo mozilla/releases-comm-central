c9cd23e470f17255268ee1a0936cf4a1f989b124
Frank-Rainer Grahl <frgrahl@gmx.net>
2021-06-06 14:20:37 +0200

    Bug 1621445 - Part 8: Fix missing declaration. r=bustage-fix

--- suite/components/nsSuiteGlue.js
+++ suite/components/nsSuiteGlue.js
@@ -535,7 +535,7 @@ SuiteGlue.prototype = {
                       "downloads", "cookies", "cache", "sessions",
                       "offlineApps" ];
 
-      for (pref of prefs) {
+      for (let pref of prefs) {
         try {
           let prefOld = "privacy.item." + pref;
 
