71e0840bd879a5cdaf2dd42faa6cb83b5a31abb6
John Bieling <john@thunderbird.net>
2021-07-08 19:54:07 +0000

    Bug 1719739 - Prevent accounts API to fail on folders without subFolders. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D119447

--- mail/components/extensions/parent/ext-mail.js
+++ mail/components/extensions/parent/ext-mail.js
@@ -1612,8 +1612,12 @@ function convertFolder(folder, accountId) {
 function traverseSubfolders(folder, accountId) {
   let f = convertFolder(folder, accountId);
   f.subFolders = [];
-  for (let subFolder of folder.subFolders) {
-    f.subFolders.push(traverseSubfolders(subFolder, accountId || f.accountId));
+  if (folder.hasSubFolders) {
+    for (let subFolder of folder.subFolders) {
+      f.subFolders.push(
+        traverseSubfolders(subFolder, accountId || f.accountId)
+      );
+    }
   }
   return f;
 }
