42d45ba8bca25713c1e29c9e71639c14c08e5c90
Martin Schröder <mschroeder@mozilla.x-home.org>
2021-08-23 13:21:47 +0300

    Bug 1524853 - Default text in task edit field does not update anymore if switching the calendar. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D122776
    
    --HG--
    extra : amend_source : 3eb45d3cff54f7081403d34f5bedcc58d1e0c7cd

--- calendar/base/content/item-editing/calendar-task-editing.js
+++ calendar/base/content/item-editing/calendar-task-editing.js
@@ -24,30 +24,6 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 
 var taskEdit = {
   /**
-   * Get the currently observed calendar.
-   */
-  mObservedCalendar: null,
-  get observedCalendar() {
-    return this.mObservedCalendar;
-  },
-
-  /**
-   * Set the currently observed calendar, removing listeners to any old
-   * calendar set and adding listeners to the new one.
-   */
-  set observedCalendar(aCalendar) {
-    if (this.mObservedCalendar) {
-      this.mObservedCalendar.removeObserver(this.calendarObserver);
-    }
-
-    this.mObservedCalendar = aCalendar;
-
-    if (this.mObservedCalendar) {
-      this.mObservedCalendar.addObserver(this.calendarObserver);
-    }
-  },
-
-  /**
    * Helper function to set readonly and aria-disabled states and the value
    * for a given target.
    *
@@ -57,7 +33,7 @@ var taskEdit = {
    */
   setupTaskField(aTarget, aDisable, aValue) {
     aTarget.value = aValue;
-    aTarget.readonly = aDisable;
+    aTarget.readOnly = aDisable;
     aTarget.ariaDisabled = aDisable;
   },
 
@@ -104,6 +80,7 @@ var taskEdit = {
     } else {
       taskEdit.setupTaskField(edit, true, cal.l10n.getCalString("taskEditInstructionsReadonly"));
     }
+
     edit.showsInstructions = true;
   },
 
@@ -127,17 +104,23 @@ var taskEdit = {
   },
 
   /**
-   * Load function to set up all quick-add inputs. The input must
-   * have the class "task-edit-field".
+   * Helper function to call onBlur for all fields with class name
+   * "task-edit-field".
    */
-  onLoad(aEvent) {
+  callOnBlurForAllTaskFields() {
     let taskEditFields = document.getElementsByClassName("task-edit-field");
     for (let i = 0; i < taskEditFields.length; i++) {
       taskEdit.onBlur({ target: taskEditFields[i] });
     }
+  },
 
+  /**
+   * Load function to set up all quick-add inputs. The input must
+   * have the class "task-edit-field".
+   */
+  onLoad(aEvent) {
     cal.view.getCompositeCalendar(window).addObserver(taskEdit.compositeObserver);
-    taskEdit.observedCalendar = getSelectedCalendar();
+    taskEdit.callOnBlurForAllTaskFields();
   },
 
   /**
@@ -145,58 +128,12 @@ var taskEdit = {
    */
   onUnload() {
     cal.view.getCompositeCalendar(window).removeObserver(taskEdit.compositeObserver);
-    taskEdit.observedCalendar = null;
-  },
-
-  /**
-   * Observer to watch for readonly, disabled and capability changes of the
-   * observed calendar.
-   *
-   * @see calIObserver
-   */
-  calendarObserver: {
-    QueryInterface: ChromeUtils.generateQI(["calIObserver"]),
-
-    // calIObserver:
-    onStartBatch() {},
-    onEndBatch() {},
-    onLoad(aCalendar) {},
-    onAddItem(aItem) {},
-    onModifyItem(aNewItem, aOldItem) {},
-    onDeleteItem(aDeletedItem) {},
-    onError(aCalendar, aErrNo, aMessage) {},
-
-    onPropertyChanged(aCalendar, aName, aValue, aOldValue) {
-      if (aCalendar.id != getSelectedCalendar().id) {
-        // Optimization: if the given calendar isn't the default calendar,
-        // then we don't need to change any readonly/disabled states.
-        return;
-      }
-      switch (aName) {
-        case "readOnly":
-        case "disabled": {
-          let taskEditFields = document.getElementsByClassName("task-edit-field");
-          for (let i = 0; i < taskEditFields.length; i++) {
-            taskEdit.onBlur({ target: taskEditFields[i] });
-          }
-          break;
-        }
-      }
-    },
-
-    onPropertyDeleting(aCalendar, aName) {
-      // Since the old value is not used directly in onPropertyChanged,
-      // but should not be the same as the value, set it to a different
-      // value.
-      this.onPropertyChanged(aCalendar, aName, null, null);
-    },
   },
 
   /**
    * Observer to watch for changes to the selected calendar.
    *
-   * XXX I think we don't need to implement calIObserver here.
-   *
+   * @see calIObserver
    * @see calICompositeObserver
    */
   compositeObserver: {
@@ -210,18 +147,35 @@ var taskEdit = {
     onModifyItem(aNewItem, aOldItem) {},
     onDeleteItem(aDeletedItem) {},
     onError(aCalendar, aErrNo, aMessage) {},
-    onPropertyChanged(aCalendar, aName, aValue, aOldValue) {},
-    onPropertyDeleting(aCalendar, aName) {},
+
+    onPropertyChanged(aCalendar, aName, aValue, aOldValue) {
+      if (aCalendar.id != getSelectedCalendar().id) {
+        // Optimization: if the given calendar isn't the selected calendar,
+        // then we don't need to change any readonly/disabled states.
+        return;
+      }
+
+      switch (aName) {
+        case "readOnly":
+        case "disabled": {
+          taskEdit.callOnBlurForAllTaskFields();
+          break;
+        }
+      }
+    },
+
+    onPropertyDeleting(aCalendar, aName) {
+      // Since the old value is not used directly in onPropertyChanged,
+      // but should not be the same as the value, set it to a different
+      // value.
+      this.onPropertyChanged(aCalendar, aName, null, null);
+    },
 
     // calICompositeObserver:
     onCalendarAdded(aCalendar) {},
     onCalendarRemoved(aCalendar) {},
     onDefaultCalendarChanged(aNewDefault) {
-      let taskEditFields = document.getElementsByClassName("task-edit-field");
-      for (let i = 0; i < taskEditFields.length; i++) {
-        taskEdit.onBlur({ target: taskEditFields[i] });
-      }
-      taskEdit.observedCalendar = aNewDefault;
+      taskEdit.callOnBlurForAllTaskFields();
     },
   },
 };
