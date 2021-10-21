a05db740d51a6496f8d1d350f85e7f3718c3cc71
Rob Lemley <rob@thunderbird.net>
2021-10-21 23:17:12 +0000

    Bug 1730711 - Include a real getopt implementation for Windows. r=kaie
    Copied from ipc/chromium/src/third_party/libevent/WIN32-Code, with small modifications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126029

--- third_party/niwcompat/getopt.h
+++ third_party/niwcompat/getopt.h
@@ -1,12 +1,33 @@
-/* getopt.h
- * For compatibility with rnp/src/common/getoptwin.h which imports
- * getopt.h unconditionally.
- * Thunderbird does not make use of any of the getopt functions so this is an
- * empty file.
- * Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
+#ifndef __GETOPT_H__
+#define __GETOPT_H__
 
-#ifndef __GETOPT_H_
-#define __GETOPT_H_
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+extern int opterr;		/* if error message should be printed */
+extern int optind;		/* index into parent argv vector */
+extern int optopt;		/* character checked for validity */
+extern int optreset;		/* reset getopt */
+extern char *optarg;		/* argument associated with option */
+
+struct option
+{
+  const char *name;
+  int has_arg;
+  int *flag;
+  int val;
+};
+
+#define no_argument       0
+#define required_argument 1
+#define optional_argument 2
+
+int getopt(int, char * const*, const char*);
+int getopt_long(int, char**, const char*, const struct option*, int*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GETOPT_H__ */
