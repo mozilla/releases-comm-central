988815e4a79a23c70e9c825d53abeb4a7f46bb72
Geoff Lankow <geoff@darktrojan.net>
2021-10-21 20:05:26 +1300

    Bug 1736800 follow-up - Fix a broken test. rs=bustage-fix

--- mail/components/addrbook/test/browser/new/browser_edit_card.js
+++ mail/components/addrbook/test/browser/new/browser_edit_card.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// TODO: Fix the UI so that we don't have to do this.
+window.maximize();
+
 let personalBook = MailServices.ab.getDirectoryFromId("ldap_2.servers.pab");
 
 async function inEditingMode() {
