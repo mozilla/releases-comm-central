8807e513f4c41a91a181f9296d7a2767e7afc358
Rob Lemley <rob@thunderbird.net>
2021-11-29 18:49:17 +0000

    Bug 1731160 - mach initialization for Thunderbird mach commands. r=darktrojan
    Creating mach commands in comm-central. The initialize function
    calls the mozilla-central mach initialize first, then modifies
    the returned mach.driver.
    
    This implementation allows for new command categories and new mach
    command modules. Mach settings providers can also be added in the
    mach_commands files.
    
    "mach commlint" is implemented, it just calls "mach lint", but with the
    Thunderbird lint configurations in comm/tools/lint.
    
    Requires bug 1731158 in mozilla-central.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125984
    
    --HG--
    extra : moz-landing-system : lando

---
+++ build/mach_initialize.py
@@ -0,0 +1,60 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+mach_initialize.py
+
+This file contains initialization code for mach commands that are outside of
+the Firefox source repository.
+"""
+
+import os
+import sys
+
+from build import mach_initialize as mach_init
+
+# Individual files that provide mach commands
+MACH_MODULES = [
+    "comm/tools/lint/mach_commands.py",
+]
+
+CATEGORIES = {
+    "thunderbird": {
+        "short": "Thunderbird Development",
+        "long": "Mach commands that aid Thunderbird Development",
+        "priority": 65,
+    },
+}
+
+
+def mach_sys_path(mozilla_dir):
+    from mach.requirements import MachEnvRequirements
+    requirements = MachEnvRequirements.from_requirements_definition(
+        mozilla_dir,
+        True,  # is_thunderbird
+        True,
+        "comm/build/mach_virtualenv_packages.txt"
+    )
+    return sorted(
+        [
+            os.path.normcase(os.path.join(mozilla_dir, pth.path))
+            for pth in requirements.pth_requirements
+        ]
+    )
+
+
+def initialize(topsrcdir):
+    driver = mach_init.initialize(topsrcdir)
+
+    # Add comm Python module paths
+    sys.path.extend(mach_sys_path(topsrcdir))
+
+    # Define Thunderbird mach command categories
+    for category, meta in CATEGORIES.items():
+        driver.define_category(category, meta["short"], meta["long"], meta["priority"])
+
+    for path in MACH_MODULES:
+        driver.load_commands_from_file(os.path.join(topsrcdir, path))
+
+    return driver
