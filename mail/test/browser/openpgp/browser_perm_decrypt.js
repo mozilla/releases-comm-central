079495c27c3cf2847a96914fdee23d7b47c6a7a2
Kai Engert <kaie@kuix.de>
2021-11-29 20:39:48 +0200

    Bug 1693332 - Add tests for permanent message decryption. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D131597
    
    --HG--
    rename : mail/test/browser/openpgp/browser_viewMessageSecurity.js => mail/test/browser/openpgp/browser_perm_decrypt.js
    rename : mailnews/mime/test/unit/test_smime_decrypt.js => mailnews/mime/test/unit/test_smime_perm_decrypt.js
    extra : amend_source : e1e24b30a122fc937cc121accdb6d80bcc38cd4b

---
+++ mail/test/browser/openpgp/browser_perm_decrypt.js
@@ -0,0 +1,160 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Tests for permanent decryption of email.
+ */
+
+"use strict";
+
+const {
+  be_in_folder,
+  get_special_folder,
+  mc,
+  select_click_row,
+} = ChromeUtils.import(
+  "resource://testing-common/mozmill/FolderDisplayHelpers.jsm"
+);
+const { OpenPGPTestUtils } = ChromeUtils.import(
+  "resource://testing-common/mozmill/OpenPGPTestUtils.jsm"
+);
+const { PromiseTestUtils } = ChromeUtils.import(
+  "resource://testing-common/mailnews/PromiseTestUtils.jsm"
+);
+const { FileUtils } = ChromeUtils.import(
+  "resource://gre/modules/FileUtils.jsm"
+);
+const { MailServices } = ChromeUtils.import(
+  "resource:///modules/MailServices.jsm"
+);
+const { EnigmailPersistentCrypto } = ChromeUtils.import(
+  "chrome://openpgp/content/modules/persistentCrypto.jsm"
+);
+var { AppConstants } = ChromeUtils.import(
+  "resource://gre/modules/AppConstants.jsm"
+);
+
+const MSG_TEXT = "Sundays are nothing without callaloo.";
+
+function getMsgBodyTxt(mc) {
+  let msgPane = mc.window.document.getElementById("messagepane");
+  return msgPane.contentDocument.firstChild.textContent;
+}
+
+var aliceAcct;
+var aliceIdentity;
+var initialKeyIdPref = "";
+var gInbox;
+
+var gDecFolder;
+
+/**
+ * Set up the base account, identity and keys needed for the tests.
+ */
+add_task(async function setup() {
+  aliceAcct = MailServices.accounts.createAccount();
+  aliceAcct.incomingServer = MailServices.accounts.createIncomingServer(
+    "alice",
+    "openpgp.example",
+    "pop3"
+  );
+  aliceIdentity = MailServices.accounts.createIdentity();
+  aliceIdentity.email = "alice@openpgp.example";
+  aliceAcct.addIdentity(aliceIdentity);
+
+  aliceAcct.incomingServer.rootFolder.createSubfolder("decrypted", null);
+
+  gDecFolder = aliceAcct.incomingServer.rootFolder.getChildNamed("decrypted");
+
+  // Set up the alice's private key.
+  let [id] = await OpenPGPTestUtils.importPrivateKey(
+    window,
+    new FileUtils.File(
+      getTestFilePath(
+        "data/keys/alice@openpgp.example-0xf231550c4f47e38e-secret.asc"
+      )
+    )
+  );
+
+  initialKeyIdPref = aliceIdentity.getUnicharAttribute("openpgp_key_id");
+  aliceIdentity.setUnicharAttribute("openpgp_key_id", id);
+
+  // Import and accept the public key for Bob, our verified sender.
+  await OpenPGPTestUtils.importPublicKey(
+    window,
+    new FileUtils.File(
+      getTestFilePath(
+        "data/keys/bob@openpgp.example-0xfbfcc82a015e7330-pub.asc"
+      )
+    )
+  );
+
+  gInbox = get_special_folder(Ci.nsMsgFolderFlags.Inbox, true);
+  be_in_folder(gInbox);
+});
+
+add_task(async function testPermanentDecrypt() {
+  // Fetch a local OpenPGP message.
+  let openPgpFile = new FileUtils.File(
+    getTestFilePath(
+      "data/eml/signed-by-0xfbfcc82a015e7330-encrypted-to-0xf231550c4f47e38e.eml"
+    )
+  );
+
+  // Add the fetched OpenPGP message to the inbox folder.
+  let copyListener = new PromiseTestUtils.PromiseCopyListener();
+  MailServices.copy.copyFileMessage(
+    openPgpFile,
+    gInbox,
+    null,
+    false,
+    0,
+    "",
+    copyListener,
+    null
+  );
+  await copyListener.promise;
+
+  // Select the first row.
+  select_click_row(0);
+
+  Assert.equal(
+    mc.window.document.getElementById("encryptionTechBtn").querySelector("span")
+      .textContent,
+    "OpenPGP"
+  );
+
+  Assert.ok(getMsgBodyTxt(mc).includes(MSG_TEXT), "message text is in body");
+  Assert.ok(
+    OpenPGPTestUtils.hasEncryptedIconState(mc.window.document, "ok"),
+    "encrypted icon is displayed"
+  );
+
+  // Get header of selected message
+  let hdr = window.gFolderDisplay.selectedMessages[0];
+
+  await EnigmailPersistentCrypto.cryptMessage(hdr, gDecFolder.URI, false, null);
+
+  await OpenPGPTestUtils.removeKeyById("0xf231550c4f47e38e", true);
+
+  be_in_folder(gDecFolder);
+
+  select_click_row(0);
+  Assert.ok(getMsgBodyTxt(mc).includes(MSG_TEXT), "message text is in body");
+  Assert.ok(
+    !OpenPGPTestUtils.hasEncryptedIconState(mc.window.document, "ok"),
+    "encrypted icon NOT displayed"
+  );
+});
+
+registerCleanupFunction(function tearDown() {
+  // Reset the OpenPGP key and delete the account.
+  aliceIdentity.setUnicharAttribute("openpgp_key_id", initialKeyIdPref);
+  MailServices.accounts.removeIncomingServer(aliceAcct.incomingServer, true);
+  MailServices.accounts.removeAccount(aliceAcct);
+  aliceAcct = null;
+
+  // Work around this test timing out at completion because of focus weirdness.
+  window.gFolderDisplay.tree.focus();
+});
---
+++ mailnews/mime/test/unit/test_smime_perm_decrypt.js
@@ -0,0 +1,280 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Tests to ensure signed and/or encrypted S/MIME messages are
+ * processed correctly, and the signature status is treated as good
+ * or bad as expected.
+ */
+
+var { PromiseTestUtils } = ChromeUtils.import(
+  "resource://testing-common/mailnews/PromiseTestUtils.jsm"
+);
+var { PromiseUtils } = ChromeUtils.import(
+  "resource://gre/modules/PromiseUtils.jsm"
+);
+var { SmimeUtils } = ChromeUtils.import(
+  "resource://testing-common/mailnews/smimeUtils.jsm"
+);
+const { EnigmailPersistentCrypto } = ChromeUtils.import(
+  "chrome://openpgp/content/modules/persistentCrypto.jsm"
+);
+var { setTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm");
+
+/* import-globals-from ../../../test/resources/logHelper.js */
+/* import-globals-from ../../../test/resources/asyncTestUtils.js */
+load("../../../resources/logHelper.js");
+load("../../../resources/asyncTestUtils.js");
+
+/* import-globals-from ../../../test/resources/MessageGenerator.jsm */
+/* import-globals-from ../../../test/resources/messageModifier.js */
+/* import-globals-from ../../../test/resources/messageInjection.js */
+load("../../../resources/MessageGenerator.jsm");
+load("../../../resources/messageModifier.js");
+load("../../../resources/messageInjection.js");
+
+let gCertValidityResult = 0;
+
+/**
+ * @implements nsICertVerificationCallback
+ */
+class CertVerificationResultCallback {
+  constructor(callback) {
+    this.callback = callback;
+  }
+  verifyCertFinished(prErrorCode, verifiedChain, hasEVPolicy) {
+    gCertValidityResult = prErrorCode;
+    this.callback();
+  }
+}
+
+function testCertValidity(cert, date) {
+  let prom = new Promise((resolve, reject) => {
+    const certificateUsageEmailRecipient = 0x0020;
+    let result = new CertVerificationResultCallback(resolve);
+    let flags = Ci.nsIX509CertDB.FLAG_LOCAL_ONLY;
+    const certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(
+      Ci.nsIX509CertDB
+    );
+    certdb.asyncVerifyCertAtTime(
+      cert,
+      certificateUsageEmailRecipient,
+      flags,
+      "Alice@example.com",
+      date,
+      result
+    );
+  });
+  return prom;
+}
+
+add_task(async function setup() {
+  gInbox = configure_message_injection({ mode: "local" });
+  SmimeUtils.ensureNSS();
+
+  SmimeUtils.loadPEMCertificate(
+    do_get_file(smimeDataDirectory + "TestCA.pem"),
+    Ci.nsIX509Cert.CA_CERT
+  );
+  SmimeUtils.loadCertificateAndKey(
+    do_get_file(smimeDataDirectory + "Alice.p12")
+  );
+  SmimeUtils.loadCertificateAndKey(do_get_file(smimeDataDirectory + "Bob.p12"));
+});
+
+var gInbox;
+
+var smimeDataDirectory = "../../../data/smime/";
+
+let smimeHeaderSink = {
+  expectResults(maxLen) {
+    // dump("Restarting for next test\n");
+    this._deferred = PromiseUtils.defer();
+    this._expectedEvents = maxLen;
+    this.countReceived = 0;
+    this._results = [];
+    this.haveSignedBad = false;
+    this.haveEncryptionBad = false;
+    this.resultSig = null;
+    this.resultEnc = null;
+    this.resultSigFirst = undefined;
+    return this._deferred.promise;
+  },
+  signedStatus(aNestingLevel, aSignedStatus, aSignerCert) {
+    console.log("signedStatus " + aSignedStatus + " level " + aNestingLevel);
+    // dump("Signed message\n");
+    Assert.equal(aNestingLevel, 1);
+    if (!this.haveSignedBad) {
+      // override with newer allowed
+      this.resultSig = {
+        type: "signed",
+        status: aSignedStatus,
+        certificate: aSignerCert,
+      };
+      if (aSignedStatus != 0) {
+        this.haveSignedBad = true;
+      }
+      if (this.resultSigFirst == undefined) {
+        this.resultSigFirst = true;
+      }
+    }
+    this.countReceived++;
+    this.checkFinished();
+  },
+  encryptionStatus(aNestingLevel, aEncryptedStatus, aRecipientCert) {
+    console.log(
+      "encryptionStatus " + aEncryptedStatus + " level " + aNestingLevel
+    );
+    // dump("Encrypted message\n");
+    Assert.equal(aNestingLevel, 1);
+    if (!this.haveEncryptionBad) {
+      // override with newer allowed
+      this.resultEnc = {
+        type: "encrypted",
+        status: aEncryptedStatus,
+        certificate: aRecipientCert,
+      };
+      if (aEncryptedStatus != 0) {
+        this.haveEncryptionBad = true;
+      }
+      if (this.resultSigFirst == undefined) {
+        this.resultSigFirst = false;
+      }
+    }
+    this.countReceived++;
+    this.checkFinished();
+  },
+  checkFinished() {
+    if (this.countReceived == this._expectedEvents) {
+      if (this.resultSigFirst) {
+        if (this.resultSig) {
+          this._results.push(this.resultSig);
+        }
+        if (this.resultEnc) {
+          this._results.push(this.resultEnc);
+        }
+      } else {
+        if (this.resultEnc) {
+          this._results.push(this.resultEnc);
+        }
+        if (this.resultSig) {
+          this._results.push(this.resultSig);
+        }
+      }
+      this._deferred.resolve(this._results);
+    }
+  },
+  QueryInterface: ChromeUtils.generateQI(["nsIMsgSMIMEHeaderSink"]),
+};
+
+/**
+ * Note on FILENAMES taken from the NSS test suite:
+ * - env: CMS enveloped (encrypted)
+ * - dsig: CMS detached signature (with multipart MIME)
+ * - sig: CMS opaque signature (content embedded inside signature)
+ * - bad: message text does not match signature
+ * - mismatch: embedded content is different
+ *
+ * Control variables used for checking results:
+ * - env: If true, we expect a report to encryptionStatus() that message
+ *        is encrypted.
+ */
+
+var gMessages = [{ filename: "alice.env.eml", enc: true }];
+
+var gDecFolder;
+
+add_task(async function copy_messages() {
+  for (let msg of gMessages) {
+    let promiseCopyListener = new PromiseTestUtils.PromiseCopyListener();
+
+    MailServices.copy.copyFileMessage(
+      do_get_file(smimeDataDirectory + msg.filename),
+      gInbox,
+      null,
+      true,
+      0,
+      "",
+      promiseCopyListener,
+      null
+    );
+
+    await promiseCopyListener.promise;
+    promiseCopyListener = null;
+  }
+  gInbox.server.rootFolder.createSubfolder("decrypted", null);
+  gDecFolder = gInbox.server.rootFolder.getChildNamed("decrypted");
+});
+
+add_task(async function check_smime_message() {
+  let hdrIndex = 0;
+
+  for (let msg of gMessages) {
+    console.log("checking " + msg.filename);
+
+    let numExpected = 1;
+
+    let eventsExpected = numExpected;
+
+    let hdr = mailTestUtils.getMsgHdrN(gInbox, hdrIndex);
+    let uri = hdr.folder.getUriForMsg(hdr);
+    let sinkPromise = smimeHeaderSink.expectResults(eventsExpected);
+
+    let conversion = apply_mime_conversion(uri, {
+      securityInfo: smimeHeaderSink,
+    });
+    await conversion.promise;
+
+    let contents = conversion._data;
+    // dump("contents: " + contents + "\n");
+
+    // Check that we're also using the display output.
+    Assert.ok(contents.includes("<html>"));
+
+    await sinkPromise;
+
+    let r = smimeHeaderSink._results;
+    Assert.equal(r.length, numExpected);
+
+    if (msg.enc) {
+      Assert.equal(r[0].type, "encrypted");
+      Assert.equal(r[0].status, 0);
+      Assert.equal(r[0].certificate, null);
+    }
+
+    await EnigmailPersistentCrypto.cryptMessage(
+      hdr,
+      gDecFolder.URI,
+      false,
+      null
+    );
+
+    eventsExpected = 0;
+
+    hdr = mailTestUtils.getMsgHdrN(gDecFolder, hdrIndex);
+    uri = hdr.folder.getUriForMsg(hdr);
+    sinkPromise = smimeHeaderSink.expectResults(eventsExpected);
+
+    conversion = apply_mime_conversion(uri, {
+      securityInfo: smimeHeaderSink,
+    });
+    await conversion.promise;
+
+    contents = conversion._data;
+    // dump("contents: " + contents + "\n");
+
+    // Check that we're also using the display output.
+    Assert.ok(contents.includes("<html>"));
+
+    // A message without S/MIME content didn't produce any events,
+    // so we must manually force this check.
+    smimeHeaderSink.checkFinished();
+    await sinkPromise;
+
+    // If the result length is 0, it wasn't decrypted.
+    Assert.equal(smimeHeaderSink._results.length, 0);
+
+    hdrIndex++;
+  }
+});
