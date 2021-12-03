0426f0046675e0d3a6e63710563b45181fdbe5e9
Rob Lemley <rob@thunderbird.net>
2021-12-03 16:07:22 +0000

    Bug 1611580 - Run clang-format for Thunderbird code in CI. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D132808
    
    Depends on D132807

---
+++ taskcluster/comm_taskgraph/transforms/source_test.py
@@ -0,0 +1,63 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import logging
+import shlex
+
+from mozpack.path import match as mozpackmatch, join as join_path
+
+from gecko_taskgraph.transforms.base import TransformSequence
+from gecko_taskgraph.files_changed import get_changed_files
+
+logger = logging.getLogger(__name__)
+
+transforms = TransformSequence()
+
+
+def get_patterns(job):
+    """Get the "run on-changed" file patterns."""
+    optimization = job.get("optimization", {})
+    if optimization:
+        return optimization.copy().popitem()[1]
+    return []
+
+
+def shlex_join(split_command):
+    """shlex.join from Python 3.8+"""
+    return " ".join(shlex.quote(arg) for arg in split_command)
+
+
+@transforms.add
+def changed_clang_format(config, jobs):
+    """
+    Transform for clang-format job to set the commandline to only check
+    C++ files that were changed in the current push rather than running on
+    the entire repository.
+    """
+    for job in jobs:
+        if job.get("name", "") == "clang-format":
+            repository = config.params.get("comm_head_repository")
+            revision = config.params.get("comm_head_rev")
+
+            match_patterns = get_patterns(job)
+            changed_files = {
+                join_path("comm", file)
+                for file in get_changed_files(repository, revision)
+            }
+
+            cpp_files = []
+            for pattern in match_patterns:
+                for path in changed_files:
+                    if mozpackmatch(path, pattern):
+                        cpp_files.append(path)
+
+            # In the event that no C/C++ files were changed in the current push,
+            # the commandline will end up being invalid. But, the clang-format
+            # job will get dropped by optimization, so it doesn't really matter.
+            if cpp_files:
+                job["run"]["command"] = job["run"]["command"].format(
+                    changed_files=shlex_join(cpp_files)
+                )
+
+        yield job
---
+++ taskcluster/scripts/source-test-clang-format.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Run the Firefox setup script
+source $HOME/checkouts/gecko/taskcluster/scripts/misc/source-test-clang-setup.sh
+
+# Append comm/.clang-format-ignore contents to $topsrcdir/.clang-format-ignore
+sed -e 's%^\([a-z]\)%comm/\1%' comm/.clang-format-ignore >> .clang-format-ignore
+
+# Update mozconfig file with Thunderbird build options
+cat <<EOT >> $MOZCONFIG
+ac_add_options --enable-application=comm/mail
+EOT
+
+# Run mach clang-format
+# shellcheck disable=SC2068
+./mach --log-no-times clang-format --output $HOME/clang-format.json --format json -p $@
+# shellcheck disable=SC2068
+./mach --log-no-times clang-format --output $HOME/clang-format.diff --format diff -p $@
+
+# Exit with an error code if clang-format.diff contains a proper diff.
+# Needed because mach clang-format will exit 0 regardless of outcome.
+# If no formatting is needed, clang-format.diff will have a single \n,
+# so check for a file size > 1 byte.
+DIFF_SIZE=$(stat -c %s $HOME/clang-format.diff)
+if [[ "$DIFF_SIZE" -gt 1 ]]; then
+  echo "Exiting with error status. DIFF_SIZE is $DIFF_SIZE."
+  exit 1
+else
+  exit 0
+fi
