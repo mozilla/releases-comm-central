319bb678b49f2f309a32ef06ddfa7d5c9e98f5f6
Ping Chen <remotenonsense@gmail.com>
2021-12-22 12:06:24 +0200

    Bug 1746860 - Do not remove tmp attachment sent via cloud. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D134356
    
    --HG--
    extra : amend_source : 7bc6b5d761ab82d3c3cf229360da04a36674ca50

--- mailnews/compose/src/nsMsgCompose.cpp
+++ mailnews/compose/src/nsMsgCompose.cpp
@@ -5215,7 +5215,9 @@ void nsMsgCompose::DeleteTmpAttachments() {
   for (nsIMsgAttachment* attachment : attachments) {
     bool isTemporary;
     attachment->GetTemporary(&isTemporary);
-    if (isTemporary) {
+    bool sentViaCloud;
+    attachment->GetSendViaCloud(&sentViaCloud);
+    if (isTemporary && !sentViaCloud) {
       nsCString url;
       attachment->GetUrl(url);
       nsCOMPtr<nsIFile> urlFile;
