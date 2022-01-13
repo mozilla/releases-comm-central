b01c4d680cc60b46fdcd18a8d15c7603cabb3091
Rob Lemley <rob@thunderbird.net>
2022-01-13 19:15:30 +0000

    Bug 1747879  - Do not set MOZ_SOURCE environment variables in build jobs. r=darktrojan
    Debug .sym files contained incorrect VCS references due to MOZ_SOURCE_REPO
    and MOZ_SOURCE_CHANGESET being set in the environment reffering to the
    Thunderbird repository.
    mozconfigure will handle setting these values in the buildconfig.
    See D135299.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135392

---
+++ taskcluster/comm_taskgraph/transforms/tb_build.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import logging
+
+from gecko_taskgraph.transforms.base import TransformSequence
+
+logger = logging.getLogger(__name__)
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def munge_environment(config, jobs):
+    for job in jobs:
+        env = job["worker"]["env"]
+        # Remove MOZ_SOURCE_CHANGESET/REPO from the job environment and discard
+        # if present. Having these variables set in the environment causes problems
+        # with generating debug sym files. Bug 1747879.
+        env.pop("MOZ_SOURCE_CHANGESET", None)
+        env.pop("MOZ_SOURCE_REPO", None)
+
+        yield job
