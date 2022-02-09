a649867618825b0582db5e3cc19f6446a7afd037
John Bieling <john@thunderbird.net>
2022-02-09 12:39:55 +0200

    Bug 1739498 - Prevent usage of incompatible address book files. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D136269
    
    --HG--
    extra : amend_source : e4d788cad6d832cb958b754548c862fb050ffdf9

--- mailnews/addrbook/test/unit/test_collection_2.js
+++ mailnews/addrbook/test/unit/test_collection_2.js
@@ -9,9 +9,10 @@
 var nsIAbPMF = Ci.nsIAbPreferMailFormat;
 
 function run_test() {
-  // Test - Get the address collecter
+  // Test - Get the address collector
+  loadABFile("data/collect", kPABData.fileName);
 
-  // Get the actual collecter
+  // Get the actual collector
   var addressCollect = Cc[
     "@mozilla.org/addressbook/services/addressCollector;1"
   ].getService(Ci.nsIAbAddressCollector);
@@ -19,10 +20,8 @@ function run_test() {
   // Set the new pref afterwards to ensure we change correctly
   Services.prefs.setCharPref("mail.collect_addressbook", kCABData.URI);
 
-  loadABFile("data/collect", kPABData.fileName);
-
   // XXX Getting all directories ensures we create all ABs because the
-  // address collecter can't currently create ABs itself (bug 314448).
+  // address collector can't currently create ABs itself (bug 314448).
   MailServices.ab.directories;
 
   addressCollect.collectAddress(
