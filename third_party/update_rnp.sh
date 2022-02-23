7ab8ea84b8d436fbfac734921b9e0e800858aec8
Rob Lemley <rob@thunderbird.net>
2022-02-23 17:11:24 +0000

    Bug 1756756 - Update RNP source update script. r=kaie
    Differential Revision: https://phabricator.services.mozilla.com/D139500

--- third_party/update_rnp.sh
+++ third_party/update_rnp.sh
@@ -62,7 +62,8 @@ $THIRDROC rnp_source_update rnp/ \
   "${BUGREPORT}"
 
 # Restore moz.build
-hg revert rnp/moz.build rnp/module.ver rnp/rnp.symbols rnp/src/lib/rnp/rnp_export.h
+hg revert rnp/moz.build rnp/module.ver rnp/rnp.symbols rnp/src/lib/rnp/rnp_export.h \
+  rnp/src/rnp/moz.build  rnp/src/rnpkeys/moz.build
 
 # Reapply Thunderbird patch to disable obsolete ciphers
 PATCH_FILE="patches/rnp/disable_obsolete_ciphers.patch"
