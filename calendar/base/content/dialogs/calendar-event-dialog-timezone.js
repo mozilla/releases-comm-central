dea0d988ebf398efcb48e6c1391c59f2a0c9f340
Magnus Melin <mkmelin+mozilla@iki.fi>
2022-03-08 12:09:57 +0200

    Bug 1703164 - convert calendar/base/content/dialogs/calendar-event-dialog-timezone.xhtml to top level <html>. r=henry
    Differential Revision: https://phabricator.services.mozilla.com/D139280
    
    --HG--
    extra : rebase_source : 0e9b70ca96035e85e246027e13548128324f3f49
    extra : amend_source : 1bbb7c82afd3ca8aefc905c96d4d5980b40b317a

--- calendar/base/content/dialogs/calendar-event-dialog-timezone.js
+++ calendar/base/content/dialogs/calendar-event-dialog-timezone.js
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* exported onLoad */
-
-/* import-globals-from ../calendar-ui-utils.js */
+/* global addMenuItem */ // From  ../calendar-ui-utils.js
 
 var { cal } = ChromeUtils.import("resource:///modules/calendar/calUtils.jsm");
+
+window.addEventListener("load", onLoad);
 
 /**
  * Sets up the timezone dialog from the window arguments, also setting up all
