6817947db03d3e903838dd1985da53a526d53742
Magnus Melin <mkmelin+mozilla@iki.fi>
2022-03-09 11:57:49 +0200

    Bug 1703164 - convert calendar/base/content/dialogs/calendar-error-prompt.xhtml to top level <html>. r=henry
    For testing use
    ```
    var paramBlock = Cc["@mozilla.org/embedcomp/dialogparam;1"].createInstance(
      Ci.nsIDialogParamBlock
    );
    paramBlock.SetNumberStrings(3);
    paramBlock.SetString(0, "An error was encountered preparing the calendar located at http://example.com for use. It will not be available.");
    paramBlock.SetString(1, "0x12334");
    paramBlock.SetString(2, "blablabla");
    Services.ww.openWindow(
      null,
      "chrome://calendar/content/calendar-error-prompt.xhtml",
      "_blank",
      "chrome,dialog=yes,alwaysRaised=yes",
      paramBlock
    );
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D139282
    
    --HG--
    extra : rebase_source : 5eea599c01b069f0db27428295673bc2314d896c
    extra : amend_source : 05840721336fbd7c4d335a833740eb0c486987a3

--- calendar/base/content/dialogs/calendar-error-prompt.js
+++ calendar/base/content/dialogs/calendar-error-prompt.js
@@ -2,21 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* exported loadErrorPrompt, toggleDetails */
+window.addEventListener("DOMContentLoaded", loadErrorPrompt);
 
 function loadErrorPrompt() {
   let args = window.arguments[0].QueryInterface(Ci.nsIDialogParamBlock);
   document.getElementById("general-text").value = args.GetString(0);
   document.getElementById("error-code").value = args.GetString(1);
   document.getElementById("error-description").value = args.GetString(2);
-  window.sizeToContent();
 }
 function toggleDetails() {
   let options = document.getElementById("details-box");
-  if (options.collapsed) {
-    options.collapsed = false;
-  } else {
-    options.collapsed = true;
-  }
-  window.sizeToContent();
+  options.collapsed = !options.collapsed;
+  // Grow the window height if the details overflow.
+  window.innerHeight = document.body.scrollHeight;
 }
