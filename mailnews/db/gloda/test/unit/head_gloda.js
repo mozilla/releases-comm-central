c17422d033f2c9c55715477398e09bec49ae58fa
Nicolai Kasper <nicolai@thunderbird.net>
2022-03-22 04:47:11 +0000

    Bug 1758452 - Part 14: Remove glodaTestHelper.js. r=mkmelin
    Depends on D141381
    
    Differential Revision: https://phabricator.services.mozilla.com/D141383
    
    --HG--
    extra : moz-landing-system : lando

--- mailnews/db/gloda/test/unit/head_gloda.js
+++ mailnews/db/gloda/test/unit/head_gloda.js
@@ -7,8 +7,6 @@ do_get_profile();
 
 var gDEPTH = "../../../../../";
 
-// glodaTestHelper.js does all the rest of the imports
-
 registerCleanupFunction(function() {
   load(gDEPTH + "mailnews/resources/mailShutdown.js");
 });
