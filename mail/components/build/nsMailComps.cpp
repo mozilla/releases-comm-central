269267cdf0f3cae99d78d5b986b79bf31aa4b147
Nicolai Kasper <nicolai@thunderbird.net>
2022-04-11 12:15:18 +0200

    Backed out changeset 2881fc1b73ca (bug 1763034) as it's no longer needed. rs=backout

--- mail/components/build/nsMailComps.cpp
+++ mail/components/build/nsMailComps.cpp
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsAtom.h"
 #include "mozilla/ModuleUtils.h"
 #include "nsMailMigrationCID.h"
 #include "nsProfileMigrator.h"
