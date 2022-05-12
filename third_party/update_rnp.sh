1a3d0d884e9af785b3ea90d040d0444f79fae893
Rob Lemley <rob@thunderbird.net>
2022-05-12 17:05:17 +0000

    Bug 1768424 - Add upstream patch for bug 1768424. r=kaie
    From https://github.com/rnpgp/rnp/pull/1745
    
    Differential Revision: https://phabricator.services.mozilla.com/D146014
    
    Depends on D146013

--- third_party/update_rnp.sh
+++ third_party/update_rnp.sh
@@ -66,7 +66,9 @@ hg revert rnp/moz.build rnp/module.ver rnp/rnp.symbols rnp/src/lib/rnp/rnp_expor
   rnp/src/rnp/moz.build  rnp/src/rnpkeys/moz.build
 
 # Reapply Thunderbird patch to disable obsolete ciphers
-PATCH_FILES=("patches/rnp/disable_obsolete_ciphers.patch" "patches/rnp/bug_1763641.patch")
+PATCH_FILES=("patches/rnp/disable_obsolete_ciphers.patch" \
+             "patches/rnp/bug_1763641.patch" \
+             "patches/rnp/bug_1768424.patch")
 for PATCH_FILE in "${PATCH_FILES[@]}"; do
   patch -p2 -i "${PATCH_FILE}" -N -r "${MY_TEMP_DIR}/${PATCH_FILE}.rej"
 done
