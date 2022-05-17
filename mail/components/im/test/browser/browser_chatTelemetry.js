3573bb032cb28e58849523292c788bf697363929
Martin Giger <martin@humanoids.be>
2022-05-17 12:32:46 +0300

    Bug 1767004 - Add telemetry probe for chat message theme. r=aleca, data-review=sancus
    Differential Revision: https://phabricator.services.mozilla.com/D145164
    
    --HG--
    extra : amend_source : d6a213f7335a159c92873b0ac13b48c0c08ebd98

---
+++ mail/components/im/test/browser/browser_chatTelemetry.js
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let { TelemetryTestUtils } = ChromeUtils.import(
+  "resource://testing-common/TelemetryTestUtils.jsm"
+);
+
+add_task(async function testMessageThemeTelemetry() {
+  Services.telemetry.clearScalars();
+
+  const account = Services.accounts.createAccount("testuser", "prpl-mochitest");
+  account.password = "this is a test";
+  account.connect();
+
+  let scalars = TelemetryTestUtils.getProcessScalars("parent");
+  ok(
+    !scalars["tb.chat.active_message_theme"],
+    "Active chat theme not reported without open conversation."
+  );
+
+  await openChatTab();
+  ok(BrowserTestUtils.is_visible(document.getElementById("chatPanel")));
+
+  const conversation = account.prplAccount.wrappedJSObject.makeDM("collapse");
+  const convNode = getConversationItem(conversation);
+  ok(convNode);
+
+  await EventUtils.synthesizeMouseAtCenter(convNode, {});
+
+  const chatConv = getChatConversationElement(conversation);
+  const conversationLoaded = waitForConversationLoad(chatConv.convBrowser);
+  ok(chatConv, "found conversation");
+  ok(BrowserTestUtils.is_visible(chatConv), "conversation visible");
+  await BrowserTestUtils.browserLoaded(chatConv.convBrowser);
+
+  await conversationLoaded;
+  scalars = TelemetryTestUtils.getProcessScalars("parent");
+  is(
+    scalars["tb.chat.active_message_theme"],
+    "mail:default",
+    "Active chat message theme and variant reported after opening conversation."
+  );
+
+  conversation.close();
+  account.disconnect();
+  Services.accounts.deleteAccount(account.id);
+});
