28a28fc574947bf134ea5db0b234bff48e111196
Magnus Melin <mkmelin+mozilla@iki.fi>
2022-06-11 12:11:44 +0300

    Bug 1772376 - Prevent double loading of filter search terms/actions when inspecting the FilterEditor dialog. r=henry

--- mailnews/search/content/FilterEditor.js
+++ mailnews/search/content/FilterEditor.js
@@ -58,7 +58,7 @@ var gActionListOrdered = null;
 
 var gFilterEditorMsgWindow = null;
 
-window.addEventListener("DOMContentLoaded", filterEditorOnLoad);
+window.addEventListener("DOMContentLoaded", filterEditorOnLoad, { once: true });
 document.addEventListener("dialogaccept", onAccept);
 
 function filterEditorOnLoad() {
