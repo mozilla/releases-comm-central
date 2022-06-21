96e876dbe3b2875e26f357152758d16c6908af4c
Geoff Lankow <geoff@darktrojan.net>
2022-06-21 15:25:29 +1200

    Bug 1775191 - Port bug 1770363 - Allow the browser toolbox to enable/disable multiprocess behavior on-demand. r=#thunderbird-reviewers,aleca
    Differential Revision: https://phabricator.services.mozilla.com/D149846
    
    --HG--
    extra : amend_source : 882c428c392697223f09e4930a2b9994caf66126
    extra : histedit_source : b4e63952c0aa708b67ac1d0e6e4911ee1909f75a

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -921,6 +921,13 @@ pref("devtools.browsertoolbox.fission", true);
 pref("devtools.browsertoolbox.fission", false);
 #endif
 
+// When the Multiprocess Browser Toolbox is enabled, you can configure the scope of it:
+// - "everything" will enable debugging absolutely everything in the browser
+//   All processes, all documents, all workers, all add-ons.
+// - "parent-process" will restrict debugging to the parent process
+//   All privileged javascript, documents and workers running in the parent process.
+pref("devtools.browsertoolbox.scope", "everything");
+
 // Toolbox Button preferences
 pref("devtools.command-button-pick.enabled", true);
 pref("devtools.command-button-frames.enabled", true);
