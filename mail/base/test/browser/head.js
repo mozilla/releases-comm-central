6348bf188d35744e4f5c61a917d663741be13c29
Geoff Lankow <geoff@darktrojan.net>
2022-06-27 11:17:57 +1200

    Bug 1835564 - Deploy focus hack at the end of tests in mail/test/browser. r=john.bieling
    Differential Revision: https://phabricator.services.mozilla.com/D180042
    
    --HG--
    extra : rebase_source : ed970c873b4ff10a87a53b71e898f72b7e02b85a
    extra : amend_source : f6305166c1257f992e7b3bff85cacd9caeac8a74

--- mail/base/test/browser/head.js
+++ mail/base/test/browser/head.js
@@ -263,5 +263,14 @@ registerCleanupFunction(function () {
     }
 
     resetSmartMailboxes();
+
+    // Some tests that open new windows don't return focus to the main window
+    // in a way that satisfies mochitest, and the test times out.
+    Services.focus.focusedWindow = window;
+    // Focus an element in the main window, then blur it again to avoid it
+    // hijacking keypresses.
+    let mainWindowElement = document.getElementById("button-appmenu");
+    mainWindowElement.focus();
+    mainWindowElement.blur();
   });
 });
