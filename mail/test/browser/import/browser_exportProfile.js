1505b99229f3c1c53c246e8877f35df4e03e44a9
Martin Giger <martin@humanoids.be>
2022-08-18 20:33:32 +1000

    Bug 1784976 - Add tests for profile import and export in tab. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D154694
    
    --HG--
    extra : amend_source : 2fcb8404198f6027ac400e81bed7bd3226124742

---
+++ mail/test/browser/import/browser_exportProfile.js
@@ -0,0 +1,97 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+const { MockFilePicker } = ChromeUtils.import(
+  "resource://specialpowers/MockFilePicker.jsm"
+);
+
+add_task(async function testProfileExport() {
+  const profileDir = await IOUtils.createUniqueDirectory(
+    PathUtils.tempDir,
+    "profile-tmp"
+  );
+  const zipFile = PathUtils.join(profileDir, "export.zip");
+  const filePath = Services.io
+    .newURI(PathUtils.toFileURI(zipFile))
+    .QueryInterface(Ci.nsIFileURL);
+  MockFilePicker.init(window);
+  MockFilePicker.setFiles([filePath.file]);
+  registerCleanupFunction(async () => {
+    await IOUtils.remove(profileDir, {
+      recursive: true,
+    });
+    MockFilePicker.cleanup();
+  });
+
+  const tab = await new Promise(resolve => {
+    const tab = window.openTab("contentTab", {
+      url: "about:import",
+      onLoad(event, browser) {
+        browser.contentWindow.showTab("tab-export", true);
+        resolve(tab);
+      },
+    });
+  });
+  const importDocument = tab.browser.contentDocument;
+  const exportPane = importDocument.getElementById("tabPane-export");
+
+  ok(
+    BrowserTestUtils.is_visible(importDocument.getElementById("exportDocs")),
+    "Export docs link is visible"
+  );
+  ok(
+    BrowserTestUtils.is_hidden(importDocument.getElementById("importDocs")),
+    "Import docs link is hidden"
+  );
+
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#exportButton",
+    {},
+    tab.browser
+  );
+  const progressPane = exportPane.querySelector(".progressPane");
+  await BrowserTestUtils.waitForMutationCondition(
+    exportPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(progressPane)
+  );
+  ok(
+    BrowserTestUtils.is_hidden(importDocument.getElementById("exportButton")),
+    "Export button is hidden while export is in progress"
+  );
+
+  const finish = exportPane.querySelector(".progressFinish");
+  await BrowserTestUtils.waitForMutationCondition(
+    exportPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(finish)
+  );
+  ok(
+    BrowserTestUtils.is_visible(progressPane),
+    "When export succeeds and finish is shown, progress is still displayed"
+  );
+
+  const tabClosedPromise = BrowserTestUtils.waitForEvent(
+    tab.tabNode,
+    "TabClose"
+  );
+  // Using BrowserTestUtils fails, because clicking the button destroys the
+  // context, which means the actor from BTU gets destroyed before it can report
+  // that it emitted the event.
+  await EventUtils.synthesizeMouseAtCenter(
+    finish,
+    {},
+    tab.browser.contentWindow
+  );
+  await tabClosedPromise;
+
+  const exportZipStat = await IOUtils.stat(zipFile);
+  info(exportZipStat.size);
+  ok(exportZipStat.size > 10, "Zip is not empty");
+});
---
+++ mail/test/browser/import/browser_importProfile.js
@@ -0,0 +1,319 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+const { MockFilePicker } = ChromeUtils.import(
+  "resource://specialpowers/MockFilePicker.jsm"
+);
+
+function checkSteps(importDocument, currentStep, totalSteps) {
+  const stepNav = importDocument.getElementById("stepNav");
+  is(stepNav.childElementCount, totalSteps, "Expected amount of steps");
+  ok(
+    stepNav
+      .querySelector(`*:nth-child(${currentStep})`)
+      .classList.contains("current"),
+    `Expected step ${currentStep} is active`
+  );
+}
+
+function checkVisiblePane(importDocument, activePaneId, activeStepId) {
+  const panes = importDocument.querySelectorAll(".tabPane");
+  for (const pane of panes) {
+    if (pane.id === activePaneId) {
+      ok(BrowserTestUtils.is_visible(pane), `Pane ${activePaneId} is visible`);
+      const steps = pane.querySelectorAll("section");
+      for (const step of steps) {
+        if (step.id === activeStepId) {
+          ok(
+            BrowserTestUtils.is_visible(step),
+            `Step ${activeStepId} is visible`
+          );
+        } else {
+          ok(BrowserTestUtils.is_hidden(step), `Step ${step.id} is hidden`);
+        }
+      }
+    } else {
+      ok(BrowserTestUtils.is_hidden(pane), `Pane ${pane.id} is not visible`);
+    }
+  }
+}
+
+add_setup(() => {
+  MockFilePicker.init(window);
+  registerCleanupFunction(() => {
+    MockFilePicker.cleanup();
+  });
+});
+
+add_task(async function testProfileImport() {
+  const profileDir = await IOUtils.createUniqueDirectory(
+    PathUtils.tempDir,
+    "profile-tmp"
+  );
+  await IOUtils.writeUTF8(
+    PathUtils.join(profileDir, "prefs.js"),
+    [
+      ["mail.smtpserver.smtp1.username", "smtp-user-1"],
+      ["mail.smtpservers", "smtp1"],
+      ["mail.identity.id1.smtpServer", "smtp1"],
+      ["mail.server.server2.type", "none"],
+      ["mail.server.server6.type", "imap"],
+      ["mail.account.account1.server", "server2"],
+      ["mail.account.account2.server", "server6"],
+      ["mail.account.account2.identities", "id1"],
+      ["mail.accountmanager.accounts", "account2,account1"],
+    ]
+      .map(
+        ([name, value]) =>
+          `user_pref(${JSON.stringify(name)}, ${JSON.stringify(value)});`
+      )
+      .join("\n")
+  );
+  const filePath = Services.io
+    .newURI(PathUtils.toFileURI(profileDir))
+    .QueryInterface(Ci.nsIFileURL);
+  MockFilePicker.setFiles([filePath.file]);
+  registerCleanupFunction(async () => {
+    await IOUtils.remove(profileDir, {
+      recursive: true,
+    });
+  });
+
+  const tab = await new Promise(resolve => {
+    const tab = window.openTab("contentTab", {
+      url: "about:import",
+      onLoad() {
+        resolve(tab);
+      },
+    });
+  });
+  const importDocument = tab.browser.contentDocument;
+
+  ok(
+    BrowserTestUtils.is_hidden(importDocument.getElementById("exportDocs")),
+    "Export docs link is hidden"
+  );
+  ok(
+    BrowserTestUtils.is_visible(importDocument.getElementById("importDocs")),
+    "Import docs link is visible"
+  );
+
+  checkSteps(importDocument, 1, 4);
+  checkVisiblePane(importDocument, "tabPane-start", "start-sources");
+  ok(
+    importDocument.querySelector('#start-sources input[value="Thunderbird"]')
+      .value,
+    "Thunderbird profile is selected by default"
+  );
+  ok(
+    BrowserTestUtils.is_hidden(
+      importDocument.getElementById("startBackButton")
+    ),
+    "Back button is hidden in first step"
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#tabPane-start .continue",
+    {},
+    tab.browser
+  );
+  const appPane = importDocument.getElementById("tabPane-app");
+  await BrowserTestUtils.waitForMutationCondition(
+    appPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(appPane)
+  );
+
+  checkSteps(importDocument, 2, 4);
+  checkVisiblePane(importDocument, "tabPane-app", "app-profiles");
+  ok(
+    BrowserTestUtils.is_visible(
+      importDocument.getElementById("profileBackButton")
+    ),
+    "Back button is visible"
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    '#filePickerList [value="file-picker-dir"]',
+    {},
+    tab.browser
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#profileNextButton",
+    {},
+    tab.browser
+  );
+  const itemsStep = importDocument.getElementById("app-items");
+  await BrowserTestUtils.waitForMutationCondition(
+    itemsStep,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(itemsStep)
+  );
+
+  checkSteps(importDocument, 3, 4);
+  checkVisiblePane(importDocument, "tabPane-app", "app-items");
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#profileNextButton",
+    {},
+    tab.browser
+  );
+  const summaryStep = importDocument.getElementById("app-summary");
+  await BrowserTestUtils.waitForMutationCondition(
+    summaryStep,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(summaryStep)
+  );
+
+  checkSteps(importDocument, 4, 4);
+  checkVisiblePane(importDocument, "tabPane-app", "app-summary");
+  ok(
+    BrowserTestUtils.is_hidden(
+      importDocument.getElementById("profileNextButton")
+    ),
+    "Can't advance from summary step"
+  );
+
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#appStartImport",
+    {},
+    tab.browser
+  );
+
+  const progressPane = importDocument.querySelector(
+    "#app-summary .progressPane"
+  );
+  await BrowserTestUtils.waitForMutationCondition(
+    appPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(progressPane)
+  );
+  ok(
+    BrowserTestUtils.is_hidden(importDocument.getElementById("appStartImport")),
+    "Import button is hidden while import is in progress"
+  );
+
+  const finish = importDocument.querySelector("#app-summary .progressFinish");
+  await BrowserTestUtils.waitForMutationCondition(
+    appPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(finish)
+  );
+  ok(
+    BrowserTestUtils.is_visible(progressPane),
+    "When import succeeds and finish is shown, progress is still displayed"
+  );
+
+  // We close the tab ourselves instead of hitting the finish button, since
+  // restarting Thunderbird within the test is a headache.
+  document.getElementById("tabmail").closeTab(tab);
+});
+
+add_task(async function testImportLargeZIP() {
+  const profileDir = await IOUtils.createUniqueDirectory(
+    PathUtils.tempDir,
+    "profile-tmp"
+  );
+  const profileZip = PathUtils.join(profileDir, "profile.zip");
+  // This block makes sure the ZIP file's fake contents go out of scope once
+  // written out.
+  {
+    const halfAGigabyte = new Uint8Array(2 ** 29);
+    await IOUtils.write(profileZip, halfAGigabyte);
+    for (let i = 0; i < 3; ++i) {
+      await IOUtils.write(profileZip, halfAGigabyte, { mode: "append" });
+    }
+  }
+  await IOUtils.write(
+    profileZip,
+    new Uint8Array(2), // These are extra bytes beyond 2 GB
+    {
+      mode: "append",
+    }
+  );
+  const filePath = Services.io
+    .newURI(PathUtils.toFileURI(profileZip))
+    .QueryInterface(Ci.nsIFileURL);
+  MockFilePicker.setFiles([filePath.file]);
+  registerCleanupFunction(async () => {
+    await IOUtils.remove(profileDir, {
+      recursive: true,
+    });
+  });
+
+  const tab = await new Promise(resolve => {
+    const tab = window.openTab("contentTab", {
+      url: "about:import",
+      onLoad() {
+        resolve(tab);
+      },
+    });
+  });
+  const importDocument = tab.browser.contentDocument;
+
+  checkSteps(importDocument, 1, 4);
+  checkVisiblePane(importDocument, "tabPane-start", "start-sources");
+  ok(
+    importDocument.querySelector('#start-sources input[value="Thunderbird"]')
+      .value,
+    "Thunderbird profile is selected by default"
+  );
+  ok(
+    BrowserTestUtils.is_hidden(
+      importDocument.getElementById("startBackButton")
+    ),
+    "Back button is hidden in first step"
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#tabPane-start .continue",
+    {},
+    tab.browser
+  );
+  const appPane = importDocument.getElementById("tabPane-app");
+  await BrowserTestUtils.waitForMutationCondition(
+    appPane,
+    {
+      attributes: true,
+    },
+    () => BrowserTestUtils.is_visible(appPane)
+  );
+
+  checkSteps(importDocument, 2, 4);
+  checkVisiblePane(importDocument, "tabPane-app", "app-profiles");
+  ok(
+    BrowserTestUtils.is_visible(
+      importDocument.getElementById("profileBackButton")
+    ),
+    "Back button is visible"
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#appFilePickerZip",
+    {},
+    tab.browser
+  );
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    "#profileNextButton",
+    {},
+    tab.browser
+  );
+
+  const notificationBox = importDocument.getElementById("errorNotifications");
+  await BrowserTestUtils.waitForMutationCondition(
+    notificationBox,
+    {
+      childList: true,
+    },
+    () => notificationBox.childElementCount > 0
+  );
+
+  document.getElementById("tabmail").closeTab(tab);
+});
