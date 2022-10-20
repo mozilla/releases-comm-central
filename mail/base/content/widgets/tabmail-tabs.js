76c66b8c91bbd1f0b670ef5d984493cd4bc99bf8
Richard Marti <richard.marti@gmail.com>
2022-10-20 21:51:00 +1100

    Bug 1795912 - Make the tabmail-tab work on modern flexbox. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D159608
    
    --HG--
    extra : amend_source : b67419dde3117ce36901ff8f3303eb6fb1c81408

--- mail/base/content/widgets/tabmail-tabs.js
+++ mail/base/content/widgets/tabmail-tabs.js
@@ -11,6 +11,9 @@
   const { AppConstants } = ChromeUtils.importESModule(
     "resource://gre/modules/AppConstants.sys.mjs"
   );
+  const { XPCOMUtils } = ChromeUtils.importESModule(
+    "resource://gre/modules/XPCOMUtils.sys.mjs"
+  );
 
   /**
    * The MozTabs widget holds all the tabs for the main tab UI.
@@ -462,12 +465,6 @@
 
       this._dragTime = 0;
 
-      this.mTabMinWidth = 100;
-
-      this.mTabMaxWidth = 250;
-
-      this.mTabClipWidth = 140;
-
       this._mAutoHide = false;
 
       this.mAllTabsButton = document.getElementById(
@@ -556,8 +553,6 @@
         document.documentElement.setAttribute("tabbarhidden", "true");
       }
 
-      this.arrowScrollbox.firstElementChild.minWidth = this.mTabMinWidth;
-      this.arrowScrollbox.firstElementChild.maxWidth = this.mTabMaxWidth;
       this._updateCloseButtons();
 
       Services.prefs.addObserver("mail.tabs.", this._prefObserver);
@@ -588,6 +583,27 @@
       this.addEventListener("TabSelect", event => {
         this._handleTabSelect();
       });
+      XPCOMUtils.defineLazyPreferenceGetter(
+        this,
+        "_tabMinWidthPref",
+        "mail.tabs.tabMinWidth",
+        null,
+        (pref, prevValue, newValue) => (this._tabMinWidth = newValue),
+        newValue => {
+          const LIMIT = 50;
+          return Math.max(newValue, LIMIT);
+        }
+      );
+      this._tabMinWidth = this._tabMinWidthPref;
+
+      XPCOMUtils.defineLazyPreferenceGetter(
+        this,
+        "_tabMaxWidthPref",
+        "mail.tabs.tabMaxWidth",
+        null,
+        (pref, prevValue, newValue) => (this._tabMaxWidth = newValue)
+      );
+      this._tabMaxWidth = this._tabMaxWidthPref;
     }
 
     get tabbox() {
@@ -823,6 +839,13 @@
       }
 
       return tabs.length;
+    }
+
+    set _tabMinWidth(val) {
+      this.arrowScrollbox.style.setProperty("--tab-min-width", `${val}px`);
+    }
+    set _tabMaxWidth(val) {
+      this.arrowScrollbox.style.setProperty("--tab-max-width", `${val}px`);
     }
 
     disconnectedCallback() {
