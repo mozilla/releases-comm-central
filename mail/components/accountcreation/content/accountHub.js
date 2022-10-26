f0f60cba6db46368960b730ab5dba2ceb1420450
Alessandro Castellani <alessandro@thunderbird.net>
2022-10-26 21:45:50 +1100

    Bug 1789968 - Implement the first view for the new account hub modal dialog. r=freaktechnik!,Paenglab,micahilbery
    Initial basic architecture for the account hub. There's not much UI inside the dialog, but it's a start. You can trigger it by calling `openAccountHub()` in the console.
    - I'm asking for a review from multiple people to validate the basic architecture.
    - I'm using a main shadowDOM custom element to keep the CSS and FTL files contained and not leak to other parts of the UI.
    - This dialog will be usable from ANY space and tab, so it needs to be tight and self contained.
    - I'm using classes with all private methods and attributes so other devs can't mess it up accidentally. Everything needs to pass through a single entry point.
    - I set an initial "potential" handling of not allowing to close the dialog if an operation is in process.
    - I'm defering the loading of the various views scripts and their modules only when needed in order to avoid loading very large JS files if a setup flow is never used.
    - I tried to be as explicit as possible with the comments and the methods.
    - The UI is borderline non existent, I just need confirmation that the basic architectural approach is correct, and it "looks" good.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157017
    
    --HG--
    extra : amend_source : f37f61b02725331efe883bb6b1c75db878a0feb4

---
+++ mail/components/accountcreation/content/accountHub.js
@@ -0,0 +1,197 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+var { MailServices } = ChromeUtils.import(
+  "resource:///modules/MailServices.jsm"
+);
+
+/**
+ * Holds the main controller class.
+ * @type {?AccountHubControllerClass}
+ */
+var AccountHubController;
+
+/**
+ * Controller class to handle the primary views of the account setup flow.
+ * This class acts as a sort of controller to lazily load the needed views upon
+ * request. It doesn't handle any data and it should only be used to switch
+ * between the different setup flows.
+ * All methods of this class should be private, except for the open() method.
+ */
+class AccountHubControllerClass {
+  /**
+   * The account hub main modal dialog.
+   * @type {?HTMLElement}
+   */
+  #modal = null;
+
+  /**
+   * The currently visible view inside the dialog.
+   * @type {?HTMLElement}
+   */
+  #currentView = null;
+
+  /**
+   * Object containing all strings to trigger the needed methods for the various
+   * views.
+   */
+  #accounts = {
+    START: () => this.#viewStart(),
+    MAIL: () => this.#viewEmailSetup(),
+    // TODO: calendar, address book, chat, newsgroups, feeds.
+  };
+
+  constructor() {
+    this.#loadScript("container");
+    const element = document.createElement("account-hub-container");
+    document.body.appendChild(element);
+    this.#modal = element.modal;
+
+    let closeButton = this.#modal.querySelector("#closeButton");
+    closeButton.hidden = !MailServices.accounts.accounts.length;
+    closeButton.addEventListener("click", () => this.#modal.close());
+
+    this.#modal.addEventListener("close", event => {
+      // Don't allow the dialog to be closed if some operations are can't be
+      // aborted or the UI can't be cleared.
+      if (!this.#reset()) {
+        event.preventDefault();
+      }
+    });
+
+    this.#modal.addEventListener("cancel", event => {
+      // Don't allow the dialog to be canceled via the ESC key if some
+      // operations are in progress and can't be aborted or the UI can't be
+      // cleared.
+      if (!this.#reset()) {
+        event.preventDefault();
+        return;
+      }
+
+      if (
+        !MailServices.accounts.accounts.length &&
+        !Services.prefs.getBoolPref("app.use_without_mail_account", false)
+      ) {
+        // Prevent closing the modal if no account is currently present and the
+        // user didn't request using Thunderbird without an email account.
+        event.preventDefault();
+      }
+    });
+  }
+
+  /**
+   * Check if we don't currently have the needed custom element for the
+   * requested view and load the needed script. We do this to avoid loading all
+   * the unnecessary account creation files.
+   *
+   * @param {string} view - The name of the view to load.
+   */
+  #loadScript(view) {
+    if (!customElements.get(`account-hub-${view}`)) {
+      Services.scriptloader.loadSubScript(
+        `chrome://messenger/content/accountcreation/views/${view}.js`,
+        window
+      );
+    }
+  }
+
+  /**
+   * Create a custom element and append it to the modal inner HTML, or simply
+   * show it if it was already loaded.
+   *
+   * @param {string} id - The ID of the template to clone.
+   */
+  #loadView(id) {
+    this.#hideViews();
+
+    let view = this.#modal.querySelector(id);
+    if (view) {
+      view.hidden = false;
+      this.#currentView = view;
+      // Update the UI to make sure we're refreshing old views.
+      this.#currentView.initUI();
+      return;
+    }
+
+    view = document.createElement(id);
+    this.#modal.appendChild(view);
+    this.#currentView = view;
+  }
+
+  /**
+   * Hide all the currently visible views.
+   */
+  #hideViews() {
+    for (let view of this.#modal.querySelectorAll(".account-hub-view")) {
+      view.hidden = true;
+    }
+  }
+
+  /**
+   * Open the main modal dialog and load the requested account setup view, or
+   * fallback to the initial start screen.
+   *
+   * @param {?string} type - Which account flow to load when the modal opens.
+   */
+  open(type = "START") {
+    // Interrupt if something went wrong while cleaning up a previously loaded
+    // view.
+    if (!this.#reset()) {
+      return;
+    }
+
+    this.#accounts[type].call();
+    if (!this.#modal.open) {
+      this.#modal.showModal();
+    }
+  }
+
+  /**
+   * Check if we have a current class and try to trigger the rest in order to
+   * handle abort operations and markup clean up, if possible.
+   *
+   * @returns {boolean} - True if the reset process was successful or we didn't
+   *   have anything to reset.
+   */
+  #reset() {
+    let isClean = this.#currentView?.reset() ?? true;
+    // If the reset operation was successful, clear the current class.
+    if (isClean) {
+      this.#hideViews();
+      this.#currentView = null;
+    }
+    return isClean;
+  }
+
+  /**
+   * Show the initial view of the account hub dialog.
+   */
+  #viewStart() {
+    this.#loadScript("start");
+    this.#loadView("account-hub-start");
+  }
+
+  /**
+   * TODO: Show the email setup view.
+   */
+  #viewEmailSetup() {
+    // Trigger the specific setup view we need to load and defer the handling
+    // of the setup flow to the dedicated module.
+  }
+}
+
+/**
+ * Open the account hub dialog and show the requested view.
+ *
+ * @param {?string} type - The type of view that should be loaded when the modal
+ *   is showed. See AccountHubController::#accounts for a list references.
+ */
+function openAccountHub(type) {
+  if (!AccountHubController) {
+    AccountHubController = new AccountHubControllerClass();
+  }
+  AccountHubController.open(type);
+}
---
+++ mail/locales/en-US/messenger/accountcreation/accountHub.ftl
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+## Header
+
+account-hub-brand = { -brand-full-name }
+
+account-hub-welcome-line = Welcome to <span data-l10n-name="brand-name">{ -brand-full-name }</span>
+
+account-hub-title = Account Hub
+
+## Footer
+
+account-hub-release-notes = Release notes
+
+account-hub-support = Support
+
+account-hub-donate = Donate
