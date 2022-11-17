288528382d00726ac698a9b5ae1e57b5c1d86057
Rob Lemley <rob@thunderbird.net>
2022-11-17 23:06:47 +0000

    Bug 1801150 - Set l10n locales-file to resolve based on release-type. r=dandarnell
    The locales list needs to be different for release/beta builds, but that isn't
    permitted by gecko_taskgraph. Some of the transforms for shippable-l10n need to
    run before the gecko_taskgraph transforms, others after. This is handled by
    creating two TransformSequence objects in a single file and arranging the kind
    configuration accordingly.
    The result is "locales-file" is now keyed by release-type.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162314
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/comm_taskgraph/transforms/l10n.py
+++ taskcluster/comm_taskgraph/transforms/l10n.py
@@ -6,14 +6,42 @@ Do transforms specific to l10n kind
 """
 
 from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.schema import resolve_keyed_by
+from gecko_taskgraph.util.attributes import (
+    task_name,
+)
 
-transforms = TransformSequence()
+transforms_pregecko = TransformSequence()
+transforms_postgecko = TransformSequence()
 
 
-@transforms.add
+@transforms_postgecko.add
 def update_dependencies(config, jobs):
     for job in jobs:
         job["dependencies"].update(
             {"shippable-l10n-pre": "shippable-l10n-pre-shippable-l10n-pre"}
+        )
+        yield job
+
+
+@transforms_pregecko.add
+def setup_name(config, jobs):
+    for job in jobs:
+        dep = job["primary-dependency"]
+        # Set the name to the same as the dep task, without kind name.
+        # Label will get set automatically with this kinds name.
+        job["name"] = job.get("name", task_name(dep))
+        yield job
+
+
+@transforms_pregecko.add
+def handle_keyed_by(config, jobs):
+    """Resolve fields that can be keyed by platform, etc."""
+    for job in jobs:
+        resolve_keyed_by(
+            job,
+            "locales-file",
+            item_name=job["name"],
+            **{"release-type": config.params["release_type"]},
         )
         yield job
