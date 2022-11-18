7328667ae845bef4ebe07394b2dfa90b32cd5984
Rob Lemley <rob@thunderbird.net>
2022-11-18 16:31:49 +0000

    Bug 1801226 - rocbuild.notify utility package. r=dandarnell
    Just a wrapper for Taskcluster email notifications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162417

---
+++ python/rocbuild/rocbuild/notify.py
@@ -0,0 +1,35 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this,
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Notification utility functions
+"""
+
+import os
+from taskcluster import Notify
+from taskcluster import optionsFromEnvironment
+
+
+TB_BUILD_ADDR = "tb-builds@thunderbird.net"
+
+
+def email_notification(subject, content, recipients=None):
+    # use proxy if configured, otherwise local credentials from env vars
+    if recipients is None:
+        recipients = [TB_BUILD_ADDR]
+
+    if "TASKCLUSTER_PROXY_URL" in os.environ:
+        notify_options = {"rootUrl": os.environ["TASKCLUSTER_PROXY_URL"]}
+    else:
+        notify_options = optionsFromEnvironment()
+
+    notify = Notify(notify_options)
+    for address in recipients:
+        notify.email(
+            {
+                "address": address,
+                "subject": subject,
+                "content": content,
+            }
+        )
