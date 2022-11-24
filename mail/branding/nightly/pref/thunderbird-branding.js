219c6396225f9c262cef4fe7766f5c31cefe2a05
Rob Lemley <rob@thunderbird.net>
2022-11-24 00:48:11 +0000

    Bug 1695529 - Disable start_page.override_url on Daily. r=mkmelin
    "override_url" is a bad name for this pref. It's opened in addition to the regular
    start page, but in a browser window. The URL it's set to just ends up back at the
    same start page after redirects and is sort of confusing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162924
    
    --HG--
    extra : amend_source : 9f9000eb20be322ee990b9a83f9794b044f96aa3

--- mail/branding/nightly/pref/thunderbird-branding.js
+++ mail/branding/nightly/pref/thunderbird-branding.js
@@ -6,7 +6,9 @@
 pref("mailnews.start_page.url", "https://live.thunderbird.net/%APP%/start?locale=%LOCALE%&version=%VERSION%&channel=%CHANNEL%&os=%OS%&buildid=%APPBUILDID%");
 
 // start page override to load after an update
-pref("mailnews.start_page.override_url", "https://live.thunderbird.net/%APP%/whatsnew?locale=%LOCALE%&version=%VERSION%&channel=%CHANNEL%&os=%OS%&buildid=%APPBUILDID%&oldversion=%OLD_VERSION%");
+// pref("mailnews.start_page.override_url", "https://live.thunderbird.net/%APP%/whatsnew?locale=%LOCALE%&version=%VERSION%&channel=%CHANNEL%&os=%OS%&buildid=%APPBUILDID%&oldversion=%OLD_VERSION%");
+// Leave blank per bug 1695529 until the website has a proper "Thunderbird Daily" landing page
+pref("mailnews.start_page.override_url", "");
 
 // There's no Thunderbird Daily specific page or release notes
 // URL user can browse to manually if for some reason all update installation
