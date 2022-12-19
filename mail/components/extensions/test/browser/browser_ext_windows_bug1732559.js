2cd3012cc6617ff4f8c52d9ca24e7a407217c2a7
John Bieling <john@thunderbird.net>
2022-12-19 21:32:14 +1100

    Bug 1797910 - Add test for bug 1732559. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D164903
    
    --HG--
    extra : amend_source : 2f4084a4d47b3dab0fcf3aaf9fd1fe3892254b6f

---
+++ mail/components/extensions/test/browser/browser_ext_windows_bug1732559.js
@@ -0,0 +1,81 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+add_task(async function check_focus() {
+  let extension = ExtensionTestUtils.loadExtension({
+    files: {
+      "background.js": async () => {
+        let win;
+
+        browser.runtime.onMessage.addListener(msg => {
+          return window.sendMessage(msg, win.id);
+        });
+
+        win = await browser.windows.create({
+          url: "focus.html",
+          type: "popup",
+          allowScriptsToClose: true,
+        });
+
+        await new Promise(resolve => {
+          browser.windows.onRemoved.addListener(id => {
+            if (id == win.id) {
+              resolve();
+            }
+          });
+        });
+
+        browser.test.notifyPass("finished");
+      },
+      "focus.html": `<!DOCTYPE html>
+      <html>
+        <head>
+          <script src="focus.js"></script>
+          <title>Focus Test</title>
+        </head>
+        <body>
+          <input id="email" type="text"/>
+          <input id="delay" type="number" min="0" max="10" size="2"/>
+        </body>
+      </html>`,
+      "focus.js": () => {
+        async function load() {
+          let email = document.getElementById("email");
+          email.focus();
+
+          await new Promise(r => window.setTimeout(r));
+          let [expectedString] = await browser.runtime.sendMessage(
+            "sendKeyStrokes"
+          );
+
+          // Fails as expected if focus is not set in
+          // https://searchfox.org/comm-central/rev/be2751632bd695d17732ff590a71acb9b1ef920c/mail/components/extensions/extensionPopup.js#126-130
+          await new Promise(r => window.setTimeout(r));
+          browser.test.assertEq(
+            expectedString,
+            email.value,
+            "Input field should have the correct value"
+          );
+
+          window.close();
+        }
+        document.addEventListener("DOMContentLoaded", load, { once: true });
+      },
+      "utils.js": await getUtilsJS(),
+    },
+    manifest: {
+      background: { scripts: ["utils.js", "background.js"] },
+    },
+  });
+
+  extension.onMessage("sendKeyStrokes", id => {
+    let window = Services.wm.getOuterWindowWithId(id);
+    EventUtils.sendString("happy typing", window);
+    extension.sendMessage("happy typing");
+  });
+
+  await extension.startup();
+  await extension.awaitFinish("finished");
+  await extension.unload();
+});
