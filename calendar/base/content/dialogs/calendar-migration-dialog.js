d0436e72020d4a866896d8f2f3df98144f78ef78
Magnus Melin <mkmelin+mozilla@iki.fi>
2023-01-12 21:25:41 +1100

    Bug 1703164 - convert calendar/base/content/dialogs/calendar-migration-dialog.xhtml to top level <html>. r=aleca
    To trigger
    mkdir -p .evolution/calendar/local
    cp foo.ics .evolution/calendar/local
    Starting with a new profile, the migration dialog will trigger.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165115
    
    --HG--
    extra : rebase_source : 6cdb732e4eae8485cbecdeffb2c48ff83167fad0
    extra : amend_source : 79e6c3901087645cd3d56e1c72666a9e752a7817

--- calendar/base/content/dialogs/calendar-migration-dialog.js
+++ calendar/base/content/dialogs/calendar-migration-dialog.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+window.addEventListener("DOMContentLoaded", event => {
+  gMigrateWizard.loadMigrators();
+});
+
 var gMigrateWizard = {
   /**
    * Called from onload of the migrator window.  Takes all of the migrators
