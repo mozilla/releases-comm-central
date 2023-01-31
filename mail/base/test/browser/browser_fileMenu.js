844e6fa61722a14b6b2f7ddaf47bb0b9c31c097f
Geoff Lankow <geoff@darktrojan.net>
2023-02-01 10:48:17 +1300

    Bug 1814322 - Test file menu item states. r=mkmelin I've created a framework for testing that menu items are visible/hidden and enabled/disabled as appropriate. In future it should be extended to cover more menus and scenarios.
    Differential Revision: https://phabricator.services.mozilla.com/D168633
    
    --HG--
    extra : rebase_source : 25fccb9d425afd471a19f9e462524b7aa2f4a715
    extra : amend_source : a4cfa5f7e185bd25758cc7cc9a64c93caf6b08bb
    extra : histedit_source : 3ec6ad220bf700e8d316614653e81e5d503ecc29

---
+++ mail/base/test/browser/browser_fileMenu.js
@@ -0,0 +1,137 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { MessageGenerator } = ChromeUtils.import(
+  "resource://testing-common/mailnews/MessageGenerator.jsm"
+);
+
+/** @type MenuData */
+const fileMenuData = {
+  menu_New: {},
+  menu_newNewMsgCmd: {},
+  "calendar-new-event-menuitem": { hidden: true },
+  "calendar-new-task-menuitem": { hidden: true },
+  menu_newFolder: { hidden: ["mailMessageTab", "contentTab"] },
+  menu_newVirtualFolder: { hidden: ["mailMessageTab", "contentTab"] },
+  newCreateEmailAccountMenuItem: {},
+  newMailAccountMenuItem: {},
+  newIMAccountMenuItem: {},
+  newFeedAccountMenuItem: {},
+  newNewsgroupAccountMenuItem: {},
+  "calendar-new-calendar-menuitem": {},
+  menu_newCard: {},
+  newIMContactMenuItem: { disabled: true },
+  menu_Open: {},
+  openMessageFileMenuitem: {},
+  "calendar-open-calendar-file-menuitem": {},
+  menu_close: {},
+  "calendar-save-menuitem": { hidden: true },
+  "calendar-save-and-close-menuitem": { hidden: true },
+  menu_saveAs: {},
+  menu_saveAsFile: { disabled: ["mail3PaneTab", "contentTab"] },
+  menu_saveAsTemplate: { disabled: ["mail3PaneTab", "contentTab"] },
+  menu_getAllNewMsg: {},
+  menu_getnewmsgs_all_accounts: { disabled: true },
+  menu_getnewmsgs_current_account: { disabled: true },
+  menu_getnextnmsg: { hidden: true },
+  menu_sendunsentmsgs: { disabled: true },
+  menu_subscribe: { disabled: true },
+  menu_deleteFolder: { disabled: true },
+  menu_renameFolder: { disabled: true },
+  menu_compactFolder: { disabled: true },
+  menu_emptyTrash: { disabled: true },
+  offlineMenuItem: {},
+  goOfflineMenuItem: {},
+  menu_synchronizeOffline: { disabled: ["mailMessageTab", "contentTab"] },
+  menu_settingsOffline: { disabled: true },
+  menu_downloadFlagged: { disabled: true },
+  menu_downloadSelected: { disabled: true },
+  printMenuItem: {},
+  menu_FileQuitItem: {},
+};
+let helper = new MenuTestHelper("menu_File", fileMenuData);
+
+let tabmail = document.getElementById("tabmail");
+let inboxFolder, plainFolder, rootFolder, testMessages, trashFolder;
+
+add_setup(async function() {
+  document.getElementById("toolbar-menubar").removeAttribute("autohide");
+
+  let generator = new MessageGenerator();
+
+  MailServices.accounts.createLocalMailAccount();
+  let account = MailServices.accounts.accounts[0];
+  account.addIdentity(MailServices.accounts.createIdentity());
+  rootFolder = account.incomingServer.rootFolder;
+
+  rootFolder.createSubfolder("inbox", null);
+  inboxFolder = rootFolder
+    .getChildNamed("inbox")
+    .QueryInterface(Ci.nsIMsgLocalMailFolder);
+  inboxFolder.setFlag(Ci.nsMsgFolderFlags.Inbox);
+  inboxFolder.addMessageBatch(
+    generator.makeMessages({ count: 5 }).map(message => message.toMboxString())
+  );
+  testMessages = [...inboxFolder.messages];
+
+  rootFolder.createSubfolder("plain", null);
+  plainFolder = rootFolder.getChildNamed("plain");
+
+  trashFolder = rootFolder.getFolderWithFlags(Ci.nsMsgFolderFlags.Trash);
+
+  window.OpenMessageInNewTab(testMessages[0], { background: true });
+  await BrowserTestUtils.waitForEvent(
+    tabmail.tabInfo[1].chromeBrowser,
+    "MsgLoaded"
+  );
+
+  window.openTab("contentTab", {
+    url: "https://example.com/",
+    background: true,
+  });
+
+  registerCleanupFunction(() => {
+    tabmail.closeOtherTabs(0);
+    MailServices.accounts.removeAccount(account, false);
+  });
+});
+
+add_task(async function test3PaneTab() {
+  tabmail.currentAbout3Pane.displayFolder(rootFolder);
+  await helper.testAllItems("mail3PaneTab");
+
+  tabmail.currentAbout3Pane.displayFolder(inboxFolder);
+  await helper.testItems({
+    menu_deleteFolder: { disabled: true },
+    menu_renameFolder: { disabled: true },
+    menu_compactFolder: { disabled: false },
+    menu_emptyTrash: { disabled: true },
+  });
+
+  tabmail.currentAbout3Pane.displayFolder(plainFolder);
+  await helper.testItems({
+    menu_deleteFolder: { disabled: false },
+    menu_renameFolder: { disabled: false },
+    menu_compactFolder: { disabled: false },
+    menu_emptyTrash: { disabled: true },
+  });
+
+  tabmail.currentAbout3Pane.displayFolder(trashFolder);
+  await helper.testItems({
+    menu_deleteFolder: { disabled: true },
+    menu_renameFolder: { disabled: true },
+    menu_compactFolder: { disabled: false },
+    menu_emptyTrash: { disabled: false },
+  });
+});
+
+add_task(async function testMessageTab() {
+  tabmail.switchToTab(1);
+  await helper.testAllItems("mailMessageTab");
+});
+
+add_task(async function testContentTab() {
+  tabmail.switchToTab(2);
+  await helper.testAllItems("contentTab");
+});
--- mail/base/test/browser/head.js
+++ mail/base/test/browser/head.js
@@ -37,6 +37,135 @@ async function openExtensionPopup(win, buttonId) {
   return { actionButton, panel, browser };
 }
 
+class MenuTestHelper {
+  /** @type {XULMenuElement} */
+  menu;
+
+  /**
+   * An object describing the state of a <menu> or <menuitem>.
+   *
+   * @typedef {Object} MenuItemData
+   * @property {boolean|string[]} [hidden] - true if the item should be hidden
+   *   in all modes, or a list of modes in which it should be hidden.
+   * @property {boolean|string[]} [disabled] - true if the item should be
+   *   disabled in all modes, or a list of modes in which it should be
+   *   disabled. If the item should be hidden this property is ignored.
+   */
+  /**
+   * An object describing the possible states of a menu's items. Object keys
+   * are the item's ID, values describe the item's state.
+   *
+   * @typedef {Object.<string, MenuItemData>} MenuData
+   */
+
+  /** @type {MenuData} */
+  baseData;
+
+  constructor(menuID, baseData) {
+    this.menu = document.getElementById(menuID);
+    this.baseData = baseData;
+  }
+
+  /**
+   * Check that an item matches the expected state.
+   *
+   * @param {XULElement} actual - A <menu> or <menuitem>.
+   * @param {MenuItemData} expected
+   */
+  checkItem(actual, expected) {
+    Assert.equal(
+      BrowserTestUtils.is_hidden(actual),
+      !!expected.hidden,
+      `${actual.id} hidden`
+    );
+    if (!expected.hidden) {
+      Assert.equal(
+        actual.disabled,
+        !!expected.disabled,
+        `${actual.id} disabled`
+      );
+    }
+  }
+
+  /**
+   * Checks every item in the menu and submenus against the expected states.
+   *
+   * @param {string} mode - The current mode, used to select the right expected
+   *   values from `baseData`.
+   */
+  async testAllItems(mode) {
+    let iterate = async popup => {
+      if (popup.state != "open") {
+        await BrowserTestUtils.waitForEvent(popup, "popupshown");
+      }
+
+      for (let item of popup.children) {
+        if (!item.id || item.localName == "menuseparator") {
+          continue;
+        }
+
+        Assert.ok(item.id in data, `${item.id} in data`);
+        let itemData = data[item.id];
+        this.checkItem(item, itemData);
+        delete data[item.id];
+
+        if (item.localName == "menu") {
+          item.openMenu(true);
+          await iterate(item.menupopup, data);
+        }
+      }
+
+      popup.hidePopup();
+    };
+
+    // Get the data for just this mode.
+    let data = {};
+    for (let [id, itemData] of Object.entries(this.baseData)) {
+      data[id] = {
+        hidden: itemData.hidden === true || itemData.hidden?.includes(mode),
+        disabled:
+          itemData.disabled === true || itemData.disabled?.includes(mode),
+      };
+    }
+
+    // Open the menu and all submenus and check the items.
+    EventUtils.synthesizeMouseAtCenter(this.menu, {});
+    await iterate(this.menu.menupopup, data);
+
+    // Report any unexpected items.
+    for (let id of Object.keys(data)) {
+      Assert.report(true, undefined, undefined, `extra item ${id} in data`);
+    }
+  }
+
+  /**
+   * Checks specific items in the menu.
+   * @note This function doesn't yet go into submenus.
+   *
+   * @param {MenuData} - The expected values to test against.
+   */
+  async testItems(data) {
+    let shownPromise = BrowserTestUtils.waitForEvent(
+      this.menu.menupopup,
+      "popupshown"
+    );
+    EventUtils.synthesizeMouseAtCenter(this.menu, {});
+    await shownPromise;
+
+    for (let [id, itemData] of Object.entries(data)) {
+      let item = document.getElementById(id);
+      this.checkItem(item, itemData);
+      delete data[item.id];
+    }
+
+    for (let id of Object.keys(data)) {
+      Assert.report(true, undefined, undefined, `extra item ${id} in data`);
+    }
+
+    this.menu.menupopup.hidePopup();
+  }
+}
+
 // Report and remove any remaining accounts/servers. If we register a cleanup
 // function here, it will run before any other cleanup function has had a
 // chance to run. Instead, when it runs register another cleanup function
