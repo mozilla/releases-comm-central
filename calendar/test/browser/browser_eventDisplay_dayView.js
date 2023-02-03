e9780910d094065e9babc9f78167c19090bc57ee
John Bieling <john@thunderbird.net>
2023-02-03 21:39:13 +1100

    Bug 1814639 - Split calendar/test/browser/browser_eventDisplay.js. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D168676
    
    --HG--
    rename : calendar/test/browser/browser_eventDisplay.js => calendar/test/browser/browser_eventDisplay_dayView.js
    rename : calendar/test/browser/browser_eventDisplay.js => calendar/test/browser/browser_eventDisplay_multiWeekView.js
    rename : calendar/test/browser/browser_eventDisplay.js => calendar/test/browser/browser_eventDisplay_weekView.js
    extra : amend_source : 8cc857a4f3c5d971f56e9d0c7ad4806a4002e690

---
+++ calendar/test/browser/browser_eventDisplay_dayView.js
@@ -0,0 +1,133 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  CalEvent: "resource:///modules/CalEvent.jsm",
+});
+
+var calendar = CalendarTestUtils.createCalendar();
+registerCleanupFunction(() => {
+  CalendarTestUtils.removeCalendar(calendar);
+});
+
+/**
+ * Create an event item in the calendar.
+ *
+ * @param {string} name - The name of the event.
+ * @param {string} start - The date time string for the start of the event.
+ * @param {string} end - The date time string for the end of the event.
+ *
+ * @returns {CalEvent} - The created event.
+ */
+async function createEvent(name, start, end) {
+  let event = new CalEvent();
+  event.title = name;
+  event.startDate = cal.createDateTime(start);
+  event.endDate = cal.createDateTime(end);
+  return calendar.addItem(event);
+}
+
+/**
+ * Assert that there is an event shown on the given date in the day-view.
+ *
+ * @param {object} date - The date to move to.
+ * @param {number} date.day - The day.
+ * @param {number} date.week - The week.
+ * @param {number} date.year - The year.
+ * @param {object} expect - Details about the expected event.
+ * @param {string} expect.name - The event name.
+ * @param {boolean} expect.startInView - Whether the event starts within the
+ *   view on the given date.
+ * @param {boolean} expect.endInView - Whether the event ends within the view
+ *   on the given date.
+ * @param {string} message - A message to use in assertions.
+ */
+async function assertDayEvent(date, expect, message) {
+  await CalendarTestUtils.goToDate(window, date.year, date.month, date.day);
+  let element = await CalendarTestUtils.dayView.waitForEventBoxAt(window, 1);
+  Assert.equal(
+    element.querySelector(".event-name-label").textContent,
+    expect.name,
+    `Event name should match: ${message}`
+  );
+  await CalendarTestUtils.assertEventBoxDraggable(
+    element,
+    expect.startInView,
+    expect.endInView,
+    message
+  );
+}
+
+/**
+ * Test an event that occurs within one day, in the day view.
+ */
+add_task(async function testInsideDayView() {
+  let event = await createEvent("Test Event", "20190403T123400", "20190403T234500");
+  await CalendarTestUtils.setCalendarView(window, "day");
+  Assert.equal(
+    document.querySelectorAll("#day-view calendar-event-column").length,
+    1,
+    "1 day column in the day view"
+  );
+
+  // This event is fully within this view.
+  await assertDayEvent(
+    { day: 3, month: 4, year: 2019 },
+    { name: "Test Event", startInView: true, endInView: true },
+    "Single day event"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
+
+/**
+ * Test an event that starts and ends at midnight, in the day view.
+ */
+add_task(async function testMidnightDayView() {
+  let event = await createEvent("Test Event", "20190403T000000", "20190404T000000");
+  await CalendarTestUtils.setCalendarView(window, "day");
+
+  // This event is fully within this view.
+  await assertDayEvent(
+    { day: 3, month: 4, year: 2019 },
+    { name: "Test Event", startInView: true, endInView: true },
+    "Single midnight event"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
+
+/**
+ * Test an event that spans multiple days, in the day view.
+ */
+add_task(async function testOutsideDayView() {
+  let event = await createEvent("Test Event", "20190402T123400", "20190404T234500");
+  await CalendarTestUtils.setCalendarView(window, "day");
+
+  // Go to the start of the event. The end of the event is beyond the current view.
+  await assertDayEvent(
+    { day: 2, month: 4, year: 2019 },
+    { name: "Test Event", startInView: true, endInView: false },
+    "First day"
+  );
+
+  // Go to the middle of the event. Both ends of the event are beyond the current view.
+  await assertDayEvent(
+    { day: 3, month: 4, year: 2019 },
+    { name: "Test Event", startInView: false, endInView: false },
+    "Middle day"
+  );
+
+  // Go to the end of the event. The start of the event is beyond the current view.
+  await assertDayEvent(
+    { day: 4, month: 4, year: 2019 },
+    { name: "Test Event", startInView: false, endInView: true },
+    "Last day"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
---
+++ calendar/test/browser/browser_eventDisplay_weekView.js
@@ -0,0 +1,151 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  CalEvent: "resource:///modules/CalEvent.jsm",
+});
+
+var calendar = CalendarTestUtils.createCalendar();
+registerCleanupFunction(() => {
+  CalendarTestUtils.removeCalendar(calendar);
+});
+
+/**
+ * Create an event item in the calendar.
+ *
+ * @param {string} name - The name of the event.
+ * @param {string} start - The date time string for the start of the event.
+ * @param {string} end - The date time string for the end of the event.
+ *
+ * @returns {CalEvent} - The created event.
+ */
+async function createEvent(name, start, end) {
+  let event = new CalEvent();
+  event.title = name;
+  event.startDate = cal.createDateTime(start);
+  event.endDate = cal.createDateTime(end);
+  return calendar.addItem(event);
+}
+
+/**
+ * Assert that there is a an event in the week-view between the expected range,
+ * and no events on the other days.
+ *
+ * @param {object} date - The date to move to.
+ * @param {number} date.day - The day.
+ * @param {number} date.week - The week.
+ * @param {number} date.year - The year.
+ * @param {object} expect - Details about the expected event.
+ * @param {string} expect.name - The event name.
+ * @param {number} expect.start - The day that the event should start in the
+ *   week. Between 1 and 7.
+ * @param {number} expect.end - The day that the event should end in the week.
+ * @param {boolean} expect.startInView - Whether the event starts within the
+ *   view on the given date.
+ * @param {boolean} expect.endInView - Whether the event ends within the view
+ *   on the given date.
+ * @param {string} message - A message to use in assertions.
+ */
+async function assertWeekEvents(date, expect, message) {
+  await CalendarTestUtils.goToDate(window, date.year, date.month, date.day);
+  // First test for expected events since these can take a short while to load,
+  // and we don't want to test for the absence of an event before they show.
+  for (let day = expect.start; day <= expect.end; day++) {
+    let element = await CalendarTestUtils.weekView.waitForEventBoxAt(window, day, 1);
+    Assert.equal(
+      element.querySelector(".event-name-label").textContent,
+      expect.name,
+      `Day ${day} event name should match: ${message}`
+    );
+    let icon = element.querySelector(".item-recurrence-icon");
+    Assert.equal(icon.src, "");
+    Assert.ok(icon.hidden);
+    await CalendarTestUtils.assertEventBoxDraggable(
+      element,
+      expect.startInView && day == expect.start,
+      expect.endInView && day == expect.end,
+      `Day ${day}: ${message}`
+    );
+  }
+  // Test no events loaded on the other days.
+  for (let day = 1; day <= 7; day++) {
+    if (day >= expect.start && day <= expect.end) {
+      continue;
+    }
+    Assert.equal(
+      CalendarTestUtils.weekView.getEventBoxes(window, day).length,
+      0,
+      `Should be no events on day ${day}: ${message}`
+    );
+  }
+}
+
+/**
+ * Test an event that occurs within one week, in the week view.
+ */
+add_task(async function testInsideWeekView() {
+  let event = await createEvent("Test Event", "20190101T123400", "20190103T234500");
+  await CalendarTestUtils.setCalendarView(window, "week");
+  Assert.equal(
+    document.querySelectorAll("#week-view calendar-event-column").length,
+    7,
+    "7 day columns in the week view"
+  );
+
+  await assertWeekEvents(
+    { day: 1, month: 1, year: 2019 },
+    { name: "Test Event", start: 3, end: 5, startInView: true, endInView: true },
+    "Single week event"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
+
+/**
+ * Test an event that starts and ends at midnight, in the week view.
+ */
+add_task(async function testMidnightWeekView() {
+  // Spans three days.
+  let event = await createEvent("Test Event", "20190101T000000", "20190104T000000");
+  await CalendarTestUtils.setCalendarView(window, "week");
+
+  // Midnight-to-midnight event only spans one day even though the end time
+  // matches the starting time of the next day (midnight).
+  await assertWeekEvents(
+    { day: 1, month: 1, year: 2019 },
+    { name: "Test Event", start: 3, end: 5, startInView: true, endInView: true },
+    "Midnight week event"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
+
+/**
+ * Test an event that spans multiple weeks, in the week view.
+ */
+add_task(async function testOutsideWeekView() {
+  let event = await createEvent("Test Event", "20190402T123400", "20190418T234500");
+  await CalendarTestUtils.setCalendarView(window, "week");
+
+  await assertWeekEvents(
+    { day: 3, month: 4, year: 2019 },
+    { name: "Test Event", start: 3, end: 7, startInView: true, endInView: false },
+    "First week"
+  );
+  await assertWeekEvents(
+    { day: 10, month: 4, year: 2019 },
+    { name: "Test Event", start: 1, end: 7, startInView: false, endInView: false },
+    "Middle week"
+  );
+  await assertWeekEvents(
+    { day: 17, month: 4, year: 2019 },
+    { name: "Test Event", start: 1, end: 5, startInView: false, endInView: true },
+    "Last week"
+  );
+
+  await CalendarTestUtils.closeCalendarTab(window);
+  await calendar.deleteItem(event);
+});
