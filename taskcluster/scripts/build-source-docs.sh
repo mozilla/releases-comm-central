11bb2ef27d98fbe01f229c91903bbbbdbed43d9d
Daniel Darnell <daniel@thunderbird.net>
2023-02-09 10:07:55 +1100

    Bug 1808350 - Add webhook task to Taskcluster to trigger rebuilds on Read the Docs. r=rjl
    Differential Revision: https://phabricator.services.mozilla.com/D169208
    
    --HG--
    extra : amend_source : 19b857194da3b49812631a72abac9cad8af0107e

---
+++ taskcluster/scripts/build-source-docs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -xe
+
+# duplicate the functionality of taskcluster-lib-urls, but in bash..
+queue_base="$TASKCLUSTER_PROXY_URL/api/queue/v1"
+
+# Get RTD secret location from task definition
+if [ -n "${TASK_ID}" ]; then
+  curl --location --retry 10 --retry-delay 10 -o /builds/worker/task.json "$queue_base/task/$TASK_ID"
+  RTD_SECRET=$(jq -r '.scopes[] | select(contains ("rtd-webhook"))' /builds/worker/task.json | awk -F: '{print $3}')
+fi
+
+# Get the secret value from the secrets service
+if [ -n "${RTD_SECRET}" ] && getent hosts taskcluster; then
+  set +x # Don't echo these
+  secrets_url="${TASKCLUSTER_PROXY_URL}/api/secrets/v1/secret/${RTD_SECRET}"
+  SECRET=$(curl "${secrets_url}")
+  TOKEN=$(echo "${SECRET}" | jq -r '.secret.token')
+elif [ -n "${RTD_TOKEN}" ]; then # Allow for local testing.
+  TOKEN="${RTD_TOKEN}"
+fi
+
+if [ -n "${TOKEN}" ]; then
+  curl \
+    -X POST \
+    -d "branches=latest" \
+    -d "token=${TOKEN}" \
+    https://readthedocs.org/api/v2/webhook/thunderbird-source-docs/222864/
+fi
+
