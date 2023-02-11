968cb2e6b5a5e4bd1e4073d1095ec39f0b500ae3
Geoff Lankow <geoff@darktrojan.net>
2023-02-12 12:27:49 +1300

    Bug 1816824 - Test Go menu item states. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D169875
    
    --HG--
    extra : rebase_source : 77b1f7327323691578c1f5998136f68ba9fe5c24
    extra : histedit_source : 97e8da0f407f2bced2ffe1c09e017068b5c27334

---
+++ mail/base/test/browser/browser_goMenu.js
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/** @type MenuData */
+const goMenuData = {
+  goNextMenu: {},
+  menu_nextMsg: { disabled: true },
+  menu_nextUnreadMsg: { disabled: true },
+  menu_nextFlaggedMsg: { disabled: true },
+  menu_nextUnreadThread: { disabled: true },
+  "calendar-go-menu-next": { hidden: true },
+  goPreviousMenu: {},
+  menu_prevMsg: { disabled: true },
+  menu_prevUnreadMsg: { disabled: true },
+  menu_prevFlaggedMsg: { disabled: true },
+  "calendar-go-menu-previous": { hidden: true },
+  menu_goForward: { disabled: true },
+  menu_goBack: { disabled: true },
+  "calendar-go-to-today-menuitem": { hidden: true },
+  menu_goChat: {},
+  goFolderMenu: { disabled: true },
+  goRecentlyClosedTabs: { disabled: true },
+  goStartPage: {},
+};
+let helper = new MenuTestHelper("menu_Go", goMenuData);
+
+add_setup(async function() {
+  document.getElementById("tabmail").clearRecentlyClosedTabs();
+  document.getElementById("toolbar-menubar").removeAttribute("autohide");
+});
+
+add_task(async function test3PaneTab() {
+  await helper.testAllItems("mail3PaneTab");
+});
