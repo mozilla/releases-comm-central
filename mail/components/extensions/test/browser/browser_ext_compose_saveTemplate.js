5f02763d472ed32b34cb8513b7159813f707333a
John Bieling <john@thunderbird.net>
2023-02-14 16:31:23 +0000

    Bug 1809654 - Request longer timeout for browser_ext_compose_saveTemplate.js. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D169830
    
    --HG--
    extra : amend_source : 8a759ad39de9fafbd45bbcf3a705ba5118ace3c1

--- mail/components/extensions/test/browser/browser_ext_compose_saveTemplate.js
+++ mail/components/extensions/test/browser/browser_ext_compose_saveTemplate.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
+requestLongerTimeout(4);
+
 var { ExtensionSupport } = ChromeUtils.import(
   "resource:///modules/ExtensionSupport.jsm"
 );
