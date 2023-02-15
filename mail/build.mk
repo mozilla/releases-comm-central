d7fdc4d6bc11129e23c9539eb91b56785a696dc6
Rob Lemley <rob@thunderbird.net>
2023-02-15 19:52:11 +0000

    Bug 1816266 - Remove use of $(moztopsrcdir) and friends from Makefiles. r=dandarnell
    $moztopsrcdir is always the same as $topsrcdir. This is a leftover from when
    it was posssible to build Thunderbird with comm-central as a parent of
    mozilla-central in the local filesystem. It's just confusing to have now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169564
    
    --HG--
    extra : moz-landing-system : lando

--- mail/build.mk
+++ mail/build.mk
@@ -2,36 +2,39 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Note that this file is "included" from $topsrcdir/Makefile.in, therefore
+# paths are relative to $topobjdir not the location of this file.
+
 package:
-	@$(MAKE) -C $(commtopobjdir)/mail/installer
+	@$(MAKE) -C comm/mail/installer
 
 package-compare:
-	@$(MAKE) -C $(commtopobjdir)/mail/installer package-compare
+	@$(MAKE) -C comm/mail/installer package-compare
 
 stage-package:
-	@$(MAKE) -C $(commtopobjdir)/mail/installer stage-package
+	@$(MAKE) -C comm/mail/installer stage-package
 
 install::
-	@$(MAKE) -C $(commtopobjdir)/mail/installer install
+	@$(MAKE) -C comm/mail/installer install
 
 source-package::
-	@$(MAKE) -C $(commtopobjdir)/mail/installer source-package
+	@$(MAKE) -C comm/mail/installer source-package
 
 upload::
-	@$(MAKE) -C $(commtopobjdir)/mail/installer upload
+	@$(MAKE) -C comm/mail/installer upload
 
 source-upload::
-	@$(MAKE) -C $(commtopobjdir)/mail/installer source-upload
+	@$(MAKE) -C comm/mail/installer source-upload
 
 hg-bundle::
-	@$(MAKE) -C $(commtopobjdir)/mail/installer hg-bundle
+	@$(MAKE) -C comm/mail/installer hg-bundle
 
 wget-en-US:
-	$(MAKE) -C $(commtopobjdir)/mail/locales wget-en-US
+	$(MAKE) -C comm/mail/locales wget-en-US
 
 merge-% post-merge-% installers-% langpack-% chrome-%:
-	$(MAKE) -C $(commtopobjdir)/mail/locales $@
+	$(MAKE) -C comm/mail/locales $@
 
 ifdef ENABLE_TESTS
-include $(commtopsrcdir)/mail/testsuite-targets.mk
+include $(topsrcdir)/comm/mail/testsuite-targets.mk
 endif
