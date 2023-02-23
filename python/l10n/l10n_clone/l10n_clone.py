5d15e1b92e4ffa09bddb979f94405ca9bad619c2
Rob Lemley <rob@thunderbird.net>
2023-02-23 04:32:18 +0000

    Bug 1818416 - Use revisions in l10n-changesets.json for locally building installers and langpacks. r=dandarnell
    Differential Revision: https://phabricator.services.mozilla.com/D170716
    
    --HG--
    extra : amend_source : d47a121537b11d975cd91bdf18c7065b0f56b2da

--- python/l10n/l10n_clone/l10n_clone.py
+++ python/l10n/l10n_clone/l10n_clone.py
@@ -7,6 +7,7 @@ use by mach build installers-$AB_CD and mach build langpack-$AB_CD.
 """
 
 import argparse
+import json
 import os
 import sys
 import tempfile
@@ -17,6 +18,7 @@ from mozpack.files import FileFinder
 from mozversioncontrol.repoupdate import update_mercurial_repo
 
 COMM_PATH = (Path(__file__).parent / "../../..").resolve()
+GECKO_PATH = COMM_PATH.parent
 COMM_PYTHON_L10N = os.path.join(COMM_PATH, "python/l10n")
 sys.path.insert(1, COMM_PYTHON_L10N)
 
@@ -36,16 +38,36 @@ def tb_locale(locale):
     raise argparse.ArgumentTypeError("Locale {} invalid.".format(locale))
 
 
+def get_revision(project, locale):
+    json_file = {
+        "browser": GECKO_PATH / "browser/locales/l10n-changesets.json",
+        "mail": COMM_PATH / "mail/locales/l10n-changesets.json",
+    }.get(project)
+    if json_file is None:
+        raise Exception(f"Invalid project {project} for l10n-changesets.json!")
+
+    with open(json_file) as fp:
+        changesets = json.load(fp)
+
+    revision = changesets.get(locale, {}).get("revision")
+    if revision is None:
+        raise Exception(f"Locale {locale} not found in {project} l10n-changesets.json!")
+
+    return revision
+
+
 def get_strings_repos(locale, destination):
     with tempfile.TemporaryDirectory() as tmproot:
         central_url = "{}/{}".format(L10N_CENTRAL, locale)
         l10n_central = Path(tmproot) / "l10n-central"
         l10n_central.mkdir()
         central_path = l10n_central / locale
-        update_mercurial_repo("hg", central_url, central_path)
+        central_revision = get_revision("browser", locale)
+        update_mercurial_repo("hg", central_url, central_path, revision=central_revision)
 
         comm_l10n = Path(tmproot) / "comm-l10n"
-        update_mercurial_repo("hg", COMM_L10N, comm_l10n)
+        comm_revision = get_revision("mail", locale)
+        update_mercurial_repo("hg", COMM_L10N, comm_l10n, revision=comm_revision)
 
         file_copier = FileCopier()
 
@@ -53,7 +75,6 @@ def get_strings_repos(locale, destination):
             finder = FileFinder(base_path)
             for pattern in patterns:
                 for _filepath, _fileobj in finder.find(pattern.format(lang=locale)):
-                    # _filepath = os.path.join("l10n-central", _filepath)
                     file_copier.add(_filepath, _fileobj)
 
         add_to_registry(l10n_central, GECKO_STRINGS_PATTERNS)
