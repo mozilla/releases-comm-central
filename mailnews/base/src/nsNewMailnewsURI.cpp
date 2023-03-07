4309055ca0bb60cd99e72c50dcd630dd8a92a463
Magnus Melin <mkmelin+mozilla@iki.fi>
2023-03-08 08:54:33 +1100

    Bug 1813422 - Fix crash in [@ nsXPCWrappedJS::AddRef] - Don't call nsMailboxService::NewURI on worker thread. r=kaie
    Differential Revision: https://phabricator.services.mozilla.com/D168518
    
    --HG--
    extra : rebase_source : 74d36b8b3f35dbf128836c3b12f24a9bd561d075

--- mailnews/base/src/nsNewMailnewsURI.cpp
+++ mailnews/base/src/nsNewMailnewsURI.cpp
@@ -42,16 +42,25 @@ nsresult NS_NewMailnewsURI(nsIURI** aURI, const nsACString& aSpec,
     if (NS_FAILED(rv)) return rv;
   }
 
+  // Creating IMAP/mailbox URIs off the main thread can lead to crashes.
+  // Seems to happen when viewing PDFs.
   if (scheme.EqualsLiteral("mailbox") ||
       scheme.EqualsLiteral("mailbox-message")) {
-    return nsMailboxService::NewURI(aSpec, aCharset, aBaseURI, aURI);
+    if (NS_IsMainThread()) {
+      return nsMailboxService::NewURI(aSpec, aCharset, aBaseURI, aURI);
+    }
+    auto NewURI = [&aSpec, &aCharset, &aBaseURI, aURI, &rv ]() -> auto{
+      rv = nsMailboxService::NewURI(aSpec, aCharset, aBaseURI, aURI);
+    };
+    nsCOMPtr<nsIRunnable> task = NS_NewRunnableFunction("NewURI", NewURI);
+    mozilla::SyncRunnable::DispatchToThread(
+        mozilla::GetMainThreadSerialEventTarget(), task);
+    return rv;
   }
   if (scheme.EqualsLiteral("imap") || scheme.EqualsLiteral("imap-message")) {
     if (NS_IsMainThread()) {
       return nsImapService::NewURI(aSpec, aCharset, aBaseURI, aURI);
     }
-    // Creating IMAP URIs off the main thread can lead to crashes.
-    // Seems to happen when viewing PDFs.
     auto NewURI = [&aSpec, &aCharset, &aBaseURI, aURI, &rv ]() -> auto{
       rv = nsImapService::NewURI(aSpec, aCharset, aBaseURI, aURI);
     };
