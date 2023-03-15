8309ae57ae1191eb5901fd8089e4f06ba75dbbbf
Kai Engert <kaie@kuix.de>
2023-03-15 16:50:10 +1100

    Bug 1822421 - Use nsDependentCString to avoid unnecessary allocation. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D172621
    
    --HG--
    extra : amend_source : efa48cc8d8ae350ffa9abcf9b7ec9c314e13c78a

--- mailnews/mime/src/mimehdrs.cpp
+++ mailnews/mime/src/mimehdrs.cpp
@@ -445,8 +445,7 @@ char* MimeHeaders_get_parameter(const char* header_value, const char* parm_name,
   if (NS_FAILED(rv)) return nullptr;
 
   nsCString result;
-  nsCString headerValue;
-  headerValue.Assign(header_value);
+  nsDependentCString headerValue(header_value);
   rv = mimehdrpar->GetParameterInternal(headerValue, parm_name, charset,
                                         language, getter_Copies(result));
   return NS_SUCCEEDED(rv) ? PL_strdup(result.get()) : nullptr;
