e2af923df6bcfd333ae7313c57a4b2e09b160284
Rob Lemley <rob@thunderbird.net>
2023-03-23 13:24:15 -0400

    No bug - Archive completed Fluent migration scripts. rs=me DONTBUILD
    --HG--
    rename : python/l10n/tb_fluent_migrations/bug_1815489_add_back_forward_and_stop.py => python/l10n/tb_fluent_migrations/completed/bug_1815489_add_back_forward_and_stop.py
    rename : python/l10n/tb_fluent_migrations/bug_1816532_about_dialog_migration.py => python/l10n/tb_fluent_migrations/completed/bug_1816532_about_dialog_migration.py
    rename : python/l10n/tb_fluent_migrations/bug_1823033_activity_indicator.py => python/l10n/tb_fluent_migrations/completed/bug_1823033_activity_indicator.py
    extra : rebase_source : fc79e44a0bf6167fde28aacf63ba34c7c7de2b2d
    extra : amend_source : 096a785f4b1b5e177b170ac046baa46a76850d3c

--- python/l10n/tb_fluent_migrations/completed/bug_1823033_activity_indicator.py
+++ python/l10n/tb_fluent_migrations/completed/bug_1823033_activity_indicator.py
@@ -1,8 +1,8 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-from fluent.migrate import COPY
-from fluent.migrate.helpers import transforms_from
+from fluent.migratetb import COPY
+from fluent.migratetb.helpers import transforms_from
 
 
 def migrate(ctx):
