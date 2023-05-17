ed76fe74fe37df2285e93a95d76a28a9bf900966
Rob Lemley <rob@thunderbird.net>
2023-05-17 09:41:44 -0400

    Bug 1817915 - Follow-up: Fix error in string migration script. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D178314
    
    --HG--
    extra : rebase_source : 2af5c312b0f89ee02b48f692dbe6a1f78ab8aad0
    extra : histedit_source : e05ac60579feb2a22364127c962f28fe9885d608

--- python/l10n/tb_fluent_migrations/bug_1817915_get_new_messages.py
+++ python/l10n/tb_fluent_migrations/bug_1817915_get_new_messages.py
@@ -1,15 +1,15 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-from fluent.migrate.helpers import transforms_from
-from fluent.migrate import COPY
+from fluent.migratetb.helpers import transforms_from
+from fluent.migratetb import COPY
 
 
 def migrate(ctx):
     """Bug 1817915 - Add context menu to Get Messages folder pane button to fetch all messages or per account, part {index}."""
 
     ctx.add_transforms(
-        "mail/chrome/messenger/messenger.dtd",
+        "mail/messenger/about3Pane.ftl",
         "mail/messenger/about3Pane.ftl",
         transforms_from(
             """
