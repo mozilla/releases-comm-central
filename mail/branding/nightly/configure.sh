c3a01c092ed415e592c3b7c347ee66ecdee69c12
Rob Lemley <rob@thunderbird.net>
2023-05-23 23:27:52 +0000

    Bug 1834502 - Port bug 1821963: Remove old accessibility configure variables. r=dandarnell
    Port of D177963.
    
    Depends on D178801
    
    Differential Revision: https://phabricator.services.mozilla.com/D178802
    
    --HG--
    extra : moz-landing-system : lando

--- mail/branding/nightly/configure.sh
+++ mail/branding/nightly/configure.sh
@@ -4,18 +4,3 @@
 
 MOZ_APP_DISPLAYNAME="Thunderbird Daily"
 MOZ_MACBUNDLE_ID="thunderbird-daily"
-
-# Only shippable builds from comm-central will set the update channel to "nightly"
-if test "$MOZ_UPDATE_CHANNEL" = "nightly"; then
-  # Official nightly (shippable) builds
-  MOZ_HANDLER_CLSID="e5643070-8ec1-4978-8e6c-f0f1753e329e"
-  MOZ_IHANDLERCONTROL_IID="d1324f09-3446-404e-ae12-a3128ba8580e"
-  MOZ_ASYNCIHANDLERCONTROL_IID="15f84f27-8a4c-4bdf-ae0a-c0fea8fefe91"
-  MOZ_IGECKOBACKCHANNEL_IID="6ab50aef-8ad3-460f-9235-e4b72e7e2e7e"
-elif test "$DEVELOPER_OPTIONS"; then
-  # Local non-debug builds
-  MOZ_HANDLER_CLSID="54b33546-ee72-4ab7-8710-200c4ec74742"
-  MOZ_IHANDLERCONTROL_IID="6ee6f1c5-d331-4fa2-8e95-b9680a0f0264"
-  MOZ_ASYNCIHANDLERCONTROL_IID="3da29f8e-d8ee-4d60-80f8-74665b7f271c"
-  MOZ_IGECKOBACKCHANNEL_IID="0195ae7b-1315-442f-a0b8-d94372d51917"
-fi
--- mail/branding/thunderbird/configure.sh
+++ mail/branding/thunderbird/configure.sh
@@ -3,18 +3,3 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME=Thunderbird
-
-# Only shippable builds will set the update channels to "beta" and "release"
-if test "$MOZ_UPDATE_CHANNEL" = "beta"; then
-  # Official beta builds
-  MOZ_IHANDLERCONTROL_IID="5a6e864b-5c25-49b3-9e78-0d4cad17f866"
-  MOZ_ASYNCIHANDLERCONTROL_IID="5da761f6-9aca-4f2f-b22a-062ee46265b4"
-  MOZ_HANDLER_CLSID="fd54d983-72e8-48d7-96a0-5c23a47f772c"
-  MOZ_IGECKOBACKCHANNEL_IID="e14a4035-43b9-4751-8ad8-c3e74c088882"
-else
-  # Official release builds
-  MOZ_IHANDLERCONTROL_IID="127c0620-d3a7-45f1-8478-13d58249d68f"
-  MOZ_ASYNCIHANDLERCONTROL_IID="fe46dfa3-16b6-4780-93cc-4f6c174dc10d"
-  MOZ_HANDLER_CLSID="6e5ac413-1ef1-4985-87c2-f02e86e3ecdb"
-  MOZ_IGECKOBACKCHANNEL_IID="0a8c4e6c-903f-41a9-b5d0-3520db8f936b"
-fi
