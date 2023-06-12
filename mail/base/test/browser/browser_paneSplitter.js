cd58a7af47fabca0a4b30cc3855886fd377cfd81
Geoff Lankow <geoff@darktrojan.net>
2023-06-12 10:05:36 +0000

    Bug 1808638 - Increase the time allowed for browser_paneSplitter.js to run. r=john.bieling
    Differential Revision: https://phabricator.services.mozilla.com/D180623
    
    --HG--
    extra : moz-landing-system : lando

--- mail/base/test/browser/browser_paneSplitter.js
+++ mail/base/test/browser/browser_paneSplitter.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This test frequently takes longer than the allowed time.
+requestLongerTimeout(2);
+
 let tabmail = document.getElementById("tabmail");
 registerCleanupFunction(() => {
   tabmail.closeOtherTabs(tabmail.tabInfo[0]);
