3bfe3225c0f6a9b616cbc5ec2bc3b9a5c238afc8
Rob Lemley <rob@thunderbird.net>
2023-07-11 10:35:12 -0400

    No bug - Archive completed Fluent migration. rs=me DONTBUILD
    --HG--
    rename : python/l10n/tb_fluent_migrations/bug_1827199_multi_message_view.py => python/l10n/tb_fluent_migrations/completed/bug_1827199_multi_message_view.py

--- python/l10n/tb_fluent_migrations/completed/bug_1827199_multi_message_view.py
+++ python/l10n/tb_fluent_migrations/completed/bug_1827199_multi_message_view.py
@@ -8,26 +8,26 @@ from fluent.migratetb.helpers import transforms_from
 def migrate(ctx):
     """Bug 1827199 - Message summary preview header buttons are not keyboard accessible"""
 
-    target = reference = "mail/locales/en-US/messenger/multimessageview.ftl"
+    target = reference = "mail/messenger/multimessageview.ftl"
     ctx.add_transforms(
         target,
         reference,
         transforms_from(
             """
 multi-message-window-title =
-    .title = {{COPY_PATTERN(from_path, "window.title")}}
+    .title = {{COPY(from_path, "window.title")}}
 
 selected-messages-label =
-    .label = {{COPY_PATTERN(from_path, "selectedmessages.label")}}
+    .label = {{COPY(from_path, "selectedmessages.label")}}
 
 multi-message-archive-button =
-    .label = {{COPY_PATTERN(from_path, "archiveButton.label")}}
-    .tooltiptext = {{COPY_PATTERN(from_path, "archiveButton.label")}}
+    .label = {{COPY(from_path, "archiveButton.label")}}
+    .tooltiptext = {{COPY(from_path, "archiveButton.label")}}
 
 multi-message-delete-button =
-    .label = {{COPY_PATTERN(from_path, "deleteButton.label")}}
-    .tooltiptext = {{COPY_PATTERN(from_path, "deleteButton.label")}}
+    .label = {{COPY(from_path, "deleteButton.label")}}
+    .tooltiptext = {{COPY(from_path, "deleteButton.label")}}
             """,
-            from_path="mail/locales/en-US/chrome/messenger/multimessageview.dtd",
+            from_path="mail/chrome/messenger/multimessageview.dtd",
         ),
     )
