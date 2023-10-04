0122e366c4e791af58b3cbaed62189a98db08366
Martin Giger <martin@humanoids.be>
2023-10-04 16:17:52 +0000

    Bug 1852030 - Fix OTR verification dialog. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D189945
    
    --HG--
    extra : moz-landing-system : lando

--- chat/content/otr-auth.js
+++ chat/content/otr-auth.js
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { IMServices } = ChromeUtils.importESModule(
-  "resource:///modules/IMServices.sys.mjs"
-);
 const { XPCOMUtils } = ChromeUtils.importESModule(
   "resource://gre/modules/XPCOMUtils.sys.mjs"
 );
