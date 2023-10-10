03925b5882657168fed9ead44bb9b235dbcdcf7d
Magnus Melin <mkmelin+mozilla@iki.fi>
2023-10-10 06:38:04 +0000

    Bug 1817769 - increase timeout even more for browser_timezones.js. r=kaie
    It's a very long test (too long, and should ideally be split up).
    
    Differential Revision: https://phabricator.services.mozilla.com/D190533
    
    --HG--
    extra : amend_source : 2aedefe7c1a1aee3c97460562f12fdb646d03688

--- calendar/test/browser/timezones/browser_timezones.js
+++ calendar/test/browser/timezones/browser_timezones.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-requestLongerTimeout(3);
+requestLongerTimeout(4);
 
 var { findEventsInNode } = ChromeUtils.import(
   "resource://testing-common/calendar/CalendarUtils.jsm"
