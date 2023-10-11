8e9ea92136060f3f6a7213954fa5cc5a1915eb13
Kai Engert <kaie@kuix.de>
2023-10-11 12:12:31 +0200

    Bug 1857831 - Backed out changeset 5707262c1484, requested by darktrojan.

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -1420,6 +1420,3 @@ pref("mailnews.imap.jsmodule", false);
 // 2: icons only
 // 3: text only
 pref("toolbar.unifiedtoolbar.buttonstyle", 0);
-
-// Temporary fix for Mozmill on Linux. See bug 1857831.
-pref("toolkit.lazyHiddenWindow", false);
