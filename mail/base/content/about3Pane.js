76a254bc68e3d2e738b1b4746771f0c9e6cf935a
welpy-cw <h.w.forms@arcor.de>
2023-10-23 23:50:39 +0000

    Bug 1841256 - Reduce indentation of subjects in Grouped By views. r=Paenglab
    Differential Revision: https://phabricator.services.mozilla.com/D191584
    
    --HG--
    extra : moz-landing-system : lando

--- mail/base/content/about3Pane.js
+++ mail/base/content/about3Pane.js
@@ -6072,7 +6072,10 @@ customElements.whenDefined("tree-view-table-row").then(() => {
           const div = cell.querySelector(".subject-line");
 
           // Indent child message of this thread.
-          div.style.setProperty("--thread-level", threadLevel.value);
+          div.style.setProperty(
+            "--thread-level",
+            gViewWrapper.showGroupedBySort ? 0 : threadLevel.value
+          );
 
           let imageFluentID = this.#getMessageIndicatorString(propertiesSet);
           const image = div.querySelector("img");
