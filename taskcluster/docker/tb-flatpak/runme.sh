e1584f0b5ec6648a6da7565d48eb8d61051dc55d
Daniel Darnell <daniel@thunderbird.net>
2023-11-21 18:48:12 +0000

    Bug 1850043 - Reorganize, clarify, and clean-up runme.sh. r=rjl
    Depends on D193747
    
    Differential Revision: https://phabricator.services.mozilla.com/D193748
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/docker/tb-flatpak/runme.sh
+++ taskcluster/docker/tb-flatpak/runme.sh
@@ -17,35 +17,50 @@ test "$RELEASE_NOTES_URL"
 : WORKSPACE                     "${WORKSPACE:=/home/worker/workspace}"
 : ARTIFACTS_DIR                 "${ARTIFACTS_DIR:=/home/worker/artifacts}"
 
+# Populate remaining environment variables
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET_TAR_XZ_FULL_PATH="$ARTIFACTS_DIR/target.flatpak.tar.xz"
+SOURCE_DEST="${WORKSPACE}/source"
+DISTRIBUTION_DIR="$SOURCE_DEST/distribution"
+FREEDESKTOP_VERSION="22.08"
+FIREFOX_BASEAPP_CHANNEL="22.08"
+
+# Create alias for ideal curl command
+CURL="curl --location --retry 10 --retry-delay 10"
+
+# Get current date
+#
 # This is used to populate the datetime in org.mozilla.Thunderbird.appdata.xml
 DATE=$(date +%Y-%m-%d)
 export DATE
 
-SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TARGET_TAR_XZ_FULL_PATH="$ARTIFACTS_DIR/target.flatpak.tar.xz"
-SOURCE_DEST="${WORKSPACE}/source"
-FREEDESKTOP_VERSION="22.08"
-FIREFOX_BASEAPP_CHANNEL="22.08"
-
-
-# XXX: these commands are temporarily, there's an upcoming fix in the upstream Docker image
-# that we work on top of, from `freedesktopsdk`, that will make these two lines go away eventually
+# Prepare directories
+#
+# This command is temporary, there's an upcoming fix in the upstream
+# Docker image that we work on top of, from 'freedesktopsdk', that will
+# make these two lines go away eventually.
 mkdir -p /root /tmp /var/tmp
 mkdir -p "$ARTIFACTS_DIR"
 rm -rf "$SOURCE_DEST" && mkdir -p "$SOURCE_DEST"
 
-# XXX ensure we have a clean slate in the local flatpak repo
+# Ensure a clean slate in the local Flatpak repo
 rm -rf ~/.local/share/flatpak/
 
-CURL="curl --location --retry 10 --retry-delay 10"
-
-# Download en-US linux64 binary
+# Download en-US linux64 (English, 64-bit Linux) Thunderbird binary
 $CURL -o "${WORKSPACE}/thunderbird.tar.bz2" \
     "${CANDIDATES_DIR}/${VERSION}-candidates/build${BUILD_NUMBER}/linux-x86_64/en-US/thunderbird-${VERSION}.tar.bz2"
 
-# Use list of locales to fetch L10N XPIs
+# Fetch list of Thunderbird locales
 $CURL -o "${WORKSPACE}/l10n-changesets.json" "$L10N_CHANGESETS"
 locales=$(python3 "$SCRIPT_DIRECTORY/extract_locales_from_l10n_json.py" "${WORKSPACE}/l10n-changesets.json")
+
+# Fetch langpack extension for each locale
+mkdir -p "$DISTRIBUTION_DIR"
+mkdir -p "$DISTRIBUTION_DIR/extensions"
+for locale in $locales; do
+    $CURL -o "$DISTRIBUTION_DIR/extensions/langpack-${locale}@thunderbird.mozilla.org.xpi" \
+        "$CANDIDATES_DIR/${VERSION}-candidates/build${BUILD_NUMBER}/linux-x86_64/xpi/${locale}.xpi"
+done
 
 # Download artifacts from dependencies and build the .desktop file.
 (
@@ -58,29 +73,25 @@ python3 /scripts/build-desktop-file.py -o "$WORKSPACE/org.mozilla.Thunderbird.de
   -f "mail/messenger/flatpak.ftl"
 )
 
-DISTRIBUTION_DIR="$SOURCE_DEST/distribution"
-mkdir -p "$DISTRIBUTION_DIR"
-
-mkdir -p "$DISTRIBUTION_DIR/extensions"
-for locale in $locales; do
-    $CURL -o "$DISTRIBUTION_DIR/extensions/langpack-${locale}@thunderbird.mozilla.org.xpi" \
-        "$CANDIDATES_DIR/${VERSION}-candidates/build${BUILD_NUMBER}/linux-x86_64/xpi/${locale}.xpi"
-done
-
+# Generate AppData XML from template, add various 
 envsubst < "$SCRIPT_DIRECTORY/org.mozilla.Thunderbird.appdata.xml.in" > "${WORKSPACE}/org.mozilla.Thunderbird.appdata.xml"
 cp -v "$SCRIPT_DIRECTORY/distribution.ini" "$WORKSPACE"
 cp -v "$SCRIPT_DIRECTORY/launch-script.sh" "$WORKSPACE"
 cd "${WORKSPACE}"
 
+# Fetch and install Firefox base app (as user, not system-wide)
 flatpak remote-add --user --if-not-exists --from flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-# XXX: added --user to `flatpak install` to avoid ambiguity
 flatpak install --user -y flathub org.mozilla.firefox.BaseApp//${FIREFOX_BASEAPP_CHANNEL} --no-deps
 
-# XXX: this command is temporarily, there's an upcoming fix in the upstream Docker image
-# that we work on top of, from `freedesktopsdk`, that will make these two lines go away eventually
+# Create build directory and add Firefox base app files
+#
+# This command is temporary, there's an upcoming fix in the upstream
+# Docker image that we work on top of, from 'freedesktopsdk', that will
+# make these two lines go away eventually.
 mkdir -p build
 cp -r ~/.local/share/flatpak/app/org.mozilla.firefox.BaseApp/current/active/files build/files
 
+# Create Flatpak build metadata file for Thunderbird
 ARCH=$(flatpak --default-arch)
 cat <<EOF > build/metadata
 [Application]
@@ -94,6 +105,7 @@ autodelete=true
 locale-subset=true
 EOF
 
+# Create Flatpak build metadata file for locales
 cat <<EOF > build/metadata.locale
 [Runtime]
 name=org.mozilla.Thunderbird.Locale
@@ -102,6 +114,7 @@ name=org.mozilla.Thunderbird.Locale
 ref=app/org.mozilla.Thunderbird/${ARCH}/${FLATPAK_BRANCH}
 EOF
 
+# Install Thunderbird files into appdir
 appdir=build/files
 install -d "${appdir}/lib/"
 (cd "${appdir}/lib/" && tar jxf "${WORKSPACE}/thunderbird.tar.bz2")
@@ -111,15 +124,20 @@ for size in 16 32 48 64 128; do
     install -D -m644 "${appdir}/lib/thunderbird/chrome/icons/default/default${size}.png" "${appdir}/share/icons/hicolor/${size}x${size}/apps/org.mozilla.Thunderbird.png"
 done
 
+# Generate AppStream metadata and add screenshots from Flathub
 appstream-compose --prefix="${appdir}" --origin=flatpak --basename=org.mozilla.Thunderbird org.mozilla.Thunderbird
 appstream-util mirror-screenshots "${appdir}"/share/app-info/xmls/org.mozilla.Thunderbird.xml.gz "https://dl.flathub.org/repo/screenshots/org.mozilla.Thunderbird-${FLATPAK_BRANCH}" build/screenshots "build/screenshots/org.mozilla.Thunderbird-${FLATPAK_BRANCH}"
 
-# XXX: we used to `install -D` before which automatically created the components
-# of target, now we need to manually do this since we're symlinking
+# Install locales, distribution, and launch-script.sh into appdir
+#
+# We must install each locale individually, since we're symlinking
+# each one.
+#
+# We put the langpacks in /app/share/locale/$LANG_CODE and symlink that
+# directory to where Thunderbird looks them up; this way only the subset
+# of locales configured on the user's system are downloaded, instead of
+# all locales.
 mkdir -p "${appdir}/lib/thunderbird/distribution/extensions"
-# XXX: we put the langpacks in /app/share/locale/$LANG_CODE and symlink that
-# directory to where Thunderbird looks them up; this way only subset configured
-# on user system is downloaded vs all locales
 for locale in $locales; do
     install -D -m644 -t "${appdir}/share/runtime/langpack/${locale%%-*}/" "${DISTRIBUTION_DIR}/extensions/langpack-${locale}@thunderbird.mozilla.org.xpi"
     ln -sf "/app/share/runtime/langpack/${locale%%-*}/langpack-${locale}@thunderbird.mozilla.org.xpi" "${appdir}/lib/thunderbird/distribution/extensions/langpack-${locale}@thunderbird.mozilla.org.xpi"
@@ -127,9 +145,16 @@ done
 install -D -m644 -t "${appdir}/lib/thunderbird/distribution" distribution.ini
 install -D -m755 launch-script.sh "${appdir}/bin/thunderbird"
 
+# Build Flatpak
+#
 # We use features=devel to enable ptrace, which we need for the crash
 # reporter.  The application is still confined in a pid namespace, so
 # that won't let us escape the flatpak sandbox.  See bug 1653852.
+#
+# We use own-name to ensure Thunderbird has access to DBus, as app ID
+# (org.mozilla.Thunderbird) does not match bus names
+# (org.mozilla.thunderbird, lowercase "t"). The app ID may be updated
+# in the future to match the default bus names.
 flatpak build-finish build                                      \
         --allow=devel                                           \
         --share=ipc                                             \
@@ -154,14 +179,20 @@ flatpak build-finish build                                      \
         --system-talk-name=org.freedesktop.NetworkManager       \
         --command=thunderbird
 
+# Export Flatpak build into repo
 flatpak build-export --disable-sandbox --no-update-summary --exclude='/share/runtime/langpack/*/*' repo build "$FLATPAK_BRANCH"
 flatpak build-export --disable-sandbox --no-update-summary --metadata=metadata.locale --files=files/share/runtime/langpack repo build "$FLATPAK_BRANCH"
+
+# Commit screenshots to repo
 ostree commit --repo=repo --canonical-permissions --branch=screenshots/x86_64 build/screenshots
 flatpak build-update-repo --generate-static-deltas repo
-tar cvfJ flatpak.tar.xz repo
 
+# Package Flatpak repo as tar
+tar cvfJ flatpak.tar.xz repo
 mv -- flatpak.tar.xz "$TARGET_TAR_XZ_FULL_PATH"
 
+# Build Flatpak bundle (.flatpak) from repo
 flatpak build-bundle "$WORKSPACE"/repo org.mozilla.Thunderbird.flatpak org.mozilla.Thunderbird "$FLATPAK_BRANCH"
 
+# Move bundle to artifacts
 mv org.mozilla.Thunderbird.flatpak "$ARTIFACTS_DIR/"
