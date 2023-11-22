a2dab0ec3728996265697f99cfa91441d25bdc47
Daniel Darnell <daniel@thunderbird.net>
2023-11-22 02:14:22 +0000

    Bug 1863619 - Add comm-release to Thunderbird cross-channel config. r=rjl
    Differential Revision: https://phabricator.services.mozilla.com/D193746
    
    --HG--
    extra : amend_source : a10563ce8774883fcbe544b5ebcbef69b07715e0

--- python/l10n/tbxchannel/__init__.py
+++ python/l10n/tbxchannel/__init__.py
@@ -27,7 +27,7 @@ def get_thunderbird_xc_config(topsrcdir, strings_path):
         "source": {
             "comm-central": {
                 "path": topsrcdir / "comm",
-                "url": "https://hg.mozilla.org/comm-central/",
+                "url": "https://hg.mozilla.org/comm-unified/",
                 "heads": {
                     # This list of repositories is ordered, starting with the
                     # one with the most recent content (central) to the oldest
@@ -35,6 +35,7 @@ def get_thunderbird_xc_config(topsrcdir, strings_path):
                     # ESR goes last (e.g. esr102 goes after esr115).
                     "comm": "comm-central",
                     "comm-beta": "releases/comm-beta",
+                    "comm-release": "releases/comm-release",
                     "comm-esr115": "releases/comm-esr115",
                 },
                 "config_files": [
