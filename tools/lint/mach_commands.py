9e2790ddf3b67754c40befd9679392284323abe0
Rob Lemley <rob@thunderbird.net>
2023-12-12 11:16:29 -0500

    Bug 1869405 - mach commlint should not use unconfigured linters. r=dandarnell
    This change will ensure that `mach commlint` only presents linters with config
    files in /comm/tools/lint as valid. There is no fallback to using configs in
    /tools/lint.
    
    Originally mach commlint was set up to use linter config files from /tools/lint
    (mozilla-central) as a fallback when a linter had no file in /comm/tools/lint.
    
    Thunderbird's needs have evolved since, and using linter configs from gecko cause
    more problems than useful feedback, mostly due to missing path excludes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196197
    
    --HG--
    extra : amend_source : 57490f4ed2869df09ee834586cde6d5a3aa7419d
    extra : histedit_source : 5244b076909ee856b240a88fac334df6d2f5a7bd

--- tools/lint/mach_commands.py
+++ tools/lint/mach_commands.py
@@ -2,7 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import copy
+import os
+import sys
+
+from mozfile import load_source
+
 from mach.decorators import Command
+from mozbuild.base import BuildEnvironmentNotFoundException
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 def setup_argument_parser():
@@ -16,9 +25,44 @@ def setup_argument_parser():
     category="thunderbird",
     description="Run linters with Thunderbird configurations.",
     parser=setup_argument_parser,
+    virtualenv_name="lint",
 )
-def commlint(command_context, paths, extra_args=[], **kwargs):
-    kwargs["config_paths"].insert(0, "comm/tools/lint")
-    return command_context._mach_context.commands.dispatch(
-        "lint", command_context._mach_context, paths=paths, argv=extra_args, **kwargs
+def lint(command_context, *runargs, **lintargs):
+    """Run linters."""
+    command_context.activate_virtualenv()
+
+    mach_lint = load_source(
+        "mach_lint", os.path.join(command_context.topsrcdir, "tools/lint/mach_commands.py")
     )
+
+    from mozlint import cli, parser
+
+    try:
+        buildargs = {}
+        buildargs["substs"] = copy.deepcopy(dict(command_context.substs))
+        buildargs["defines"] = copy.deepcopy(dict(command_context.defines))
+        buildargs["topobjdir"] = command_context.topobjdir
+        lintargs.update(buildargs)
+    except BuildEnvironmentNotFoundException:
+        pass
+
+    lintargs.setdefault("root", command_context.topsrcdir)
+    if lintargs["extra_args"] is None:
+        lintargs["extra_args"] = []
+
+    lintargs["exclude"] = mach_lint.get_global_excludes(**lintargs)
+
+    # Add the linter code from gecko to sys.path. Normally handled by "config_paths"
+    sys.path.insert(0, mach_lint.here)
+    lintargs["config_paths"].insert(0, HERE)
+
+    lintargs["virtualenv_bin_path"] = command_context.virtualenv_manager.bin_path
+    lintargs["virtualenv_manager"] = command_context.virtualenv_manager
+    if mach_lint.REPORT_WARNINGS and lintargs.get("show_warnings") is None:
+        lintargs["show_warnings"] = "soft"
+    for path in mach_lint.EXCLUSION_FILES:
+        parser.GLOBAL_SUPPORT_FILES.append(os.path.join(command_context.topsrcdir, path))
+    setupargs = {
+        "mach_command_context": command_context,
+    }
+    return cli.run(*runargs, setupargs=setupargs, **lintargs)
