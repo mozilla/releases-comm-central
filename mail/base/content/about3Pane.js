f9fd9417e3fa87d29010b4b63de82aaa4dbf85e8
welpy-cw <h.w.forms@arcor.de>
2023-12-19 22:23:02 +0000

    Bug 1868816 - Fix reversing sort order of Grouped By view in quick search. r=freaktechnik
    Reversing the sort order of a Grouped By view requires special arrangements for single-folder
    search views, that were only met for saved searches. This patch considers quick search views as
    well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195796
    
    --HG--
    extra : moz-landing-system : lando

--- mail/base/content/about3Pane.js
+++ mail/base/content/about3Pane.js
@@ -6864,7 +6864,7 @@ var sortController = {
       }
 
       if (gViewWrapper.isSingleFolder) {
-        if (gViewWrapper.isVirtual) {
+        if (gViewWrapper.isVirtual || gViewWrapper.search.hasSearchTerms) {
           gViewWrapper.showGroupedBySort = false;
         } else {
           // Must ensure rows are collapsed and kExpandAll is unset.
@@ -6881,7 +6881,10 @@ var sortController = {
 
     // Restore Grouped By state post sort direction change.
     if (grouped) {
-      if (gViewWrapper.isVirtual && gViewWrapper.isSingleFolder) {
+      if (
+        gViewWrapper.isSingleFolder &&
+        (gViewWrapper.isVirtual || gViewWrapper.search.hasSearchTerms)
+      ) {
         this.groupBySort();
       }
       // Restore Grouped By selection post sort direction change.
