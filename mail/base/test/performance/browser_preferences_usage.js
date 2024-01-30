6904135ab1be4ab4da1e4982fccbd54d7ea64be8
Magnus Melin <mkmelin+mozilla@iki.fi>
2024-01-30 09:24:50 +0200

    Bug 1877379 - Port bug 1876811 - change 'dom.send_after_paint_to_content' to a static pref. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D199957
    
    --HG--
    extra : rebase_source : c65e821e415927731ee607d1507aec89b455ba89

--- mail/base/test/performance/browser_preferences_usage.js
+++ mail/base/test/performance/browser_preferences_usage.js
@@ -124,7 +124,6 @@ add_task(async function startup() {
   // These preferences are used in PresContext or layout areas and all have a
   // similar number of errors - probably being loaded in the same component.
   const prefsUsedInLayout = [
-    "dom.send_after_paint_to_content",
     "image.animation_mode",
     "layout.reflow.dumpframebyframecounts",
     "layout.reflow.dumpframecounts",
