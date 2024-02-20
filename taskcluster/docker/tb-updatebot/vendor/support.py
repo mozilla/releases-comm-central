7f7afd29b48db6ed6b773fa542b9c73c0d00f261
Rob Lemley <rob@thunderbird.net>
2024-02-20 15:27:19 -0500

    Bug 1879159 - [tb-rust ci] Check for open Phabricator revisions and abandon if needed. r=dandarnell
    Differential Revision: https://phabricator.services.mozilla.com/D201837
    
    --HG--
    extra : amend_source : cd5d922aa05ebbcb9a6ef6cc5f53a430736872f4

--- taskcluster/docker/tb-updatebot/vendor/support.py
+++ taskcluster/docker/tb-updatebot/vendor/support.py
@@ -6,20 +6,29 @@
 Support functions for comm-central third party code management
 """
 
+import argparse
 import json
 import os
+import shutil
 import stat
 import subprocess
 from pathlib import Path
+from typing import Union
 
 import requests
+from mozphab.conduit import ConduitAPI
+from mozphab.detect_repository import repo_from_args
 
 import taskcluster
 
 SECRET_URL_BASE = "http://taskcluster/secrets/v1/secret/"
 NOTIFY_URL_BASE = "http://taskcluster/api/notify/v1/matrix"
+INDEX_URL_BASE = "http://taskcluster/api/index/v1/task"
 
 TB_SHERIFF_MATRIX_ID = "!TWztIhgqLawNpRBZTC:mozilla.org"
+
+MOZ_PHAB = shutil.which("moz-phab")
+assert MOZ_PHAB is not None
 
 
 def log(*args):
@@ -131,3 +140,41 @@ def fetch_indexed_artifact(previous_task_id: str, artifact_path: str) -> str or 
             return None
         raise
     return response.text
+
+
+class ExtendedConduit(ConduitAPI):
+    def __init__(self, repo_root: Union[Path, None] = None) -> None:
+        super().__init__()
+        if repo_root is None:
+            return
+        repo = repo_from_args(argparse.Namespace(path=str(repo_root), safe_mode=True))
+        self.set_repo(repo)
+
+    def is_revision_open(self, phab_rev_id: str) -> bool:
+        rev_id = int(phab_rev_id[1:])
+        revisions = self.get_revisions([rev_id])
+        if revisions:
+            return not revisions[0]["fields"]["status"]["closed"]
+
+    def abandon_revision(self, phab_rev_id: str):
+        transactions = [
+            {"type": "abandon", "value": True},
+        ]
+        self.apply_transactions_to_revision(phab_rev_id, transactions)
+
+    def submit(self):
+        return run_cmd([MOZ_PHAB, "submit", "-s", "--no-lint"])
+
+
+class DevConduit:
+    def __init__(self, repo_root: Path) -> None:
+        self.repo = str(repo_root)
+
+    def is_revision_open(self, phab_rev_id: str) -> bool:
+        return False
+
+    def abandon_revision(self, phab_rev_id: str):
+        return
+
+    def submit(self):
+        return subprocess.CompletedProcess([], 0, stdout="output /D12345")
