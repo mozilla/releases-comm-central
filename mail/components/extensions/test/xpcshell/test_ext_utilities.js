0fbfb8328584c7d0934d394967a157510da8f737
John Bieling <john@thunderbird.net>
2024-03-15 11:43:07 +1300

    Bug 1606575 - Follow-up to fix locale issue with formatFileSize. r=benc
    Differential Revision: https://phabricator.services.mozilla.com/D204707
    
    --HG--
    extra : amend_source : c47394a3edb0c0c93021704130bfffbdf376f918

--- mail/components/extensions/test/xpcshell/test_ext_utilities.js
+++ mail/components/extensions/test/xpcshell/test_ext_utilities.js
@@ -14,14 +14,18 @@ add_task(async function test_formatFileSize() {
       "background.js": async () => {
         const tests = [
           { sizeInBytes: 12, expectedFormat: "12 bytes" },
-          { sizeInBytes: 2454, expectedFormat: "2,4 KB" },
+          { sizeInBytes: 2454, expectedFormat: "2.4 KB" },
           { sizeInBytes: 312312, expectedFormat: "305 KB" },
-          { sizeInBytes: 12312331, expectedFormat: "11,7 MB" },
-          { sizeInBytes: 2344234234, expectedFormat: "2,2 GB" },
+          { sizeInBytes: 12312331, expectedFormat: "11.7 MB" },
+          { sizeInBytes: 2344234234, expectedFormat: "2.2 GB" },
         ];
         for (const { sizeInBytes, expectedFormat } of tests) {
+          const formated = await browser.messengerUtilities.formatFileSize(
+            sizeInBytes
+          );
+          // On some systems we get a , seperator.
           browser.test.assertEq(
-            await browser.messengerUtilities.formatFileSize(sizeInBytes),
+            formated.replaceAll(",", "."),
             expectedFormat,
             `Formated file size for ${sizeInBytes} bytes should show correctly`
           );
