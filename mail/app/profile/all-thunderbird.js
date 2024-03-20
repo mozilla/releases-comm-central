700aa48c638e9d88b395dfb12b88b3faa88dc942
Geoff Lankow <geoff@darktrojan.net>
2024-03-20 12:18:13 +0200

    Bug 1886239 - Remove obsolete prompting code. r=leftmostcat
    The pref removal is a port of bug 1705440. Everything else is dead code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205141
    
    --HG--
    extra : amend_source : 2c201c8ffbba571c54b3ffac250c321fa056efd6

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -1326,7 +1326,6 @@ pref("mail.minimizeToTray", false);
 #endif
 
 pref("prompts.defaultModalType", 3);
-pref("prompts.contentPromptSubDialog", false);
 
 // The URL for the privacy policy related to recommended extensions.
 pref("extensions.recommendations.privacyPolicyUrl", "https://www.mozilla.org/en-US/privacy/thunderbird/#addons");
