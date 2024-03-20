de72b627d66c4cdc0acd0b3398c614d415f15c46
Rob Lemley <rob@thunderbird.net>
2024-03-20 21:48:57 +0000

    Bug 1886196 - Remove the "skip-suite" optimization strategies. r=dandarnell
    These are not really needed anymore because comm_taskgraph.get_decision_parameters()
    checks to see if a push is only "suite/" code and forces a DONTBUILD in those cases.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205103
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/comm_taskgraph/optimize.py
+++ taskcluster/comm_taskgraph/optimize.py
@@ -7,8 +7,7 @@ Thunderbird specific taskgraph optimizers.
 
 import logging
 
-from taskgraph.optimize.base import Any, OptimizationStrategy, register_strategy
-from taskgraph.util.memoize import memoize
+from taskgraph.optimize.base import OptimizationStrategy, register_strategy
 from taskgraph.util.path import join as join_path
 from taskgraph.util.path import match as match_path
 from taskgraph.util.yaml import load_yaml
@@ -18,71 +17,7 @@ from gecko_taskgraph import GECKO
 from gecko_taskgraph.optimize.schema import default_optimizations
 from mozlint.pathutils import filterpaths
 
-from comm_taskgraph import files_changed
-
 logger = logging.getLogger(__name__)
-
-
-def is_excluded(check_path, file_patterns):
-    for pattern in file_patterns:
-        if match_path(check_path, pattern):
-            return True
-    return False
-
-
-def is_suite(check_path):
-    suite_patterns = ("editor", "suite")
-    return is_excluded(check_path, suite_patterns)
-
-
-@memoize
-def get_non_suite_changed_files(repository, revision):
-    """
-    Returns the list of changed files from comm- repository (no prefixing)
-    with suite/** and editor/** files removed.
-    """
-    return {
-        file
-        for file in files_changed.get_changed_files(repository, revision)
-        if not is_suite(file)
-    }
-
-
-@register_strategy("comm-skip-unless-changed")
-class SkipUnlessChanged(OptimizationStrategy):
-    def should_remove_task(self, task, params, file_patterns):
-        # pushlog_id == -1 - this is the case when run from a cron.yml job
-        if params.get("pushlog_id") == -1:
-            return False
-
-        changed = files_changed.check(params, file_patterns)
-        if not changed:
-            logger.debug(
-                "no files found matching a pattern in `skip-unless-changed` for " + task.label
-            )
-            return True
-        return False
-
-
-@register_strategy("skip-suite-only")
-class SkipSuiteOnly(OptimizationStrategy):
-    def should_remove_task(self, task, params, arg):
-        # pushlog_id == -1 - this is the case when run from a cron.yml job
-        if params.get("pushlog_id") == -1:
-            return False
-
-        if params.get("project") == "try-comm-central":
-            # Do not try to use this optimization on try-c-c builds
-            return False
-
-        repository = params.get("comm_head_repository")
-        revision = params.get("comm_head_rev")
-        non_suite_changed_files = get_non_suite_changed_files(repository, revision)
-        # non_suite_changed_files will be an empty set (Falsy) for suite-only pushes
-        # so "skip" this task
-        if not non_suite_changed_files:
-            return True
-        return False
 
 
 @register_strategy("skip-unless-mozlint")
@@ -133,18 +68,7 @@ class SkipUnlessMozlint(OptimizationStrategy):
         return False
 
 
-register_strategy(
-    "skip-unless-backstop-no-suite", args=("skip-unless-backstop", "skip-suite-only")
-)(Any)
-
-register_strategy(
-    "skip-unless-changed-no-suite", args=("comm-skip-unless-changed", "skip-suite-only")
-)(Any)
-
 optimizations = (
-    {"skip-suite-only": None},
-    {"skip-unless-backstop-no-suite": None},
-    {"skip-unless-changed-no-suite": [str]},
     {"skip-unless-mozlint": SkipUnlessMozlint.schema},
 )
 
