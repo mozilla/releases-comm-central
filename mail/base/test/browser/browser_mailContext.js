ac8d70b86872d5cb9fa604fd61288af39a401241
Magnus Melin <mkmelin+mozilla@iki.fi>
2024-04-23 12:06:47 +0200

    Bug 1860255 - Try to fix intermittents: increase timeouts for browser_mailContext.js. r=vineet
    Almost all are CCov builds timing out. Looking at the logs it seems the tests many times just is slow
    to get started there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208170
    
    --HG--
    extra : amend_source : ab33d7a98b5a6018916e2b99790c7ef2c794bb6e

--- mail/base/test/browser/browser_mailContext.js
+++ mail/base/test/browser/browser_mailContext.js
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-requestLongerTimeout(2);
+requestLongerTimeout(
+  AppConstants.MOZ_CODE_COVERAGE || AppConstants.DEBUG ? 4 : 3
+);
 
 /**
  * Tests that items on the mail context menu are correctly shown in context.
