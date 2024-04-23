9eee1dc9386561ac322d64adaa25e9cd55960ea1
Rob Lemley <rob@thunderbird.net>
2024-04-23 22:16:43 +0000

    Bug 1885550 - Port bug 1885361: Refactor for SkipUnlessMozlint moving to gecko_taskgraph. r=dandarnell
    SkipUnlessMozlint is now in gecko_taskgraph and received a minor facelift so
    that the code can be used for Firefox CI as well as Thunderbird.
    
    The strategy needs to be registered in comm_taskgraph.optimize to set the
    the path to the mozlint config files. It does not need to be listed in
    `optimizations` as that will be brought in from gecko_taskgraph.
    
    This reduces the configuration needed in the job itself, all that's needed is
    the mozlint yaml filename.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204768
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/comm_taskgraph/optimize.py
+++ taskcluster/comm_taskgraph/optimize.py
@@ -7,67 +7,22 @@ Thunderbird specific taskgraph optimizers.
 
 import logging
 
-from taskgraph.optimize.base import OptimizationStrategy, register_strategy
-from taskgraph.util.path import join as join_path
-from taskgraph.util.path import match as match_path
-from taskgraph.util.yaml import load_yaml
-from voluptuous import Optional, Required
+from taskgraph.optimize.base import register_strategy, registry
 
-from gecko_taskgraph import GECKO
+from gecko_taskgraph.optimize.mozlint import SkipUnlessMozlint
 from gecko_taskgraph.optimize.schema import default_optimizations
-from mozlint.pathutils import filterpaths
 
 logger = logging.getLogger(__name__)
 
+# Register "skip-unless-mozlint" with the correct mozlint path.
+del registry["skip-unless-mozlint"]
+register_strategy("skip-unless-mozlint", args=("comm/tools/lint",))(SkipUnlessMozlint)
 
-@register_strategy("skip-unless-mozlint")
-class SkipUnlessMozlint(OptimizationStrategy):
-    schema = {
-        "root-path": Optional(str),
-        "mozlint-config": Required(str),
-    }
+# Currently no Thunderbird-specific optimization strategies. Keep for future
+# reference.
+# optimizations = (
+#    {"skip-suite-only": None},
+# )
 
-    def should_remove_task(self, task, params, args):
-        include = []
-        exclude = []
-        extensions = []
-        support_files = []
-
-        root_path = join_path(GECKO, args.get("root-path", ""))
-        mozlint_root = join_path(root_path, "tools", "lint")
-        mozlint_yaml = join_path(mozlint_root, args["mozlint-config"])
-
-        logger.info(f"Loading file patterns for {task.label} from {mozlint_yaml}.")
-        linter_config = load_yaml(mozlint_yaml)
-        for check, config in linter_config.items():
-            include += config.get("include", [])
-            exclude += config.get("exclude", [])
-            extensions += [e.strip(".") for e in config.get("extensions", [])]
-            support_files += config.get("support-files", [])
-
-        changed_files = params["files_changed"]
-
-        # Support files may not be part of "include" patterns, so check first
-        # Do not remove (return False) if any changed
-        for pattern in support_files:
-            for path in changed_files:
-                if match_path(path, pattern):
-                    return False
-
-        to_lint, to_exclude = filterpaths(
-            GECKO,
-            list(changed_files),
-            include=include,
-            exclude=exclude,
-            extensions=extensions,
-        )
-
-        # to_lint should be an empty list if there is nothing to check
-        if not to_lint:
-            return True
-        return False
-
-
-optimizations = ({"skip-unless-mozlint": SkipUnlessMozlint.schema},)
-
+optimizations = ()
 thunderbird_optimizations = default_optimizations + optimizations
