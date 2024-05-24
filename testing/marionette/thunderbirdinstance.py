c4bae31832b23ef051a66678ec0e58ad5b193f91
Geoff Lankow <geoff@darktrojan.net>
2024-05-24 16:34:06 +1200

    Bug 1898400 - Reinstate Marionette tests on the CI. r=rjl
    Differential Revision: https://phabricator.services.mozilla.com/D211305
    
    --HG--
    extra : amend_source : caed4e90276a46154ee32ec763925f2227faa3dd

--- testing/marionette/thunderbirdinstance.py
+++ testing/marionette/thunderbirdinstance.py
@@ -2,14 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-# ALL CHANGES TO THIS FILE MUST HAVE REVIEW FROM A MARIONETTE PEER!
-#
-# The Marionette Python client is used out-of-tree with various builds of
-# Firefox. Removing a preference from this file will cause regressions,
-# so please be careful and get review from a Testing :: Marionette peer
-# before you make any changes to this file.
-
-
 thunderbird_prefs = {
     # Disable application updates
     "app.update.disabledForTesting": True,
@@ -18,6 +10,7 @@ thunderbird_prefs = {
     # Do not show the EULA notification which can interfer with tests
     "browser.EULA.override": True,
     # Do not start first-run items
+    "app.donation.eoy.version.viewed": 1000,
     "mail.provider.suppress_dialog_on_startup": True,
     "mail.spotlight.firstRunDone": True,
     "mail.winsearch.firstRunDone": True,
@@ -26,52 +19,4 @@ thunderbird_prefs = {
     "mailnews.start_page.url": "about:blank",
     # Do not check it is the default client at startup
     "mail.shell.checkDefaultClient": False,
-    # Set up a dummy account to unlock various actions
-    "mail.root.none-rel": "[ProfD]Mail",
-    "mail.root.pop3-rel": "[ProfD]Mail",
-    "mail.account.account1.server": "server1",
-    "mail.account.account2.identities": "id1,id2",
-    "mail.account.account2.server": "server2",
-    "mail.account.account3.server": "server3",
-    "mail.accountmanager.accounts": "account1,account2,account3",
-    "mail.accountmanager.defaultaccount": "account2",
-    "mail.accountmanager.localfoldersserver": "server1",
-    "mail.identity.id1.fullName": "Tinderbox",
-    "mail.identity.id1.htmlSigFormat": False,
-    "mail.identity.id1.htmlSigText": "Tinderbox is soo 90ies",
-    "mail.identity.id1.smtpServer": "smtp1",
-    "mail.identity.id1.useremail": "tinderbox@foo.invalid",
-    "mail.identity.id1.valid": True,
-    "mail.identity.id2.fullName": "Tinderboxpushlog",
-    "mail.identity.id2.htmlSigFormat": True,
-    "mail.identity.id2.htmlSigText": "Tinderboxpushlog is the new <b>hotness!</b>",
-    "mail.identity.id2.smtpServer": "smtp1",
-    "mail.identity.id2.useremail": "tinderboxpushlog@foo.invalid",
-    "mail.identity.id2.valid": True,
-    "mail.server.server1.directory-rel": "[ProfD]Mail/Local Folders",
-    "mail.server.server1.hostname": "Local Folders",
-    "mail.server.server1.name": "Local Folders",
-    "mail.server.server1.type": "none",
-    "mail.server.server1.userName": "nobody",
-    "mail.server.server2.check_new_mail": False,
-    "mail.server.server2.directory-rel": "[ProfD]Mail/tinderbox",
-    "mail.server.server2.download_on_biff": True,
-    "mail.server.server2.hostname": "tinderbox123",
-    "mail.server.server2.login_at_startup": False,
-    "mail.server.server2.name": "tinderbox@foo.invalid",
-    "mail.server.server2.type": "pop3",
-    "mail.server.server2.userName": "tinderbox",
-    "mail.server.server2.whiteListAbURI": "",
-    "mail.server.server3.hostname": "prpl-irc",
-    "mail.server.server3.imAccount": "account1",
-    "mail.server.server3.type": "im",
-    "mail.server.server3.userName": "mozmilltest@irc.mozilla.invalid",
-    "mail.smtp.defaultserver": "smtp1",
-    "mail.smtpserver.smtp1.hostname": "tinderbox123",
-    "mail.smtpserver.smtp1.username": "tinderbox",
-    "mail.smtpservers": "smtp1",
-    "messenger.account.account1.autoLogin": False,
-    "messenger.account.account1.firstConnectionState": 1,
-    "messenger.account.account1.name": "mozmilltest@irc.mozilla.invalid",
-    "messenger.account.account1.prpl": "prpl-irc",
 }
