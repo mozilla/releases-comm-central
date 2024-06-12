ffaa84d4288c32ccc79eb8ab4583b75cecf5b089
welpy-cw <h.w.forms@arcor.de>
2024-06-12 00:10:47 +0000

    Bug 1787143 - Fix opening 'imap:' and 'mailbox:' links. r=mkmelin
    Besides restoring the functionality itself, embedded messages with `Content-Type: message/rfc822` are opened directly in a new message tab.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211755
    
    --HG--
    extra : moz-landing-system : lando

--- mail/actors/MailLinkParent.sys.mjs
+++ mail/actors/MailLinkParent.sys.mjs
@@ -36,15 +36,21 @@ export class MailLinkParent extends JSWindowActorParent {
   }
 
   _handleMailboxLink({ data, target }) {
-    // AttachmentInfo is defined in msgHdrView.js.
     const url = new URL(data);
+    const filename = url.searchParams.get("filename");
+    // When a message is received with a 'news:' link embedded as a MIME part
+    // of the message, the internal 'cid:' link is converted to a mailbox link
+    // containing `filename=message.rfc822`. In the following function, an
+    // attachment with content-type "message/rfc822" is processed before the
+    // filename is evaluated (by its extension), so we explicitly set the
+    // content-type in this case.
     new lazy.AttachmentInfo({
-      contentType: "",
+      contentType: /\.rfc822$/.test(filename) ? "message/rfc822" : "",
       url: data,
-      name: url.searchParams.get("filename"),
+      name: filename,
       uri: "",
       isExternalAttachment: false,
-    }).open(target.browsingContext.topChromeWindow, target.browsingContext.id);
+    }).open(target.browsingContext);
   }
 
   _handleMailToLink({ data, target }) {
