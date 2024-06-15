bfd191594545464add50a100945cb3fedd90017a
Bill Gianopoulos <wgianopoulos@gmail.com>
2024-06-15 09:27:25 +0000

    Bug 1927299 - Move MOZ_APP_ID and MOZ_APP_VENDOR from old-configure to suite moz.configure. r=frg
    Ports:
    - Bug 1898060 "Move MOZ_APP_ID from old-configure to moz.configure".
    - Bug 1898177 "Move MOZ_APP_VENDOR from confvars.sh to moz.configure".

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_VENDOR=Mozilla
 MOZ_APP_NAME=seamonkey
 MOZ_APP_DISPLAYNAME=SeaMonkey
 
@@ -21,7 +20,6 @@ ACCEPTED_MAR_CHANNEL_IDS=seamonkey-comm-central
 # The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
 MAR_CHANNEL_ID=seamonkey-comm-central
 
-MOZ_APP_ID={92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}
 MOZ_PROFILE_MIGRATOR=1
 
 # Include the DevTools client, not just the server (which is the default)
