c45b9b74eb8f9c7d934c12f885e02ea4504c74b2
Martin Giger <martin@humanoids.be>
2024-10-08 17:11:38 +0000

    Bug 1922554 - Make QR export pane searchable. r=tobyp
    Differential Revision: https://phabricator.services.mozilla.com/D224489
    
    --HG--
    extra : moz-landing-system : lando

--- mail/components/preferences/qrExport.mjs
+++ mail/components/preferences/qrExport.mjs
@@ -19,6 +19,7 @@ export const qrExportPane = {
   init() {
     this.showIntro();
     this.addEventListeners();
+    delete document.getElementById("qrExportContent").dataset.hiddenFromSearch;
   },
 
   /**
