4742c2f1c38983da40a5b812a590b0ff5f05809e
Ian Neal <iann_cvs@blueyonder.co.uk>
2024-10-22 20:48:55 +0100

    Bug 1926406 - Tidy up chatzilla-service.js. r=frg f=tobin

--- suite/chatzilla/js/lib/chatzilla-service.js
+++ suite/chatzilla/js/lib/chatzilla-service.js
@@ -10,24 +10,19 @@ var { XPCOMUtils } = ChromeUtils.importESModule(
 
 const RDFS_CONTRACTID =
     "@mozilla.org/rdf/rdf-service;1";
-const PPMM_CONTRACTID =
-    "@mozilla.org/parentprocessmessagemanager;1";
-
 const CLINE_SERVICE_CID =
     Components.ID("{38a95514-1dd2-11b2-97e7-9da958640f2c}");
 const STARTUP_CID =
     Components.ID("{ae6ad015-433b-42ab-9afc-1636af5a7fc4}");
 
-
-var {
-  ChatZillaProtocols,
-  IRCProtocolHandlerFactory,
-  IRCSProtocolHandlerFactory,
-  IRCPROT_HANDLER_CID,
-  IRCSPROT_HANDLER_CID,
-} = ChromeUtils.import(
-  "chrome://chatzilla/content/lib/js/protocol-handlers.jsm"
-);
+const IRCPROT_HANDLER_CONTRACTID =
+    "@mozilla.org/network/protocol;1?name=irc";
+const IRCSPROT_HANDLER_CONTRACTID =
+    "@mozilla.org/network/protocol;1?name=ircs";
+const IRCPROT_HANDLER_CID =
+    Components.ID("{f21c35f4-1dd1-11b2-a503-9bf8a539ea39}");
+const IRCSPROT_HANDLER_CID =
+    Components.ID("{f21c35f4-1dd1-11b2-a503-9bf8a539ea3a}");
 
 function spawnChatZilla(uri, count)
 {
@@ -99,6 +94,8 @@ function CommandLineService()
 
 CommandLineService.prototype =
 {
+    classID: CLINE_SERVICE_CID,
+
     /* nsISupports */
     QueryInterface: XPCOMUtils.generateQI([Ci.nsICommandLineHandler]),
 
@@ -144,9 +141,10 @@ function ProcessHandler()
 
 ProcessHandler.prototype =
 {
+    classID: STARTUP_CID,
+
     /* nsISupports */
-    QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
-                                           Ci.nsIMessageListener]),
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
 
     /* nsIObserver */
     observe(subject, topic, data)
@@ -154,27 +152,15 @@ ProcessHandler.prototype =
         if (topic !== "profile-after-change")
             return;
 
-        var ppmm;
-        // Ci.nsIMessageBroadcaster went in Gecko 61.
-        if (Ci.nsIMessageBroadcaster)
-        {
-            ppmm = Cc[PPMM_CONTRACTID].getService(Ci.nsIMessageBroadcaster);
-        }
-        else
-        {
-            ppmm = Cc[PPMM_CONTRACTID].getService();
-        }
-        ppmm.loadProcessScript("chrome://chatzilla/content/lib/js/chatzilla-protocol-script.js", true);
-        ppmm.addMessageListener("ChatZilla:SpawnChatZilla", this);
-    },
-
-    /* nsIMessageListener */
-    receiveMessage(msg)
-    {
-        if (msg.name !== "ChatZilla:SpawnChatZilla")
-            return;
-
-        spawnChatZilla(msg.data.uri);
+        const compMgr = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+        compMgr.registerFactory(IRCPROT_HANDLER_CID,
+                                "IRC protocol handler",
+                                IRCPROT_HANDLER_CONTRACTID,
+                                IRCProtocolHandlerFactory);
+        compMgr.registerFactory(IRCSPROT_HANDLER_CID,
+                                "IRCS protocol handler",
+                                IRCSPROT_HANDLER_CONTRACTID,
+                                IRCSProtocolHandlerFactory);
     },
 };
 
@@ -194,6 +180,170 @@ const StartupFactory =
     },
 };
 
+
+const IRC_MIMETYPE = "application/x-irc";
+const IRCS_MIMETYPE = "application/x-ircs";
+
+//XXXgijs: Because necko is annoying and doesn't expose this error flag, we
+//         define our own constant for it. Throwing something else will show
+//         ugly errors instead of seeminly doing nothing.
+const NS_ERROR_MODULE_NETWORK_BASE = 0x804b0000;
+const NS_ERROR_NO_CONTENT = NS_ERROR_MODULE_NETWORK_BASE + 17;
+
+
+function GenericIRCProtocolHandler(isSecure)
+{
+    this.isSecure = isSecure;
+    this.scheme = isSecure ? "ircs" : "irc";
+    this.classID = isSecure ? IRCSPROT_HANDLER_CID : IRCPROT_HANDLER_CID;
+    this.defaultPort = isSecure ? 6697 : 6667;
+}
+
+GenericIRCProtocolHandler.prototype =
+{
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIProtocolHandler,
+                                           Ci.nsISupports]),
+
+    protocolFlags: Ci.nsIProtocolHandler.URI_NORELATIVE |
+                   Ci.nsIProtocolHandler.ALLOWS_PROXY |
+                   Ci.nsIProtocolHandler.URI_LOADABLE_BY_ANYONE |
+                   Ci.nsIProtocolHandler.URI_NON_PERSISTABLE |
+                   Ci.nsIProtocolHandler.URI_DOES_NOT_RETURN_DATA,
+
+    allowPort(port, scheme)
+    {
+        // Allow all ports to connect, so long as they are irc: or ircs:
+        return (scheme === 'irc' || scheme === 'ircs');
+    },
+
+    newURI(spec, charset, baseURI)
+    {
+        return Cc["@mozilla.org/network/standard-url-mutator;1"]
+                 .createInstance(Ci.nsIStandardURLMutator)
+                 .init(Ci.nsIStandardURL.URLTYPE_STANDARD, this.defaultPort,
+                       spec, charset, baseURI)
+                 .finalize()
+                 .QueryInterface(Ci.nsIStandardURL);
+    },
+
+    newChannel(URI)
+    {
+        if (!Services.io.allowPort(URI.port, URI.scheme))
+            throw Cr.NS_ERROR_FAILURE;
+
+        return new BogusChannel(URI, this.isSecure);
+    },
+};
+
+
+/* Bogus IRC channel used by the IRCProtocolHandler */
+function BogusChannel(URI, isSecure)
+{
+    this.URI = URI;
+    this.originalURI = URI;
+    this.isSecure = isSecure;
+    this.contentType = this.isSecure ? IRCS_MIMETYPE : IRC_MIMETYPE;
+}
+
+BogusChannel.prototype =
+{
+    /* nsISupports */
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIChannel, Ci.nsIRequest]),
+
+    /* nsIChannel */
+    loadAttributes: null,
+    contentLength: 0,
+    owner: null,
+    loadGroup: null,
+    notificationCallbacks: null,
+    securityInfo: null,
+
+    open(observer, context)
+    {
+        spawnChatZilla(this.URI.spec);
+        // We don't throw this (a number, not a real 'resultcode') because it
+        // upsets xpconnect if we do (error in the js console).
+        Components.returnCode = NS_ERROR_NO_CONTENT;
+    },
+
+    asyncOpen(observer, context)
+    {
+        spawnChatZilla(this.URI.spec);
+        // We don't throw this (a number, not a real 'resultcode') because it
+        // upsets xpconnect if we do (error in the js console).
+        Components.returnCode = NS_ERROR_NO_CONTENT;
+    },
+
+    asyncRead(listener, context)
+    {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+    },
+
+    /* nsIRequest */
+    isPending()
+    {
+        return true;
+    },
+
+    status: Cr.NS_OK,
+
+    cancel(status)
+    {
+        this.status = status;
+    },
+
+    suspend()
+    {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+    },
+
+    resume()
+    {
+        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+    },
+};
+
+
+function IRCProtocolHandler()
+{
+}
+
+IRCProtocolHandler.prototype = new GenericIRCProtocolHandler(false);
+
+function IRCSProtocolHandler()
+{
+}
+
+IRCSProtocolHandler.prototype = new GenericIRCProtocolHandler(true);
+
+const IRCProtocolHandlerFactory =
+{
+    createInstance(outer, iid)
+    {
+        if (outer != null)
+            throw Cr.NS_ERROR_NO_AGGREGATION;
+
+        if (!iid.equals(Ci.nsIProtocolHandler) && !iid.equals(Ci.nsISupports))
+            throw Cr.NS_ERROR_INVALID_ARG;
+
+        return new IRCProtocolHandler(false);
+    },
+};
+
+
+const IRCSProtocolHandlerFactory =
+{
+    createInstance(outer, iid)
+    {
+        if (outer != null)
+            throw Cr.NS_ERROR_NO_AGGREGATION;
+
+        if (!iid.equals(Ci.nsIProtocolHandler) && !iid.equals(Ci.nsISupports))
+            throw Cr.NS_ERROR_INVALID_ARG;
+
+        return new IRCProtocolHandler(true);
+    },
+};
 
 /* entrypoint */
 function NSGetFactory(cid)
