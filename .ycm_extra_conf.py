91ba408082d88ead6906d2e7ee9e930aa9145002
John Bieling <john@thunderbird.net>
2024-10-30 17:30:23 +0000

    Bug 1928027 - fix ycm_extra_conf.py according to ../mach commlint --fix. r=freaktechnik
    Differential Revision: https://phabricator.services.mozilla.com/D227326
    
    --HG--
    extra : moz-landing-system : lando

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import imp, os, sys
+import imp
+import os
+import sys
 
 old_bytecode = sys.dont_write_bytecode
 sys.dont_write_bytecode = True
