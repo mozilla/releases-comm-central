9ee816abd8f2906fa140016f64a8366ba4d4393d
John Bieling <john@thunderbird.net>
2024-11-07 08:13:42 +0100

    no bug - Fix linting issues in taskcluster/comm_taskgraph/__init__.py. rs=me

--- taskcluster/comm_taskgraph/__init__.py
+++ taskcluster/comm_taskgraph/__init__.py
@@ -40,11 +40,10 @@ def register(graph_config):
     the process.
     """
     extend_sys_path(GECKO)
-    from comm_taskgraph.util import taskgraph_attributes  # noqa: patch gecko_taskgraph
-
     from gecko_taskgraph.util import dependencies  # noqa: trigger group_by registration
 
     from comm_taskgraph.parameters import register_parameters
+    from comm_taskgraph.util import taskgraph_attributes  # noqa: patch gecko_taskgraph
 
     logger.info("{} path registered".format(__name__))
     register_parameters()
