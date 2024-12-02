2c01101db96f03208663f2a56357a26ff0ab1285
Martin Giger <martin@humanoids.be>
2024-12-02 13:47:14 +0200

    Bug 1932006 - Followup: fix import from AccountCreationUtils.sys.mjs. r=mkmelin DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D230761
    
    --HG--
    extra : amend_source : ae199a76dd11f842d83f2ce03fb04cb8afbf5ede

--- mail/components/accountcreation/views/email.mjs
+++ mail/components/accountcreation/views/email.mjs
@@ -2,10 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { gAccountSetupLogger, SuccessiveAbortable, UserCancelledException } =
-  ChromeUtils.importESModule(
-    "resource:///modules/accountcreation/AccountCreationUtils.sys.mjs"
-  );
+const {
+  AccountCreationUtils: {
+    gAccountSetupLogger,
+    SuccessiveAbortable,
+    UserCancelledException,
+  },
+} = ChromeUtils.importESModule(
+  "resource:///modules/accountcreation/AccountCreationUtils.sys.mjs"
+);
 const { AccountConfig } = ChromeUtils.importESModule(
   "resource:///modules/accountcreation/AccountConfig.sys.mjs"
 );
