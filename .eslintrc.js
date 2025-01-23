c14d0255ba0212400518006580a087db8a04984a
Geoff Lankow <geoff@darktrojan.net>
2025-01-24 11:24:53 +1300

    Bug 1943461 - Port bug 1936092 - Remove the reject-chromeutils-import rule. r=john.bieling,arschmitz
    Differential Revision: https://phabricator.services.mozilla.com/D235379
    
    --HG--
    extra : amend_source : 0e36a91e285b887aceece4f36992c4016b3f3d4d

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,7 +69,6 @@ module.exports = {
     "mozilla/prefer-boolean-length-check": "off",
     // Enforce using `let` only when variables are reassigned.
     "prefer-const": ["error", { destructuring: "all" }],
-    "mozilla/reject-chromeutils-import": "error",
   },
 
   overrides: [
