b160e4470c45535836c76e3883f4307a8a7a5356
serge-sans-paille <sguelton@mozilla.com>
2025-02-25 22:32:03 +0000

    Bug 1943547 - Remove legacy configuration variables out of confvars.sh r=dandarnell
    MOZ_VERIFY_MAR_SIGNATURE can be set from mail/moz.configure
    THUNDERBIRD_OFFICIAL_BRANDING  can be used to directly set MOZ_OFFICIAL_BRANDING_DIRECTORY
    
    MOZ_BINARY_EXTENSIONS, MOZ_SEPARATE_MANIFEST_FOR_THEME_OVERRIDES and
    MOZ_ENABLE_SIGNMAR are actually obsolete and never used.
    
    Differential Revision: https://phabricator.services.mozilla.com/D235417
    
    --HG--
    extra : moz-landing-system : lando

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -6,21 +6,4 @@
 MOZ_APP_NAME=thunderbird
 MOZ_APP_PROFILE=thunderbird
 
-if test "$OS_ARCH" = "WINNT"; then
-  if ! test "$HAVE_64BIT_BUILD"; then
-    MOZ_VERIFY_MAR_SIGNATURE=1
-  fi
-fi
-
 MOZ_BRANDING_DIRECTORY=comm/mail/branding/nightly
-MOZ_OFFICIAL_BRANDING_DIRECTORY=comm/mail/branding/thunderbird
-# This looks silly, but it's for the benefit of MSIX repackaging.
-if test -n "$THUNDERBIRD_OFFICIAL_BRANDING"; then
-  MOZ_OFFICIAL_BRANDING_DIRECTORY=comm/mail/branding/$THUNDERBIRD_OFFICIAL_BRANDING
-fi
-
-MOZ_BINARY_EXTENSIONS=1
-MOZ_SEPARATE_MANIFEST_FOR_THEME_OVERRIDES=1
-
-# Enable building ./signmar and running libmar signature tests
-MOZ_ENABLE_SIGNMAR=1
