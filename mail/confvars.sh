4018d9269b4a26e008641517db0734b1632f912a
serge-sans-paille <sguelton@mozilla.com>
2025-03-03 09:47:33 +0000

    Bug 1950637 - Move configuration variables from confvars.sh to moz.configure r=coreycb
    This matches the recent configuration changes in mozilla-central: only
    variable flagged as confvar should be set in confvars.sh
    
    Differential Revision: https://phabricator.services.mozilla.com/D239871
    
    --HG--
    extra : moz-landing-system : lando

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -3,7 +3,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=thunderbird
-MOZ_APP_PROFILE=thunderbird
-
 MOZ_BRANDING_DIRECTORY=comm/mail/branding/nightly
