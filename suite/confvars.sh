3bcd18f00e013f22dc612fb6c47a4ca9e69abc12
serge-sans-paille <sguelton@mozilla.com>
2025-03-04 16:29:47 +0000

    Bug 1951143 - Move configuration variables from confvars.sh to moz.configure r=coreycb
    This matches the recent configuration changes in mozilla-central: only
    variable flagged as confvar should be set in confvars.sh
    
    Differential Revision: https://phabricator.services.mozilla.com/D239871
    
    --HG--
    extra : amend_source : 0f9e8edf2e09c11bfd6efc95843ed4f1ae8af5ac

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -3,17 +3,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=seamonkey
-MOZ_APP_DISPLAYNAME=SeaMonkey
-
-MOZ_APP_VERSION=$SEAMONKEY_VERSION
-MOZ_APP_VERSION_DISPLAY=$SEAMONKEY_VERSION_DISPLAY
-# MOZ_PKG_VERSION=$SEAMONKEY_VERSION_PACKAGE
-
-BROWSER_CHROME_URL=chrome://navigator/content/navigator.xul
-
 MOZ_BRANDING_DIRECTORY=comm/suite/branding/seamonkey
 MOZ_OFFICIAL_BRANDING_DIRECTORY=comm/suite/branding/seamonkey
-MOZ_UPDATER=1
-
-MOZ_PROFILE_MIGRATOR=1
