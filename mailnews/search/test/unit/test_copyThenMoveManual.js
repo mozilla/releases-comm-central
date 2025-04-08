779bfe1752a8b78f08155928ce7158b3825e4284
Magnus Melin <mkmelin+mozilla@iki.fi>
2025-04-08 10:25:39 +0000

    Bug 1946063 - Modernize and try to fix intermittent comm/mailnews/search/test/unit/test_copyThenMoveManual.js | verifyFolders1 - [verifyFolders1 : 65] 0 == 1. r=BenC
    Modernize the test.
    With this it passes with --verify for me. Obviously there's still something wrong as the 2 added timeouts should not be needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D244293
    
    --HG--
    extra : moz-landing-system : lando

--- mailnews/search/test/unit/test_copyThenMoveManual.js
+++ mailnews/search/test/unit/test_copyThenMoveManual.js
@@ -14,103 +14,104 @@ var { MailServices } = ChromeUtils.importESModule(
 const { PromiseTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/mailnews/PromiseTestUtils.sys.mjs"
 );
+const { setTimeout } = ChromeUtils.importESModule(
+  "resource://gre/modules/Timer.sys.mjs"
+);
 
 var gFiles = ["../../../data/bugmail1"];
 var gCopyFolder;
 var gMoveFolder;
 var gFilter; // the test filter
 var gFilterList;
-var gTestArray = [
-  function createFilters() {
-    // setup manual copy then move mail filters on the inbox
-    gFilterList = localAccountUtils.incomingServer.getFilterList(null);
-    gFilter = gFilterList.createFilter("copyThenMoveAll");
-    const searchTerm = gFilter.createTerm();
-    searchTerm.matchAll = true;
-    gFilter.appendTerm(searchTerm);
-    const copyAction = gFilter.createAction();
-    copyAction.type = Ci.nsMsgFilterAction.CopyToFolder;
-    copyAction.targetFolderUri = gCopyFolder.URI;
-    gFilter.appendAction(copyAction);
-    const moveAction = gFilter.createAction();
-    moveAction.type = Ci.nsMsgFilterAction.MoveToFolder;
-    moveAction.targetFolderUri = gMoveFolder.URI;
-    gFilter.appendAction(moveAction);
-    gFilter.enabled = true;
-    gFilter.filterType = Ci.nsMsgFilterType.Manual;
-    gFilterList.insertFilterAt(0, gFilter);
-  },
-  // just get a message into the local folder
-  async function getLocalMessages1() {
-    gPOP3Pump.files = gFiles;
-    await gPOP3Pump.run();
-  },
-  // test applying filters to a message header
-  async function applyFilters() {
-    const promiseFolderEvent = PromiseTestUtils.promiseFolderEvent(
-      localAccountUtils.inboxFolder,
-      "DeleteOrMoveMsgCompleted"
-    );
-    MailServices.filters.applyFilters(
-      Ci.nsMsgFilterType.Manual,
-      [localAccountUtils.inboxFolder.firstNewMessage],
-      localAccountUtils.inboxFolder,
-      null
-    );
-    await promiseFolderEvent;
-  },
-  function verifyFolders1() {
-    // Copy and Move should each now have 1 message in them.
-    Assert.equal(folderCount(gCopyFolder), 1);
-    Assert.equal(folderCount(gMoveFolder), 1);
-    // the local inbox folder should now be empty, since the second
-    // operation was a move
-    Assert.equal(folderCount(localAccountUtils.inboxFolder), 0);
-  },
-  // just get a message into the local folder
-  async function getLocalMessages2() {
-    gPOP3Pump.files = gFiles;
-    await gPOP3Pump.run();
-  },
-  // use the alternate call into the filter service
-  async function applyFiltersToFolders() {
-    const folders = [localAccountUtils.inboxFolder];
-    const promiseFolderEvent = PromiseTestUtils.promiseFolderEvent(
-      localAccountUtils.inboxFolder,
-      "DeleteOrMoveMsgCompleted"
-    );
-    MailServices.filters.applyFiltersToFolders(gFilterList, folders, null);
-    await promiseFolderEvent;
-  },
-  function verifyFolders2() {
-    // Copy and Move should each now have 2 message in them.
-    Assert.equal(folderCount(gCopyFolder), 2);
-    Assert.equal(folderCount(gMoveFolder), 2);
-    // the local inbox folder should now be empty, since the second
-    // operation was a move
-    Assert.equal(folderCount(localAccountUtils.inboxFolder), 0);
-  },
-  function endTest() {
-    // Cleanup, null out everything, close all cached connections and stop the
-    // server
-    dump(" Exiting mail tests\n");
-    gPOP3Pump = null;
-  },
-];
 
-function folderCount(folder) {
-  return [...folder.msgDatabase.enumerateMessages()].length;
-}
-
-function run_test() {
-  if (!localAccountUtils.inboxFolder) {
-    localAccountUtils.loadLocalMailAccount();
-  }
+add_setup(async function () {
+  localAccountUtils.loadLocalMailAccount();
 
   gCopyFolder = localAccountUtils.rootFolder.createLocalSubfolder("CopyFolder");
   gMoveFolder = localAccountUtils.rootFolder.createLocalSubfolder("MoveFolder");
 
-  gTestArray.forEach(x => add_task(x));
+  // setup manual copy then move mail filters on the inbox
+  gFilterList = localAccountUtils.incomingServer.getFilterList(null);
+  gFilter = gFilterList.createFilter("copyThenMoveAll");
+  const searchTerm = gFilter.createTerm();
+  searchTerm.matchAll = true;
+  gFilter.appendTerm(searchTerm);
+  const copyAction = gFilter.createAction();
+  copyAction.type = Ci.nsMsgFilterAction.CopyToFolder;
+  copyAction.targetFolderUri = gCopyFolder.URI;
+  gFilter.appendAction(copyAction);
+  const moveAction = gFilter.createAction();
+  moveAction.type = Ci.nsMsgFilterAction.MoveToFolder;
+  moveAction.targetFolderUri = gMoveFolder.URI;
+  gFilter.appendAction(moveAction);
+  gFilter.enabled = true;
+  gFilter.filterType = Ci.nsMsgFilterType.Manual;
+  gFilterList.insertFilterAt(0, gFilter);
 
-  run_next_test();
+  registerCleanupFunction(() => {
+    if (gPOP3Pump._server) {
+      gPOP3Pump._server.stop();
+    }
+    gPOP3Pump = null;
+  });
+});
+
+add_task(async function getLocalMessages1() {
+  // just get a message into the local folder
+  gPOP3Pump.files = gFiles;
+  await gPOP3Pump.run();
+
+  // test applying filters to a message header
+
+  const promiseFolderEvent = PromiseTestUtils.promiseFolderEvent(
+    localAccountUtils.inboxFolder,
+    "DeleteOrMoveMsgCompleted"
+  );
+  MailServices.filters.applyFilters(
+    Ci.nsMsgFilterType.Manual,
+    [localAccountUtils.inboxFolder.firstNewMessage],
+    localAccountUtils.inboxFolder,
+    null
+  );
+  await promiseFolderEvent;
+  // FIXME: this timeout should not be necessary
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  // Copy and Move should each now have 1 message in them.
+  Assert.equal(folderCount(gCopyFolder), 1, "gCopyFolder should have one msg");
+  Assert.equal(folderCount(gMoveFolder), 1, "gMoveFolder should have one msg");
+  // the local inbox folder should now be empty, since the second
+  // operation was a move
+  Assert.equal(folderCount(localAccountUtils.inboxFolder), 0);
+});
+
+add_task(async function getLocalMessages2() {
+  // just get a message into the local folder
+  gPOP3Pump.files = gFiles;
+  await gPOP3Pump.run();
+
+  // use the alternate call into the filter service
+
+  const folders = [localAccountUtils.inboxFolder];
+  const promiseFolderEvent = PromiseTestUtils.promiseFolderEvent(
+    localAccountUtils.inboxFolder,
+    "DeleteOrMoveMsgCompleted"
+  );
+  MailServices.filters.applyFiltersToFolders(gFilterList, folders, null);
+  await promiseFolderEvent;
+  // FIXME: this timeout should not be necessary
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  // Copy and Move should each now have 2 message in them.
+  Assert.equal(folderCount(gCopyFolder), 2, "gCopyFolder should have 2 msgs");
+  Assert.equal(folderCount(gMoveFolder), 2, "gMoveFolder should have 2 msgs");
+  // the local inbox folder should now be empty, since the second
+  // operation was a move
+  Assert.equal(folderCount(localAccountUtils.inboxFolder), 0);
+});
+
+function folderCount(folder) {
+  return [...folder.msgDatabase.enumerateMessages()].length;
 }
