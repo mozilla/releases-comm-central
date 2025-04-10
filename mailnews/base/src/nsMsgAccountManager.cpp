3bf367ac4fded0bb5275567de14b2b6d17b05c56
Geoff Lankow <geoff@darktrojan.net>
2025-04-10 09:30:51 +0300

    Bug 1959340 - Port bug 1954988: Deprecate and remove AsyncShutdown.quitApplicationGranted in favor of appShutdownConfirmed. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D244883
    
    --HG--
    extra : amend_source : d7f7769847567b0661e73dc95f870f4c902d4bac

--- mailnews/base/src/nsMsgAccountManager.cpp
+++ mailnews/base/src/nsMsgAccountManager.cpp
@@ -141,10 +141,10 @@ static nsCOMPtr<nsIAsyncShutdownService> GetShutdownService() {
   return service;
 }
 
-static nsCOMPtr<nsIAsyncShutdownClient> GetQuitApplicationGranted() {
+static nsCOMPtr<nsIAsyncShutdownClient> GetAppShutdownConfirmed() {
   nsCOMPtr<nsIAsyncShutdownClient> barrier;
   nsresult rv =
-      GetShutdownService()->GetQuitApplicationGranted(getter_AddRefs(barrier));
+      GetShutdownService()->GetAppShutdownConfirmed(getter_AddRefs(barrier));
   MOZ_RELEASE_ASSERT(NS_SUCCEEDED(rv));
   MOZ_RELEASE_ASSERT(barrier);
   return barrier;
@@ -195,7 +195,7 @@ nsresult nsMsgAccountManager::Init() {
     observerService->AddObserver(this, "sleep_notification", true);
   }
 
-  GetQuitApplicationGranted()->AddBlocker(
+  GetAppShutdownConfirmed()->AddBlocker(
       this, NS_LITERAL_STRING_FROM_CSTRING(__FILE__), __LINE__,
       u"nsMsgAccountManager cleanup on exit"_ns);
   GetProfileBeforeChange()->AddBlocker(
@@ -1669,7 +1669,7 @@ nsresult nsMsgAccountManager::CleanupOnExit() {
     }
   }
 
-  GetQuitApplicationGranted()->RemoveBlocker(this);
+  GetAppShutdownConfirmed()->RemoveBlocker(this);
 
   // Try to do this early on in the shutdown process before
   // necko shuts itself down.
