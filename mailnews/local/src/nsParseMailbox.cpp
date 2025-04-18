f47e6cb953ff11f219a9a48d404a6fa3f8a12c0b
Ben Campbell <benc@thunderbird.net>
2025-04-18 10:28:45 +0000

    Bug 1958052 - Remove old database-tied write functions from nsIMsgPluggableStore. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D245843
    
    --HG--
    extra : moz-landing-system : lando

--- mailnews/local/src/nsParseMailbox.cpp
+++ mailnews/local/src/nsParseMailbox.cpp
@@ -1301,7 +1301,8 @@ void nsParseNewMailState::PublishMsgHeader(nsIMsgWindow* msgWindow) {
               nsresult rv =
                   m_downloadFolder->GetMsgStore(getter_AddRefs(msgStore));
               if (NS_SUCCEEDED(rv)) {
-                rv = msgStore->DiscardNewMessage2(m_downloadFolder, m_outputStream);
+                rv = msgStore->DiscardNewMessage2(m_downloadFolder,
+                                                  m_outputStream);
                 if (NS_FAILED(rv))
                   m_rootFolder->ThrowAlertMsg("dupDeleteFolderTruncateFailed",
                                               msgWindow);
