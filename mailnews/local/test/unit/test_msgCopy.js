75b2d2040cc64025ae01c4ce41c7495cc493cd3b
Ben Campbell <benc@thunderbird.net>
2025-05-27 02:07:53 +0000

    Bug 1968168 - Modernise test_msgCopy.js. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D251230
    
    --HG--
    extra : amend_source : 12e0acedacce2ffb6104b4bbee42946f0e6e5a1e

--- mailnews/local/test/unit/test_msgCopy.js
+++ mailnews/local/test/unit/test_msgCopy.js
@@ -2,26 +2,37 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Test of setting keywords with copyFileMessage
+const { PromiseTestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/mailnews/PromiseTestUtils.sys.mjs"
+);
 
-var bugmail11 = do_get_file("../../../data/bugmail11");
+/**
+ * Test setting keywords with copyFileMessage().
+ */
+add_task(async function test_fileCopySetKeywords() {
+  localAccountUtils.loadLocalMailAccount();
+  try {
+    const inbox = localAccountUtils.inboxFolder;
 
-// main test
+    const copyListener = new PromiseTestUtils.PromiseCopyListener();
+    const bugmail11 = do_get_file("../../../data/bugmail11");
+    const tag1 = "istag";
+    MailServices.copy.copyFileMessage(
+      bugmail11,
+      inbox,
+      null,
+      false,
+      0 /* message flags */,
+      tag1 /* keywords */,
+      copyListener,
+      null /* window */
+    );
+    const copied = await copyListener.promise;
 
-// tag used with test messages
-var tag1 = "istag";
-
-function run_test() {
-  do_test_pending();
-  copyFileMessageInLocalFolder(bugmail11, 0, tag1, null, test_keywords);
-}
-
-function test_keywords(aMessageHeaderKeys) {
-  const headerKeys = aMessageHeaderKeys;
-  Assert.notEqual(headerKeys, null);
-  const copiedMessage = localAccountUtils.inboxFolder.GetMessageHeader(
-    headerKeys[0]
-  );
-  Assert.equal(copiedMessage.getStringProperty("keywords"), tag1);
-  do_test_finished();
-}
+    // Check the keywords on the copied message.
+    const msg = inbox.GetMessageHeader(copied.messageKeys[0]);
+    Assert.equal(msg.getStringProperty("keywords"), tag1);
+  } finally {
+    localAccountUtils.clearAll();
+  }
+});
