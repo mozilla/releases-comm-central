6a936b00fc5b3565faf8a314be1b7caa69683321
Ben Campbell <benc@thunderbird.net>
2025-06-04 13:54:18 +0300

    Bug 1968168 - Modernise test_saveMessage.js. r=mkmelin
    Differential Revision: https://phabricator.services.mozilla.com/D252299
    
    --HG--
    extra : amend_source : 1db6478a1883e47331ec70d82a09cbfcdcdf847a

--- mailnews/local/test/unit/test_saveMessage.js
+++ mailnews/local/test/unit/test_saveMessage.js
@@ -1,81 +1,81 @@
-/**
- * Test bug 460636 - Saving message in local folder as .EML removes starting dot in all lines, and ignores line if single dot only line.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var { PromiseTestUtils } = ChromeUtils.importESModule(
+const { PromiseTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/mailnews/PromiseTestUtils.sys.mjs"
 );
 
-var MSG_LINEBREAK = "\r\n";
-var dot = do_get_file("data/dot");
-var saveFile = Services.dirsvc.get("TmpD", Ci.nsIFile);
-saveFile.append(dot.leafName + ".eml");
-saveFile.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600);
+/**
+ * Test bug 460636 - Saving message in local folder as .EML removes starting
+ * dot in all lines, and ignores line if single dot only line.
+ */
+add_task(async function test_saveMessage() {
+  localAccountUtils.loadLocalMailAccount();
 
-// Strip the extra X-Mozilla- headers which are slipped in to messages
-// as they are written to local folders. Not exactly robust RFC5322 parsing,
-// but enough to handle this test.
-function strip_x_moz_headers(s) {
-  // List to make sure headers show up when grepping codebase.
-  for (const hdr of [
-    "X-Mozilla-Status",
-    "X-Mozilla-Status2",
-    "X-Mozilla-Keys",
-  ]) {
-    s = s.replace(new RegExp("^" + hdr + ":.*?\r?\n", "gm"), "");
-  }
-  return s;
-}
-
-function run_test() {
-  registerCleanupFunction(teardown);
-  do_test_pending();
-  do_timeout(10000, function () {
-    do_throw(
-      "saveMessageToDisk did not complete within 10 seconds" +
-        "(incorrect messageURI?). ABORTING."
-    );
-  });
-  copyFileMessageInLocalFolder(dot, 0, "", null, save_message);
-}
-
-async function save_message(aMessageHeaderKeys) {
-  const headerKeys = aMessageHeaderKeys;
-  Assert.notEqual(headerKeys, null);
-
-  const message = localAccountUtils.inboxFolder.GetMessageHeader(headerKeys[0]);
-  const msgURI = localAccountUtils.inboxFolder.getUriForMsg(message);
   const messageService = Cc[
     "@mozilla.org/messenger/messageservice;1?type=mailbox-message"
   ].getService(Ci.nsIMsgMessageService);
-  const promiseUrlListener = new PromiseTestUtils.PromiseUrlListener();
-  messageService.saveMessageToDisk(
-    msgURI,
-    saveFile,
-    false,
-    promiseUrlListener,
-    true,
-    null
-  );
-  await promiseUrlListener.promise;
-  let savedMsg = await IOUtils.readUTF8(saveFile.path);
-  savedMsg = strip_x_moz_headers(savedMsg);
-  check_each_line(await IOUtils.readUTF8(dot.path), savedMsg);
-  do_test_finished();
-}
 
-function check_each_line(aExpectedLines, aActualLines) {
-  const expectedStrings = aExpectedLines.split(MSG_LINEBREAK);
-  const actualStrings = aActualLines.split(MSG_LINEBREAK);
+  const inFile = do_get_file("data/dot");
+  const savedFile = Services.dirsvc.get("TmpD", Ci.nsIFile);
+  savedFile.append(inFile.leafName + ".eml");
+  savedFile.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600);
 
-  Assert.equal(expectedStrings.length, actualStrings.length);
-  for (let line = 0; line < expectedStrings.length; line++) {
-    Assert.equal(expectedStrings[line], actualStrings[line]);
+  // Not exactly robust RFC5322 parsing, but good enough here.
+  const strip_x_moz_headers = function (s) {
+    for (const hdr of [
+      "X-Mozilla-Status",
+      "X-Mozilla-Status2",
+      "X-Mozilla-Keys",
+    ]) {
+      s = s.replace(new RegExp("^" + hdr + ":.*?\r?\n", "gm"), "");
+    }
+    return s;
+  };
+
+  try {
+    const inbox = localAccountUtils.inboxFolder;
+
+    // Install a message.
+    const copyListener = new PromiseTestUtils.PromiseCopyListener();
+    MailServices.copy.copyFileMessage(
+      inFile,
+      inbox,
+      null,
+      false,
+      0,
+      "",
+      copyListener,
+      null
+    );
+    const copied = await copyListener.promise;
+
+    // Save it out.
+    const msgHdr = inbox.GetMessageHeader(copied.messageKeys[0]);
+    const msgUri = inbox.getUriForMsg(msgHdr);
+    const promiseUrlListener = new PromiseTestUtils.PromiseUrlListener();
+    messageService.saveMessageToDisk(
+      msgUri,
+      savedFile,
+      false,
+      promiseUrlListener,
+      true,
+      null
+    );
+    await promiseUrlListener.promise;
+
+    // Check output against the original message, accounting for added
+    // X-Mozilla-* headers.
+    let got = await IOUtils.readUTF8(savedFile.path);
+    got = strip_x_moz_headers(got);
+    const expect = await IOUtils.readUTF8(inFile.path);
+    Assert.equal(got, expect, "Saved message should match original");
+  } finally {
+    // Clean up.
+    localAccountUtils.clearAll();
+    if (savedFile.exists()) {
+      savedFile.remove(false);
+    }
   }
-}
-
-function teardown() {
-  if (saveFile.exists()) {
-    saveFile.remove(false);
-  }
-}
+});
