10a9b3fd849670711910080139fafd550dd1c37d
Ben Campbell <benc@thunderbird.net>
2025-08-05 17:19:48 -0400

    Bug 1981064 - Migrate NS_MSG_ errorcodes out of msgCore.h defines and into mail/errors.json. r=babolivier
    Differential Revision: https://phabricator.services.mozilla.com/D259901
    
    --HG--
    extra : amend_source : af7afda59078ae3272d40fd9c992578acaebe273

--- mailnews/base/public/msgCore.h
+++ mailnews/base/public/msgCore.h
@@ -44,31 +44,6 @@ class nsIMsgFolder;
 // Most recently moved to, for recent folders list in move menu
 #define MRM_TIME_PROPERTY "MRMTime"
 
-/*
- * NS_ERROR macros - use these macros to generate error constants
- * to be used by XPCOM interfaces and possibly other useful things
- * do not use these macros in your code - declare error macros for
- * each specific error you need.
- *
- * for example:
- * #define NS_MSG_ERROR_NO_SUCH_FOLDER NS_MSG_GENERATE_FAILURE(4)
- *
- */
-
-/* use these routines to generate error values */
-#define NS_MSG_GENERATE_RESULT(severity, value) \
-  NS_ERROR_GENERATE(severity, NS_ERROR_MODULE_MAILNEWS, value)
-
-#define NS_MSG_GENERATE_SUCCESS(value) \
-  NS_ERROR_GENERATE_SUCCESS(NS_ERROR_MODULE_MAILNEWS, value)
-
-#define NS_MSG_GENERATE_FAILURE(value) \
-  NS_ERROR_GENERATE_FAILURE(NS_ERROR_MODULE_MAILNEWS, value)
-
-/* these are shortcuts to generate simple errors with a zero value */
-#define NS_MSG_SUCCESS NS_MSG_GENERATE_SUCCESS(0)
-#define NS_MSG_FAILURE NS_MSG_GENERATE_FAILURE(0)
-
 #define IS_SPACE(VAL) \
   (((((PRIntn)(VAL)) & 0x7f) == ((PRIntn)(VAL))) && isspace((PRIntn)(VAL)))
 
@@ -79,92 +54,6 @@ class nsIMsgFolder;
 #  define IS_ALPHA(VAL) \
     ((((unsigned int)(VAL)) > 0x7f) ? (int)0 : isalpha((int)(VAL)))
 #endif
-
-/* for retrieving information out of messenger nsresults */
-
-#define NS_IS_MSG_ERROR(err) \
-  (NS_ERROR_GET_MODULE(err) == NS_ERROR_MODULE_MAILNEWS)
-
-#define NS_MSG_SUCCEEDED(err) (NS_IS_MSG_ERROR(err) && NS_SUCCEEDED(err))
-
-#define NS_MSG_FAILED(err) (NS_IS_MSG_ERROR(err) && NS_FAILED(err))
-
-#define NS_MSG_PASSWORD_PROMPT_CANCELLED NS_MSG_GENERATE_SUCCESS(1)
-
-/**
- * Indicates that a search is done/terminated because it was interrupted.
- * Interrupting a search originally notified listeners with
- * OnSearchDone(NS_OK), so we define a success value to continue doing this,
- * and because the search was fine except for an explicit call to interrupt it.
- */
-#define NS_MSG_SEARCH_INTERRUPTED NS_MSG_GENERATE_SUCCESS(2)
-
-/* This is where we define our errors. There has to be a central
-   place so we don't use the same error codes for different errors.
-*/
-#define NS_MSG_ERROR_FOLDER_SUMMARY_OUT_OF_DATE NS_MSG_GENERATE_FAILURE(5)
-#define NS_MSG_ERROR_FOLDER_SUMMARY_MISSING NS_MSG_GENERATE_FAILURE(6)
-#define NS_MSG_ERROR_FOLDER_MISSING NS_MSG_GENERATE_FAILURE(7)
-
-#define NS_MSG_MESSAGE_NOT_FOUND NS_MSG_GENERATE_FAILURE(8)
-#define NS_MSG_NOT_A_MAIL_FOLDER NS_MSG_GENERATE_FAILURE(9)
-
-#define NS_MSG_FOLDER_BUSY NS_MSG_GENERATE_FAILURE(10)
-
-#define NS_MSG_COULD_NOT_CREATE_DIRECTORY NS_MSG_GENERATE_FAILURE(11)
-#define NS_MSG_CANT_CREATE_FOLDER NS_MSG_GENERATE_FAILURE(12)
-
-#define NS_MSG_FILTER_PARSE_ERROR NS_MSG_GENERATE_FAILURE(13)
-
-#define NS_MSG_FOLDER_UNREADABLE NS_MSG_GENERATE_FAILURE(14)
-
-#define NS_MSG_ERROR_WRITING_MAIL_FOLDER NS_MSG_GENERATE_FAILURE(15)
-
-#define NS_MSG_ERROR_NO_SEARCH_VALUES NS_MSG_GENERATE_FAILURE(16)
-
-#define NS_MSG_ERROR_INVALID_SEARCH_SCOPE NS_MSG_GENERATE_FAILURE(17)
-
-#define NS_MSG_ERROR_INVALID_SEARCH_TERM NS_MSG_GENERATE_FAILURE(18)
-
-#define NS_MSG_FOLDER_EXISTS NS_MSG_GENERATE_FAILURE(19)
-
-#define NS_MSG_ERROR_OFFLINE NS_MSG_GENERATE_FAILURE(20)
-
-#define NS_MSG_POP_FILTER_TARGET_ERROR NS_MSG_GENERATE_FAILURE(21)
-
-#define NS_MSG_INVALID_OR_MISSING_SERVER NS_MSG_GENERATE_FAILURE(22)
-
-#define NS_MSG_INVALID_DBVIEW_INDEX NS_MSG_GENERATE_FAILURE(24)
-
-#define NS_MSG_ERROR_COPY_FOLDER_ABORTED NS_MSG_GENERATE_FAILURE(26)
-// this error means a url was queued but never run because one of the urls
-// it was queued after failed. We send an OnStopRunningUrl with this error code
-// so the listeners can know that we didn't run the url.
-#define NS_MSG_ERROR_URL_ABORTED NS_MSG_GENERATE_FAILURE(27)
-
-// when custom header has invalid characters (as per rfc 2822)
-#define NS_MSG_INVALID_CUSTOM_HEADER NS_MSG_GENERATE_FAILURE(29)
-
-// when local caches are password protect and user isn't auth
-#define NS_MSG_USER_NOT_AUTHENTICATED NS_MSG_GENERATE_FAILURE(30)
-
-// The code tried to stream a message using the aLocalOnly argument, but
-// the message was not cached locally.
-#define NS_MSG_ERROR_MSG_NOT_OFFLINE NS_MSG_GENERATE_FAILURE(32)
-
-// The imap server returned NO or BAD for an IMAP command
-#define NS_MSG_ERROR_IMAP_COMMAND_FAILED NS_MSG_GENERATE_FAILURE(33)
-
-#define NS_MSG_ERROR_INVALID_FOLDER_NAME NS_MSG_GENERATE_FAILURE(34)
-
-// Mbox message deviates too far from expected size.
-#define NS_MSG_ERROR_UNEXPECTED_SIZE NS_MSG_GENERATE_FAILURE(35)
-
-// Mbox message doesn't start with "From " separator line.
-#define NS_MSG_ERROR_MBOX_MALFORMED NS_MSG_GENERATE_FAILURE(36)
-
-// Folder compaction could not proceed because of pending offline/pseudo ops.
-#define NS_MSG_ERROR_BLOCKED_COMPACTION NS_MSG_GENERATE_FAILURE(37)
 
 /* Error codes for message compose are defined in
    compose\src\nsMsgComposeStringBundle.h. Message compose use the same error
