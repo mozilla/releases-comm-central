7ad761fce46172cb17d4dfd60ec475e153c7dbe3
Richard Marti <richard.marti@gmail.com>
2025-09-21 07:20:19 +0000

    Bug 1988718 - Port bug 1610741: Remove old DoH UI. r=aleca
    Differential Revision: https://phabricator.services.mozilla.com/D265547
    
    --HG--
    extra : amend_source : f7cea289d220465bacf4f2c9dd53d742df73c66b

--- mail/components/preferences/connection.js
+++ mail/components/preferences/connection.js
@@ -30,10 +30,6 @@ Preferences.addAll([
   { id: "pref.advanced.proxies.disable_button.reload", type: "bool" },
   { id: "network.proxy.backup.ssl", type: "string" },
   { id: "network.proxy.backup.ssl_port", type: "int" },
-  { id: "network.trr.mode", type: "int" },
-  { id: "network.trr.uri", type: "string" },
-  { id: "network.trr.resolvers", type: "string" },
-  { id: "network.trr.custom_uri", type: "string" },
 ]);
 
 window.addEventListener(
@@ -47,14 +43,6 @@ window.addEventListener(
       "change",
       gConnectionsDialog.updateDNSPref.bind(gConnectionsDialog)
     );
-
-    Preferences.get("network.trr.uri").on("change", () => {
-      gConnectionsDialog.updateDnsOverHttpsUI();
-    });
-
-    Preferences.get("network.trr.resolvers").on("change", () => {
-      gConnectionsDialog.initDnsOverHttpsUI();
-    });
 
     Preferences.addSyncFromPrefListener(
       document.getElementById("networkProxyType"),
@@ -92,24 +80,6 @@ window.addEventListener(
       document.getElementById("networkProxySOCKSVersion"),
       () => gConnectionsDialog.updateDNSPref()
     );
-
-    // XXX: We can't init the DNS-over-HTTPs UI until the syncfrompref for network.trr.mode
-    //      has been called. The uiReady promise will be resolved after the first call to
-    //      readDnsOverHttpsMode and the subsequent call to initDnsOverHttpsUI has happened.
-    gConnectionsDialog.uiReady = new Promise(resolve => {
-      gConnectionsDialog._areTrrPrefsReady = false;
-      gConnectionsDialog._handleTrrPrefsReady = resolve;
-    }).then(() => {
-      gConnectionsDialog.initDnsOverHttpsUI();
-    });
-
-    const element = document.getElementById("networkDnsOverHttps");
-    Preferences.addSyncFromPrefListener(element, () =>
-      gConnectionsDialog.readDnsOverHttpsMode()
-    );
-    Preferences.addSyncToPrefListener(element, () =>
-      gConnectionsDialog.writeDnsOverHttpsMode()
-    );
     document.documentElement.addEventListener("beforeaccept", e =>
       gConnectionsDialog.beforeAccept(e)
     );
@@ -125,25 +95,6 @@ window.addEventListener(
 
 var gConnectionsDialog = {
   beforeAccept(event) {
-    const dnsOverHttpsResolverChoice = document.getElementById(
-      "networkDnsOverHttpsResolverChoices"
-    ).value;
-    if (dnsOverHttpsResolverChoice == "custom") {
-      const customValue = document
-        .getElementById("networkCustomDnsOverHttpsInput")
-        .value.trim();
-      if (customValue) {
-        Services.prefs.setStringPref("network.trr.uri", customValue);
-      } else {
-        Services.prefs.clearUserPref("network.trr.uri");
-      }
-    } else {
-      Services.prefs.setStringPref(
-        "network.trr.uri",
-        dnsOverHttpsResolverChoice
-      );
-    }
-
     var proxyTypePref = Preferences.get("network.proxy.type");
     if (proxyTypePref.value == 2) {
       this.doAutoconfigURLFixup();
@@ -421,181 +372,6 @@ var gConnectionsDialog = {
       hideControllingProxyExtension();
     } else {
       handleControllingProxyExtension().then(setInputsDisabledState);
-    }
-  },
-
-  get dnsOverHttpsResolvers() {
-    const rawValue = Preferences.get("network.trr.resolvers", "").value;
-    // if there's no default, we'll hold its position with an empty string
-    const defaultURI = Preferences.get("network.trr.uri", "").defaultValue;
-    let providers = [];
-    if (rawValue) {
-      try {
-        providers = JSON.parse(rawValue);
-      } catch (ex) {
-        console.error(
-          `Bad JSON data in pref network.trr.resolvers: ${rawValue}`
-        );
-      }
-    }
-    if (!Array.isArray(providers)) {
-      console.error(
-        `Expected a JSON array in network.trr.resolvers: ${rawValue}`
-      );
-      providers = [];
-    }
-    const defaultIndex = providers.findIndex(p => p.url == defaultURI);
-    if (defaultIndex == -1 && defaultURI) {
-      // the default value for the pref isn't included in the resolvers list
-      // so we'll make a stub for it. Without an id, we'll have to use the url as the label
-      providers.unshift({ url: defaultURI });
-    }
-    return providers;
-  },
-
-  isDnsOverHttpsLocked() {
-    return Services.prefs.prefIsLocked("network.trr.mode");
-  },
-
-  isDnsOverHttpsEnabled() {
-    // values outside 1:4 are considered falsey/disabled in this context
-    const trrPref = Preferences.get("network.trr.mode");
-    const enabled = trrPref.value > 0 && trrPref.value < 5;
-    return enabled;
-  },
-
-  readDnsOverHttpsMode() {
-    // called to update checked element property to reflect current pref value
-    const enabled = this.isDnsOverHttpsEnabled();
-    const uriPref = Preferences.get("network.trr.uri");
-    uriPref.updateControlDisabledState(!enabled || this.isDnsOverHttpsLocked());
-    // this is the first signal we get when the prefs are available, so
-    // lazy-init if appropriate
-    if (!this._areTrrPrefsReady) {
-      this._areTrrPrefsReady = true;
-      this._handleTrrPrefsReady();
-    } else {
-      this.updateDnsOverHttpsUI();
-    }
-    return enabled;
-  },
-
-  writeDnsOverHttpsMode() {
-    // called to update pref with user change
-    const trrModeCheckbox = document.getElementById("networkDnsOverHttps");
-    // we treat checked/enabled as mode 2
-    return trrModeCheckbox.checked ? 2 : 0;
-  },
-
-  updateDnsOverHttpsUI() {
-    // init and update of the UI must wait until the pref values are ready
-    if (!this._areTrrPrefsReady) {
-      return;
-    }
-    const [menu, customInput] = this.getDnsOverHttpsControls();
-    const customContainer = document.getElementById(
-      "customDnsOverHttpsContainer"
-    );
-    const customURI = Preferences.get("network.trr.custom_uri").value;
-    const currentURI = Preferences.get("network.trr.uri").value;
-    const resolvers = this.dnsOverHttpsResolvers;
-    const isCustom = menu.value == "custom";
-
-    if (this.isDnsOverHttpsEnabled()) {
-      this.toggleDnsOverHttpsUI(false);
-      if (isCustom) {
-        // if the current and custom_uri values mismatch, update the uri pref
-        if (
-          currentURI &&
-          !customURI &&
-          !resolvers.find(r => r.url == currentURI)
-        ) {
-          Services.prefs.setStringPref("network.trr.custom_uri", currentURI);
-        }
-      }
-    } else {
-      this.toggleDnsOverHttpsUI(true);
-    }
-
-    if (!menu.disabled && isCustom) {
-      customContainer.hidden = false;
-      customInput.disabled = false;
-    } else {
-      customContainer.hidden = true;
-      customInput.disabled = true;
-    }
-
-    // The height has likely changed, find our SubDialog and tell it to resize.
-    requestAnimationFrame(() => {
-      const dialogs = window.opener.gSubDialog._dialogs;
-      const dialog = dialogs.find(d => d._frame.contentDocument == document);
-      if (dialog) {
-        dialog.resizeVertically();
-      }
-    });
-  },
-
-  getDnsOverHttpsControls() {
-    return [
-      document.getElementById("networkDnsOverHttpsResolverChoices"),
-      document.getElementById("networkCustomDnsOverHttpsInput"),
-      document.getElementById("networkDnsOverHttpsResolverChoicesLabel"),
-      document.getElementById("networkCustomDnsOverHttpsInputLabel"),
-    ];
-  },
-
-  toggleDnsOverHttpsUI(disabled) {
-    for (const element of this.getDnsOverHttpsControls()) {
-      element.disabled = disabled;
-    }
-  },
-
-  initDnsOverHttpsUI() {
-    const resolvers = this.dnsOverHttpsResolvers;
-    const defaultURI = Preferences.get("network.trr.uri").defaultValue;
-    const currentURI = Preferences.get("network.trr.uri").value;
-    const menu = document.getElementById("networkDnsOverHttpsResolverChoices");
-
-    // populate the DNS-Over-HTTPs resolver list
-    menu.removeAllItems();
-    for (const resolver of resolvers) {
-      const item = menu.appendItem(undefined, resolver.url);
-      if (resolver.url == defaultURI) {
-        document.l10n.setAttributes(
-          item,
-          "connection-dns-over-https-url-item-default",
-          {
-            name: resolver.name || resolver.url,
-          }
-        );
-      } else {
-        item.label = resolver.name || resolver.url;
-      }
-    }
-    const lastItem = menu.appendItem(undefined, "custom");
-    document.l10n.setAttributes(
-      lastItem,
-      "connection-dns-over-https-url-custom"
-    );
-
-    // set initial selection in the resolver provider picker
-    let selectedIndex = currentURI
-      ? resolvers.findIndex(r => r.url == currentURI)
-      : 0;
-    if (selectedIndex == -1) {
-      // select the last "Custom" item
-      selectedIndex = menu.itemCount - 1;
-    }
-    menu.selectedIndex = selectedIndex;
-
-    if (this.isDnsOverHttpsLocked()) {
-      // disable all the options and the checkbox itself to disallow enabling them
-      this.toggleDnsOverHttpsUI(true);
-      document.getElementById("networkDnsOverHttps").disabled = true;
-    } else {
-      this.toggleDnsOverHttpsUI(false);
-      this.updateDnsOverHttpsUI();
-      document.getElementById("networkDnsOverHttps").disabled = false;
     }
   },
 };
--- mail/locales/en-US/messenger/preferences/connection.ftl
+++ mail/locales/en-US/messenger/preferences/connection.ftl
@@ -2,22 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-connection-dns-over-https-url-resolver = Use Provider
-    .accesskey = r
-
-# Variables:
-#   $name (String) - Display name or URL for the DNS over HTTPS provider
-connection-dns-over-https-url-item-default =
-    .label = { $name } (Default)
-    .tooltiptext = Use the default URL for resolving DNS over HTTPS
-
-connection-dns-over-https-url-custom =
-    .label = Custom
-    .accesskey = C
-    .tooltiptext = Enter your preferred URL for resolving DNS over HTTPS
-
-connection-dns-over-https-custom-label = Custom
-
 connection-dialog-title = Connection Settings
 
 disable-extension-button = Disable Extension
@@ -108,7 +92,3 @@ proxy-password-prompt =
 proxy-remote-dns =
     .label = Proxy DNS when using SOCKS v5
     .accesskey = d
-
-proxy-enable-doh =
-    .label = Enable DNS over HTTPS
-    .accesskey = b
