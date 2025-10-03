6810b222cf53beb948245c34032af161ecb693b4
Justin Tracey <jtracey@thunderbird.net>
2025-10-03 16:30:07 -0600

    Bug 1982671 - Implement DoOperation for MarkAsJunk. r=edicharry
    Differential Revision: https://phabricator.services.mozilla.com/D266411
    
    --HG--
    extra : amend_source : 4b168cf998b7c82c599b4a16fc0a3aebb5b1784b

--- rust/ews_xpcom/src/client/mark_as_junk.rs
+++ rust/ews_xpcom/src/client/mark_as_junk.rs
@@ -15,10 +15,94 @@ use crate::{
     authentication::credentials::AuthenticationProvider,
     client::{
         copy_move_operations::move_generic::CopyMoveSuccess, process_response_message_class,
-        validate_response_message_count, XpComEwsClient, XpComEwsError,
+        validate_response_message_count, DoOperation, XpComEwsClient, XpComEwsError,
     },
-    safe_xpcom::{handle_error, SafeEwsSimpleOperationListener, SafeListener},
+    safe_xpcom::{SafeEwsSimpleOperationListener, SafeListener},
 };
+
+struct DoMarkAsJunk {
+    ews_ids: ThinVec<nsCString>,
+    is_junk: bool,
+    legacy_destination_folder_id: String,
+}
+
+impl DoOperation for DoMarkAsJunk {
+    const NAME: &'static str = MarkAsJunk::NAME;
+    type Okay = Option<ThinVec<nsCString>>;
+    type Listener = SafeEwsSimpleOperationListener;
+
+    async fn do_operation<ServerT>(
+        &mut self,
+        client: &XpComEwsClient<ServerT>,
+    ) -> Result<Self::Okay, XpComEwsError>
+    where
+        ServerT: AuthenticationProvider + UserInteractiveServer + RefCounted,
+    {
+        let server_version = client.server_version.get();
+
+        // The `MarkAsJunk` operation was added in Exchange2013.
+        let use_mark_as_junk = server_version >= ExchangeServerVersion::Exchange2013;
+
+        let item_ids: Vec<BaseItemId> = self
+            .ews_ids
+            .iter()
+            .map(|raw_id| BaseItemId::ItemId {
+                id: raw_id.to_string(),
+                change_key: None,
+            })
+            .collect();
+
+        if use_mark_as_junk {
+            let mark_as_junk = MarkAsJunk {
+                is_junk: self.is_junk,
+                move_item: true,
+                item_ids,
+            };
+
+            let response = client
+                .make_operation_request(mark_as_junk, Default::default())
+                .await?;
+
+            let response_messages = response.into_response_messages();
+            validate_response_message_count(&response_messages, self.ews_ids.len())?;
+
+            let new_ids = response_messages
+                .into_iter()
+                .map(|response_message| {
+                    process_response_message_class(MarkAsJunk::NAME, response_message)
+                })
+                .map(|response| response.map(|v| nsCString::from(v.moved_item_id.id)))
+                .collect::<Result<ThinVec<nsCString>, _>>()?;
+
+            Ok(Some(new_ids))
+        } else if !self.legacy_destination_folder_id.is_empty() {
+            // We have to move the items to the junk folder using a regular move operation.
+            let CopyMoveSuccess {
+                new_ids,
+                requires_resync,
+            } = client
+                .copy_move_item_functional::<MoveItem>(
+                    self.legacy_destination_folder_id.to_string(),
+                    self.ews_ids.iter().map(|s| s.to_string()).collect(),
+                )
+                .await?;
+            Ok(if requires_resync {
+                None
+            } else {
+                Some(new_ids.iter().map(nsCString::from).collect())
+            })
+        } else {
+            Err(XpComEwsError::Processing { message: "Unable to determine junk folder and Exchange version is too old for `MarkAsJunk` operation.".to_string() })
+        }
+    }
+
+    fn into_success_arg(self, ids: Self::Okay) -> <Self::Listener as SafeListener>::OnSuccessArg {
+        let use_legacy_fallback = ids.is_none();
+        (ids.unwrap_or(ThinVec::new()), use_legacy_fallback).into()
+    }
+
+    fn into_failure_arg(self) {}
+}
 
 impl<ServerT> XpComEwsClient<ServerT>
 where
@@ -31,84 +115,11 @@ where
         is_junk: bool,
         legacy_destination_folder_id: String,
     ) {
-        // Call an inner function to perform the operation in order to allow us
-        // to handle errors while letting the inner function simply propagate.
-        match self
-            .mark_as_junk_inner(ews_ids, is_junk, legacy_destination_folder_id)
-            .await
-        {
-            Ok(ids) => {
-                let use_legacy_fallback = ids.is_none();
-                let _ = listener
-                    .on_success((ids.unwrap_or(ThinVec::new()), use_legacy_fallback).into());
-            }
-            Err(err) => handle_error(&listener, MarkAsJunk::NAME, &err, ()),
+        let operation = DoMarkAsJunk {
+            ews_ids,
+            is_junk,
+            legacy_destination_folder_id,
         };
-    }
-
-    async fn mark_as_junk_inner(
-        self,
-        ews_ids: ThinVec<nsCString>,
-        is_junk: bool,
-        legacy_destination_folder_id: String,
-    ) -> Result<Option<ThinVec<nsCString>>, XpComEwsError>
-    where
-        ServerT: AuthenticationProvider + UserInteractiveServer + RefCounted,
-    {
-        let server_version = self.server_version.get();
-
-        // The `MarkAsJunk` operation was added in Exchange2013.
-        let use_mark_as_junk = server_version >= ExchangeServerVersion::Exchange2013;
-
-        let item_ids: Vec<BaseItemId> = ews_ids
-            .iter()
-            .map(|raw_id| BaseItemId::ItemId {
-                id: raw_id.to_string(),
-                change_key: None,
-            })
-            .collect();
-
-        if use_mark_as_junk {
-            let mark_as_junk = MarkAsJunk {
-                is_junk,
-                move_item: true,
-                item_ids,
-            };
-
-            let response = self
-                .make_operation_request(mark_as_junk, Default::default())
-                .await?;
-
-            let response_messages = response.into_response_messages();
-            validate_response_message_count(&response_messages, ews_ids.len())?;
-
-            let new_ids = response_messages
-                .into_iter()
-                .map(|response_message| {
-                    process_response_message_class(MarkAsJunk::NAME, response_message)
-                })
-                .map(|response| response.map(|v| nsCString::from(v.moved_item_id.id)))
-                .collect::<Result<ThinVec<nsCString>, _>>()?;
-
-            Ok(Some(new_ids))
-        } else if !legacy_destination_folder_id.is_empty() {
-            // We have to move the items to the junk folder using a regular move operation.
-            let CopyMoveSuccess {
-                new_ids,
-                requires_resync,
-            } = self
-                .copy_move_item_functional::<MoveItem>(
-                    legacy_destination_folder_id.to_string(),
-                    ews_ids.iter().map(|s| s.to_string()).collect(),
-                )
-                .await?;
-            Ok(if requires_resync {
-                None
-            } else {
-                Some(new_ids.iter().map(nsCString::from).collect())
-            })
-        } else {
-            Err(XpComEwsError::Processing { message: "Unable to determine junk folder and Exchange version is too old for `MarkAsJunk` operation.".to_string() })
-        }
+        operation.handle_operation(&self, &listener).await;
     }
 }
