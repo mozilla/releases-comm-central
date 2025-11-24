b3656e2549ad7bf5b5c167eb5dc538c2fbf29448
Kai Engert <kaie@kuix.de>
2025-11-24 15:20:50 +0100

    Bug 1893950 - Incremental update to RNP 0.18.1. r=dandarnell
    Differential Revision: https://phabricator.services.mozilla.com/D273831
    
    --HG--
    extra : rebase_source : 8760df0e188651e3fe960e7ecc1c799867d3eaec

--- mail/extensions/openpgp/content/modules/RNPLib.sys.mjs
+++ mail/extensions/openpgp/content/modules/RNPLib.sys.mjs
@@ -11,7 +11,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
 });
 
 // MIN_RNP_VERSION is updated by mach vendor when updating librnp from upstream.
-const MIN_RNP_VERSION = [0, 18, 0];
+const MIN_RNP_VERSION = [0, 18, 1];
 
 var systemOS = Services.appinfo.OS.toLowerCase();
 var abi = ctypes.default_abi;
