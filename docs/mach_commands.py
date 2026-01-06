37ad83f9878fb2619a374753ae54ac9e8b344fd9
Martin Giger <martin@humanoids.be>
2026-01-06 12:15:21 -0500

    Bug 2008692 - Fix index to list ADR 0003. r=vineet
    Also fixes tb-doc command, porting the changes from bug 1704891.
    
    Differential Revision: https://phabricator.services.mozilla.com/D277981
    
    --HG--
    extra : amend_source : 70213db191700bc91f65e575d148a17f08fa8016

--- docs/mach_commands.py
+++ docs/mach_commands.py
@@ -114,9 +114,11 @@ def build_docs(
     # in the PATH.
     import setup_helper
 
+    import mozbuild.nodeutil as nodeutil
+
     setup_helper.set_project_root(command_context.topsrcdir)
 
-    if not setup_helper.check_node_executables_valid():
+    if not nodeutil.check_node_executables_valid():
         return 1
 
     setup_helper.eslint_maybe_setup()
