916675e001fb1511fcb5147b0403ef8067d38165
Magnus Melin <mkmelin+mozilla@iki.fi>
2026-02-03 13:02:24 +0000

    Bug 2014082 - Increase timeout for comm/mailnews/import/test/browser/browser_importProfile.js. r=arschmitz
    Differential Revision: https://phabricator.services.mozilla.com/D281608
    
    --HG--
    extra : amend_source : 9a69bfc73678a06ceef4c1b3eea891cb7cdb35ec

--- mailnews/import/test/browser/browser_importProfile.js
+++ mailnews/import/test/browser/browser_importProfile.js
@@ -5,7 +5,7 @@
  */
 
 // Writing the fake zip and deleting it can take some time.
-requestLongerTimeout(2);
+requestLongerTimeout(3);
 
 const { MockFilePicker } = ChromeUtils.importESModule(
   "resource://testing-common/MockFilePicker.sys.mjs"
