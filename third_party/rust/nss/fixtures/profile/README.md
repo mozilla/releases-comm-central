8fad22d1b828a44f05a8ca0a1189480e609fe492
Thunderbird Updatebot <tbirdtry@mozilla.com>
2026-02-12 17:35:57 +0000

    Bug 1878375 - Synchronize vendored Rust libraries with mozilla-central. rs=kaie
    mozilla-central: 61fb3eacd57b64cd271585444316d141805e34ab
    comm-central: 6380b207fd02057d9e0460923d8cc9d898e9af24
    
    Differential Revision: https://phabricator.services.mozilla.com/D283083
    
    --HG--
    extra : amend_source : 4fe2e071ac16144da7c2ec1e25f1a575e2aa378b

--- third_party/rust/nss/fixtures/profile/README.md
+++
@@ -1,6 +0,0 @@
-# Profile Fixture
-
-To test the integration of logins using the `keydb` feature in a profile with
-an activated primary password, we use this profile, which was created
-externally as long as Rust-side bindings for setting a primary password are not
-yet implemented.
--- third_party/rust/webext-storage/build.rs
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    uniffi::generate_scaffolding("./src/webext-storage.udl").unwrap();
-}
