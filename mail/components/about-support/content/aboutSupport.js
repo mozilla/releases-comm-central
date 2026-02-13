73124b732f598c4891c7f4704640d2bfc9b94ace
Magnus Melin <mkmelin+mozilla@iki.fi>
2026-02-13 08:56:28 +0200

    Bug 1935334 - Remove usage of PluralForm.sys.mjs from aboutSupport.js. r=arschmitz
    Differential Revision: https://phabricator.services.mozilla.com/D280635
    
    --HG--
    extra : rebase_source : 9678fdfd9154cb707e30460edf1603cf0ec3b5a8
    extra : amend_source : 598850003d98177a2e138cfb3cc06c6e0e36150c

--- mail/components/about-support/content/aboutSupport.js
+++ mail/components/about-support/content/aboutSupport.js
@@ -26,7 +26,6 @@ var { AppConstants } = ChromeUtils.importESModule(
 ChromeUtils.defineESModuleGetters(this, {
   DownloadUtils: "resource://gre/modules/DownloadUtils.sys.mjs",
   PlacesDBUtils: "resource://gre/modules/PlacesDBUtils.sys.mjs",
-  PluralForm: "resource:///modules/PluralForm.sys.mjs",
   ProcessType: "resource://gre/modules/ProcessType.sys.mjs",
 });
 
