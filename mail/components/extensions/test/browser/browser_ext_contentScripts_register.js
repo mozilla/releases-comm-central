57ca72e813d0d2171b82470b3315bc2b3371dbc7
John Bieling <john@thunderbird.net>
2026-02-19 11:57:37 +0200

    Bug 2016161 - Part 7: Move Manifest V2 content script register tests into browser_ext_contentScripts_register.js. r=darktrojan
    Differential Revision: https://phabricator.services.mozilla.com/D283313
    
    --HG--
    extra : rebase_source : b342ec205ba946541ee2cb746113a79bf8088eac

--- mail/components/extensions/test/browser/browser_ext_contentScripts_register.js
+++ mail/components/extensions/test/browser/browser_ext_contentScripts_register.js
@@ -6,12 +6,202 @@
 
 const CONTENT_PAGE =
   "http://mochi.test:8888/browser/comm/mail/components/extensions/test/browser/data/content.html";
-const UNCHANGED_VALUES = {
+const CONTENT_VALUES = {
   backgroundColor: "rgba(0, 0, 0, 0)",
   color: "rgb(0, 0, 0)",
   foo: null,
   textContent: "\n  This is text.\n  This is a link with text.\n  \n\n\n",
 };
+
+/**
+ * Tests browser.contentScripts.register correctly adds CSS and JavaScript to
+ * message composition windows opened after it was called. Also tests calling
+ * `unregister` on the returned object.
+ */
+add_task(async function testRegister_mv2() {
+  const extension = ExtensionTestUtils.loadExtension({
+    files: {
+      "background.js": async () => {
+        const registeredScript = await browser.contentScripts.register({
+          css: [{ code: "body { color: white }" }, { file: "test.css" }],
+          js: [
+            {
+              code: `document.body.setAttribute("foo", "bar"); browser.test.sendMessage("expected code injection");`,
+            },
+            { file: "test.js" },
+          ],
+          matches: ["*://mochi.test/*"],
+        });
+        await window.sendMessage("registered");
+
+        await registeredScript.unregister();
+        await window.sendMessage("unregistered");
+
+        browser.test.notifyPass("finished");
+      },
+      "test.css": "body { background-color: green; }",
+      "test.js": () => {
+        document.body.textContent = "Hey look, the script ran!";
+        browser.test.sendMessage("expected file injection");
+      },
+      "utils.js": await getUtilsJS(),
+    },
+    manifest: {
+      background: { scripts: ["utils.js", "background.js"] },
+      permissions: ["*://mochi.test/*"],
+    },
+  });
+
+  // Tab 1: loads before the script is registered.
+  const tab1 = window.openContentTab(CONTENT_PAGE + "?tab1");
+  await awaitBrowserLoaded(tab1.browser, CONTENT_PAGE + "?tab1");
+
+  await extension.startup();
+
+  await extension.awaitMessage("registered");
+  // Registering a script will not inject it into already open tabs, wait a moment
+  // to make sure we still get the unchanged values.
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(r => setTimeout(r, 1000));
+  await checkContent(tab1.browser, CONTENT_VALUES);
+
+  // Tab 2: loads after the script is registered.
+  const tab2 = window.openContentTab(CONTENT_PAGE + "?tab2");
+  await awaitBrowserLoaded(tab2.browser, CONTENT_PAGE + "?tab2");
+  await extension.awaitMessage("expected code injection");
+  await extension.awaitMessage("expected file injection");
+  await checkContent(tab2.browser, {
+    backgroundColor: "rgb(0, 128, 0)",
+    color: "rgb(255, 255, 255)",
+    foo: "bar",
+    textContent: "Hey look, the script ran!",
+  });
+
+  extension.sendMessage();
+  await extension.awaitMessage("unregistered");
+
+  await checkContent(tab2.browser, {
+    backgroundColor: "rgb(0, 128, 0)",
+    color: "rgb(255, 255, 255)",
+    foo: "bar",
+    textContent: "Hey look, the script ran!",
+  });
+
+  // Tab 3: loads after the script is unregistered.
+  const tab3 = window.openContentTab(CONTENT_PAGE + "?tab3");
+  await awaitBrowserLoaded(tab3.browser, CONTENT_PAGE + "?tab3");
+  await checkContent(tab3.browser, CONTENT_VALUES);
+
+  extension.sendMessage();
+
+  await extension.awaitFinish("finished");
+  await extension.unload();
+
+  // Tab 2 should have the CSS removed.
+  await checkContent(tab2.browser, {
+    backgroundColor: CONTENT_VALUES.backgroundColor,
+    color: CONTENT_VALUES.color,
+    foo: "bar",
+    textContent: "Hey look, the script ran!",
+  });
+
+  const tabmail = document.getElementById("tabmail");
+  tabmail.closeOtherTabs(tabmail.tabInfo[0]);
+});
+
+/** Tests content_scripts in the manifest with permission work. */
+add_task(async function testManifest_mv2() {
+  const extension = ExtensionTestUtils.loadExtension({
+    files: {
+      "test.css": "body { background-color: lime; }",
+      "test.js": () => {
+        document.body.textContent = "Hey look, the script ran!";
+        browser.test.sendMessage("expected file injection");
+      },
+    },
+    manifest: {
+      content_scripts: [
+        {
+          matches: ["<all_urls>"],
+          css: ["test.css"],
+          js: ["test.js"],
+        },
+      ],
+    },
+  });
+
+  // Tab 1: loads before the script is registered.
+  const tab1 = window.openContentTab(CONTENT_PAGE + "?tab1");
+  await awaitBrowserLoaded(tab1.browser, CONTENT_PAGE + "?tab1");
+
+  // The extension is not running, no script should be injected, wait a moment to
+  // make sure we still get the unchanged values.
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(r => setTimeout(r, 1000));
+  await checkContent(tab1.browser, CONTENT_VALUES);
+
+  await extension.startup();
+
+  // The extension started and the content script defined in the manifest should
+  // be injected into the already open tab.
+  await extension.awaitMessage("expected file injection");
+  await checkContent(tab1.browser, {
+    backgroundColor: "rgb(0, 255, 0)",
+    textContent: "Hey look, the script ran!",
+  });
+
+  // Tab 2: loads after the script is registered.
+  const tab2 = window.openContentTab(CONTENT_PAGE + "?tab2");
+  await awaitBrowserLoaded(tab2.browser, CONTENT_PAGE + "?tab2");
+  await extension.awaitMessage("expected file injection");
+  await checkContent(tab2.browser, {
+    backgroundColor: "rgb(0, 255, 0)",
+    textContent: "Hey look, the script ran!",
+  });
+
+  await extension.unload();
+
+  // Tab 2 should have the CSS removed.
+  await checkContent(tab2.browser, {
+    backgroundColor: CONTENT_VALUES.backgroundColor,
+    textContent: "Hey look, the script ran!",
+  });
+
+  const tabmail = document.getElementById("tabmail");
+  tabmail.closeOtherTabs(tabmail.tabInfo[0]);
+});
+
+/** Tests content_scripts match patterns in the manifest. */
+add_task(async function testManifestNoPermissions_mv2() {
+  const extension = ExtensionTestUtils.loadExtension({
+    files: {
+      "test.css": "body { background-color: red; }",
+      "test.js": () => {
+        document.body.textContent = "Hey look, the script ran!";
+        browser.test.sendMessage("unexpected file injection");
+      },
+    },
+    manifest: {
+      content_scripts: [
+        {
+          matches: ["*://example.org/*"],
+          css: ["test.css"],
+          js: ["test.js"],
+        },
+      ],
+    },
+  });
+
+  await extension.startup();
+
+  const tab = window.openContentTab(CONTENT_PAGE);
+  await awaitBrowserLoaded(tab.browser, CONTENT_PAGE);
+  await checkContent(tab.browser, CONTENT_VALUES);
+
+  await extension.unload();
+
+  document.getElementById("tabmail").closeTab(tab);
+});
 
 /**
  * Tests `browser.scripting.registerContentScripts()` correctly adds CSS and
@@ -180,7 +370,7 @@ add_task(async function testRegister_mv3() {
   // to make sure we still get the unchanged values.
   // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
   await new Promise(r => setTimeout(r, 1000));
-  await checkContent(tab1.browser, UNCHANGED_VALUES);
+  await checkContent(tab1.browser, CONTENT_VALUES);
 
   // Tab 2: loads after the script is registered.
   const tab2 = window.openContentTab(CONTENT_PAGE + "?tab2");
@@ -206,7 +396,7 @@ add_task(async function testRegister_mv3() {
   // Tab 3: loads after the script is unregistered.
   const tab3 = window.openContentTab(CONTENT_PAGE + "?tab3");
   await awaitBrowserLoaded(tab3.browser, CONTENT_PAGE + "?tab3");
-  await checkContent(tab3.browser, UNCHANGED_VALUES);
+  await checkContent(tab3.browser, CONTENT_VALUES);
 
   extension.sendMessage();
 
@@ -215,8 +405,8 @@ add_task(async function testRegister_mv3() {
 
   // Tab 2 should have the CSS removed.
   await checkContent(tab2.browser, {
-    backgroundColor: UNCHANGED_VALUES.backgroundColor,
-    color: UNCHANGED_VALUES.color,
+    backgroundColor: CONTENT_VALUES.backgroundColor,
+    color: CONTENT_VALUES.color,
     foo: "bar",
     textContent: "Hey look, the script ran!",
   });
@@ -255,7 +445,7 @@ add_task(async function testManifest_mv3() {
   // make sure we still get the unchanged values.
   // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
   await new Promise(r => setTimeout(r, 1000));
-  await checkContent(tab1.browser, UNCHANGED_VALUES);
+  await checkContent(tab1.browser, CONTENT_VALUES);
 
   await extension.startup();
 
@@ -280,7 +470,7 @@ add_task(async function testManifest_mv3() {
 
   // Tab 2 should have the CSS removed.
   await checkContent(tab2.browser, {
-    backgroundColor: UNCHANGED_VALUES.backgroundColor,
+    backgroundColor: CONTENT_VALUES.backgroundColor,
     textContent: "Hey look, the script ran!",
   });
 
@@ -314,7 +504,7 @@ add_task(async function testManifestNoPermissions_mv3() {
 
   const tab = window.openContentTab(CONTENT_PAGE);
   await awaitBrowserLoaded(tab.browser, CONTENT_PAGE);
-  await checkContent(tab.browser, UNCHANGED_VALUES);
+  await checkContent(tab.browser, CONTENT_VALUES);
 
   await extension.unload();
 
