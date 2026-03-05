4112a54d267c6f474354fe6e10e7b4d5c20d85eb
welpy-cw <h.w.forms@arcor.de>
2026-03-05 09:22:51 -0500

    Bug 2017003 - Remove unused code from foldersummary.js. r=mkmelin
    The only caller of `MozFolderSummary.parseFolder` has been removed with bug 1789774.
    
    Differential Revision: https://phabricator.services.mozilla.com/D285396
    
    --HG--
    extra : amend_source : 516fecea38a9776aa23295e4228381303654fb75

--- mail/base/content/widgets/foldersummary.js
+++ mail/base/content/widgets/foldersummary.js
@@ -4,7 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-/* global MozElements */
 /* global MozXULElement */
 /* import-globals-from ../../../../mailnews/base/content/newmailalert.js */
 
@@ -12,9 +11,6 @@
 {
   const { MailServices } = ChromeUtils.importESModule(
     "resource:///modules/MailServices.sys.mjs"
-  );
-  const { VirtualFolderHelper } = ChromeUtils.importESModule(
-    "resource:///modules/VirtualFolderWrapper.sys.mjs"
   );
 
   /**
@@ -74,162 +70,6 @@
       vbox.appendChild(hbox);
       vbox.appendChild(preview);
       return vbox;
-    }
-
-    /**
-     * Check the given folder for NEW messages.
-     *
-     * @param {nsIMsgFolder} folder - The folder to examine.
-     * @param {nsIUrlListener} urlListener - Listener to notify if we run urls
-     *   to fetch msgs.
-     * @param {object} outAsync - Object with value property set to true if there
-     *   are async fetches pending (a message preview will be available later).
-     * @returns {boolean} true if the folder knows about messages that should be shown.
-     */
-    parseFolder(folder, urlListener, outAsync) {
-      // Skip servers, Trash, Junk folders and newsgroups.
-      if (
-        !folder ||
-        folder.isServer ||
-        !folder.hasNewMessages ||
-        folder.getFlag(Ci.nsMsgFolderFlags.Junk) ||
-        folder.getFlag(Ci.nsMsgFolderFlags.Trash) ||
-        folder.server instanceof Ci.nsINntpIncomingServer
-      ) {
-        return false;
-      }
-
-      const folders = [];
-      let msgDatabase;
-      try {
-        msgDatabase = folder.msgDatabase;
-      } catch (e) {
-        // The database for this folder may be missing (e.g. outdated/missing .msf),
-        // so just skip this folder.
-        return false;
-      }
-
-      if (folder.flags & Ci.nsMsgFolderFlags.Virtual) {
-        const wrapper = VirtualFolderHelper.wrapVirtualFolder(folder);
-        for (const realFolder of wrapper.searchFolders) {
-          if (!realFolder.isServer) {
-            folders.push(realFolder);
-          }
-        }
-      } else {
-        folders.push(folder);
-      }
-
-      let haveMsgsToShow = false;
-      // eslint-disable-next-line no-shadow
-      for (const folder of folders) {
-        // now get the database
-        try {
-          msgDatabase = folder.msgDatabase;
-        } catch (e) {
-          // The database for this folder may be missing (e.g. outdated/missing .msf),
-          // then just skip this folder.
-          continue;
-        }
-
-        folder.msgDatabase = null;
-        let msgKeys = msgDatabase.getNewList();
-
-        const numNewMessages = folder.getNumNewMessages(false);
-        if (!numNewMessages) {
-          continue;
-        }
-        // NOTE: getNewlist returns all nsMsgMessageFlagType::New messages,
-        // while getNumNewMessages returns count of new messages since the last
-        // biff. Only show newly received messages since last biff in
-        // notification.
-        msgKeys = msgKeys.slice(-numNewMessages);
-        if (!msgKeys.length) {
-          continue;
-        }
-
-        if (this.showPreview) {
-          // fetchMsgPreviewText forces the previewText property to get generated
-          // for each of the message keys.
-          try {
-            outAsync.value = folder.fetchMsgPreviewText(msgKeys, urlListener);
-            folder.msgDatabase = null;
-          } catch (ex) {
-            // fetchMsgPreviewText throws an error when we call it on a news
-            // folder
-            folder.msgDatabase = null;
-            continue;
-          }
-        }
-
-        // If fetching the preview text is going to be an asynch operation and the
-        // caller is set up to handle that fact, then don't bother filling in any
-        // of the fields since we'll have to do this all over again when the fetch
-        // for the preview text completes.
-        // We don't expect to get called with a urlListener if we're doing a
-        // virtual folder.
-        if (outAsync.value && urlListener) {
-          return false;
-        }
-
-        // In the case of async fetching for more than one folder, we may
-        //  already have got enough to show (added by another urllistener).
-        const curHdrsInPopup = this.children.length;
-        if (curHdrsInPopup >= this.maxMsgHdrsInPopup) {
-          return false;
-        }
-
-        for (
-          let i = 0;
-          i + curHdrsInPopup < this.maxMsgHdrsInPopup && i < msgKeys.length;
-          i++
-        ) {
-          const msgBox = MozFolderSummary.createFolderSummaryMessage();
-          const msgHdr = msgDatabase.getMsgHdrForKey(msgKeys[i]);
-          msgBox.addEventListener("click", event => {
-            if (event.button !== 0) {
-              return;
-            }
-            this.MailUtils.displayMessageInFolderTab(msgHdr, true);
-          });
-
-          if (this.showSubject) {
-            let msgSubject = msgHdr.mime2DecodedSubject;
-            const kMsgFlagHasRe = 0x0010; // MSG_FLAG_HAS_RE
-            if (msgHdr.flags & kMsgFlagHasRe) {
-              msgSubject = msgSubject ? "Re: " + msgSubject : "Re: ";
-            }
-            msgBox.querySelector(".folderSummary-subject").textContent =
-              msgSubject;
-          }
-
-          if (this.showSender) {
-            const addrs = MailServices.headerParser.parseEncodedHeader(
-              msgHdr.author,
-              msgHdr.effectiveCharset,
-              false
-            );
-            const folderSummarySender = msgBox.querySelector(
-              ".folderSummary-sender"
-            );
-            // Set the label value instead of textContent to avoid wrapping.
-            folderSummarySender.value =
-              addrs.length > 0 ? addrs[0].name || addrs[0].email : "";
-            if (addrs.length > 1) {
-              folderSummarySender.value +=
-                " " + MozFolderSummary.l10n.formatValueSync("and-others");
-            }
-          }
-
-          if (this.showPreview) {
-            msgBox.querySelector(".folderSummary-previewText").textContent =
-              msgHdr.getStringProperty("preview") || "";
-          }
-          this.appendChild(msgBox);
-          haveMsgsToShow = true;
-        }
-      }
-      return haveMsgsToShow;
     }
 
     /**
