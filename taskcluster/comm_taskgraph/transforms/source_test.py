d0dfb899f9507ff7ef6b4b0a5bf44d14cef461e5
Martin Giger <martin@humanoids.be>
2026-03-17 16:14:30 -0600

    Bug 2023526 - Port bug 1670949: use clang-format as linter instead of its own command/job. r=sking
    Differential Revision: https://phabricator.services.mozilla.com/D287933
    
    --HG--
    extra : amend_source : 1d49843c91c63d16f58122115999c026f08f5557

--- taskcluster/comm_taskgraph/transforms/source_test.py
+++ taskcluster/comm_taskgraph/transforms/source_test.py
@@ -4,11 +4,9 @@
 
 import logging
 import os
-import shlex
 
 import taskgraph
 from taskgraph.transforms.base import TransformSequence
-from taskgraph.util.path import match as match_path
 
 logger = logging.getLogger(__name__)
 
@@ -21,43 +19,6 @@ def get_patterns(job):
     if optimization:
         return optimization.copy().popitem()[1]
     return []
-
-
-@transforms.add
-def changed_clang_format(config, jobs):
-    """
-    Transform for clang-format job to set the commandline to only check
-    C++ files that were changed in the current push rather than running on
-    the entire repository.
-    """
-    for job in jobs:
-        if job.get("name", "") == "clang-format":
-            prefix = config.params.get("comm_src_path")
-            files_changed = config.params.get("files_changed")
-
-            match_patterns = get_patterns(job)
-            changed_files = {file for file in files_changed if file.startswith(prefix)}
-
-            cpp_files = []
-            for pattern in match_patterns:
-                for path in changed_files:
-                    if match_path(path, pattern):
-                        cpp_files.append(path)
-
-            # In the event that no C/C++ files were changed in the current push,
-            # the commandline will end up being invalid. But, the clang-format
-            # job will get dropped by optimization, so it doesn't really matter.
-            if cpp_files:
-                job["task-context"] = {
-                    "from-object": {
-                        "changed_files": shlex.join(cpp_files),
-                    },
-                    "substitution-fields": [
-                        "run.command",
-                    ],
-                }
-
-        yield job
 
 
 @transforms.add
