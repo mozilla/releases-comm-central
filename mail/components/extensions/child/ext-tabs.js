5e4f016a9d16784deaf00be3f08a6127f6054e05
Toby Pilling <toby@thunderbird.net>
2026-06-14 00:18:22 +0200

    Bug 2047351 - Port documentId tabs API changes from bug 1891478 to Thunderbird. r=freaktechnik,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D306598
    
    --HG--
    extra : amend_source : 4df104aa99a29ebdbcc2742c31fe9f3c02f8a20b

--- mail/components/extensions/child/ext-tabs.js
+++ mail/components/extensions/child/ext-tabs.js
@@ -9,12 +9,27 @@ this.tabs = class extends ExtensionAPI {
     return {
       tabs: {
         connect(tabId, options) {
-          const { frameId = null, name = "" } = options || {};
-          return context.messenger.connect({ name, tabId, frameId });
+          const {
+            frameId = null,
+            name = "",
+            documentId = null,
+          } = options || {};
+          return context.messenger.connect({
+            name,
+            tabId,
+            frameId,
+            documentId,
+          });
         },
 
         sendMessage(tabId, message, options, callback) {
-          const arg = { tabId, frameId: options?.frameId, message, callback };
+          const arg = {
+            tabId,
+            frameId: options?.frameId,
+            documentId: options?.documentId,
+            message,
+            callback,
+          };
           return context.messenger.sendRuntimeMessage(arg);
         },
       },
