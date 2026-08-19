a4d31b11c1105ed4162584c5d5bd01c5193614cf
Justin Tracey <jtracey@thunderbird.net>
2026-08-19 21:39:06 +0000

    Bug 2044684 - Enable the external browser flow for Yandex. r=tobyp
    Differential Revision: https://phabricator.services.mozilla.com/D319875
    
    
    --HG--
    extra : git_commit : 319c5657b4b9cf8619db79291e1bf10d22409526

--- mailnews/base/src/OAuth2Providers.sys.mjs
+++ mailnews/base/src/OAuth2Providers.sys.mjs
@@ -250,6 +250,7 @@ var kIssuers = new Map([
       clientSecret: "3ded85b4ec574c2187a55dc49d361280",
       authorizationEndpoint: "https://oauth.yandex.com/authorize",
       tokenEndpoint: "https://oauth.yandex.com/token",
+      useExternalBrowser: true,
     },
   ],
   [
