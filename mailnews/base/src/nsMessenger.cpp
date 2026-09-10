32fd6a40f18bd9b4fef01b3b6a3a6b6dd53d17d2
Brendan Abolivier <brendan@thunderbird.net>
2026-09-10 13:15:50 +0300

    Bug 2070858 - Port bug 2066435: Update call to nsITransfer::Init following recent argument changes. r=mkmelin
    Looking at the patch on the upstream bug, it seems like using a null pointer should be fine.
    
    Differential Revision: https://phabricator.services.mozilla.com/D324906
    
    
    --HG--
    extra : git_commit : 21170dfce65b85c2541a028fbfee8484c535a37d

--- mailnews/base/src/nsMessenger.cpp
+++ mailnews/base/src/nsMessenger.cpp
@@ -931,9 +931,10 @@ nsresult nsSaveMsgListener::InitializeDownload(nsIRequest* aRequest) {
 
         nsCOMPtr<nsIURI> url;
         channel->GetURI(getter_AddRefs(url));
-        rv = tr->Init(url, nullptr, outputURI, EmptyString(), mimeinfo,
-                      timeDownloadStarted, nullptr, this, false,
-                      nsITransfer::DOWNLOAD_ACCEPTABLE, nullptr, false);
+        rv =
+            tr->Init(url, nullptr, outputURI, EmptyString(), mimeinfo,
+                     timeDownloadStarted, nullptr, this, false,
+                     nsITransfer::DOWNLOAD_ACCEPTABLE, nullptr, false, nullptr);
 
         // now store the web progresslistener
         mTransfer = tr;
