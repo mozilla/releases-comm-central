8008847c667df856155338e28ddd9957b0db9d22
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2026-09-11 14:43:48 -0600

    Bug 2069899 - Add TaskCluster graph symbol verification for Thunderbird Enterprise r=coreycb
    Differential Revision: https://phabricator.services.mozilla.com/D324017
    
    
    --HG--
    extra : git_commit : 1d90f18dbbe11db1ae0fac00f455fad338d4e85e

--- taskcluster/comm_taskgraph/util/verify.py
+++ taskcluster/comm_taskgraph/util/verify.py
@@ -457,3 +457,196 @@ def verify_task_graph_no_shippable_enterprise_level_not_1(
             raise Exception(
                 f"Thunderbird Enterprise job {task.label} missing {required_env}"
             )
+
+
+def task_matcher_exception_generator(
+    description,
+    task_label,
+    label_contains,
+    group_symbol=None,
+    expected_group=None,
+    symbol=None,
+    expected_symbol=None,
+):
+    if expected_group and group_symbol:
+        if label_contains in task_label and expected_group not in group_symbol:
+            raise Exception(
+                f"Enterprise {description} job `{task_label}` should have enterprise groupSymbol `{expected_group}`: {group_symbol}"
+            )
+    elif expected_symbol and symbol:
+        if label_contains in task_label and expected_symbol not in symbol:
+            raise Exception(
+                f"Enterprise {description} job `{task_label}` should have enterprise symbol `{expected_symbol}`: {symbol}"
+            )
+    else:
+        raise Exception(
+            f"Enterprise {description} job verify with nothing `{task_label}`: group_symbol={group_symbol} expected_group={expected_group} symbol={symbol} expected_symbol={expected_symbol}"
+        )
+
+
+@verifications.add("full_task_graph")
+def verify_task_graph_symbol_enterprise(
+    task, taskgraph, scratch_pad, graph_config, parameters
+):
+    """
+    This function verifies that treeherder data is sane for Enterprise.
+    """
+
+    if parameters["project"] != "enterprise-thunderbird":
+        return
+
+    if task is None:
+        return
+
+    if task.kind in ("complete",):
+        return
+
+    task_dict = task.task
+    if "extra" in task_dict:
+        extra = task_dict["extra"]
+        if "treeherder" in extra:
+            treeherder = extra["treeherder"]
+            platform = treeherder.get("machine", {}).get("platform")
+            group_symbol = treeherder.get("groupSymbol")
+            symbol = treeherder.get("symbol")
+
+            if "enterprise" in task.label:
+                if "enterprise" not in platform:
+                    raise Exception(
+                        f"Enterprise job `{task.label}` should have enterprise platform: {platform}"
+                    )
+
+                if "win64" in task.label:
+                    if "-msi-" in task.label:
+                        task_matcher_exception_generator(
+                            "repacks MSI",
+                            task.label,
+                            "repackage-enterprise-repack-msi-",
+                            group_symbol=group_symbol,
+                            expected_group="MSI-Ent",
+                        )
+                        task_matcher_exception_generator(
+                            "repacks MSI signed",
+                            task.label,
+                            "repackage-signing-enterprise-repack-msi-",
+                            group_symbol=group_symbol,
+                            expected_group="MSIs-Ent",
+                        )
+
+                        if "moz_sbxTb" in task.label:
+                            task_matcher_exception_generator(
+                                "repacks MSI",
+                                task.label,
+                                "repackage-enterprise-repack-msi-",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+                            task_matcher_exception_generator(
+                                "repacks MSI signed",
+                                task.label,
+                                "repackage-signing-enterprise-repack-msi-",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+
+                    if "-msix-" in task.label:
+                        task_matcher_exception_generator(
+                            "repacks MSIX",
+                            task.label,
+                            "repackage-enterprise-repack-msix-",
+                            group_symbol=group_symbol,
+                            expected_group="MSIX-Ent",
+                        )
+                        task_matcher_exception_generator(
+                            "repacks MSIX signed",
+                            task.label,
+                            "repackage-signing-enterprise-repack-msix-",
+                            group_symbol=group_symbol,
+                            expected_group="MSIXs-Ent",
+                        )
+
+                        if "moz_sbxTb" in task.label:
+                            task_matcher_exception_generator(
+                                "repacks MSIX",
+                                task.label,
+                                "repackage-enterprise-repack-msix-",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+                            task_matcher_exception_generator(
+                                "repacks MSIX signed",
+                                task.label,
+                                "repackage-signing-enterprise-repack-msix-",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+
+                if "macosx64" in task.label:
+                    if "enterprise-repack-mac-" in task.label:
+                        task_matcher_exception_generator(
+                            "repacks mac signing",
+                            task.label,
+                            "enterprise-repack-mac-signing",
+                            group_symbol=group_symbol,
+                            expected_group="BMS-Ent",
+                        )
+                        task_matcher_exception_generator(
+                            "repacks mac notarization",
+                            task.label,
+                            "enterprise-repack-mac-notarization",
+                            group_symbol=group_symbol,
+                            expected_group="BMN-Ent",
+                        )
+
+                        if "moz_sbxTb" in task.label:
+                            task_matcher_exception_generator(
+                                "repacks mac signing",
+                                task.label,
+                                "enterprise-repack-mac-signing",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+                            task_matcher_exception_generator(
+                                "repacks mac notarization",
+                                task.label,
+                                "enterprise-repack-mac-notarization",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
+
+                    """
+                    if "build-mac-" in task.label:
+                        task_matcher_exception_generator(
+                            "builds mac signing",
+                            task.label,
+                            "build-mac-signing",
+                            symbol=symbol,
+                            expected_symbol="BMS",
+                        )
+                        task_matcher_exception_generator(
+                            "builds mac notarization",
+                            task.label,
+                            "build-mac-notarization",
+                            symbol=symbol,
+                            expected_symbol="BMN",
+                        )
+                    """
+
+                if "linux64" in task.label:
+                    if "-deb-" in task.label:
+                        task_matcher_exception_generator(
+                            "deb package",
+                            task.label,
+                            "repackage-deb",
+                            symbol=symbol,
+                            expected_symbol="Rpk-deb",
+                        )
+
+                        if "moz_sbxTb" in task.label:
+                            task_matcher_exception_generator(
+                                "repacks deb package",
+                                task.label,
+                                "repackage-enterprise-repack-deb",
+                                symbol=symbol,
+                                expected_symbol="moz/sbxTb",
+                            )
